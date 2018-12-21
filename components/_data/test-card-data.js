445d1ceba978504efdced17d3fc6d46352826581
Andy MacLean <andyjmaclean@gmail.com>
2018-12-21 13:36:51 +0100

    EC-3229 Establish Storybokk JS - make test data dynamic

--- components/_data/test-card-data.js
+++ components/_data/test-card-data.js
@@ -1,115 +1,19 @@
-const cardData = [
-  {
-    id: '1',
-    thumbnail: '/img/storybook/card-img-1.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1786 - 1849', '1850 - 1881'],
-    url: '/cards/1'
-  },
-  {
-    id: '2',
-    thumbnail: '/img/storybook/card-img-2.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1837 - 1901'],
-    url: '/cards/2'
-  },
-  {
-    id: '3',
-    thumbnail: '/img/storybook/card-img-3.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/3'
-  },
-  {
-    id: '4',
-    thumbnail: '/img/storybook/card-img-4.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/4'
-  },
-  {
-    id: '5',
-    thumbnail: '/img/storybook/card-img-5.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/5'
-  },
-  {
-    id: '6',
-    thumbnail: '/img/storybook/card-img-6.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/6'
-  },
-  {
-    id: '7',
-    thumbnail: '/img/storybook/card-img-7.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/7'
-  },
-  {
-    id: '8',
-    thumbnail: '/img/storybook/card-img-8.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/8'
-  },
-  {
-    id: '9',
-    thumbnail: '/img/storybook/card-img-9.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/9'
-  },
-  {
-    id: '10',
-    thumbnail: '/img/storybook/card-img-10.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/10'
-  },
-  {
-    id: '11',
-    thumbnail: '/img/storybook/card-img-11.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/11'
-  },
-  {
-    id: '12',
-    thumbnail: '/img/storybook/card-img-12.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/12'
-  },
-  {
-    id: '13',
-    thumbnail: '/img/storybook/card-img-13.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/13'
-  },
-  {
-    id: '14',
-    thumbnail: '/img/storybook/card-img-14.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/14'
-  },
-  {
-    id: '15',
-    thumbnail: '/img/storybook/card-img-15.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/15'
-  },
-  {
-    id: '16',
-    thumbnail: '/img/storybook/card-img-16.jpg',
-    linkText: 'The card link text - the card link text - the card link text - the card link text - the card link text',
-    texts: ['1870 - 1920'],
-    url: '/cards/16'
-  }
-];
+const randomYear = function() {
+  const y1 = 1800 + Math.floor(Math.random() * Math.floor(100));
+  const y2 = 1800 + Math.floor(Math.random() * Math.floor(100));
+
+  return y2 > y1 ? y1 + ' - ' + y2 : y2 + ' - ' + y1
+}
+
+let cardData = [];
+
+[...Array(16).keys()].forEach((i)=>{
+  let cd       = {}
+  cd.id        = i + '';
+  cd.linkText  = '(' + (i + 1) + ') The card link text - the card link text - the card link text - the card link text - the card link text - the card link text - the card link text - the card link text - the card link text';
+  cd.thumbnail = '/img/storybook/card-img-' + (i + 1) + '.jpg';
+  cd.url       = '/cards/' + (i + 1);
+  cd.texts     = [randomYear()]
+  cardData.push(cd)
+})
 export default cardData;
