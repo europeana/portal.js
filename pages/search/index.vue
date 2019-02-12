1b171ddea1140a1af9be3f979ba379097348ced9
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-02-12 15:13:18 +0100

    Front-end refinements search page

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,69 +1,108 @@
 <template>
-  <section class="container">
-    <h1 class="title">
-      Search
-    </h1>
-
-    <p v-if="error">
-      <strong>Error:</strong> {{ error }}
-    </p>
-    <b-container v-else>
-      <b-form
-        inline
-        @submit.prevent="submitSearchForm"
-      >
-        <b-form-input
-          id="searchQuery"
-          v-model="query"
-          placeholder="What are you looking for?"
-          name="query"
-        />
-        <b-button
-          variant="primary"
-          type="submit"
+  <b-container>
+    <b-row>
+      <b-col><h1>Search</h1></b-col>
+    </b-row>
+    <b-row
+      v-if="error"
+      class="mb-3"
+    >
+      <b-col>
+        <b-alert
+          show
+          variant="dark"
         >
-          Search
-        </b-button>
-      </b-form>
-      <p v-if="totalResults === 0">
-        No results.
-      </p>
-      <b-list-group
-        v-if="results !== null"
-        id="searchResults"
-      >
-        <!-- TODO: switch :href for :to if/when API permits CORS requests to record.json -->
-        <b-list-group-item
-          v-for="result in results"
-          :key="result.europeanaId"
-          :href="result.linkTo"
-          class="flex-column align-items-start"
+          <strong>Error:</strong> {{ error }}
+        </b-alert>
+      </b-col>
+    </b-row>
+    <b-row
+      v-else
+      class="mb-5"
+    >
+      <b-col>
+        <b-form
+          inline
+          @submit.prevent="submitSearchForm"
         >
-          <b-media right-align>
-            <b-img
-              v-if="result.edmPreview"
-              slot="aside"
-              :src="result.edmPreview"
+          <b-form-input
+            id="searchQuery"
+            v-model="query"
+            placeholder="What are you looking for?"
+            name="query"
+            class="mr-2 w-75"
+          />
+          <b-button
+            variant="primary"
+            type="submit"
+          >
+            Search
+            <Spinner
+              v-show="isLoading"
+              class="ml-2 mb-1"
             />
-            <dl>
-              <div
-                v-for="(value, key) in result.fields"
-                :key="key"
-              >
-                <template v-if="value">
-                  <dt>{{ key }}</dt>
-                  <dd><pre>{{ value }}</pre></dd>
-                </template>
-              </div>
-            </dl>
-          </b-media>
-        </b-list-group-item>
-      </b-list-group>
-    </b-container>
-  </section>
+          </b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+    <b-row
+      v-if="totalResults === 0"
+      class="mb-3"
+    >
+      <b-col>
+        <b-alert
+          show
+          variant="dark"
+        >
+          No results.
+        </b-alert>
+      </b-col>
+    </b-row>
+    <b-row
+      v-if="results !== null"
+      class="mb-3"
+    >
+      <b-col>
+        <b-list-group
+          id="searchResults"
+        >
+          <!-- TODO: switch :href for :to if/when API permits CORS requests to record.json -->
+          <b-list-group-item
+            v-for="result in results"
+            :key="result.europeanaId"
+            :href="result.linkTo"
+            class="flex-column align-items-start mb-3"
+          >
+            <b-media no-body>
+              <b-media-aside class="w-25 mr-3">
+                <b-img
+                  v-if="result.edmPreview"
+                  slot="aside"
+                  :src="result.edmPreview"
+                  class="mw-100"
+                />
+              </b-media-aside>
+              <b-media-body>
+                <div
+                  v-for="(value, key) in result.fields"
+                  :key="key"
+                >
+                  <template v-if="value">
+                    <div><strong>{{ key }}</strong></div>
+                    <pre><code>{{ value }}</code></pre>
+                  </template>
+                </div>
+              </b-media-body>
+            </b-media>
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+  import Spinner from '~/components/Spinner.vue';
   import axios from 'axios';
 
   function genericThumbnail(edmType) {
@@ -91,9 +130,13 @@
   }
 
   export default {
+    components: {
+      Spinner
+    },
     data () {
       return {
         error: null,
+        isLoading: false,
         results: null,
         totalResults: null,
         query: null
@@ -115,7 +158,8 @@
           return {
             results: resultsFromApiResponse(response),
             totalResults: response.data.totalResults,
-            query: query.query
+            query: query.query,
+            isLoading: false
           };
         })
         .catch((error) => {
@@ -139,6 +183,7 @@
     },
     methods: {
       submitSearchForm () {
+        this.isLoading = true;
         this.$router.push({ name: 'search', query: { query: this.query } });
       }
     },
