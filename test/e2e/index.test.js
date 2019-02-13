2ac065d7c70f35901acb6cc55463735f77d221b2
Richard Doe <richard.doe@rwdit.net>
2019-02-13 15:22:35 +0000

    Update e2e test for homepage, using nock

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -1,28 +1,36 @@
 import test from 'ava';
+import nock from 'nock';
 import createNuxt from '../helpers/createNuxt.js';
+import fs from 'fs';
+import path from 'path';
 
 // We keep a reference to Nuxt so we can close
 // the server at the end of the test
-let nuxt = null;
+let nuxt;
 
 // Init Nuxt.js and start listening on localhost:4000
 test.before('Init Nuxt.js', async () => {
   nuxt = await createNuxt();
+
+  const json = fs.readFileSync(path.resolve(__dirname, '../fixtures/contentful/homepage.json'), 'utf8');
+  nock('https://cdn.contentful.com')
+    .get(/^\/spaces\//)
+    .query(query => {
+      if (query['fields.identifier'] === '/') {
+        return true;
+      }
+    })
+    .reply(200, json);
+
   return nuxt;
 });
 
-// Example of testing only generated html
+// Test output of page-level headline and text fields
 test('Route / exists and renders HTML', async t => {
   let context = {};
   const { html } = await nuxt.renderRoute('/', context);
-  t.true(html.includes('Transforming the world with culture'));
-});
-
-// Example of testing only generated html
-test('Route / with "what" param exists and renders HTML', async t => {
-  let context = {};
-  const { html } = await nuxt.renderRoute('/?what=hello%20world', context);
-  t.true(html.includes('Transforming the world with hello world'));
+  t.true(/<title[^>]*>Home<\/title>/.test(html));
+  t.true(html.includes('<p>We transform the world with culture!</p>'));
 });
 
 // Close the Nuxt server
