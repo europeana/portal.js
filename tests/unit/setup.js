d2fd2788f198381b62331d6beb9c48b8acf267e3
Richard Doe <richard.doe@rwdit.net>
2019-03-06 12:58:38 +0000

    Install and use chai-nock

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const chaiNock = require('chai-nock');
 chai.use(chaiAsPromised);
+chai.use(chaiNock);
 chai.should();
 global.should = chai.should;
