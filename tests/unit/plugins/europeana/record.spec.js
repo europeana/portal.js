50b1554b81a6d18786bd31961e8992c5096c3744
Richard Doe <richard.doe@rwdit.net>
2019-03-08 11:28:37 +0000

    DRY up test plugin spec

--- tests/unit/plugins/europeana/record.spec.js
+++ tests/unit/plugins/europeana/record.spec.js
@@ -9,80 +9,82 @@ const apiUrl = 'https://api.europeana.eu';
 const apiEndpoint = `/api/v2/record${europeanaId}.json`;
 const apiKey = 'abcdef';
 
+const baseRequest = nock(apiUrl).get(apiEndpoint);
+
 describe('plugins/europeana/record', () => {
   describe('getRecord()', () => {
+    describe('API response', () => {
+      describe('with "Invalid record identifier: ..." error', () => {
+        const errorMessage = `Invalid record identifier: ${europeanaId}`;
 
-    describe('when trying to request an unknown record', () => {
-      const errorMessage = `Invalid record identifier: ${europeanaId}`;
+        beforeEach('stub API response', () => {
+          baseRequest
+            .query(true)
+            .reply(404, {
+              success: false,
+              error: errorMessage
+            });
+        });
 
-      beforeEach('stub API response', () => {
-        nock(apiUrl)
-          .get(apiEndpoint)
-          .query(true)
-          .reply(404, {
-            success: false,
-            error: errorMessage
-          });
+        it('throws API error message', () => {
+          const response = getRecord(europeanaId, { wskey: apiKey });
+
+          return response.should.be.rejectedWith(errorMessage);
+        });
       });
 
-      it('throws API error message', () => {
-        const response = getRecord(europeanaId, { wskey: apiKey });
-
-        return response.should.be.rejectedWith(errorMessage);
-      });
-    });
-
-    describe('when trying to request a record', () => {
-      const apiResponse = {
-        success: true,
-        object: {
-          aggregations: [{
-            edmIsShownAt: 'https://example.org',
-            webResources: [{
-              about: 'https://example.org',
-              dcDescription: {
+      describe('with object in response', () => {
+        const apiResponse = {
+          success: true,
+          object: {
+            aggregations: [{
+              edmIsShownAt: 'https://example.org',
+              webResources: [{
+                about: 'https://example.org',
+                dcDescription: {
+                  'en': [
+                    'This is an example'
+                  ]
+                },
+                webResourceEdmRights: {
+                  'def': [
+                    'https://example.org'
+                  ]
+                }
+              }]
+            }],
+            europeanaAggregation: {
+              edmRights: { def: [ 'https://example.org' ] },
+              edmPreview: 'https://example.org'
+            },
+            proxies: [{
+              europeanaProxy: false,
+              dcTitle: {
                 'en': [
-                  'This is an example'
+                  'This is a title'
                 ]
               },
-              webResourceEdmRights: {
-                'def': [
-                  'https://example.org'
+              dcDescription: {
+                'en': [
+                  'This is a description'
                 ]
               }
             }]
-          }],
-          europeanaAggregation: {
-            edmRights: { def: [ 'https://example.org' ] },
-            edmPreview: 'https://example.org'
-          },
-          proxies: [{
-            europeanaProxy: false,
-            dcTitle: {
-              'en': [
-                'This is a title'
-              ]
-            },
-            dcDescription: {
-              'en': [
-                'This is a description'
-              ]
-            }
-          }]
-        }
-      };
+          }
+        };
 
-      beforeEach('stub API response', () => {
-        nock(apiUrl)
-          .get(apiEndpoint)
-          .query(true)
-          .reply(200, apiResponse);
-      });
+        beforeEach('stub API response', () => {
+          nock(apiUrl)
+            .get(apiEndpoint)
+            .query(true)
+            .reply(200, apiResponse);
+        });
 
-      it('returns record data', () => {
-        const response = getRecord(europeanaId, { wskey: apiKey });
+        it('returns record data', async () => {
+          const response = await getRecord(europeanaId, { wskey: apiKey });
 
-        return response.should.eventually.have.property('record');
+          response.record.should.exist;
+        });
       });
     });
   });
