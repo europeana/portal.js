cf0cf63cb0cf43fb4f894eb3e154cf99938fce05
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-03-12 11:50:58 +0100

    Pagination component
    + Storybook update
    + Unit tests

---
+++ components/generic/PaginationNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-pagination-nav
+    v-model="currentPage"
+    base-url="#page="
+    align="center"
+    :limit="limit"
+    :hide-ellipsis="hideEllipsis"
+    :number-of-pages="totalPages"
+  />
+</template>
+
+<script>
+
+  const maxResults = 1000;
+
+  export default {
+    props: {
+      perPage: {
+        type: Number,
+        default: 24
+      },
+      limit: {
+        type: Number,
+        default: 12
+      },
+      hideEllipsis: {
+        type: Boolean,
+        default: true
+      },
+      totalResults: {
+        type: Number,
+        default: 0
+      }
+    },
+    data () {
+      return {
+        currentPage: null
+      };
+    },
+    computed: {
+      totalPages: function () {
+        if (this.totalResults > maxResults) {
+          return Math.ceil(maxResults / this.perPage);
+        } else {
+          return Math.ceil(this.totalResults / this.perPage);
+        }
+      }
+    }
+  };
+</script>
