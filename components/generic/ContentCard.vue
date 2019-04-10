7094e807f3a77363a734f76f3fbdda84d727e7d7
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-04-10 14:09:25 +0200

    WIP styling cards

--- components/generic/ContentCard.vue
+++ components/generic/ContentCard.vue
@@ -4,8 +4,7 @@
     :img-src="imageUrl"
     :img-alt="imageTitle"
     img-top
-    footer="Creator of this object"
-    class="mb-4"
+    class="mb-4 text-left"
     data-qa="content card"
   >
     <p class="card-text">
@@ -48,7 +47,33 @@
 </script>
 
 <style lang="scss">
+  @import "./assets/scss/variables.scss";
+
   .card-img-top {
     max-height: 10rem;
+  }
+
+  .card {
+    background: $extralightgrey;
+    border-radius: $border-radius-small;
+    box-shadow: $boxshadow-small;
+    color: $black;
+    font-size: $font-size-extrasmall;
+    line-height: 1.1875rem;
+    transition: box-shadow 0.25s;
+
+    &:hover {
+      box-shadow: 0 4px 12px 0 rgba(0,0,0,0.4)
+    }
+
+    .card-title {
+      font-size: $font-size-small;
+      font-weight: normal;
+    }
+
+    .card-body {
+      padding: 1rem;
+    }
+
   }
 </style>
