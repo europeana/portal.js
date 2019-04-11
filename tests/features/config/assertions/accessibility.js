9859129a6e20c6e70b5ad3cf38a68ce22e82a20d
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-04-11 16:07:40 +0200

    WIP: custom assertions

--- tests/features/config/assertions/accessibility.js
+++ tests/features/config/assertions/accessibility.js
@@ -1,3 +1,7 @@
+/*jshint esversion: 6 */
+
+const util = require('util');
+
 const script = function (context, options, done) {
   if (!window.axe) done({ error: 'aXe not found. Make sure it has been injected' })
 
@@ -12,46 +16,93 @@ const script = function (context, options, done) {
     })
 }
 
-module.exports.assertion = function (context, options, callback) {
-  this.message = `${context} passes accessibility scan`;
-  this.expected = true;
-  
-  this.pass = (result) => result
-  this.value = (result) => result
-  
-  this.command = (done) => {
-    this.api
-    .executeAsync(script, [context, options], function (response) {
-      const result = response.value.results;
+exports.assertion = function(context, config) {
 
-      if (!result) {
-        const error = 'aXe failed to execute';
-        callback ? callback(error) && done(true) : this.assert.fail(error) && done(false);
-        return;
-      }
+  //const FAILURE_MSG = ' Issue: %s\n Target: (%s)\n Impact: %s,\n Type: %s,\n Help: %s \n';
+  const FAILURE_MSG = 'Accessibility violations found: %s \n\n%s'
+  const PASS_MSG = '%d aXe a11y tests passed';
 
-      if (options.verbose) {
-        for (const pass of result.passes) {
-          this.assert.ok(true, pass.help);
-        }
-      } else {
-        if (result.passes.length > 0) {
-          this.assert.ok(true, `${result.passes.length} aXe Tests Passed`);
-        }
-      }
+  /**
+   * The message which will be used in the test output and
+   * inside the XML reports
+   * @type {string}
+   */
+  this.message = null;
 
-      if (result.violations.length) {// we got errors
-        let message = '\nViolations: ' + result.violations.length + '\n';
+  /**
+   * Cached results
+   * inside the XML reports
+   * @type {object|string}
+   */
+  this.results = null;
 
-        for (const violation of result.violations) {
-          message += '# ' + violation.impact + ': ' + violation.help + ' (' + violation.helpUrl + ')\n';
-        }
+  /**
+   * A value to perform the assertion on. If a function is
+   * defined, its result will be used.
+   * @type {function|*}
+   */
+  this.expected = null;
 
-        this.verify.fail(message);
-      }
+  /**
+   * The method which performs the actual assertion. It is
+   * called with the result of the value method as the argument.
+   *
+   * Some hackiness in here to get legible logging
+   * @type {function}
+   */
+  this.pass = function(value) {
+    var passed = !this.results.violations.length;
 
-      const success = result.violations.length === 0;
-      callback ? callback(success) && done(true) : done(success);
-    })
-  }
-}
+    if (passed) {
+      this.message = util.format(PASS_MSG, this.results.passes.length);
+    } 
+
+    return passed;
+  };
+
+  this.failure = function (result) {
+
+    const violations = result.value.results.violations;
+    let failMessage = '';
+
+    for (const violation of violations) {
+      failMessage += '# ' + violation.impact + ': ' + violation.help + ' (' + violation.helpUrl + ')\n';
+    } 
+
+    this.expected = 0;
+    this.message = util.format(FAILURE_MSG, result.value.results.violations.length, failMessage);
+    
+    return false;
+  };
+
+  /**
+   * The method which returns the value to be used on the
+   * assertion. It is called with the result of the command's
+   * callback as argument.
+   *
+   * Some hackiness in here to get legible logging
+   * @type {function}
+   */
+  this.value = function(result) {
+
+    var value = this.results = result.value.results;
+    
+    if (value.violations.length) {// we got errors
+      return value.violations.length;
+    }
+    return result.value;
+  };
+
+  /**
+   * Performs a protocol command/action and its result is
+   * passed to the value method via the callback argument.
+   * @type {function}
+   */
+  this.command = function(callback) {
+    this.api.executeAsync(script, [context, config], function (result) {
+      callback(result);
+    });
+    return this;
+  };
+
+};
