19d05c0ff4c452efa31d1a1ef83e2dbfd6201c4e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-04-18 15:44:15 +0200

    A browsechip component

---
+++ components/generic/BrowseChip.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-badge
+      to="#"
+      pill
+      variant="nocolor"
+      class="mb-3 mr-3 font-weight-normal bg-transparent"
+    >
+      Maps
+    </b-badge>
+    <b-badge
+      to="#"
+      pill
+      variant="nocolor"
+      class="mb-3 mr-3 font-weight-normal bg-transparent"
+    >
+      Painting
+    </b-badge>
+    <b-badge
+      to="#"
+      pill
+      variant="nocolor"
+      class="mb-3 mr-3 font-weight-normal bg-transparent"
+    >
+      Sculpture
+    </b-badge>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "./assets/scss/variables.scss";
+
+  .badge-nocolor {
+    border: 1px solid $darkgrey;
+    color: $black;
+    font-size: 0.875rem;
+    height: 4rem;
+    line-height: 3.5rem;
+    min-width: 6rem;
+  }
+
+</style>
