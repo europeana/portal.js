7097f79b23ada3de683b0396599f6f0dba57be91
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-04-29 11:44:11 +0200

    Entity page (#101)
    * WIP setup entity page
    
    * Entity page, plus basic tests
    
    * update API key (temporary)
    
    * Tweaks: names, order, .env
    
    * Update tests
    
    * Update Jenkins file
    
    * Tweaks
    
    * Add redirection
    
    * Update redirect
    
    * Update unit test
    
    * Update redirect and change plurals to singles
    
    * Update tests

---
+++ plugins/europeana/entity.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+/**
+ * Get the entity data from the API
+ * @param {string} type of entity
+ * @param {string} id of entity
+ * @param {Object} params additional parameters sent to the API
+ * @param {string} params.wskey API key
+ * @return {Object} parsed entity data
+ */
+function getEntity(type, id, params) {
+  return axios.get(`https://www.europeana.eu/api/entities/${getEntityTypeApi(type)}/base/${getEntityId(id)}`, {
+    params: {
+      wskey: params.wskey
+    }
+  })
+    .then((response) => {
+      return {
+        error: null,
+        entity: response.data
+      };
+    })
+    .catch((error) => {
+      const message = error.response ? error.response.data.error : error.message;
+      throw new Error(message);
+    });
+}
+
+/**
+ * Retrieve the API name of the type using the human readable name
+ * @param {string} type of entity
+ * @return {string} retrieved API name of type
+ */
+function getEntityTypeApi(type) {
+  const names = {
+    person: 'agent',
+    topic: 'concept'
+  };
+  if (!type) return;
+  return names[type];
+}
+
+/**
+ * Retrieve the entity id from the slug
+ * @param {string} url of entity
+ * @return {string} retrieved id
+ */
+function getEntityId(url) {
+  if (!url) return;
+  return url.split('-')[0];
+}
+
+export default getEntity;
---
+++ tests/unit/plugins/europeana/entity.spec.js
@@ -0,0 +1,67 @@
+import nock from 'nock';
+import getEntity from '../../../../plugins/europeana/entity';
+
+const axios = require('axios');
+axios.defaults.adapter = require('axios/lib/adapters/http');
+
+const entityId = '94-architecture';
+const entityType = 'topic';
+const entityIdMisspelled = '94-architectuz';
+const apiUrl = 'https://www.europeana.eu';
+const apiEndpoint = '/api/entities/concept/base/94';
+const apiKey = 'abcdef';
+
+const baseRequest = nock(apiUrl).get(apiEndpoint);
+
+describe('plugins/europeana/entity', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  describe('getEntity()', () => {
+    describe('API response', () => {
+      describe('with "No resource found with ID: ..." error', () => {
+        const errorMessage = 'No resource found with ID:';
+
+        beforeEach('stub API response', () => {
+          baseRequest
+            .query(true)
+            .reply(404, {
+              error: errorMessage
+            });
+        });
+
+        it('throws API error message', () => {
+          const response = getEntity(entityType, entityId, { wskey: apiKey });
+          return response.should.be.rejectedWith(errorMessage);
+        });
+      });
+
+      describe('with object in response', () => {
+        const apiResponse = {
+          prefLabel: {
+            en: 'Architecture'
+          }
+        };
+
+        beforeEach('stub API response', () => {
+          nock(apiUrl)
+            .get(apiEndpoint)
+            .query(true)
+            .reply(200, apiResponse);
+        });
+
+        it('returns entity title', async () => {
+          const response = await getEntity(entityType, entityId, { wskey: apiKey });
+          response.entity.prefLabel.en.should.eq('Architecture');
+        });
+
+        it('has a misspelled id and returns entity title', async () => {
+          const response = await getEntity(entityType, entityIdMisspelled, { wskey: apiKey });
+          response.entity.prefLabel.en.should.eq('Architecture');
+        });
+
+      });
+    });
+  });
+});
