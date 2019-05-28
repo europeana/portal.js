4cf108d95200a2698214e868d7825aeb8c473e4c
Lutz Biedinger <lutz.biedinger@gmail.com>
2019-05-28 15:10:39 +0200

    entity items WIP

--- pages/entity/_type/_.vue
+++ pages/entity/_type/_.vue
@@ -32,6 +32,38 @@
         />
       </b-col>
     </b-row>
+    <b-row>
+      <b-col
+        cols="12"
+        md="9"
+      >
+        <p
+          v-if="results.length == 0"
+          data-qa="warning notice"
+        >
+          There are no more results for your search query.
+        </p>
+        <SearchResultsList
+          v-else
+          :results="results"
+        />
+        <InfoMessage
+          v-if="lastAvailablePage"
+          message="Additional results are not shown as only the first 1000 most relevant results are shown. If you haven't found what you're looking for, please consider refining your search."
+        />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <PaginationNav
+          v-if="totalResults > perPage"
+          v-model="page"
+          :total-results="totalResults"
+          :per-page="perPage"
+          :link-gen="paginationLink"
+        />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -39,28 +71,62 @@
   import axios from 'axios';
 
   import AlertMessage from '../../../components/generic/AlertMessage';
+  import InfoMessage from '../../../components/generic/InfoMessage';
   import BrowseChip from '../../../components/browse/BrowseChip';
+  import SearchResultsList from '../../../components/search/SearchResultsList';
+  import PaginationNav from '../../../components/generic/PaginationNav';
 
   import getEntity, { getEntityPath, relatedEntities } from '../../../plugins/europeana/entity';
+  import search, { pageFromQuery } from '../../../plugins/europeana/search';
 
   export default {
     components: {
       AlertMessage,
-      BrowseChip
+      InfoMessage,
+      BrowseChip,
+      SearchResultsList,
+      PaginationNav
+    },
+    props: {
+      perPage: {
+        type: Number,
+        default: 24
+      }
     },
     data () {
       return {
         error: null,
+        errorNoResults: 'No results',
+        results: null,
+        totalResults: null,
+        lastAvailablePage: false,
+        query: this.entityQuery,
+        page: 1,
         title: null,
         relatedEntities: null
       };
     },
-    asyncData ({ env, params, res, redirect }) {
+    computed: {
+      hasResults: function () {
+        return this.results !== null && this.totalResults > 0;
+      },
+      entityQuery: function (params) {
+        return `http://data.europeana.eu/agent/base/${params.pathMatch}`;
+      }
+    },
+    asyncData ({ env, query, params, res, redirect, app }) {
+      const currentPage = pageFromQuery(query.page);
+      if (currentPage === null) {
+        // Redirect non-positive integer values for `page` to `page=1`
+        query.page = '1';
+        return redirect(app.localePath({ name: 'entity', type: params.type, id: params.pathMatch }));
+      }
       return axios.all([
         getEntity(params.type, params.pathMatch, { wskey: env.EUROPEANA_ENTITY_API_KEY }),
-        relatedEntities(params.type, params.pathMatch, { wskey: env.EUROPEANA_API_KEY, entityKey: env.EUROPEANA_ENTITY_API_KEY })
+        relatedEntities(params.type, params.pathMatch, { wskey: env.EUROPEANA_API_KEY, entityKey: env.EUROPEANA_ENTITY_API_KEY }),
+        search({ page: currentPage, query: `*:http://data.europeana.eu/agent/base/${params.pathMatch}`, wskey: env.EUROPEANA_API_KEY })
       ])
-        .then(axios.spread((entity, related) => {
+        .then(axios.spread((entity, related, searchResults) => {
           const desiredPath = getEntityPath(params.pathMatch, entity.entity.prefLabel.en);
 
           if (params.pathMatch !== desiredPath) {
@@ -70,20 +136,34 @@
           return {
             error: null,
             title: entity.entity.prefLabel.en,
-            relatedEntities: related
+            relatedEntities: related,
+            results: { ...searchResults, isLoading:false, page: Number(currentPage) }
           };
         }))
         .catch((err) => {
+          let errorMessage = err.message;
           if (typeof res !== 'undefined') {
             res.statusCode = err.message.startsWith('No resource found with ID:') ? 404 : 500;
+            if (err.message.startsWith('Invalid query')) {
+              res.statusCode = 400;
+            } else {
+              const paginationError = err.message.match(/It is not possible to paginate beyond the first (\d+)/);
+              if (paginationError !== null) {
+                res.statusCode = 400;
+                errorMessage = `It is only possible to view the first ${paginationError[1]} search results.`;
+              } else {
+                res.statusCode = 500;
+              }
+            }
           }
-          return { error: err.message };
+          return { error: errorMessage };
         });
     },
     head () {
       return {
         title: this.$t('entity')
       };
-    }
+    },
+    watchQuery: ['page']
   };
 </script>
--- tests/features/entity-page.feature
+++ tests/features/entity-page.feature
@@ -14,13 +14,28 @@ Feature: Entity page
   Scenario: View related entities
 
     When I open an `entity page`
-    Then I see the `entity page`
+    And I see the `entity page`
     Then I should have 2 `browse chip`s
 
   Scenario: Click on a related entity
 
     When I open an `entity page`
-    Then I see the `entity page`
+    And I see the `entity page`
     And I click a `browse chip`
     And I wait 2 seconds
     Then I should not be on the `entity page`
+
+  Scenario: Viewing related records
+
+    When I open an `entity page`
+    And I see the `entity page`
+    Then I see a `search result`
+
+  Scenario: Navigating to a related record
+
+    When I open an `entity page`
+    And I see the `entity page`
+    And I see a `search result`
+    And I click a `search result`
+    Then I see a `record page`
+
