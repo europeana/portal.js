ec6070a1cef0f5554b9d90f2fb99b6b870e01ddc
Lutz Biedinger <lutz.biedinger@gmail.com>
2019-06-19 17:15:25 +0200

    footer links from contentful

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,9 +1,68 @@
 <template>
-  <footer>{{ $t('footerText') }}</footer>
+  <footer
+    data-qa="footer"
+    class="footer"
+  >
+    <b-container>
+      <ul
+        v-if="footerLinks"
+        class="footer-link-list"
+      >
+        <li
+          v-for="footerLink in footerLinks"
+          :key="footerLink.url"
+        >
+          <b-link
+            v-if="footerLink.url"
+            :href="footerLink.url"
+            class="footer-link"
+          >
+            {{ footerLink.text }}
+          </b-link>
+          <p v-else>
+            {{ footerLink.text }}
+          </p>
+        </li>
+      </ul>
+    </b-container>
+  </footer>
 </template>
 
 <script>
- </script>
+  import contentfulClient from '../plugins/contentful.js';
+
+  export default {
+    data () {
+      return {
+        footerLinks: []
+      };
+    },
+    async created () {
+      this.footerLinks = await this.getFooterLinks();
+    },
+    methods: {
+      getFooterLinks () {
+        let links = contentfulClient.getEntries({
+          'content_type': 'linkGroup',
+          'fields.identifier': 'footer',
+          'limit': 1
+        })
+          .then((response) => {
+            if (response.total == 0) {
+              return [];
+            }
+            return response.items[0].fields.links.map(item => {
+              return item.fields;
+            });
+          }).catch((e) => {
+            // This will just output the error as text
+            return [{ text: e.toString() }];
+          });
+        return links;
+      }
+    }
+  };
+</script>
 
 <style lang="scss">
   @import '../assets/scss/variables.scss';
--- tests/unit/components/PageFooter.spec.js
+++ tests/unit/components/PageFooter.spec.js
@@ -1,17 +1,59 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
 import PageFooter from '../../../components/PageFooter.vue';
+//import { __RewireAPI__ as PageFooterRewireAPI } from '../../../components/PageFooter.vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const dummyLinkGroup = {
+  sys: { type: 'Array' },
+  total: 1,
+  skip: 0,
+  limit: 1,
+  items: [ { sys: { id: '100', type: 'Entry' }, fields: {
+    identifier: 'footer',
+    links: [
+      {
+        sys: { id: '123' },
+        fields: { url: 'https://www.example.org', text: 'Example link' }
+      },
+      {
+        sys: { id: '456' },
+        fields: { url: 'https://www.europeana.eu', text: 'Europeana link' }
+      }
+    ]
+  } } ],
+  mocks: { $t: () => {} }
+};
+const dummyContentful = {
+  getEntries: () => {
+    return new Promise((resolve) => {
+      resolve(dummyLinkGroup);
+    });
+  }
+};
+
+PageFooter.__Rewire__('contentfulClient', dummyContentful);
 
 const factory = () => shallowMount(PageFooter, {
+  localVue,
   mocks: {
     $t: () => {}
+    //contentfulClient: dummyContentful
   }
 });
 
-describe('components/search/PageFooter', () => {
-  it('it exists', () => {
-    const wrapper = factory();
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
 
-    const footer = wrapper.find('footer');
-    footer.isVisible().should.equal(true);
+describe('components/search/PageFooter', () => {
+  it('it contains elements for each link', async () => {
+    const wrapper = factory();
+    await sleep(20);
+    const renderedList = wrapper.findAll('footer ul li');
+    renderedList.length.should.eq(2);
+    //dummyLinkGroup.items[0].links[0].fields.url
   });
 });
