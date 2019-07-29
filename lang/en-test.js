264c2b32245320395d2840a71eeb2aaf22c70b34
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-07-29 16:00:15 +0200

    Test file

---
+++ lang/en-test.js
@@ -0,0 +1,10 @@
+export default {
+  "searchViews": {
+    "grid": "Grid view",
+    "list": "List view"
+  },
+  "layout": {
+    "skipToMain": "Skip to main content"
+  },
+  "searchPlaceholder": "What are you looking for?",
+};
