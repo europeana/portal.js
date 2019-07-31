0e202ae606dbe234720a347e9681fa415864c47e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-07-31 14:26:00 +0200

    Static hero banner

---
+++ components/generic/HeroBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-jumbotron
+    fluid
+    header="Unique Selling Point 1. Limit of two lines"
+    header-tag="h2"
+    header-level="4"
+    lead="Can be a static image stating briefly all the Unique Selling Points of Europeana or a caroussel with 3/4 highlighted USP’s. Limit of 3 lines."
+    text-variant="white"
+  >
+    <a
+      href="/record/90402/SK_A_1709"
+      class="attribution"
+    >
+      The Lake of Zurich, 1660, Jan Hackaert, Rijksmuseum
+      <span>Public Domain</span>
+    </a>
+  </b-jumbotron>
+</template>
