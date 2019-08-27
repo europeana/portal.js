33b84efbeb9f125eadcd998f52a31e359e2e9bee
Richard Doe <richard.doe@rwdit.net>
2019-08-27 09:09:26 +0100

    Whitelist web resource metadata fields to display

--- components/record/WebResources.vue
+++ components/record/WebResources.vue
@@ -8,10 +8,10 @@
       class="mb-3"
     >
       <MetadataField
-        v-for="(value, name) in webResource"
-        :key="name"
-        :name="name"
-        :value="value"
+        v-for="field of fields"
+        :key="field"
+        :name="field"
+        :value="webResource[field]"
         context="webResource"
       />
     </b-list-group-item>
@@ -30,6 +30,11 @@
         type: Array,
         default: () => []
       }
+    },
+    data: function () {
+      return {
+        fields: ['about', 'dcDescription', 'edmRights', 'ebucoreHasMimeType']
+      };
     }
   };
 </script>
