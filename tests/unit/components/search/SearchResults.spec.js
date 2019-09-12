c89377ef117d54540281ebb9b61fa403a4501622
Richard Doe <richard.doe@rwdit.net>
2019-09-12 16:59:10 +0200

    Group unit tests

--- tests/unit/components/search/SearchResults.spec.js
+++ tests/unit/components/search/SearchResults.spec.js
@@ -32,64 +32,50 @@ const factory = (propsData = {}) => mount(SearchResults, {
 });
 
 describe('components/search/SearchResults', () => {
-  context('with `error` prop', () => {
-    it('displays the message', () => {
-      const errorMessage = 'Something went very wrong';
-      const wrapper = factory({ error: errorMessage });
+  describe('output', () => {
+    context('with `error` prop', () => {
+      it('displays the message', () => {
+        const errorMessage = 'Something went very wrong';
+        const wrapper = factory({ error: errorMessage });
 
-      const errorNotice =  wrapper.find('[data-qa="error notice"]');
+        const errorNotice =  wrapper.find('[data-qa="error notice"]');
 
-      errorNotice.text().should.include(errorMessage);
-    });
-  });
-
-  describe('contentTierActiveState', () => {
-    context('when contentTier facet includes "*"', () => {
-      it('is `true`', () => {
-        const wrapper = factory({
-          selectedFacets: {
-            contentTier: ['*']
-          }
-        });
-
-        wrapper.vm.contentTierActiveState.should.be.true;
-      });
-    });
-
-    context('when contentTier facet does not include "*"', () => {
-      it('is `false`', () => {
-        const wrapper = factory({
-          selectedFacets: {
-            contentTier: ['1 OR 2 OR 3 OR 4']
-          }
-        });
-
-        wrapper.vm.contentTierActiveState.should.be.false;
+        errorNotice.text().should.include(errorMessage);
       });
     });
   });
 
-  describe('noMoreResults', () => {
-    context('when there are 0 results in total', () => {
-      const wrapper = factory({
-        totalResults: 0
-      });
-
-      it('is `false`', () => {
-        wrapper.vm.noMoreResults.should.be.false;
-      });
-    });
-
-    context('when there are some results in total', () => {
-      context('and results here', () => {
-        const wrapper = factory({
-          totalResults: 100,
-          results: [{
-            europeanaId: '/123/abc',
-            fields: {
-              dcTitle: ['Title']
+  describe('computed properties', () => {
+    describe('contentTierActiveState', () => {
+      context('when contentTier facet includes "*"', () => {
+        it('is `true`', () => {
+          const wrapper = factory({
+            selectedFacets: {
+              contentTier: ['*']
             }
-          }]
+          });
+
+          wrapper.vm.contentTierActiveState.should.be.true;
+        });
+      });
+
+      context('when contentTier facet does not include "*"', () => {
+        it('is `false`', () => {
+          const wrapper = factory({
+            selectedFacets: {
+              contentTier: ['1 OR 2 OR 3 OR 4']
+            }
+          });
+
+          wrapper.vm.contentTierActiveState.should.be.false;
+        });
+      });
+    });
+
+    describe('noMoreResults', () => {
+      context('when there are 0 results in total', () => {
+        const wrapper = factory({
+          totalResults: 0
         });
 
         it('is `false`', () => {
@@ -97,130 +83,152 @@ describe('components/search/SearchResults', () => {
         });
       });
 
-      context('but no results here', () => {
+      context('when there are some results in total', () => {
+        context('and results here', () => {
+          const wrapper = factory({
+            totalResults: 100,
+            results: [{
+              europeanaId: '/123/abc',
+              fields: {
+                dcTitle: ['Title']
+              }
+            }]
+          });
+
+          it('is `false`', () => {
+            wrapper.vm.noMoreResults.should.be.false;
+          });
+        });
+
+        context('but no results here', () => {
+          const wrapper = factory({
+            totalResults: 100
+          });
+
+          it('is `true`', () => {
+            wrapper.vm.noMoreResults.should.be.true;
+          });
+        });
+      });
+    });
+
+    describe('orderedFacets', () => {
+      const wrapper = factory({
+        facets: [
+          { name: 'COUNTRY' },
+          { name: 'RIGHTS' },
+          { name: 'DATA_PROVIDER' },
+          { name: 'REUSABILITY' },
+          { name: 'TYPE' }
+        ]
+      });
+
+      it('injects `theme` pseudo-facet first', () => {
+        wrapper.vm.orderedFacets[0].name.should.eq('THEME');
+      });
+
+      it('follows with ordered default facets from search plugin', () => {
+        wrapper.vm.orderedFacets[1].name.should.eq('TYPE');
+        wrapper.vm.orderedFacets[2].name.should.eq('REUSABILITY');
+        wrapper.vm.orderedFacets[3].name.should.eq('COUNTRY');
+      });
+
+      it('ends with any other facets in their original order', () => {
+        wrapper.vm.orderedFacets[4].name.should.eq('RIGHTS');
+        wrapper.vm.orderedFacets[5].name.should.eq('DATA_PROVIDER');
+      });
+    });
+
+    describe('qf', () => {
+      const wrapper = factory({
+        selectedFacets: {
+          'THEME': 'art',
+          'REUSABILITY': ['open'],
+          'TYPE': ['IMAGE', 'SOUND'],
+          'contentTier': ['4']
+        }
+      });
+
+      it('omits THEME', () => {
+        wrapper.vm.qf.should.not.include('THEME:art');
+        wrapper.vm.qf.should.not.include('THEME:"art"');
+      });
+
+      it('omits REUSABILITY', () => {
+        wrapper.vm.qf.should.not.include('REUSABILITY:open');
+        wrapper.vm.qf.should.not.include('REUSABILITY:"open"');
+      });
+
+      context('for default facets from search plugin', () => {
+        it('includes fielded and quoted queries for each value', () => {
+          wrapper.vm.qf.should.include('TYPE:"IMAGE"');
+          wrapper.vm.qf.should.include('TYPE:"SOUND"');
+        });
+      });
+
+      context('for any other facetes', () => {
+        it('includes fielded but unquoted queries for each value', () => {
+          wrapper.vm.qf.should.include('contentTier:4');
+        });
+      });
+    });
+
+    describe('reusability', () => {
+      context('when REUSABILITY facet is not set', () => {
         const wrapper = factory({
-          totalResults: 100
+          selectedFacets: {
+            'TYPE': ['IMAGE', 'SOUND']
+          }
         });
 
-        it('is `true`', () => {
-          wrapper.vm.noMoreResults.should.be.true;
+        it('is `undefined`', () => {
+          (typeof wrapper.vm.reusability).should.eql('undefined');
+        });
+      });
+
+      context('when REUSABILITY facet is set', () => {
+        const wrapper = factory({
+          selectedFacets: {
+            'REUSABILITY': ['open', 'permission'],
+            'TYPE': ['IMAGE', 'SOUND']
+          }
+        });
+
+        it('is its value joined with ","', () => {
+          wrapper.vm.reusability.should.eq('open,permission');
+        });
+      });
+    });
+
+    describe('theme', () => {
+      context('when THEME facet is not set', () => {
+        const wrapper = factory({
+          selectedFacets: {
+            'TYPE': ['IMAGE', 'SOUND']
+          }
+        });
+
+        it('is `undefined`', () => {
+          (typeof wrapper.vm.theme).should.eql('undefined');
+        });
+      });
+
+      context('when THEME facet is set', () => {
+        const wrapper = factory({
+          selectedFacets: {
+            'THEME': 'migration',
+            'TYPE': ['IMAGE', 'SOUND']
+          }
+        });
+
+        it('is its value', () => {
+          wrapper.vm.theme.should.eq('migration');
         });
       });
     });
   });
 
-  describe('orderedFacets', () => {
-    const wrapper = factory({
-      facets: [
-        { name: 'COUNTRY' },
-        { name: 'RIGHTS' },
-        { name: 'DATA_PROVIDER' },
-        { name: 'REUSABILITY' },
-        { name: 'TYPE' }
-      ]
-    });
-
-    it('injects `theme` pseudo-facet first', () => {
-      wrapper.vm.orderedFacets[0].name.should.eq('THEME');
-    });
-
-    it('follows with ordered default facets from search plugin', () => {
-      wrapper.vm.orderedFacets[1].name.should.eq('TYPE');
-      wrapper.vm.orderedFacets[2].name.should.eq('REUSABILITY');
-      wrapper.vm.orderedFacets[3].name.should.eq('COUNTRY');
-    });
-
-    it('ends with any other facets in their original order', () => {
-      wrapper.vm.orderedFacets[4].name.should.eq('RIGHTS');
-      wrapper.vm.orderedFacets[5].name.should.eq('DATA_PROVIDER');
-    });
-  });
-
-  describe('qf', () => {
-    const wrapper = factory({
-      selectedFacets: {
-        'THEME': 'art',
-        'REUSABILITY': ['open'],
-        'TYPE': ['IMAGE', 'SOUND'],
-        'contentTier': ['4']
-      }
-    });
-
-    it('omits THEME', () => {
-      wrapper.vm.qf.should.not.include('THEME:art');
-      wrapper.vm.qf.should.not.include('THEME:"art"');
-    });
-
-    it('omits REUSABILITY', () => {
-      wrapper.vm.qf.should.not.include('REUSABILITY:open');
-      wrapper.vm.qf.should.not.include('REUSABILITY:"open"');
-    });
-
-    context('for default facets from search plugin', () => {
-      it('includes fielded and quoted queries for each value', () => {
-        wrapper.vm.qf.should.include('TYPE:"IMAGE"');
-        wrapper.vm.qf.should.include('TYPE:"SOUND"');
-      });
-    });
-
-    context('for any other facetes', () => {
-      it('includes fielded but unquoted queries for each value', () => {
-        wrapper.vm.qf.should.include('contentTier:4');
-      });
-    });
-  });
-
-  describe('reusability', () => {
-    context('when REUSABILITY facet is not set', () => {
-      const wrapper = factory({
-        selectedFacets: {
-          'TYPE': ['IMAGE', 'SOUND']
-        }
-      });
-
-      it('is `undefined`', () => {
-        (typeof wrapper.vm.reusability).should.eql('undefined');
-      });
-    });
-
-    context('when REUSABILITY facet is set', () => {
-      const wrapper = factory({
-        selectedFacets: {
-          'REUSABILITY': ['open', 'permission'],
-          'TYPE': ['IMAGE', 'SOUND']
-        }
-      });
-
-      it('is its value joined with ","', () => {
-        wrapper.vm.reusability.should.eq('open,permission');
-      });
-    });
-  });
-
-  describe('theme', () => {
-    context('when THEME facet is not set', () => {
-      const wrapper = factory({
-        selectedFacets: {
-          'TYPE': ['IMAGE', 'SOUND']
-        }
-      });
-
-      it('is `undefined`', () => {
-        (typeof wrapper.vm.theme).should.eql('undefined');
-      });
-    });
-
-    context('when THEME facet is set', () => {
-      const wrapper = factory({
-        selectedFacets: {
-          'THEME': 'migration',
-          'TYPE': ['IMAGE', 'SOUND']
-        }
-      });
-
-      it('is its value', () => {
-        wrapper.vm.theme.should.eq('migration');
-      });
-    });
+  describe('methods', () => {
+    // TODO
   });
 });
