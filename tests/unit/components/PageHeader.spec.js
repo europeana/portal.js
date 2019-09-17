a29e0c997e113767fd74b6afeb1b9ed0496ac42d
Richard Doe <richard.doe@rwdit.net>
2019-09-17 14:24:03 +0100

    Update page header unit tests

--- tests/unit/components/PageHeader.spec.js
+++ tests/unit/components/PageHeader.spec.js
@@ -1,23 +1,19 @@
-import { createLocalVue, createWrapper, shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import BootstrapVue from 'bootstrap-vue';
-import VueRouter from 'vue-router';
 import PageHeader from '../../../components/PageHeader.vue';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
-localVue.use(VueRouter);
-const router = new VueRouter();
 
 const factory = () => shallowMount(PageHeader, {
   localVue,
-  router,
   mocks: {
     $t: () => {},
     localePath: (code) => window.location.href + code
   }
 });
 
-describe('components/search/PageHeader', () => {
+describe('components/PageHeader', () => {
   it('contains a search form', () => {
     const wrapper = factory();
     const form =  wrapper.find('[data-qa="search form"]');
@@ -37,15 +33,5 @@ describe('components/search/PageHeader', () => {
     const selector = wrapper.find('[data-qa="language selector"]');
 
     selector.isVisible().should.equal(true);
-  });
-
-  describe('submitSearchForm()', () => {
-    it('emits submit:searchForm on $root', () => {
-      const wrapper = factory();
-      const rootWrapper = createWrapper(wrapper.vm.$root);
-
-      wrapper.vm.submitSearchForm();
-      rootWrapper.emitted()['submit:searchForm'].length.should.equal(1);
-    });
   });
 });
