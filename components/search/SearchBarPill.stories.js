637f1db5d03211d02ec464b35c36377f002bce00
monkemedia <info@monkemedia.co.uk>
2019-09-25 11:27:22 +0100

    Created 3 stories for Pills plus added i18n functionality to story

--- components/search/SearchBarPill.stories.js
+++ components/search/SearchBarPill.stories.js
@@ -1,14 +1,37 @@
 import { storiesOf } from '@storybook/vue';
-
 import SearchBarPill from './SearchBarPill.vue';
 
-storiesOf('Search page', module)
-  .add('Search bar pill', () => ({
+const i18n = {
+  locale: 'en',
+  messages: {
+    en: {
+      delete: 'Delete'
+    }
+  }
+};
+
+storiesOf('Search page / Pills', module)
+  .add('Default aria label', () => ({
     components: { SearchBarPill },
-    template: ` <b-container
-      class="mt-3"
-      >
+    i18n,
+    template: `
+      <b-container class="mt-3">
         <SearchBarPill text="This is a pill" :removeLinkTo="{}"/>
-        <SearchBarPill text="This is a pill trunacted" :removeLinkTo="{}"/>
+      </b-container>`
+  }))
+  .add('Custom aria label', () => ({
+    components: { SearchBarPill },
+    i18n,
+    template: `
+      <b-container class="mt-3">
+        <SearchBarPill text="This is a pill" :removeLinkTo="{}" removeLinkLabel="Delete button" />
+      </b-container>`
+  }))
+  .add('Truncated label', () => ({
+    components: { SearchBarPill },
+    i18n,
+    template: `
+      <b-container class="mt-3">
+        <SearchBarPill text="This is a pill truncated truncated" :removeLinkTo="{}"/>
       </b-container>`
   }));
