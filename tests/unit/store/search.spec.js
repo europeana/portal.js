ad1ab08444f1c41b718214af45aaed6d3c040854
Richard Doe <richard.doe@rwdit.net>
2019-09-26 13:58:31 +0100

    Extend search store unit tests

--- tests/unit/store/search.spec.js
+++ tests/unit/store/search.spec.js
@@ -13,7 +13,6 @@ const apiEndpoint = '/api/v2/search.json';
 const baseRequest = nock(apiUrl).get(apiEndpoint);
 const defaultResponse = { success: true, items: [], totalResults: 123456 };
 
-
 describe('store/search', () => {
   describe('actions', () => {
     describe('run', () => {
@@ -22,6 +21,8 @@ describe('store/search', () => {
       });
 
       it('searches the Record API', async() => {
+        const commit = sinon.spy();
+        const dispatch = sinon.spy();
         const searchQuery = 'anything';
 
         baseRequest
@@ -30,8 +31,6 @@ describe('store/search', () => {
           })
           .reply(200, defaultResponse);
 
-        const commit = sinon.spy();
-        const dispatch = sinon.spy();
         const params = {
           query: searchQuery
         };
@@ -40,16 +39,83 @@ describe('store/search', () => {
         nock.isDone().should.be.true;
       });
 
-      it('includes "hidden" `qf` params in API request');
+      it('includes "hidden" `qf` params in API request', async() => {
+        const commit = sinon.spy();
+        const dispatch = sinon.spy();
+        const unhiddenQf = 'TYPE:"IMAGE"';
+        const hiddenQf = 'edm_agent:"http://data.europeana.eu/agent/base/200"';
 
-      it('writes params to the store');
+        baseRequest
+          .query(query => {
+            return query.qf.includes(unhiddenQf) && query.qf.includes(hiddenQf);
+          })
+          .reply(200, defaultResponse);
+
+        const params = {
+          qf: [unhiddenQf],
+          hidden: {
+            qf: [hiddenQf]
+          }
+        };
+        await store.actions.run({ commit, dispatch }, params);
+
+        nock.isDone().should.be.true;
+      });
+
+      it('commits params to the store', async() => {
+        const commit = sinon.spy();
+        const dispatch = sinon.spy();
+        baseRequest
+          .query(true)
+          .reply(200, defaultResponse);
+
+        const params = {
+          page: 10,
+          qf: ['TYPE:"VIDEO"'],
+          query: 'migration',
+          reusability: 'open'
+        };
+        const selectedFacets = { 'REUSABILITY': ['open'], 'TYPE': ['VIDEO'] };
+
+        await store.actions.run({ commit, dispatch }, params);
+
+        commit.should.have.been.calledWith('setPage', params.page);
+        commit.should.have.been.calledWith('setQf', params.qf);
+        commit.should.have.been.calledWith('setQuery', params.query);
+        commit.should.have.been.calledWith('setReusability', params.reusability);
+        commit.should.have.been.calledWith('setSelectedFacets', selectedFacets);
+      });
 
       context('on success', () => {
-        it('dispatches updateForSuccess');
+        it('dispatches updateForSuccess', async() => {
+          const commit = sinon.spy();
+          const dispatch = sinon.spy();
+          baseRequest
+            .query(true)
+            .reply(200, defaultResponse);
+
+          await store.actions.run({ commit, dispatch }, {});
+
+          dispatch.should.have.been.calledWith('updateForSuccess');
+        });
       });
 
       context('on failure', () => {
-        it('dispatches updateForFailure');
+        it('dispatches updateForFailure', async() => {
+          const commit = sinon.spy();
+          const dispatch = sinon.spy();
+          const errorMessage = 'Invalid query parameter.';
+          baseRequest
+            .query(true)
+            .reply(400, {
+              success: false,
+              error: errorMessage
+            });
+
+          await store.actions.run({ commit, dispatch }, {});
+
+          dispatch.should.have.been.calledWith('updateForFailure');
+        });
       });
     });
   });
