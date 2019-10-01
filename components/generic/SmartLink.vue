0474828867124c289c773047373b445593e16e42
monkemedia <info@monkemedia.co.uk>
2019-10-01 12:31:35 +0100

    External links now show icon and open in new tab

--- components/generic/SmartLink.vue
+++ components/generic/SmartLink.vue
@@ -9,13 +9,22 @@
   <b-link
     v-else
     :href="path"
+    :target="isExternalLink ? '_blank' : '_self'"
     :class="linkClass"
   >
     <slot />
+    <b-img
+      v-if="isExternalLink"
+      src="../../assets/img/icons/link-external-white.svg"
+      class="external-icon"
+      :alt="$t('externalLink')"
+    />
   </b-link>
 </template>
 
 <script>
+  const parseDomain = require('parse-domain');
+
   export default {
     props: {
       destination: {
@@ -45,7 +54,17 @@
           });
         }
         return this.destination;
+      },
+      isExternalLink() {
+        return parseDomain(this.destination);
       }
     }
   };
 </script>
+
+<style lang="scss" scoped>
+  .external-icon {
+    margin-left: .2rem;
+    margin-bottom: .2rem;
+  }
+</style>
