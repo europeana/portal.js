169f6e44a5a5f5cacd7ecfd59752776775433eed
Lutz Biedinger <lutz.biedinger@gmail.com>
2019-10-01 16:50:22 +0200

    disable i18n browser language detection

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,18 +74,19 @@ module.exports = {
       // Enable browser language detection to automatically redirect user
       // to their preferred language as they visit your app for the first time
       // Set to false to disable
-      detectBrowserLanguage: {
-        // If enabled, a cookie is set once a user has been redirected to his
-        // preferred language to prevent subsequent redirections
-        // Set to false to redirect every time
-        useCookie: true,
-        // Cookie name
-        cookieKey: 'i18n_redirected',
-        // Set to always redirect to value stored in the cookie, not just once
-        alwaysRedirect: false,
-        // If no locale for the browsers locale is a match, use this one as a fallback
-        fallbackLocale: 'en'
-      },
+      detectBrowserLanguage: false,
+      //   {
+      //   // If enabled, a cookie is set once a user has been redirected to his
+      //   // preferred language to prevent subsequent redirections
+      //   // Set to false to redirect every time
+      //   useCookie: true,
+      //   // Cookie name
+      //   cookieKey: 'i18n_redirected',
+      //   // Set to always redirect to value stored in the cookie, not just once
+      //   alwaysRedirect: false,
+      //   // If no locale for the browsers locale is a match, use this one as a fallback
+      //   fallbackLocale: 'en'
+      // },
       vuex: {
         // Module namespace
         moduleName: 'i18n',
