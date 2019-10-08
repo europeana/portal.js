d247789958dba6bd6e7886618f7b6e051ff81817
monkemedia <info@monkemedia.co.uk>
2019-10-08 11:07:28 +0100

    Added unit tests

--- tests/unit/components/generic/CompareImageSlider.spec.js
+++ tests/unit/components/generic/CompareImageSlider.spec.js
@@ -6,7 +6,6 @@ import CompareImageSlider from '../../../../components/generic/CompareImageSlide
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-
 const factory = () => shallowMount(CompareImageSlider, {
   localVue,
   propsData: {
@@ -67,18 +66,7 @@ describe('components/generic/CompareImageSlider', () => {
     wrapper.vm.sliderBarPosition.left.should.eq('300px');
   });
 
-  it('calls `setImageWidth` method when browser has been resized', () => {
-    factory();
-    const setImageWidth = sinon.spy();
-    window.dispatchEvent(new Event('resize'));
-
-    setTimeout(() => {
-      setImageWidth.should.have.been.called;
-    });
-
-  });
-
-  it('calls `initDrag` method when the user clicks on the slider', async() => {
+  it('sets dragging property to `true` when the user clicks on the slider', async() => {
     const wrapper = factory();
     const slider = wrapper.find('[data-qa="compare image slider"]');
 
@@ -87,43 +75,12 @@ describe('components/generic/CompareImageSlider', () => {
     wrapper.vm.dragging.should.eq(true);
   });
 
-  // it('has a title', () => {
-  //   const wrapper = factory();
-  //   wrapper.setProps({ headline: 'Welcome at Europeana' });
+  it('sets dragging property to `false` when the user clicks off the slider', async() => {
+    const wrapper = factory();
+    const slider = wrapper.find('[data-qa="compare image slider"]');
 
-  //   const title = wrapper.find('[data-qa="hero banner"] h2');
-  //   title.text().should.contain('Welcome at Europeana');
-  // });
+    slider.trigger('mouseup');
 
-  // it('has a description', () => {
-  //   const wrapper = factory();
-  //   wrapper.setProps({ description: 'Explore artworks, artefacts, books, films and music' });
-
-  //   const description = wrapper.find('[data-qa="hero banner"] .lead');
-  //   description.text().should.contain('Explore artworks, artefacts, books, films and music');
-  // });
-
-  // it('has a link', () => {
-  //   const wrapper = factory();
-  //   wrapper.setProps({ identifier: '/15508/DG2014_46_12' });
-
-  //   const link = wrapper.find('[data-qa="hero banner"] a');
-  //   link.attributes().href.should.contain('15508/DG2014_46_12');
-  // });
-
-  // it('has an attribution', () => {
-  //   const wrapper = factory();
-  //   wrapper.setProps({ attribution: 'Johannes Vermeer' });
-
-  //   const attribution = wrapper.find('[data-qa="hero banner"] a');
-  //   attribution.text().should.contain('Johannes Vermeer');
-  // });
-
-  // it('has a rights statement', () => {
-  //   const wrapper = factory();
-  //   wrapper.setProps({ rightsStatement: 'http://rightsstatements.org/vocab/InC/1.0/' });
-
-  //   const rights = wrapper.find('[data-qa="hero banner"] a span');
-  //   rights.text().should.contain('In Copyright');
-  // });
+    wrapper.vm.dragging.should.eq(false);
+  });
 });
