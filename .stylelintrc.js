ffff16892408e608ada8fbf551b60cf141f59dba
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-10-09 12:32:07 +0200

    Reuse icomoon CSS

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     'declaration-colon-space-after': 'always',
     'no-missing-end-of-source-newline': true,
-    'indentation': 2
-  }  
+    'indentation': 2,
+    'at-rule-no-unknown': null
+  }
 }
