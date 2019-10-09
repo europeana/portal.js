f5c033b62c882442cb29c55faf2065624fa5a7f4
monkemedia <info@monkemedia.co.uk>
2019-10-09 17:35:14 +0100

    Now tests both mutations and actions

---
+++ tests/unit/store/request.spec.js
@@ -0,0 +1,30 @@
+import * as store from '../../../store/request';
+import sinon from 'sinon';
+
+describe('store/request', () => {
+  describe('actions', () => {
+    it('adds current domain to state', async() => {
+      const commit = sinon.spy();
+      const req = {
+        host: 'www.example.org'
+      };
+
+      await store.actions.domain({ commit }, { req });
+
+      commit.should.have.been.calledWith('SET_DOMAIN', req.host);
+    });
+  });
+
+  describe('mutations', () => {
+    it('joins both domain name and top level domain together with a `.` and then sets state', () => {
+      const host = 'example.org';
+      const state = {
+        domain: null
+      };
+
+      store.mutations.SET_DOMAIN(state, host);
+
+      state.domain.should.eq('example.org');
+    });
+  });
+});
