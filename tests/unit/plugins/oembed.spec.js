61580c58f6b3c1d81918aabbc9bff8477a883e25
Richard Doe <richard.doe@europeana.eu>
2019-10-10 12:58:29 +0200

    oEmbed client (#276)
    * Replace oembed-parser with own implementation
    
    * Unit test oembeddable
    
    * YouTube is not supported; EUscreen is
    
    * Full RegExp escaping
    
    * Pending expectations for oembed()
    
    * Unit test oEmbed()
    
    * More atomic functions

---
+++ tests/unit/plugins/oembed.spec.js
@@ -0,0 +1,65 @@
+import nock from 'nock';
+import oEmbed, { oEmbeddable } from '../../../plugins/oembed.js';
+import supportedProviders from '../../../plugins/oembed/providers';
+
+const axios = require('axios');
+axios.defaults.adapter = require('axios/lib/adapters/http');
+
+describe('oEmbed()', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  for (const provider of supportedProviders) {
+    context(`when provider is "${provider.name}"`, () => {
+      const endpointUrl = new URL(provider.endpoint);
+
+      for (const scheme of provider.schemes) {
+        const embeddableUrl = scheme.replace(/\*/g, 'abcdef');
+
+        const nockRequest = (embeddableUrl) => {
+          nock(endpointUrl.origin)
+            .get(endpointUrl.pathname)
+            .query(query => {
+              return query.url === embeddableUrl;
+            })
+            .reply(200);
+        };
+
+        it('sends GET request to provider oEmbed endpoint', async() => {
+          nockRequest(embeddableUrl);
+
+          await oEmbed(embeddableUrl);
+
+          nock.isDone().should.be.true;
+        });
+
+        it('returns a promise', () => {
+          nockRequest(embeddableUrl);
+
+          const response = oEmbed(embeddableUrl);
+
+          response.should.be.fulfilled;
+        });
+      }
+    });
+  }
+});
+
+describe('oEmbeddable()', () => {
+  for (const provider of supportedProviders) {
+    context(`when provider is "${provider.name}"`, () => {
+      for (const scheme of provider.schemes) {
+        it(`is \`true\` for scheme "${scheme}"`, () => {
+          const embeddableUrl = scheme.replace(/\*/g, 'abcdef');
+          oEmbeddable(embeddableUrl).should.be.true;
+        });
+      }
+    });
+  }
+
+  it('is `false` for unsupported providers', () => {
+    const unsupportedembeddableUrl = 'https://www.youtube.com/watch?v=abcdef';
+    oEmbeddable(unsupportedembeddableUrl).should.be.false;
+  });
+});
