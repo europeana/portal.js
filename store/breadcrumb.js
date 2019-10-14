8cfb6097667634a88340a56070dfa73ac11522da
monkemedia <info@monkemedia.co.uk>
2019-10-14 15:46:46 +0100

    Correct path included

--- store/breadcrumb.js
+++ store/breadcrumb.js
@@ -4,9 +4,14 @@ export const state = () => ({
 
 export const mutations = {
   setBreadcrumb(state, value) {
-    const breadcrumb = [ { text:  this.app.i18n.t('blog.blog'), href: '#' } ];
+    if (!value) return;
+    const breadcrumb = [ { text:  this.app.i18n.t('blog.blog'), href: '/blog' } ];
     breadcrumb.push(value);
 
     state.data = breadcrumb;
+  },
+
+  clearBreadcrumb(state) {
+    state.data = null;
   }
 };
