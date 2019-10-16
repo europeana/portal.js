ceaa3f91c6ad7bed4db2fbfbd1ce2ff62b4500ca
Richy <info@monkemedia.co.uk>
2019-10-16 15:35:13 +0100

    Blog post page (#277)
    * First commit
    
    * Implementing blog post page
    
    * added tags
    
    * Added related posts
    
    * Breadcrumb is now handled in asyncData
    
    * Removed components from blog layout to make it more like a blog page
    
    * Cards are now wrapped with an anchor tag
    
    * Added elements to author component
    
    * Added components for right side bar
    
    * Using correct locale property
    
    * Tidied up html
    
    * Setting up store for breadcrumb
    
    * Breadcrumb is now in default layout and uses store
    
    * Now uses locale for Blog string
    
    * Correct path included
    
    * Renamed components to use 2 words
    
    * Now uses field identifier instead of system id
    
    * Removed capslock
    
    * Added some card styling
    
    * Now uses multiword names
    
    * Renamed files and added hr instead of bottom border
    
    * Now uses breadcrumb plugin
    
    * Added date published property
    
    * Added unit tests for blog
    
    * Added a browser title
    
    * Moved date to below title and added new attribute to element
    
    * Link is now on Authors name
    
    * Removed Hashes
    
    * Renamed component
    
    * Rewmoved related posts and and related post
    
    * Removed links from tags and categories
    
    * Now uses b-badge instead of span
    
    * Removed unused class

---
+++ components/blog/BlogTags.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-row class="flex-md-row py-3">
+    <b-col
+      v-if="tags.length > 0"
+      cols="10"
+      data-qa="blog tags"
+    >
+      <strong>Tags:</strong>
+      <!-- We will need URLS for the tags -->
+      <b-badge
+        v-for="(tag, index) in tags"
+        :key="index"
+        variant="secondary"
+      >
+        {{ tag }}
+      </b-badge>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+  export default {
+    name: 'TagAndShare',
+
+    props: {
+      tags: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .badge:not(:last-child) {
+    margin-right: .3rem;
+  }
+</style>
--- lang/en.js
+++ lang/en.js
@@ -79,5 +79,13 @@ export default {
     }
   },
   "removeFilter": "Remove {filterLabel} filter",
-  "newWindow": "opens in new window"
+  "newWindow": "opens in new window",
+  "blog": {
+    "blog": "Blog",
+    "author": "Author",
+    "authors": "Authors",
+    "category": "Category",
+    "categories": "Categories",
+    "relatedPosts": "Related posts"
+  }
 };
