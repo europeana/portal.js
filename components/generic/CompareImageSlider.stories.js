9cbef827c93ac36e63ea1dd61f5fbf9bd4c61945
monkemedia <info@monkemedia.co.uk>
2019-10-18 11:41:56 +0100

    Compare image component now works in StoryBookJs

--- components/generic/CompareImageSlider.stories.js
+++ components/generic/CompareImageSlider.stories.js
@@ -5,7 +5,7 @@ const i18n = {
   locale: 'en',
   messages: {
     en: {
-      'directions.right': 'foo'
+      directions: { right: 'foo', left: 'foo' }
     }
   }
 };
@@ -16,15 +16,31 @@ storiesOf('Generic', module)
     i18n,
     data() {
       return {
-        imageLeft: 'img/landscape.jpg',
-        imageRight: 'img/landscape-monochrome.jpg'
+        leftImageSrc: 'img/landscape.jpg',
+        rightImageSrc: 'img/landscape-monochrome.jpg',
+        leftImageAttribute: {
+          name: 'Name',
+          creator: 'Creator',
+          provider: 'Provider',
+          rightsStatement: 'Rights statement',
+          url: 'Url path'
+        },
+        rightImageAttribute: {
+          name: 'Name',
+          creator: 'Creator',
+          provider: 'Provider',
+          rightsStatement: 'Rights statement',
+          url: 'Url path'
+        }
       };
     },
     template: `
       <b-container class="mt-3">
         <CompareImageSlider
-          :image-left="imageLeft"
-          :image-right="imageRight"
+          :left-image-src="leftImageSrc"
+          :right-image-src="rightImageSrc"
+          :left-image-attribution="leftImageAttribute"
+          :right-image-attribution="rightImageAttribute"
         />
       </b-container>`
   }));
