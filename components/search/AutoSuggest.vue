3ee168181486c0242d919a046edd46a1fe510372
Richard <info@monkemedia.co.uk>
2019-11-04 15:54:06 +0000

    Now uses double quotes

--- components/search/AutoSuggest.vue
+++ components/search/AutoSuggest.vue
@@ -53,7 +53,7 @@
         id="autocomplete-results"
         class="auto-suggest-dropdown"
         data-qa="search suggestions"
-        role=”listbox”
+        role="listbox"
         :aria-hidden="!isActive"
       >
         <b-list-group-item
