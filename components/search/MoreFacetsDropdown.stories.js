d746abc81a0da6c18a2751864790cc291e39bfa9
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-11-05 14:57:18 +0100

    CodeClimate fixes

--- components/search/MoreFacetsDropdown.stories.js
+++ components/search/MoreFacetsDropdown.stories.js
@@ -1,12 +1,10 @@
-import Vuex from 'vuex';
-import VueRouter from 'vue-router';
 import { storiesOf } from '@storybook/vue';
 import MoreFacetsDropdown from './MoreFacetsDropdown.vue';
 
 storiesOf('Search/MoreFacetsDropdown', module)
   .add('More Filters', () => ({
     components: { MoreFacetsDropdown },
-     template: ` <b-container
+    template: ` <b-container
       class="mt-3"
       >
         <MoreFacetsDropdown />
