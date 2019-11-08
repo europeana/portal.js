1c2351f1442f4e614ff51ccdcf0e7665dd43d725
Richard <info@monkemedia.co.uk>
2019-11-08 13:27:34 +0000

    Now uses checkbox group component

--- components/search/MoreFacetsDropdownOptions.vue
+++ components/search/MoreFacetsDropdownOptions.vue
@@ -5,47 +5,61 @@
     >
       {{ $t(`facets.${facet.name}`).name }}
     </strong>
-    <div class="option-group">
-      <MoreFacetsDropdownOption
-        v-for="(filter, indx) in [].concat(facet.fields).splice(0, limitTo)"
-        :key="indx"
-        :filter="filter"
-        :facet-name="$t(`facets.${facet.name}`).name"
-      />
-    </div>
-    <div
-      v-if="facet.fields.length > limitTo"
-      v-show="isActive"
+    <b-form-checkbox-group
       class="option-group"
+      plain
     >
-      <MoreFacetsDropdownOption
-        v-for="(filter, indx) in [].concat(facet.fields).splice(limitTo)"
-        :key="indx"
-        :filter="filter"
-        :facet-name="$t(`facets.${facet.name}`).name"
-      />
-    </div>
+      <b-form-checkbox
+        v-for="(filter, i) in [].concat(facet.fields).splice(0, limitTo)"
+        :id="`${$t(`facets.${facet.name}`).name} - ${filter.label}`"
+        :key="i"
+        :value="filter.label"
+        :name="filter.label"
+        :data-qa="`${$t(`facets.${facet.name}`).name} checkbox`"
+        class="mb-3"
+      >
+        {{ filter.label }}
+        <span
+          class="reset icon-close"
+          :aria-label="$t('facets.button.reset')"
+        />
+      </b-form-checkbox>
+      <div
+        v-if="facet.fields.length > limitTo && isActive"
+        class="option-group"
+      >
+        <b-form-checkbox
+          v-for="(filter, i) in [].concat(facet.fields).splice(limitTo)"
+          :id="`${$t(`facets.${facet.name}`).name} - ${filter.label}`"
+          :key="i"
+          :value="filter.label"
+          :name="filter.label"
+          :data-qa="`${$t(`facets.${facet.name}`).name} checkbox`"
+          class="mb-3"
+        >
+          {{ filter.label }}
+          <span
+            class="reset icon-close"
+            :aria-label="$t('facets.button.reset')"
+          />
+        </b-form-checkbox>
+      </div>
+    </b-form-checkbox-group>
     <button
       v-if="facet.fields.length > limitTo"
       type="button"
       class="btn btn-link btn-toggle"
       :class="{ 'is-active': isActive }"
-      :data-qa="(isActive ? $t(`facets.button.hideAll`) : $t(`facets.button.showAll`)) + ' ' + $t(`facets.${facet.name}`).plural + ' button'"
+      :data-qa="(isActive ? $t(`facets.button.showLess`, { label: $t(`facets.${facet.name}`).plural }) + ' button' : $t(`facets.button.showAll`, { label: $t(`facets.${facet.name}`).plural }) + ' button')"
       @click.prevent="isActive = !isActive"
     >
-      {{ isActive ? $t(`facets.button.hideAll`) : $t(`facets.button.showAll`) }} {{ $t(`facets.${facet.name}`).plural }}
+      {{ isActive ? $t(`facets.button.showLess`, { label: $t(`facets.${facet.name}`).plural }) : $t(`facets.button.showAll`, { label: $t(`facets.${facet.name}`).plural }) }}
     </button>
   </b-dropdown-form>
 </template>
 
 <script>
-  import MoreFacetsDropdownOption from '../../components/search/MoreFacetsDropdownOption';
-
   export default {
-    components: {
-      MoreFacetsDropdownOption
-    },
-
     props: {
       index: {
         type: Number,
