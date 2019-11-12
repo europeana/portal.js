be1171bf11392f652aa648d3abb7b566f00fa184
Richy <info@monkemedia.co.uk>
2019-11-12 14:28:50 +0000

    Number of selected "more filters" (#334)
    * Added new icon - Circle
    
    * Added new active state and added styling for selected styling
    
    * Selected amount functionality implemented
    
    * Added unit test
    
    * Removed circle icon
    
    * Select amount now shows when value is greater than 0
    
    * Cleaned up code
    
    * Back to using Object.keys
    
    * added weight to bullet
    
    * removed any reference to circle icon
    
    * cleaned up code
    
    * Cleaned up code so its easier to read
    
    * Created unit test for MoreFacetsDropdownOptions
    
    * Minimised function

--- components/search/MoreFacetsDropdown.vue
+++ components/search/MoreFacetsDropdown.vue
@@ -1,17 +1,27 @@
 <template>
   <b-dropdown
-    :text="$t('facets.button.morefilters')"
     variant="light"
     no-caret
     class="more-facets position-static"
+    :class="{ 'is-active' : selectedAmount > 0 }"
     data-qa="more filters dropdown button"
   >
+    <template v-slot:button-content>
+      {{ $t('facets.button.morefilters') }}
+      <span
+        v-if="selectedAmount > 0"
+        class="selected-amount"
+      >
+        {{ selectedAmount }}
+      </span>
+    </template>
     <b-dropdown-group class="more-facets-wrapper">
       <MoreFacetsDropdownOptions
         v-for="(facet, index) in moreFacets"
         :key="index"
         :fields="facet.fields"
         :name="facet.name"
+        @selectedOptions="updateSelected"
       />
     </b-dropdown-group>
     <li
@@ -33,6 +43,7 @@
 </template>
 
 <script>
+  import Vue from 'vue';
   import MoreFacetsDropdownOptions from '../../components/search/MoreFacetsDropdownOptions';
 
   export default {
@@ -45,7 +56,24 @@
         type: Array,
         default: () => []
       }
+    },
+
+    data() {
+      return {
+        selected: {}
+      };
+    },
+
+    computed: {
+      selectedAmount() {
+        return [].concat(...Object.values(this.selected)).length;
+      }
+    },
+
+    methods: {
+      updateSelected(facetName, selectedFields) {
+        Vue.set(this.selected, facetName, selectedFields);
+      }
     }
   };
 </script>
-
---
+++ tests/unit/components/search/MoreFacetsDropdownOptions.spec.js
@@ -0,0 +1,39 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
+import MoreFacetsDropdownOptions from '../../../../components/search/MoreFacetsDropdownOptions.vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const factory = () => mount(MoreFacetsDropdownOptions, {
+  localVue,
+  mocks: {
+    $t: (key) => key,
+    $tc: (key) => key
+  },
+  propsData: {
+    name: 'LANGUAGE',
+    fields: [
+      {
+        label: 'de',
+        count: 123
+      },
+      {
+        label: 'sv',
+        count: 12
+      }
+    ]
+  }
+});
+
+describe('components/search/MoreFacetsDropdownOptions', () => {
+  it('emits `selectedOptions` event when selected method is called', async() => {
+    const wrapper = factory();
+    const checkbox = wrapper.find('[data-qa="facets.LANGUAGE.name checkbox"]');
+
+    wrapper.setData({ limitTo: 9 });
+
+    checkbox.trigger('click');
+    wrapper.emitted()['selectedOptions'].should.eql([ [ 'LANGUAGE', [ 'de' ] ] ]);
+  });
+});
