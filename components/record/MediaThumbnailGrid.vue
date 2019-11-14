361857f4d17a8e8dfb9a40d39e65f1871156987e
Richard Doe <richard.doe@rwdit.net>
2019-11-14 11:58:46 +0000

    Document thumbnail grid component

--- components/record/MediaThumbnailGrid.vue
+++ components/record/MediaThumbnailGrid.vue
@@ -22,29 +22,46 @@
     name: 'MediaThumbnailGrid',
 
     props: {
+      // Array of media items as returned by the API's standard JSON response.
+      // Expected to have `about` property with the web resource's URI.
       media: {
         type: Array,
         required: true
       },
 
+      // URI of the pre-selected thumbnail, to highlight.
       selected: {
         type: String,
         required: true
       },
 
+      // Size of thumbnail to display, passed to the thumbnail API.
+      // Valid values are 'w200' and 'w400'.
       size: {
         type: String,
-        default: 'w200'
+        default: 'w200',
+        validator: (size) => {
+          return ['w200', 'w400'].includes(size);
+        }
       }
     },
 
     data() {
       return {
+        // URI of the currently selected thumbnail.
         currentSelection: this.selected
       };
     },
 
     computed: {
+      /**
+       * Construct thumbnails for media items using the Thumbnail API.
+       *
+       * Each object in the response has properties:
+       * * `key`: the URI of the media item
+       * * `src`: the URL of the thumbnail
+       * @return {Object[]} Array of thumbnail objects
+       */
       thumbnails() {
         return this.media.map((item) => {
           return {
@@ -57,10 +74,20 @@
     },
 
     methods: {
+      /**
+       * Is this thumbnail the currently selected one?
+       * @param {Object} thumbnail Thumbnail object constructed by `thumbnails()`
+       * @return {Boolean}
+       */
       isSelected(thumbnail) {
         return thumbnail.key === this.currentSelection;
       },
 
+      /**
+       * Handle clicking on a thumbnail to make it the selected one, and
+       * emit the `select` event with the media's URI.
+       * @param {string} thumbnailKey Key of the thumbnail object, i.e. its URI
+       */
       clickThumbnail(thumbnailKey) {
         this.currentSelection = thumbnailKey;
         this.$emit('select', thumbnailKey);
