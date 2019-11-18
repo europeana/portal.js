6b01e0a9774b91844d66534a64804f22169965b6
Richard <info@monkemedia.co.uk>
2019-11-18 14:27:43 +0000

    Added breakpoint for mobile only

--- components/generic/SimilarItems.vue
+++ components/generic/SimilarItems.vue
@@ -38,6 +38,8 @@
 </script>
 
 <style lang="scss">
+  @import "./assets/scss/variables.scss";
+
   .similar-items {
     .content-card {
       background: transparent;
@@ -45,6 +47,11 @@
       box-shadow: none;
       min-height: auto;
       margin-bottom: 0;
+
+      @media (max-width: $bp-medium) {
+        flex: 100%;
+        margin-bottom: 1rem;
+      }
     }
 
     .card-link {
