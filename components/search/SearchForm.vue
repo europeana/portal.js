907c73fa6326fe29249bd37207099489e4daed6e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-11-19 09:49:11 +0100

    Remove role to see if tests succeed

--- components/search/SearchForm.vue
+++ components/search/SearchForm.vue
@@ -27,7 +27,6 @@
         :placeholder="$t('searchPlaceholder')"
         name="query"
         data-qa="search box"
-        role="searchbox"
         aria-autocomplete="list"
         :aria-controls="enableAutoSuggest ? 'search-form-auto-suggest' : null"
         :aria-label="$t('search')"
