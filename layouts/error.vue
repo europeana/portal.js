930c57e644490895e8a529827f273a6ab459f131
Richard <info@monkemedia.co.uk>
2019-11-22 10:51:38 +0000

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    data-qa="error page"
+  >
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1>Error {{ error.statusCode }}</h1>
+          <p>{{ error.message }}</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorPage',
+
+    props: {
+      error: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+</script>
