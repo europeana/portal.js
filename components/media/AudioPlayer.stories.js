bca3383ecfeaad44cfa3392df8c360bf1e396530
Richard <info@monkemedia.co.uk>
2019-11-25 11:21:16 +0000

    Added example audio files

--- components/media/AudioPlayer.stories.js
+++ components/media/AudioPlayer.stories.js
@@ -1,16 +1,15 @@
 import { storiesOf } from '@storybook/vue';
-
 import AudioPlayer from './AudioPlayer.vue';
 
 storiesOf('Media/Audio Player', module)
-  .add('WebM video', () => ({
+  .add('OGG Format', () => ({
     components: {
       AudioPlayer
     },
     data() {
       return {
-        src: 'http://rs.kystreise.no/filestore/5/0/1/9_325fd46a56fb430/5019_alt_1773_d79cc2c9d9c9b45.webm',
-        type: 'video/webm'
+        src: 'https://proxy.europeana.eu/9200369/webclient_DeliveryManager_pid_8412047_custom_att_2_simple_viewer?api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi&view=http%3A%2F%2Fatena.beic.it%2Fwebclient%2FDeliveryManager%3Fpid%3D8412117%26custom_att_2%3Ddeeplink',
+        type: 'audio/ogg'
       };
     },
     template: `
@@ -22,14 +21,33 @@ storiesOf('Media/Audio Player', module)
       </b-container>
     `
   }))
-  .add('MP4 video', () => ({
+  .add('FLAC Format', () => ({
     components: {
       AudioPlayer
     },
     data() {
       return {
-        src: 'https://europeana1914-1918.s3.amazonaws.com/attachments/236696/9555.236696.original.mp4',
-        type: 'video/mp4'
+        src: 'https://hpr.dogphilosophy.net/test/flac.flac',
+        type: 'audio/flac'
+      };
+    },
+    template: `
+      <b-container class="mt-3">
+        <AudioPlayer
+          :src="src"
+          :type="type"
+        />
+      </b-container>
+    `
+  }))
+  .add('MP3 Format', () => ({
+    components: {
+      AudioPlayer
+    },
+    data() {
+      return {
+        src: 'https://proxy.europeana.eu/2059203/data_sounds_http___www_statsbiblioteket_dk_nationalbibliotek_adgang_til_samlingerne_musik_og_lyd_rubensamlingen_lydoptagelser_resolveuid_12eb142eb7cd491199a60d5b89bd8773?api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi&view=http%3A%2F%2Fwww.statsbiblioteket.dk%2Fnationalbibliotek%2Fadgang-til-samlingerne%2Fmusik-og-lyd%2Frubensamlingen-lydoptagelser%2Fresolveuid%2F12eb142eb7cd491199a60d5b89bd8773',
+        type: 'audio/mp3'
       };
     },
     template: `
