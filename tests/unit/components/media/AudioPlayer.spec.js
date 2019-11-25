8ab7bc71505f4ed194917fc4dc18502f12d204a4
Richard <info@monkemedia.co.uk>
2019-11-25 15:01:31 +0000

    Added unit tests for audio player and media presentation

---
+++ tests/unit/components/media/AudioPlayer.spec.js
@@ -0,0 +1,30 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
+import AudioPlayer from '../../../../components/media/AudioPlayer.vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const factory = () => shallowMount(AudioPlayer, {
+  propsData: {
+    src: 'https://example.org',
+    type: 'audio/mp3'
+  },
+  localVue
+});
+
+describe('components/media/AudioPlayer', () => {
+  it('has a source', async() => {
+    const wrapper = factory();
+    const audioSource = wrapper.find('[data-qa="audio source"]');
+
+    audioSource.attributes().src.should.eq('https://example.org');
+  });
+
+  it('has a MIME type', async() => {
+    const wrapper = factory();
+    const audioSource = wrapper.find('[data-qa="audio source"]');
+
+    audioSource.attributes().type.should.eq('audio/mp3');
+  });
+});
