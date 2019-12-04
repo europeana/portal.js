332d1ec15bdb7ed381103bf8ce1b9b707d025ec4
Richard Doe <richard.doe@rwdit.net>
2019-12-04 14:45:46 +0100

    Test field display

--- tests/unit/components/search/SearchResult.spec.js
+++ tests/unit/components/search/SearchResult.spec.js
@@ -5,40 +5,85 @@ import SearchResult from '../../../../components/search/SearchResult.vue';
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-const factory = () => mount(SearchResult, {
-  localVue
+const factory = (propsData) => mount(SearchResult, {
+  localVue,
+  propsData,
+  mocks: {
+    $i18n: {
+      locale: 'en'
+    },
+    $t: (key) => key
+  }
 });
 
 describe('components/search/SearchResult', () => {
-  describe('result.edmPreview', () => {
-    it('is displayed as an img element', () => {
-      const wrapper = factory();
+  const requiredProps = {
+    edmPreview: 'https://www.example.org/thumbnail.jpg',
+    edmDataProvider: ['Data Provider']
+  };
 
-      wrapper.setProps({ result: { edmPreview: 'https://www.example.org/thumbnail.jpg' } });
-      const image =  wrapper.find('img[data-field-name="edmPreview"]');
+  it('displays edmPreview as an img element', () => {
+    const wrapper = factory(requiredProps);
 
-      image.attributes().src.should.eq('https://www.example.org/thumbnail.jpg');
+    const image =  wrapper.find('img[data-field-name="edmPreview"]');
+
+    image.attributes().src.should.eq(requiredProps.edmPreview);
+  });
+
+  it('displays edmDataProvider', () => {
+    const wrapper = factory(requiredProps);
+
+    const element = wrapper.find('[data-field-name="edmDataProvider"]');
+
+    element.text().should.eq(requiredProps.edmDataProvider[0]);
+  });
+
+  it('displays dcCreator as a list', () => {
+    const props = Object.assign({
+      dcCreator: { def: ['Anna', 'Bob'] }
+    }, requiredProps);
+    const wrapper = factory(props);
+
+    const fields = wrapper.findAll('[data-field-name="dcCreator"] ul li');
+
+    fields.at(0).text().should.eq(props.dcCreator.def[0]);
+    fields.at(1).text().should.eq(props.dcCreator.def[1]);
+  });
+
+  context('when dcTitle is present', () => {
+    const props = Object.assign({
+      dcTitle: { def: ['Title'] },
+      dcDescription: { def: ['Description'] }
+    }, requiredProps);
+
+    it('displays dcTitle', () => {
+      const wrapper = factory(props);
+
+      const title = wrapper.find('[data-field-name="dcTitle"]');
+
+      title.text().should.eq(props.dcTitle.def[0]);
+    });
+
+    it('ignores dcDescription', () => {
+      const wrapper = factory(props);
+
+      const description = wrapper.find('[data-field-name="dcDescription"]');
+
+      description.exists().should.be.false;
     });
   });
 
-  describe('result.fields properties', () => {
-    it('is displayed unadorned if a single value', () => {
-      const wrapper = factory();
+  context('when dcTitle is absent', () => {
+    const props = Object.assign({
+      dcDescription: { def: ['Description'] }
+    }, requiredProps);
 
-      wrapper.setProps({ result: { fields: { dcSpatial: ['Paris'] } } });
+    it('displays dcDescription', () => {
+      const wrapper = factory(props);
 
-      const field =  wrapper.find('[data-field-name="dcSpatial"]');
-      field.text().should.eq('Paris');
-    });
+      const title = wrapper.find('[data-field-name="dcDescription"]');
 
-    it('is displayed as a list if multi-value', () => {
-      const wrapper = factory();
-
-      wrapper.setProps({ result: { fields: { dcSpatial: ['Paris', 'London'] } } });
-
-      const fields = wrapper.findAll('[data-field-name="dcSpatial"] ul li');
-      fields.at(0).text().should.eq('Paris');
-      fields.at(1).text().should.eq('London');
+      title.text().should.eq(props.dcDescription.def[0]);
     });
   });
 });
