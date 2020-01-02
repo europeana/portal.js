dbe66c30dcf0e4611059e4a45dee85ae90dc970b
Richard Doe <richard.doe@rwdit.net>
2020-01-02 13:25:24 +0000

    DRY up MoreFiltersDropdownFacet

---
+++ components/search/MoreFiltersDropdownFacetOption.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-form-checkbox
+    :value="option"
+    :data-qa="`${option} checkbox`"
+    class="mb-3"
+  >
+    <ColourSwatch
+      v-if="forColourPalette"
+      :hex-code="option"
+      :aria-labelledby="fieldLabelId"
+    />
+    <FacetFieldLabel
+      :id="fieldLabelId"
+      :facet-name="facetName"
+      :field-value="option"
+    />
+    <span
+      class="reset icon-close"
+      :aria-label="$t('facets.button.reset')"
+    />
+  </b-form-checkbox>
+</template>
+
+<script>
+  import ColourSwatch from '../generic/ColourSwatch';
+  import FacetFieldLabel from './FacetFieldLabel';
+
+  export default {
+    name: 'MoreFiltersDropdownFacetOption',
+
+    components: {
+      ColourSwatch,
+      FacetFieldLabel
+    },
+
+    props: {
+      facetName: {
+        type: String,
+        required: true
+      },
+
+      option: {
+        type: String,
+        required: true
+      },
+
+      index: {
+        type: Number,
+        required: true
+      }
+    },
+
+    data() {
+      return {
+        COLOURPALETTE: 'COLOURPALETTE'
+      };
+    },
+
+    computed: {
+      forColourPalette() {
+        return this.facetName === this.COLOURPALETTE;
+      },
+
+      fieldLabelId() {
+        return `facet-field-${this.facetName}-${this.index}`;
+      }
+    }
+  };
+</script>
