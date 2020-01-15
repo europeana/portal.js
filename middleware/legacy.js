59dd010bc16a59313aac681def5eb4a9f18a01d5
Richard Doe <richard.doe@rwdit.net>
2020-01-15 14:38:37 +0000

    Legacy entity page URL redirects

--- middleware/legacy.js
+++ middleware/legacy.js
@@ -1,31 +1,45 @@
 // This middleware is responsible for handling redirects for legacy URLs used on
 // previous versions of the Europeana Portal.
 
-function stringifyPathMatch(regexpMatch) {
-  return regexpMatch.slice(1).filter((substring) => typeof substring !== undefined).join('');
+function stringifyPathChunks(chunks) {
+  return chunks.filter((chunk) => typeof chunk !== undefined).join('');
 }
 
-export default ({ redirect, route }) => {
+const rules = [
   // Remove legacy /portal prefix
-  const legacyPortalPrefixPattern = /^\/portal(\/.*)$/;
-  const legacyPortalPrefixMatch = route.path.match(legacyPortalPrefixPattern);
-  if (legacyPortalPrefixMatch) {
-    return redirect({
-      path: legacyPortalPrefixMatch[1],
-      query: route.query
-    });
-  }
-
+  (routePath) => {
+    const legacyPortalPrefixPattern = /^\/portal(\/.*)$/;
+    const legacyPortalPrefixMatch = routePath.match(legacyPortalPrefixPattern);
+    return legacyPortalPrefixMatch ? legacyPortalPrefixMatch[1] : null;
+  },
   // Remove .html from record page URLs
-  const legacyRecordPagePattern = /^(\/[a-z]{2})?(\/record\/[0-9]+\/[a-zA-Z0-9_]+)\.html$/;
-  const legacyRecordPageMatch = route.path.match(legacyRecordPagePattern);
-  if (legacyRecordPageMatch) {
-    const redirectPath = stringifyPathMatch(legacyRecordPageMatch);
-    return redirect({
-      path: redirectPath,
-      query: route.query
-    });
+  (routePath) => {
+    const legacyRecordPagePattern = /^(\/[a-z]{2})?(\/record\/[0-9]+\/[a-zA-Z0-9_]+)\.html$/;
+    const legacyRecordPageMatch = routePath.match(legacyRecordPagePattern);
+    return legacyRecordPageMatch ? stringifyPathChunks(legacyRecordPageMatch.slice(1)) : null;
+  },
+  // Redirect legacy entity page URLs
+  (routePath) => {
+    const legacyAgentEntityPagePattern = /^(\/[a-z]{2})?(\/explore\/(people|topics))(\/[0-9]+)/;
+    const legacyAgentEntityPageMatch = routePath.match(legacyAgentEntityPagePattern);
+    return legacyAgentEntityPageMatch ? stringifyPathChunks([
+      legacyAgentEntityPageMatch[1],
+      legacyAgentEntityPageMatch[2].replace('/explore', '/entity').replace('/people', '/person').replace('/topics', '/topic'),
+      legacyAgentEntityPageMatch[4]
+    ]) : null;
   }
+];
 
-  return;
+export default ({ redirect, route }) => {
+  for (const rule of rules) {
+    const redirectPath = rule(route.path);
+    if (redirectPath) {
+      // TODO: instead of returning here, should we keep looping over other rules
+      //       so multiple rules get applied all at once?
+      return redirect({
+        path: redirectPath,
+        query: route.query
+      });
+    }
+  }
 };
--- tests/features/common/legacy-redirects.feature
+++ tests/features/common/legacy-redirects.feature
@@ -7,3 +7,11 @@ Feature: Legacy redirects
   Scenario: Remove record page .html suffix
     When I open `/de/record/90402/SK_A_2344.html`
     Then I should be on `/de/record/90402/SK_A_2344`
+
+  Scenario: Redirect legacy agent entity URLs
+    When I open `/fr/explore/people/60404-johannes-vermeer.html`
+    Then I should be on `/fr/entity/person/60404-johannes-vermeer`
+
+  Scenario: Redirect legacy topic entity URLs
+    When I open `/nl/explore/topics/47-painting.html`
+    Then I should be on `/nl/entity/topic/47-painting`
