6161e480dd3330e17d57361468bfba0882f6b02f
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-01-15 12:24:06 +0100

    Update icons and labelling for rights statements

--- components/generic/RightsStatement.vue
+++ components/generic/RightsStatement.vue
@@ -35,23 +35,23 @@
           break;
         case new RegExp('https?://creativecommons.org/licenses/by/').test(val):
           rights.name = 'CC BY';
-          rights.iconClass = ['icon-license-cc'];
+          rights.iconClass = ['icon-license-cc', 'icon-license-by'];
           break;
         case new RegExp('https?://creativecommons.org/licenses/by-sa').test(val):
           rights.name = 'CC BY-SA';
-          rights.iconClass = ['icon-license-cc', 'icon-license-by'];
+          rights.iconClass = ['icon-license-cc', 'icon-license-by', 'icon-license-sa'];
           break;
         case new RegExp('https?://creativecommons.org/licenses/by-nc/').test(val):
           rights.name = 'CC BY-NC';
           rights.iconClass = ['icon-license-cc', 'icon-license-by', 'icon-license-nc'];
           break;
         case new RegExp('https?://creativecommons.org/licenses/by-nc-sa').test(val):
-          rights.name = 'CC BY-NC-ND';
-          rights.iconClass = ['icon-license-cc', 'icon-license-by', 'icon-license-nc', 'icon-license-nd'];
-          break;
-        case new RegExp('https?://creativecommons.org/licenses/by-nc-nd').test(val):
           rights.name = 'CC BY-NC-SA';
           rights.iconClass = ['icon-license-cc', 'icon-license-by', 'icon-license-nc', 'icon-license-sa'];
+          break;
+        case new RegExp('https?://creativecommons.org/licenses/by-nc-nd').test(val):
+          rights.name = 'CC BY-NC-ND';
+          rights.iconClass = ['icon-license-cc', 'icon-license-by', 'icon-license-nc', 'icon-license-nd'];
           break;
         case new RegExp('https?://creativecommons.org/licenses/by-nd').test(val):
           rights.name = 'CC BY-ND';
