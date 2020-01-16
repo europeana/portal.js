7e2d156e6baacb8928398e006853c4cc80f103d3
Richard Doe <richard.doe@rwdit.net>
2020-01-16 12:46:00 +0000

    Redirect thematic collections

---
+++ middleware/legacy-redirects-static.js
@@ -0,0 +1,15 @@
+export default {
+  '/collections/world-war-I': '/entity/topic/83-1914-1918',
+  '/collections/archaeology': '/entity/topic/80-archaeology',
+  '/collections/art': '/entity/topic/190-art',
+  '/collections/fashion': '/search?query=&theme=fashion',
+  '/collections/industrial-heritage': '/entity/topic/129-industrial-heritage',
+  '/collections/manuscripts': '/entity/topic/17-manuscripts',
+  '/collections/maps': '/entity/topic/151-maps-and-geography',
+  '/collections/migration': '/entity/topic/128-migration',
+  '/collections/music': '/entity/topic/62-music',
+  '/collections/natural-history': '/entity/topic/156-natural-history',
+  '/collections/newspapers': '/entity/topic/18-newspaper',
+  '/collections/photography': '/entity/topic/48-photography',
+  '/collections/sport': '/entity/topic/114-sport'
+};
