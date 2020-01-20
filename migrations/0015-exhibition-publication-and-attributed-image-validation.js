fbdbfdaa3aa93f921b9da7424e16b82ccbb222d0
Lutz Biedinger <lutz.biedinger@gmail.com>
2020-01-20 13:50:50 +0100

    only update validation on image with attribution. Field order for exhibitions published on.

--- migrations/0015-exhibition-publication-and-attributed-image-validation.js
+++ migrations/0015-exhibition-publication-and-attributed-image-validation.js
@@ -11,14 +11,12 @@ module.exports = function(migration) {
     .disabled(false)
     .omitted(false);
 
+  exhibitionPage.moveField('datePublished').afterField('primaryImageOfPage');
+
   const imageWithAttribution = migration.editContentType('imageWithAttribution');
 
   imageWithAttribution
     .editField('url')
-    .name('URL')
-    .type('Symbol')
-    .localized(false)
-    .required(false)
     .validations([
       {
         regexp: {
@@ -26,7 +24,5 @@ module.exports = function(migration) {
             '^(?![\\s\\S])|^(ftp|http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-\\/]))?$'
         }
       }
-    ])
-    .disabled(false)
-    .omitted(false);
+    ]);
 };
