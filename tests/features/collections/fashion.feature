2cb7647116c7e33d4a1ec77d4b6ee2838b401da9
Lutz Biedinger <lutz.biedinger@gmail.com>
2020-01-21 10:26:56 +0100

    tests for additional fashion facets

--- tests/features/collections/fashion.feature
+++ tests/features/collections/fashion.feature
@@ -4,3 +4,7 @@ Feature: Fashion collection
     Given I am on the `Fashion collection search page`
     When I click the `more filters dropdown button`
     Then I see the `CREATOR facet`
+    And I see the `proxy_dc_format facet`
+    # TODO: proxy_dcterms_medium should be displayed twice, once as colour and once as material
+    And I see the `proxy_dcterms_medium facet`
+    And I see the `proxy_dc_type facet`
--- tests/unit/store/collections/fashion.spec.js
+++ tests/unit/store/collections/fashion.spec.js
@@ -8,7 +8,19 @@ describe('store/collections/fashion', () => {
       };
 
       it('includes CREATOR in facet param', () => {
-        store.getters.apiParams(state).facet.should.startWith('CREATOR,');
+        store.getters.apiParams(state).facet.should.include('CREATOR,');
+      });
+
+      it('includes proxy_dc_format in facet param', () => {
+        store.getters.apiParams(state).facet.should.include('proxy_dc_format,');
+      });
+
+      it('includes proxy_dcterms_medium in facet param', () => {
+        store.getters.apiParams(state).facet.should.include('proxy_dcterms_medium,');
+      });
+
+      it('includes proxy_dc_type in facet param', () => {
+        store.getters.apiParams(state).facet.should.include('proxy_dc_type,');
       });
     });
   });
