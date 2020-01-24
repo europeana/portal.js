436dbd8d6cc2906b3651274437e0dc253f0643f9
Richard Doe <richard.doe@rwdit.net>
2020-01-24 07:52:40 +0000

    Filter other fashion facet fields

--- store/collections/fashion.js
+++ store/collections/fashion.js
@@ -1,7 +1,16 @@
 import { defaultFacetNames } from '../search';
 
-const fashionFacetNames = ['CREATOR', 'proxy_dc_format.en', 'proxy_dcterms_medium.en', 'proxy_dc_type.en'].concat(defaultFacetNames);
+const fashionFacetNames = [
+  'CREATOR', 'proxy_dc_format.en', 'proxy_dcterms_medium.en', 'proxy_dc_type.en'
+].concat(defaultFacetNames);
 const fashionFacetParam = fashionFacetNames.join(',');
+
+const facetFilters = {
+  'CREATOR': (field) => field.label.endsWith(' (Designer)'),
+  'proxy_dc_type.en': (field) => field.label.startsWith('Object Type: '),
+  'proxy_dc_format.en': (field) => field.label.startsWith('Technique: '),
+  'proxy_dcterms_medium.en': (field) => field.label.startsWith('Material: ')
+};
 
 export const state = () => ({
   apiParams: {},
@@ -23,8 +32,8 @@ export const mutations = {
   },
   filterFacets(state, facets) {
     facets.forEach((facet, index) => {
-      if (facet.name === 'CREATOR') {
-        facets[index]['fields'] = facets[index].fields.filter(creator => creator.label.endsWith('(Designer)'));
+      if (facetFilters[facet.name]) {
+        facets[index]['fields'] = facets[index].fields.filter(facetFilters[facet.name]);
       }
     });
 
