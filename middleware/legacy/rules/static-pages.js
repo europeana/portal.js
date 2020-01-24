35733cf23321db27e9ec362ba1f9259d2d02f4b9
Richard Doe <richard.doe@rwdit.net>
2020-01-24 14:20:52 +0000

    More static page redirects

--- middleware/legacy/rules/static-pages.js
+++ middleware/legacy/rules/static-pages.js
@@ -3,34 +3,52 @@
 import escapeRegExp from 'lodash/escapeRegExp';
 
 const redirects = {
-  '/collections/world-war-I': { path: '/entity/topic/83-1914-1918' },
-  '/collections/archaeology': { path: '/entity/topic/80-archaeology' },
-  '/collections/art': { path: '/entity/topic/190-art' },
+  '/about': '/about-us',
+  '/collections/archaeology': '/entity/topic/80-archaeology',
+  '/collections/art': '/entity/topic/190-art',
   '/collections/fashion': { path: '/search', query: { query: '', theme: 'fashion' } },
-  '/collections/industrial-heritage': { path: '/entity/topic/129-industrial-heritage' },
-  '/collections/manuscripts': { path: '/entity/topic/17-manuscripts' },
-  '/collections/maps': { path: '/entity/topic/151-maps-and-geography' },
-  '/collections/migration': { path: '/entity/topic/128-migration' },
-  '/collections/music': { path: '/entity/topic/62-music' },
-  '/collections/natural-history': { path: '/entity/topic/156-natural-history' },
-  '/collections/newspapers': { path: '/entity/topic/18-newspaper' },
-  '/collections/photography': { path: '/entity/topic/48-photography' },
-  '/collections/sport': { path: '/entity/topic/114-sport' },
-  '/exhibitions/foyer': { path: '/exhibitions' },
-  '/explore/galleries': { path: '/galleries' }
+  '/collections/industrial-heritage': '/entity/topic/129-industrial-heritage',
+  '/collections/manuscripts': '/entity/topic/17-manuscripts',
+  '/collections/maps': '/entity/topic/151-maps-and-geography',
+  '/collections/migration': '/entity/topic/128-migration',
+  '/collections/migration/collection-days-stories.html': '/migration/explore-migration-stories-from-events-in-these-cities',
+  '/collections/migration/collection-days': '/europeana-migration-collection-days',
+  '/collections/migration/explore-migration-stories': '/entity/topic/128-migration',
+  '/collections/music': '/entity/topic/62-music',
+  '/collections/natural-history': '/entity/topic/156-natural-history',
+  '/collections/newspapers': '/entity/topic/18-newspaper',
+  '/collections/newspapers/a-z-all': '/entity/topic/18-newspapers',
+  '/collections/newspapers/newspapers-help-guides': '/entity/topic/18-newspapers',
+  '/collections/photography': '/entity/topic/48-photography',
+  '/collections/sport': '/entity/topic/114-sport',
+  '/collections/world-war-I': '/entity/topic/83-1914-1918',
+  '/contact': '/contact-us',
+  '/exhibitions/foyer': '/exhibitions',
+  '/explore/galleries': '/galleries',
+  '/rights/accessibility': '/rights/accessibility-policy',
+  '/rights/api': '/rights/api-terms-of-use',
+  '/rights/contributions': '/rights/terms-for-user-contributions',
+  '/rights/data-sources': '/rights/europeana-data-sources',
+  '/rights/language': '/rights/language-policy',
+  '/rights/metadata': '/rights/usage-guidelines-for-metadata',
+  '/rights/privacy': '/rights/privacy-policy',
+  '/rights/public-domain': '/rights/public-domain-usage-guidelines',
+  '/rights/terms': '/rights/terms-of-use'
 };
 
 export default (route) => {
   for (const redirectFrom in redirects) {
+    const redirectTo = typeof redirects[redirectFrom] === 'string' ? { path: redirects[redirectFrom] } : redirects[redirectFrom];
+
     const pattern = new RegExp(`^/portal(/[a-z]{2})?${escapeRegExp(redirectFrom)}$`);
     const match = route.path.match(pattern);
 
     if (match) return {
       path: [
         match[1],
-        redirects[redirectFrom].path
+        redirectTo.path
       ],
-      query: redirects[redirectFrom].query
+      query: redirectTo.query
     };
   }
   return null;
