f15c711bd74823f9742dc1a976f887ab6706ef32
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-01-30 15:13:53 +0100

    Creation of gallery page
    - Update gallery foyer to include links to pages
    - Feature tests

---
+++ pages/gallery/_.vue
@@ -0,0 +1,74 @@
+<template>
+  <b-container>
+    <b-row class="flex-md-row pb-5">
+      <b-col
+        cols="9"
+        class="pb-3"
+      >
+        <h1 data-qa="gallery title">{{ title }}</h1>
+        <p
+          v-if="description"
+        >
+          {{ description }}
+        </p>
+      </b-col>
+      <b-col cols="12">
+        <b-card-group
+          class="card-deck-4-cols"
+          deck
+          data-qa="gallery images"
+        >
+          <ContentCard
+            v-for="image in images"
+            :key="image.fields.identifier"
+            :title="image.fields.name"
+            :image-url="image.fields.thumbnailUrl"
+            :texts="[image.fields.description]"
+            :url="{ name: 'record-all', params: { pathMatch: image.fields.identifier } }"
+          />
+        </b-card-group>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  import createClient from '../../plugins/contentful';
+  import ContentCard from '../../components/generic/ContentCard';
+
+  export default {
+    name: 'GalleryFoyer',
+    components: {
+      ContentCard
+    },
+    asyncData({ params, query, error, app }) {
+      const contentfulClient = createClient(query.mode);
+      return contentfulClient.getEntries({
+        'locale': app.i18n.isoLocale(),
+        'content_type': 'imageGallery',
+        'fields.identifier': params.pathMatch
+      })
+        .then((response) => {
+          return {
+            description: response.items[0].fields.description,
+            images: response.items[0].fields.hasPart,
+            title: response.items[0].fields.name
+          };
+        })
+        .catch((e) => {
+          error({ statusCode: 500, message: e.toString() });
+        });
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          { hid: 'title', name: 'title', content: this.title },
+          { hid: 'description', name: 'description', content: this.description },
+          { hid: 'og:title', property: 'og:title', content: this.title },
+          { hid: 'og:description', property: 'og:description', content: this.description }
+        ]
+      };
+    }
+  };
+</script>
