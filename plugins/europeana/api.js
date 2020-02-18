c14bd40fe1f7fee9f204fc598f660ed016ff55bc
Richard Doe <richard.doe@rwdit.net>
2020-02-18 12:15:26 +0000

    Deprecate EUROPEANA_API_KEY usage

--- plugins/europeana/api.js
+++ plugins/europeana/api.js
@@ -1,4 +1,3 @@
-// TODO: deprecate EUROPEANA_API_KEY env var
 export default {
   entity: {
     origin: process.env.EUROPEANA_ENTITY_API_ORIGIN || process.env.EUROPEANA_API_ORIGIN || 'https://api.europeana.eu',
@@ -6,10 +5,10 @@ export default {
   },
   newspaper: {
     origin: process.env.EUROPEANA_NEWSPAPER_API_ORIGIN || 'https://newspapers.eanadev.org',
-    key: process.env.EUROPEANA_NEWSPAPER_API_KEY || process.env.EUROPEANA_RECORD_API_KEY || process.env.EUROPEANA_API_KEY
+    key: process.env.EUROPEANA_NEWSPAPER_API_KEY || process.env.EUROPEANA_RECORD_API_KEY
   },
   record: {
     origin: process.env.EUROPEANA_RECORD_API_ORIGIN || process.env.EUROPEANA_API_ORIGIN || 'https://api.europeana.eu',
-    key: process.env.EUROPEANA_RECORD_API_KEY || process.env.EUROPEANA_API_KEY
+    key: process.env.EUROPEANA_RECORD_API_KEY
   }
 };
