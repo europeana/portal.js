d188ca931e03546ee0b0e0c8376bbe0a8cddf68e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-02-19 09:32:45 +0100

    WIP: Updated styling of exhibition pages

--- pages/exhibition/_exhibition/_chapter.vue
+++ pages/exhibition/_exhibition/_chapter.vue
@@ -35,6 +35,7 @@
             v-if="page"
             :sections="page.hasPart"
             :rich-text-is-card="false"
+            class="exhibition-sections"
           />
         </b-col>
       </b-row>
@@ -164,6 +165,17 @@
 </script>
 
 <style lang="scss" scoped>
+
+  .exhibition-sections {
+    text-align: center;
+  }
+
+  /deep/ .exhibition-sections .col {
+    margin-left: auto;
+    margin-right: auto;
+    text-align: left;
+  }
+
   /deep/ figure {
     display: inline-block;
     max-width: 100%;
