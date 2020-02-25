6caa4a8ccd2d01dd55329c4699936b2b498ec0ff
Richard Doe <richard.doe@rwdit.net>
2020-02-25 11:17:40 +0000

    Unit test coverage with Istanbul

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
+  env: {
+    mocha: true
+  },
   rules: {
     'no-console': 'off'
   }
-}
+};
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// Courtesy of https://gist.github.com/lsapan/3bfd0ffc0fb3d4a036fce84f6eea142e
+
+const { execSync } = require('child_process');
+
+module.exports = {
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'test') {
+      execSync('sed -i "s/source: pathutils.relativeTo(start.source, origFile),/source: origFile,/" node_modules/istanbul-lib-source-maps/lib/get-mapping.js');
+
+      config.devtool('cheap-module-eval-source-map');
+      config.module.rule('js')
+        .test(/\.js$/)
+        .use('istanbul-instrumenter-loader')
+        .loader('istanbul-instrumenter-loader')
+        .before('babel-loader')
+        .options({
+          esModules: true
+        });
+    }
+  }
+};
