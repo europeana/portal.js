1b746197f824fb69c50dd722638c96bfa0a7e72c
Lutz Biedinger <lutz.biedinger@gmail.com>
2020-02-27 13:10:39 +0100

    update legacy redirect specs with additional cases

--- tests/unit/plugins/europeana/legacy-search.spec.js
+++ tests/unit/plugins/europeana/legacy-search.spec.js
@@ -6,7 +6,7 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: '' };
       it('redirects to the classic portal', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/search?q=');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/search?q=');
       });
     });
 
@@ -14,7 +14,7 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: 'test' };
       it('redirects to the classic portal', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/search?q=test');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/search?q=test');
       });
     });
 
@@ -22,7 +22,7 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: '', qf: ['COUNTRY%3A"Germany"', 'TYPE%3A"IMAGE"', 'TYPE%3A"TEXT"'] };
       it('redirects to the classic portal reformatting the params', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/search?q=&f[COUNTRY][]="Germany"&f[TYPE][]="IMAGE"&f[TYPE][]="TEXT"');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/search?q=&f[COUNTRY][]="Germany"&f[TYPE][]="IMAGE"&f[TYPE][]="TEXT"');
       });
     });
 
@@ -30,7 +30,7 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: '', reusability: 'open' };
       it('redirects to the classic portal reformatting the params', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/search?q=&f[REUSABILITY][]=open');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/search?q=&f[REUSABILITY][]=open');
       });
     });
 
@@ -38,7 +38,7 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: '', qf: ['collection%3Aww1'] };
       it('returns the classic portal URL for the collection search', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/collections/world-war-I?q=');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/world-war-I?q=');
       });
     });
 
@@ -46,7 +46,81 @@ describe('plugins/europeana/legacy-search', () => {
       const searchParams = { query: 'test', qf: ['collection%3Aww1'] };
       it('returns the classic portal URL for the collection search', () => {
         const redirectUrl = legacyUrl(searchParams);
-        redirectUrl.should.eq('https://classic.europeana.eu/portal/collections/world-war-I?q=test');
+        redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/world-war-I?q=test');
+      });
+    });
+
+    context('for the newspapers collection', () => {
+      let newspaperQfParam = 'collection%3Anewspaper';
+      context('for a simple a fulltext search', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam], api: 'fulltext' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=collection');
+        });
+      });
+
+      context('for a simple a metadata search', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam], api: 'metadata' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=default');
+        });
+      });
+
+      context('for a search filtering by a start date', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam, 'proxy_dcterms_issued%3A[1900-01-01 TO *]'], api: 'fulltext' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=default&range[proxy_dcterms_issued][begin]=1900-01-01&range[proxy_dcterms_issued][end]=*');
+        });
+      });
+
+      context('for a search filtering by an end date', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam, 'proxy_dcterms_issued%3A[* TO 2000-12-31]'], api: 'fulltext' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=default&range[proxy_dcterms_issued][begin]=*&range[proxy_dcterms_issued][end]=2000-12-31');
+        });
+      });
+
+      context('for a search filtering by start and end date', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam, 'proxy_dcterms_issued%3A[1900-01-01 TO 2000-12-31]'], api: 'fulltext' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=default&range[proxy_dcterms_issued][begin]=1900-01-01&range[proxy_dcterms_issued][end]=2000-12-31');
+        });
+      });
+
+      context('for a search filtering by a specific date', () => {
+        const searchParams = { query: 'test', qf: [newspaperQfParam, 'proxy_dcterms_issued%3A1940-12-01'], api: 'fulltext' };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[api][]=default&range[proxy_dcterms_issued][begin]=1940-01-12&range[proxy_dcterms_issued][end]=1940-01-12');
+        });
+      });
+    });
+
+    context('for the fashion collection', () => {
+      let fashionQfParam = 'collection%3Afashion';
+      context('for a simple a fulltext search', () => {
+        const searchParams = { query: 'test', qf: [fashionQfParam, 'CREATOR%3A"Chanel (Designer)"', 'CREATOR%3A"Valens (Designer)"', 'proxy_dcterms_medium.en%3A"Material%3A silk"', 'proxy_dc_format.en%3A"Technique%3A weaving techniques"', 'proxy_dc_type.en%3A"Object Type%3A ensemble"'] };
+        it('returns the classic portal URL for the collection search', () => {
+          const redirectUrl = legacyUrl(searchParams);
+          redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/newspapers?q=test&f[CREATOR][]=Chanel+(Designer)&f[CREATOR][]=Valens+(Designer)&f[proxy_dc_format.en][]=Technique%3A+weaving+techniques&f[proxy_dc_type.en][]=Object+Type%3A+ensemble&f[proxy_dcterms_medium.en][]=Material%3A+silk');
+        });
+      });
+    });
+
+    context('for the migration collection', () => {
+      //let migrationQfParam = 'collection%3Afashion';
+      context('for a simple search only containing User generated Content', () => {
+        // Pending
+        // const searchParams = { query: 'test', qf: [migrationQfParam] };
+        // it('returns the classic portal URL for the collection search', () => {
+        //   const redirectUrl = legacyUrl(searchParams);
+        //   redirectUrl.should.eq('https://classic.europeana.eu/portal/en/collections/migration?q=test&&f[edm_UGC][]=true');
+        // });
       });
     });
   });
