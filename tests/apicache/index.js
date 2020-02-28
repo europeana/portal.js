ad63688733e8997724368830160b82c9415661f0
Richard Doe <richard.doe@rwdit.net>
2020-02-28 11:22:00 +0000

    Handle Newspapers API requests; ditch warmup

--- tests/apicache/index.js
+++ tests/apicache/index.js
@@ -1,22 +1,29 @@
 require('dotenv').config();
 
+console.log('process.env.APICACHE_LOG', process.env.APICACHE_LOG);
+
+const config = {
+  duration: process.env.APICACHE_DURATION || '1 day',
+  log: Number(process.env.APICACHE_LOG),
+  port: process.env.APICACHE_PORT || 4000
+};
+
 const express = require('express');
 const apicache = require('apicache');
 const cors = require('cors');
-// TODO: make logging configurable by env var?
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(cors());
-// app.use(morgan('combined'));
+if (config.log) app.use(morgan('combined'));
 
 // TODO: consider using Redis, then having Travis cache the db.
 //       would need a way to have it clear the cache. by an env var set in Travis
 //       causing a docker volume for the redis db to be recreated?
 const cache = apicache.options({
-  // debug: true
+  debug: config.log
 }).middleware;
 
 app.get('/', (req, res) => {
@@ -24,18 +31,30 @@ app.get('/', (req, res) => {
   res.send('OK');
 });
 
-// TODO: handle newspapers API too. As a separate server? Or wait til it is
-//       available via API gateway at /newspaper or similar.
 // TODO: handle Contentful too?
+//       would need SSL, e.g. w/ package "greenlock"
+
+// API gateway
 app.use(
   ['/api/v2', '/entity', '/record'],
-  cache(process.env.CACHE_DURATION || '1 day'),
+  cache(config.duration),
   proxy({
     target: 'https://api.europeana.eu/',
     changeOrigin: true
   })
 );
 
-const server = app.listen(process.env.APICACHE_PORT || 4000, () => {
+// Newspaper API (not yet available via gateway)
+app.use(
+  ['/newspaper'],
+  cache(config.duration),
+  proxy({
+    target: 'https://newspapers.eanadev.org',
+    changeOrigin: true,
+    pathRewrite: { '^/newspaper' : '' }
+  })
+);
+
+const server = app.listen(config.port, () => {
   console.log('Listening on port ' + server.address().port);
 });
