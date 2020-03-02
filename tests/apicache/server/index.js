f52fc6f8b73e639a1eb8b2c55f1b1f428b6921ee
Richard Doe <richard.doe@rwdit.net>
2020-03-02 14:51:11 +0100

    Remove Redis

--- tests/apicache/server/index.js
+++ tests/apicache/server/index.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const config = {
   duration: process.env.APICACHE_DURATION || '1 day',
   log: Number(process.env.APICACHE_LOG),
-  port: process.env.PORT || 80,
-  redisUrl: process.env.REDIS_URL,
-  contentfulCdaAccessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  port: process.env.PORT || 80
 };
 
 const apicache = require('apicache');
@@ -13,32 +11,13 @@ const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const redis = require('redis');
 
 const app = express();
 
 app.use(cors());
 if (config.log) app.use(morgan('combined'));
 
-const apicacheOptions = {
-  // debug: true
-};
-
-// Use Redis cache if configured in env
-//
-// To create one in Docker with persistent storage:
-//   docker volume create portal.js-test-apicache-redis-data
-//   docker run -d -p=16379:6379 --name=portal.js-test-apicache-redis \
-//     --mount source=portal.js-test-apicache-redis-data,target=/data \
-//     redis redis-server --appendonly yes
-if (config.redisUrl) {
-  const redisOptions = {
-    url: config.redisUrl
-  };
-  apicacheOptions.redisClient = redis.createClient(redisOptions);
-}
-
-const cache = apicache.options(apicacheOptions).middleware;
+const cache = apicache.options().middleware;
 
 app.get('/', (req, res) => {
   res.type('text/plain');
@@ -73,8 +52,6 @@ app.use(
   createProxyMiddleware({
     target: 'https://cdn.contentful.com/',
     changeOrigin: true,
-    // FIXME: why must we add this when it's in the source request?
-    // headers: { authorization: `Bearer ${config.contentfulCdaAccessToken}` },
     pathRewrite: { '^/contentful' : '' }
   })
 );
