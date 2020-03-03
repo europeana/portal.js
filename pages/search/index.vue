f25dd55ba55efc418d45feb2c2b0153f3e976325
Richard Doe <richard.doe@rwdit.net>
2020-03-03 17:14:38 +0100

    Include GA params in classic search links

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -42,7 +42,8 @@
     },
     computed: {
       notificationUrl() {
-        return legacyUrl(this.$route.query, this.$store.state.i18n.locale);
+        return legacyUrl(this.$route.query, this.$store.state.i18n.locale) +
+          '&utm_source=new-website&utm_medium=button';
       },
       redirectNotificationsEnabled() {
         return Boolean(Number(process.env.ENABLE_LINKS_TO_CLASSIC));
