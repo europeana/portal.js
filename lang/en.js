7a71c1305e0a4ce99f0b8cd087696cd936814e1e
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-03-04 10:20:40 +0100

    Replace slashes

--- lang/en.js
+++ lang/en.js
@@ -158,7 +158,7 @@ export default {
     "MIME_TYPE": {
       "name": "File format | File formats",
       "options": {
-        "text\/plain": "Plain text"
+        "text/plain": "Plain text"
       }
     },
     "COLOURPALETTE": {
