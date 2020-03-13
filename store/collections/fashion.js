dea02b9d8bd5e20c7aa2af446d205c5c235bda56
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-03-13 14:19:28 +0100

    Remove tier filter when fashion collection applied

--- store/collections/fashion.js
+++ store/collections/fashion.js
@@ -3,6 +3,7 @@ import { defaultFacetNames } from '../search';
 const fashionFacetNames = [
   'CREATOR', 'proxy_dc_format.en', 'proxy_dcterms_medium.en', 'proxy_dc_type.en'
 ].concat(defaultFacetNames);
+fashionFacetNames.splice(fashionFacetNames.indexOf('contentTier'), 1);
 const fashionFacetParam = fashionFacetNames.join(',');
 
 const facetFieldLabelPatterns = {
