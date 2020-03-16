bbc991a76ac967f0ec27182fd98b687c92702f2a
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-03-16 16:06:12 +0100

    Latest section

--- components/browse/LatestSection.vue
+++ components/browse/LatestSection.vue
@@ -1,23 +1,45 @@
 <template>
-  <div>
-    {{ entries }}
-    <BrowseContentCard
-      v-for="card in entries.entries"
-      :key="card.sys.id"
-      :fields="card.fields"
-      :card-type="card.sys.contentType ? card.sys.contentType.sys.id : ''"
-    />
-    {{ entries.total }}
-  </div>
+  <section class="row mb-5">
+    <div class="col-12">
+      <h2>
+        {{ contentType.name }}
+      </h2>
+      <b-card-group
+        class="card-deck-4-cols"
+        deck
+        :data-qa="`latest ${contentType.name}`"
+      >
+        <ContentCard
+          v-for="card in cards"
+          :key="card.fields.identifier"
+          :title="card.fields.name"
+          :texts="[card.fields.description]"
+          :url="cardData(card.fields).cardLink"
+          :image-url="cardData(card.fields).imageUrl"
+          :image-content-type="cardData(card.fields).imageContentType"
+          :image-optimisation-options="{ width: 510 }"
+        />
+      </b-card-group>
+      <b-button
+        v-if="total > 4"
+        variant="light"
+        :to="localePath({ name: category.toLowerCase() })"
+      >
+        {{ showMoreLink }}
+      </b-button>
+    </div>
+  </section>
 </template>
 
 <script>
-  import { getLatestEntries } from '../../plugins/europeana/latest-entries';
-  import BrowseContentCard from './BrowseContentCard';
+  import ContentCard from '../../components/generic/ContentCard';
+
+  import createClient from '../../plugins/contentful';
+  const contentfulClient = createClient();
 
   export default {
     components: {
-      BrowseContentCard
+      ContentCard
     },
 
     props: {
@@ -29,23 +51,68 @@
 
     data() {
       return {
-        entries: []
+        cards: [],
+        total: 0
       };
     },
 
     computed: {
       contentType() {
-        return 'exhibitionPage';
+        if (this.category === 'Exhibitions') {
+          return { type: 'exhibitionPage', name: this.$t('exhibitions.exhibitions') };
+        } else if (this.category === 'Galleries') {
+          return { type: 'imageGallery', name: this.$t('galleries.galleries') };
+        } else {
+          return false;
+        }
+      },
+      showMoreLink() {
+        return `${this.$t('showMore')} ${this.contentType.name.toLowerCase()} (${this.total})`;
       }
     },
 
-    created() {
-      this.entries = this.getEntries();
+    async mounted() {
+      await contentfulClient.getEntries({
+        locale: this.$i18n.isoLocale(),
+        'content_type': this.contentType.type,
+        order: '-fields.datePublished',
+        limit: 4
+      })
+        .then((response) => {
+          this.cards = response.items;
+          this.total = response.total;
+        }).catch(error => {
+          throw error;
+        });
     },
 
     methods: {
-      async getEntries() {
-        return await getLatestEntries(this.contentType, this.$i18n.isoLocale());
+      cardData(card) {
+        const identifier = card.identifier;
+        let cardLink;
+        let imageUrl;
+        let imageContentType;
+
+        if (this.category === 'Exhibitions') {
+          const image = card.primaryImageOfPage;
+
+          if (image && image.fields && image.fields.image && image.fields.image.fields && image.fields.image.fields.file) {
+            imageUrl = image.fields.image.fields.file.url;
+            imageContentType = image.fields.image.fields.file.contentType;
+          }
+
+          cardLink = { name: 'exhibitions-exhibition', params: { exhibition: identifier } };
+
+        } else if (this.category === 'Galleries') {
+          imageUrl = card.hasPart[0].fields.thumbnailUrl;
+          cardLink = { name: 'galleries-all', params: { pathMatch: identifier } };
+        }
+
+        return {
+          cardLink,
+          imageUrl,
+          imageContentType
+        };
       }
     }
   };
