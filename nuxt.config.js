6fa8999485a95c2518f1d603922e36d3183d89c9
Richard Doe <richard.doe@rwdit.net>
2020-03-16 15:33:37 +0000

    Nuxt plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,6 +84,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/path',
     '~/plugins/europeana',
     '~/plugins/vue/index',
     '~/plugins/i18n.js',
--- plugins/vue/index.js
+++ plugins/vue/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 
 import facets from './facets';
 import i18n from './i18n';
-import path from './path';
 
 Vue.use(facets);
 Vue.use(i18n);
-Vue.use(path);
