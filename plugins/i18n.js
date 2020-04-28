3c027a65eef24071e43d3c285a68d4fdd78507c1
Richard <info@monkemedia.co.uk>
2020-04-28 12:08:50 +0100

    Added a new a param

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -5,7 +5,7 @@
 export default ({ app }) => {
   const locales = app.i18n.locales;
   // Set the function directly on the context.app object
-  app.i18n.isoLocale = function() {
-    return locales.find(locale => locale.code === app.i18n.locale)['iso'];
+  app.i18n.isoLocale = function(code = app.i18n.locale) {
+    return locales.find(locale => locale.code === code)['iso'];
   };
 };
