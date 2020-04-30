35179439b7bc839d126422d468af6e7f0e8cd47c
Alan Sutherland <sutherland_a@me.com>
2020-04-30 16:25:41 +0900

    Aligned cards heights

--- components/search/SearchResults.vue
+++ components/search/SearchResults.vue
@@ -36,6 +36,7 @@
       :limit-values-within-each-text="3"
       :omit-all-uris="true"
       :blank-image-height="280"
+      variant="search"
     />
   </b-card-group>
 </template>
--- pages/galleries/index.vue
+++ pages/galleries/index.vue
@@ -11,7 +11,7 @@
     <b-row class="flex-md-row pb-5">
       <b-col cols="12">
         <b-card-group
-          class="card-deck-4-cols"
+          class="card-deck-4-cols aligned-cards"
           deck
           data-qa="gallery foyer"
         >
