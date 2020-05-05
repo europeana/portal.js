972177fddee84bc1ec15789ae64133181c157240
Richard <info@monkemedia.co.uk>
2020-05-05 14:38:31 +0100

    Removed unused object

--- components/Basics.stories.js
+++ components/Basics.stories.js
@@ -3,9 +3,7 @@ import VueI18n from 'vue-i18n';
 
 storiesOf('Basics', module)
   .add('typography', () => ({
-    i18n: new VueI18n({
-      locale: 'en'
-    }),
+    i18n: new VueI18n(),
     template:  `<b-container class="mt-3">
         <h1>This is a h1 heading</h1>
         <h2>This is a h2 heading</h2>
