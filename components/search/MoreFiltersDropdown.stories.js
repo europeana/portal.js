a607b0f93d06983992c0ab0bdce6d3c957e262bf
Richard <info@monkemedia.co.uk>
2020-05-07 08:48:54 +0100

    Was missing a prop

--- components/search/MoreFiltersDropdown.stories.js
+++ components/search/MoreFiltersDropdown.stories.js
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/vue';
+import Vuex from 'vuex';
 import MoreFiltersDropdown from './MoreFiltersDropdown.vue';
 
 const i18n = {
@@ -28,125 +29,144 @@ const i18n = {
   }
 };
 
+const store = () => new Vuex.Store({
+  getters: {
+    'search/collection': () => ({}),
+    'search/formatFacetFieldLabel': () => ({})
+  }
+});
+
+console.log('store', store);
+
 storiesOf('Search/MoreFiltersDropdown', module)
   .add('More Filters', () => ({
     components: { MoreFiltersDropdown },
     i18n,
+    store,
     data() {
-      return { facets: [
-        {
-          'name': 'LANGUAGE',
-          'fields': [
-            {
-              'label': 'en',
-              'count': 9230749
-            },
-            {
-              'label': 'de',
-              'count': 6714554
-            },
-            {
-              'label': 'no',
-              'count': 4676848
-            },
-            {
-              'label': 'nl',
-              'count': 3991384
-            },
-            {
-              'label': 'sv',
-              'count': 3604902
-            },
-            {
-              'label': 'fr',
-              'count': 3325609
-            },
-            {
-              'label': 'pl',
-              'count': 2722115
-            },
-            {
-              'label': 'es',
-              'count': 2138894
-            },
-            {
-              'label': 'it',
-              'count': 1788264
-            },
-            {
-              'label': 'da',
-              'count': 1012003
-            },
-            {
-              'label': 'fi',
-              'count': 938530
-            }
-          ]
-        },
-        {
-          'name': 'PROVIDER',
-          'fields': [
-            {
-              'label': 'OpenUp!',
-              'count': 8117543
-            },
-            {
-              'label': 'The European Library',
-              'count': 8098206
-            },
-            {
-              'label': 'LoCloud',
-              'count': 3386113
-            },
-            {
-              'label': 'Swedish Open Cultural Heritage | K-samsök',
-              'count': 2762220
-            },
-            {
-              'label': 'Federacja Bibliotek Cyfrowych',
-              'count': 2002605
-            }
-          ]
-        },
-        {
-          'name': 'DATA_PROVIDER',
-          'fields': [
-            {
-              'label': 'Naturalis Biodiversity Center',
-              'count': 4512439
-            },
-            {
-              'label': 'The National Archives of Norway',
-              'count': 2995795
-            },
-            {
-              'label': 'Bibliothèque nationale de France',
-              'count': 1596589
-            },
-            {
-              'label': 'Österreichische Nationalbibliothek - Austrian National Library',
-              'count': 1499818
-            },
-            {
-              'label': 'The Trustees of the Natural History Museum, London',
-              'count': 1451168
-            },
-            {
-              'label': 'National Library of the Netherlands',
-              'count': 1294871
-            },
-            {
-              'label': 'National Library of France',
-              'count': 1224986
-            }
-          ]
+      return {
+        facets: [
+          {
+            'name': 'LANGUAGE',
+            'fields': [
+              {
+                'label': 'en',
+                'count': 9230749
+              },
+              {
+                'label': 'de',
+                'count': 6714554
+              },
+              {
+                'label': 'no',
+                'count': 4676848
+              },
+              {
+                'label': 'nl',
+                'count': 3991384
+              },
+              {
+                'label': 'sv',
+                'count': 3604902
+              },
+              {
+                'label': 'fr',
+                'count': 3325609
+              },
+              {
+                'label': 'pl',
+                'count': 2722115
+              },
+              {
+                'label': 'es',
+                'count': 2138894
+              },
+              {
+                'label': 'it',
+                'count': 1788264
+              },
+              {
+                'label': 'da',
+                'count': 1012003
+              },
+              {
+                'label': 'fi',
+                'count': 938530
+              }
+            ]
+          },
+          {
+            'name': 'PROVIDER',
+            'fields': [
+              {
+                'label': 'OpenUp!',
+                'count': 8117543
+              },
+              {
+                'label': 'The European Library',
+                'count': 8098206
+              },
+              {
+                'label': 'LoCloud',
+                'count': 3386113
+              },
+              {
+                'label': 'Swedish Open Cultural Heritage | K-samsök',
+                'count': 2762220
+              },
+              {
+                'label': 'Federacja Bibliotek Cyfrowych',
+                'count': 2002605
+              }
+            ]
+          },
+          {
+            'name': 'DATA_PROVIDER',
+            'fields': [
+              {
+                'label': 'Naturalis Biodiversity Center',
+                'count': 4512439
+              },
+              {
+                'label': 'The National Archives of Norway',
+                'count': 2995795
+              },
+              {
+                'label': 'Bibliothèque nationale de France',
+                'count': 1596589
+              },
+              {
+                'label': 'Österreichische Nationalbibliothek - Austrian National Library',
+                'count': 1499818
+              },
+              {
+                'label': 'The Trustees of the Natural History Museum, London',
+                'count': 1451168
+              },
+              {
+                'label': 'National Library of the Netherlands',
+                'count': 1294871
+              },
+              {
+                'label': 'National Library of France',
+                'count': 1224986
+              }
+            ]
+          }
+        ],
+        name: 'LANGUAGE',
+        selected: {
+          LANGUAGE: ['en']
         }
-      ] };
+      };
     },
     template: ` <b-container
       class="mt-3"
       >
-        <MoreFiltersDropdown :more-facets="facets" />
+        <MoreFiltersDropdown
+          :more-facets="facets"
+          :selected="selected"
+          :name="name" />
       </b-container>`
   }));
 
