69fb7ee75b3962be989bc67b4cf0f4992984ee17
Richard <info@monkemedia.co.uk>
2020-05-12 10:52:54 +0100

    Added mini card to cards section

--- components/generic/ContentCard.stories.js
+++ components/generic/ContentCard.stories.js
@@ -4,8 +4,8 @@ import VueI18n from 'vue-i18n';
 import ContentCard from './ContentCard.vue';
 import ContentCardSection from '../browse/ContentCardSection.vue';
 
-storiesOf('Generic', module)
-  .add('Content card', () => ({
+storiesOf('Generic/Content cards', module)
+  .add('Default card', () => ({
     i18n: new VueI18n(),
     components: { ContentCard },
     template: `<b-col cols="3" class="mt-3">
@@ -13,6 +13,17 @@ storiesOf('Generic', module)
         title="This is a Storybook Card"
         contentSource="card"
         imageUrl="img/landscape.jpg" 
+      />
+    </b-col>`
+  }))
+  .add('Mini card', () => ({
+    i18n: new VueI18n(),
+    components: { ContentCard },
+    template: `<b-col cols="3" class="mt-3">
+    	<ContentCard 
+        title="This is a Storybook Card"
+        imageUrl="img/landscape.jpg" 
+        variant="mini"
       />
     </b-col>`
   }))
