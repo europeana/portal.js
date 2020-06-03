cacb14d688750d8238a11d8c320e7ab8d211da26
Alan Sutherland <sutherland_a@me.com>
2020-06-03 16:43:25 +0900

    Related chips

---
+++ components/search/RelatedChip.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <b-badge
+      v-if="title"
+      :to="linkTo"
+      :lang="localisedTitle.code"
+      pill
+      variant="nocolor"
+      class="mb-3 mr-3 font-weight-normal bg-transparent"
+      data-qa="browse chip"
+    >
+      {{ localisedTitle.values[0] }}
+    </b-badge>
+  </li>
+</template>
+
+<script>
+  import { langMapValueForLocale } from  '../../plugins/europeana/utils';
+  export default {
+    props: {
+      linkTo: {
+        type: [String, Object],
+        required: true
+      },
+      title: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    computed: {
+      localisedTitle() {
+        return langMapValueForLocale(this.title, this.$i18n.locale);
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/scss/variables.scss";
+  .badge-nocolor {
+    border: 1px solid $mediumgrey;
+    color: $black;
+    font-size: $font-size-small;
+    height: 4rem;
+    line-height: 3.5rem;
+    min-width: 6rem;
+  }
+</style>
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -11,7 +11,14 @@
       <b-row>
         <b-col>
           <h1>{{ $t('search') }}</h1>
+          <RelatedChip
+            link-to="/"
+            title="Venice"
+          />
         </b-col>
+        <b-container>
+          <h4>Related Collections</h4>
+        </b-container>
         <SearchInterface
           :per-row="4"
         />
@@ -25,6 +32,8 @@
   import { pageFromQuery } from '../../plugins/utils';
   import legacyUrl from '../../plugins/europeana/legacy-search';
   import NotificationBanner from '../../components/generic/NotificationBanner';
+  import RelatedChip from '../../components/search/RelatedChip';
+  import { getEntitySuggestions } from '../../plugins/europeana/entity';
 
   export default {
     components: {
@@ -40,6 +49,11 @@
         return redirect(app.$path({ name: 'search', query: { ...query, ...{ page: '1' } } }));
       }
     },
+    data() {
+      return {
+        relatedEntities: null
+      };
+    },
     computed: {
       notificationUrl() {
         return legacyUrl(this.$route.query, this.$store.state.i18n.locale) +
@@ -49,6 +63,39 @@
         return Boolean(Number(process.env.ENABLE_LINKS_TO_CLASSIC));
       }
     },
+
+    // asyncData({ env, query, params, res, store }) {
+    //   const entityUri = entities.getEntityUri(params.type, params.pathMatch);
+    //   // Prevent re-requesting entity content from APIs if already loaded,
+    //   // e.g. when paginating through entity search results
+    //   if (entityUri === store.state.entity.id) {
+    //     return {
+    //       relatedEntities: store.state.entity.relatedEntities
+    //     };
+    //   }
+    //   return axios.all([
+    //     entities.relatedEntities(params.type, params.pathMatch, {
+    //       wskey: env.EUROPEANA_API_KEY,
+    //       entityKey: env.EUROPEANA_ENTITY_API_KEY
+    //     })
+    //   ]
+    //     .then(axios.spread((entity, related) => {
+    //       // Store content for reuse should a redirect be needed, below, or when
+    //       // navigating back to this page, e.g. from a search result.
+    //       store.commit('entity/setRelatedEntities', related);
+    //       return {
+    //         relatedEntities: related
+    //       };
+    //     }))
+    //     .catch((error) => {
+    //       if (typeof res !== 'undefined') {
+    //         res.statusCode = (typeof error.statusCode !== 'undefined') ? error.statusCode : 500;
+    //       }
+    //       return { error: error.message };
+    //     })
+    //   );
+    // },
+
     async fetch({ store, query, res }) {
       await store.dispatch('search/activate');
       store.commit('search/set', ['userParams', query]);
@@ -61,6 +108,42 @@
 
     mounted() {
       this.$store.commit('search/enableCollectionFacet');
+      this.getSearchSuggestions();
+    },
+
+    methods: {
+      async getSearchSuggestions(query) {
+        if (!this.enableAutoSuggest) return;
+
+        // Don't go getting more suggestions if we are already waiting for some
+        if (this.gettingSuggestions) return;
+
+        if (query === '') {
+          this.suggestions = {};
+          return;
+        }
+
+        this.gettingSuggestions = true;
+
+        // Query in the user's language, and English, removing duplicates
+        const languageParam = Array.from(new Set([this.$i18n.locale, 'en'])).join(',');
+
+        const suggestions = await getEntitySuggestions(query, {
+          language: languageParam
+        }, {
+          recordValidation: this.enableSuggestionValidation
+        });
+
+        this.suggestions = suggestions.reduce((memo, suggestion) => {
+          memo[suggestion.id] = suggestion.prefLabel;
+          return memo;
+        }, {});
+
+        this.gettingSuggestions = false;
+
+        // If the query has changed in the meantime, go get new suggestions now
+        if (query !== this.query) this.getSearchSuggestions(this.query);
+      }
     },
 
     head() {
