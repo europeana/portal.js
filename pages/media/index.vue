7662a56ee8c96bafe52e424b1a3d97e8f691832a
Lutz Biedinger <lutz.biedinger@gmail.com>
2020-06-10 16:15:08 +0200

    media player 0.7.4 (#668)

--- pages/media/index.vue
+++ pages/media/index.vue
@@ -12,7 +12,7 @@
 
     data() {
       return {
-        MEDIA_PLAYER_VERSION: '0.7.3',
+        MEDIA_PLAYER_VERSION: '0.7.4',
         JQUERY_VERSION: '3.4.1',
         JQUERY_UI_VERSION: '1.12.1'
       };
