2c9716a04861a9f91a4db6278b57bc2cce0cb2ce
Richard Doe <richard.doe@rwdit.net>
2020-06-18 14:04:57 +0100

    Support use of proxy/cache, and preview access token

--- modules/contentful-graphql/plugin.js
+++ modules/contentful-graphql/plugin.js
@@ -1,21 +1,33 @@
 import axios from 'axios';
 
 const queries = <%= JSON.stringify(options, null, 2) %>;
-// TODO: make configurable, e.g. to support proxy/cache
-const origin = 'https://graphql.contentful.com';
+
+const origin = process.env['CTF_GRAPHQL_ORIGIN'] || 'https://graphql.contentful.com';
 const path = `/content/v1/spaces/${process.env['CTF_SPACE_ID'] || 'master'}/environments/${process.env['CTF_ENVIRONMENT_ID']}`;
 
 const query = (alias, variables = {}) => {
+  const accessToken = variables.preview ? process.env['CTF_CPA_ACCESS_TOKEN'] : process.env['CTF_CDA_ACCESS_TOKEN'];
+
   const url = `${origin}${path}`;
+
   const body = {
     query: queries[alias],
     variables
   };
+
   const headers = {
-    'Authorization': `Bearer ${process.env['CTF_CDA_ACCESS_TOKEN']}`
+    'Authorization': `Bearer ${accessToken}`
   };
 
-  return axios.post(url, body, { headers });
+  // These params will go into the URL query which will not be used by the
+  // GraphQL service itself as it's a POST request, but facilitate intermediary
+  // caching based on the URL alone, as with the apicache module.
+  const params = {
+    _query: alias,
+    ...variables
+  };
+
+  return axios.post(url, body, { headers, params });
 };
 
 const plugin = {
