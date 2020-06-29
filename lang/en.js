c1472ed89cd53f11d0fec53b4858a288bbeba2b7
EuropeanaDevelopment <development@europeana.eu>
2020-06-29 14:52:24 +0200

    Lokalise: Translations update (#695)
    * Lokalise: updates - i18n.js
    
    * Lokalise: updates - lang/bg.js
    
    * Lokalise: updates - lang/cs.js
    
    * Lokalise: updates - lang/da.js
    
    * Lokalise: updates - lang/de.js
    
    * Lokalise: updates - lang/el.js
    
    * Lokalise: updates - lang/en.js
    
    * Lokalise: updates - lang/es.js
    
    * Lokalise: updates - lang/et.js
    
    * Lokalise: updates - lang/fi.js
    
    * Lokalise: updates - lang/fr.js
    
    * Lokalise: updates - lang/ga.js
    
    * Lokalise: updates - lang/hr.js
    
    * Lokalise: updates - lang/hu.js
    
    * Lokalise: updates - lang/it.js
    
    * Lokalise: updates - lang/lt.js
    
    * Lokalise: updates - lang/lv.js
    
    * Lokalise: updates - lang/mt.js
    
    * Lokalise: updates - lang/nl.js
    
    * Lokalise: updates - lang/pl.js
    
    * Lokalise: updates - lang/pt.js
    
    * Lokalise: updates - lang/ro.js
    
    * Lokalise: updates - lang/sk.js
    
    * Lokalise: updates - lang/sl.js
    
    * Lokalise: updates - lang/sv.js
    
    * remove i18n.js
    
    * replace slashes
    
    Co-authored-by: lokalise-bot <support@lokalise.com>
    Co-authored-by: Lutz Biedinger <lutz.biedinger@gmail.com>

--- lang/en.js
+++ lang/en.js
@@ -385,7 +385,7 @@ export default {
       "edmIsSuccessorOf": "Is successor of",
       "edmProvider": "Provider",
       "edmRealizes": "Realises",
-      "edmRights": "License of the media in this item (unless otherwise specified)",
+      "edmRights": "Rights statement for the media in this item (unless otherwise specified)",
       "edmUgc": "User generated content",
       "europeanaCollectionName": "Collection name",
       "keywords": "Keywords (provided by the community)",
@@ -397,7 +397,7 @@ export default {
       "about": "RDF About",
       "dcDescription": "Description",
       "ebucoreHasMimeType": "Ebucore MIME Type",
-      "edmRights": "License for this media resource",
+      "edmRights": "Rights statement for this media resource",
       "rdfAbout": "RDF About"
     }
   },
@@ -421,12 +421,12 @@ export default {
   },
   "goToRecord": "Go to item",
   "header": {
-    "showSidebar": "Show Sidebar",
     "backToMenu": "Back to menu",
-    "searchFor": "Search for '{query}'",
-    "inCollection": "Search for '{query}' in {collection}",
+    "clearQuery": "Clear Search Query",
     "entireCollection": "Search for '{query}' in our entire collection",
-    "clearQuery": "Clear Search Query"
+    "inCollection": "Search for '{query}' in {collection}",
+    "searchFor": "Search for",
+    "showSidebar": "Show Sidebar"
   },
   "homeLinkAlt": "Europeana home",
   "imageSlider": {
@@ -464,21 +464,21 @@ export default {
   "noResults": "No results",
   "readMore": "Read more",
   "record": {
+    "allMetaData": "All metadata",
     "downloadCopyrightInfo": "This item is in copyright and can not be downloaded.",
     "extendedInformation": "Extended information",
+    "goodToKnow": "Good to know",
     "hideAll": "Hide all information",
     "record": "Item",
     "showAll": "Show all information",
     "similarItems": "Similar items",
+    "transcription": "Transcription",
+    "transcriptionDisclaimer": "This content is contributed by the public, not by the institution that provided this item.",
     "view": {
       "image": "View image",
       "media": "View media",
       "pdf": "View PDF"
-    },
-    "goodToKnow": "Good to know",
-    "allMetaData": "All metadata",
-    "transcription": "Transcription",
-    "transcriptionDisclaimer": "This content was provided by users and might not be entirely accurate."
+    }
   },
   "relatedCollections": "Related collections",
   "removeFilter": "Remove {filterLabel} filter",
