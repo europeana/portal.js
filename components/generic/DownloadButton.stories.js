6d4a200e85563c0a954533d4bde5d7bbab0d3541
Alan Sutherland <sutherland_a@me.com>
2020-07-10 21:41:05 +0900

    Download button

---
+++ components/generic/DownloadButton.stories.js
@@ -0,0 +1,51 @@
+import { storiesOf } from '@storybook/vue';
+import VueI18n from 'vue-i18n';
+import DownloadButton from './DownloadButton.vue';
+
+const i18n = new VueI18n();
+
+storiesOf('Generic', module)
+  .add('Download Button', () => ({
+    i18n,
+    components: { DownloadButton },
+    data() {
+      return {
+        url: "ggpht.com",
+        downloadUrl: "https://proxy.europeana.eu/90402/RP_P_2010_310_78?view=https%3A%2F%2Flh3.ggpht.com%2F2fUZ-S-q0_qtKWYyA4lcTjnP5ezTGP8QV2KG6QUuB844vf-Yupo2NxV-_h3vsR71IAc5Jz_ucngoJMNizDeWWhsvVqM%3Ds0&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi",
+        useProxy: false,
+        downloadDisabled: false
+      };
+    },
+    template: `
+      <b-container class="mt-3 p-5 bg-white">
+        <DownloadButton
+          :url="url"
+          :download-url="downloadUrl"
+          :download-disabled="downloadDisabled"
+          :use-proxy="useProxy"
+        />
+      </b-container>
+    `
+  }))
+  .add('Download Button Disabled', () => ({
+    i18n,
+    components: { DownloadButton },
+    data() {
+      return {
+        url: "ggpht.com",
+        downloadDisabled: true
+      };
+    },
+    template: `
+      <b-container class="mt-3 p-5 bg-white">
+        <DownloadButton
+          v-if="!downloadDisabled"
+          :url="url"
+          :download-url="downloadUrl"
+          :download-disabled="downloadDisabled"
+          :use-proxy="useProxy"
+        />
+        <p v-else><span>i<span>Because of its license, this item is not available for download.</p>
+      </b-container>
+    `
+  }));
---
+++ components/generic/DownloadButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-button
+    v-if="url"
+    :href="downloadUrl"
+    :disabled="downloadDisabled"
+    variant="light text-decoration-none"
+    data-qa="download button"
+    size="lg"
+    class="download-button mr-lg-3"
+    :target="!useProxy ? '_blank' : '_self'"
+  >
+    <span class="ic-download">⍗</span>
+    {{ $t('actions.download') }}
+  </b-button>
+</template>
+
+<script>
+  export default {
+    props: {
+      url: {
+        type: String,
+        default: null
+      },
+      downloadUrl: {
+        type: String,
+        default: ''
+      },
+      downloadDisabled: {
+        type: Boolean,
+        default: false
+      },
+      useProxy: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import './assets/scss/variables.scss';
+
+  .download-button {
+    text-transform: capitalize;
+    background: $offwhite;
+    color: $mediumgrey;
+    font-size: $font-size-base;
+    border-color: transparent;
+    box-shadow: none;
+    border-radius: 0.25rem;
+  }
+</style>
