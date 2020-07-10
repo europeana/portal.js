e2ec7db7c973889e9ae5b24a4f82cdfb735e18cf
Richard Doe <richard.doe@rwdit.net>
2020-07-10 10:08:02 +0100

    Restore use of apicache

--- tests/features/support/stack/app.js
+++ tests/features/support/stack/app.js
@@ -1,28 +1,28 @@
 const path = require('path');
 
-// const apicacheOrigin = 'http://localhost:3002';
-//
-// const rc = {
-//   defaults: {
-//     annotation: {
-//       origin: apicacheOrigin
-//     },
-//     entity: {
-//       origin: apicacheOrigin
-//     },
-//     record: {
-//       origin: apicacheOrigin
-//     },
-//     thumbnail: {
-//       origin: apicacheOrigin
-//     }
-//   }
-// };
+const apicacheOrigin = 'http://localhost:3002';
+
+const rc = {
+  defaults: {
+    annotation: {
+      origin: apicacheOrigin
+    },
+    entity: {
+      origin: apicacheOrigin
+    },
+    record: {
+      origin: apicacheOrigin
+    },
+    thumbnail: {
+      origin: apicacheOrigin
+    }
+  }
+};
 
 process.env['PORT'] = '3001';
 process.env['NODE_ENV'] = 'test';
-// process.env['EUROPEANA_APIS'] = JSON.stringify(rc);
-// process.env['CTF_GRAPHQL_ORIGIN'] = apicacheOrigin;
+process.env['EUROPEANA_APIS'] = JSON.stringify(rc);
+process.env['CTF_GRAPHQL_ORIGIN'] = apicacheOrigin;
 process.env['DISABLE_SSL_NEGOTIATION'] = '1';
 
 require(path.resolve(__dirname, '../../../../server/index.js'));
