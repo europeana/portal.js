09f44721ef54e962ac2126950f1bc3e3a8449ff4
Richard Doe <richard.doe@rwdit.net>
2020-07-17 08:40:25 +0100

    Rename percy e2e tests to visual

--- cucumber.js
+++ cucumber.js
@@ -6,6 +6,6 @@ module.exports = {
   common: 'tests/features/common/**/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty',
   default: '--require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty',
   pages: 'tests/features/pages/**/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty',
-  percy: 'tests/visual-regressions/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty',
-  search: 'tests/features/search/**/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty'
+  search: 'tests/features/search/**/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty',
+  visual: 'tests/features/visual/**/*.feature --require tests/features/config/cucumber.conf.js --format node_modules/cucumber-pretty'
 };
