d7fb7ea02d759d797a155fcde7cb38774b1ead05
Richard Doe <richard.doe@rwdit.net>
2020-07-22 14:54:49 +0100

    Always run stack for production env

--- docker/stack/app/docker-entrypoint.sh
+++ docker/stack/app/docker-entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-echo "Running for NODE_ENV=${NODE_ENV}"
-
+# TODO: remove when client-side bundle no longer relies on dotenv
 env > .env
 
-case ${NODE_ENV} in
-  test) npm run build:test;;
-  production) npm run build;;
-esac
-
-case ${NODE_ENV} in
-  development) npm run dev "$@";;
-  test|production) npm run start:cluster "$@";;
-  *) echo "Unsupported NODE_ENV \"${NODE_ENV}\"" && exit 1;;
-esac
+npm run build
+npm run start:cluster "$@"
