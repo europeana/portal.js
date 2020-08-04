3323f08d44dce0e37755e39c31cdda98d7d5e842
Alan Sutherland <sutherland_a@me.com>
2020-08-04 22:27:06 +0900

    switch info on swipe

--- components/item/AwesomeSwiper.stories.js
+++ components/item/AwesomeSwiper.stories.js
@@ -29,11 +29,31 @@ const store = new Vuex.Store({
 });
 
 const media = [
-  { thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg' } },
-  { thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg' } },
-  { thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119203%2F10265.119203.original.jpg' } },
-  { thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119639%2F10265.119639.original.jpg' } },
-  { thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119640%2F10265.119640.original.jpg' } }
+  {
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/',
+    downloadUrl: 'https://proxy.europeana.eu/2020601/https___1914_1918_europeana_eu_contributions_10265?view=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi'
+  },
+  {
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg' },
+    rightsStatement: 'https://creativecommons.org/licenses/by-nd/4.0/',
+    downloadUrl: 'https://proxy.europeana.eu/2020601/https___1914_1918_europeana_eu_contributions_10265?view=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi'
+  },
+  {
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119203%2F10265.119203.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/',
+    downloadUrl: 'https://proxy.europeana.eu/2020601/https___1914_1918_europeana_eu_contributions_10265?view=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119203%2F10265.119203.original.jpg&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi'
+  },
+  {
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119639%2F10265.119639.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/',
+    downloadUrl: 'https://proxy.europeana.eu/2020601/https___1914_1918_europeana_eu_contributions_10265?view=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119639%2F10265.119639.original.jpg&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi'
+  },
+  {
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119640%2F10265.119640.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/',
+    downloadUrl: 'https://proxy.europeana.eu/2020601/https___1914_1918_europeana_eu_contributions_10265?view=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119640%2F10265.119640.original.jpg&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi'
+  }
 ];
 
 storiesOf('Item page/Awesome Swiper', module)
@@ -67,21 +87,34 @@ storiesOf('Item page/Awesome Swiper', module)
     },
     data() {
       return {
+        activeIndex: 0,
         media,
         mediaUrl: 'https://www.example.org/media',
         id: '/2020601/https___1914_1918_europeana_eu_contributions_10265',
         rightsStatementIsUrl: true,
-        rightsStatement: 'https://creativecommons.org/licenses/by-sa/4.0/',
-        url: 'https://proxy.europeana.eu/90402/RP_P_2010_310_78?view=https%3A%2F%2Flh3.ggpht.com%2F2fUZ-S-q0_qtKWYyA4lcTjnP5ezTGP8QV2KG6QUuB844vf-Yupo2NxV-_h3vsR71IAc5Jz_ucngoJMNizDeWWhsvVqM%3Ds0&api_url=https%3A%2F%2Fapi.europeana.eu%2Fapi',
         disabled: false,
         target: '_blank'
       };
+    },
+    computed: {
+      rightsStatement() {
+        return media[this.activeIndex].rightsStatement;
+      },
+      downloadUrl() {
+        return media[this.activeIndex].downloadUrl;
+      }
+    },
+    methods: {
+      onSwipe (index) {
+        this.activeIndex = index;
+      }
     },
     template: `
       <div class="mt-3">
         <AwesomeSwiper
           :europeana-identifier="id"
           :media="media"
+          @slideChanged="onSwipe"
         />
         <b-container class="d-flex justify-content-around mt-5">
           <RightsStatementButton
@@ -89,7 +122,7 @@ storiesOf('Item page/Awesome Swiper', module)
             :rights-statement="rightsStatement"
           />
           <DownloadButton
-            :url="url"
+            :url="downloadUrl"
             :disabled="disabled"
             :target="target"
           />
