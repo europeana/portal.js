70bc32ce358aa26ae6251abd085b4b6a48ad1c3a
Anna Christaki <annachristaki@think-code.io>
2020-08-05 15:05:22 +0300

    revert to correct version

--- store/set.js
+++ store/set.js
@@ -1,19 +1,9 @@
 export const state = () => ({
-  likesId: null,
-  liked: []
+  likesId: null
 });
 
 export const mutations = {
   setLikesId(state, value) {
     state.likesId = value;
-  },
-  addGallery(state, { likesArray }) {
-    state.liked = likesArray;
-  },
-  like(state, value) {
-    state.liked.push(value);
-  },
-  unlike(state, value) {
-    state.liked.splice(state.liked.indexOf(value), 1);
   }
 };
