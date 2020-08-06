e220796d2d27b1cbe844c11d1d71781a349a1a4f
Alan Sutherland <sutherland_a@me.com>
2020-08-06 22:48:02 +0900

    unit test

--- tests/unit/components/item/AwesomeSwiper.spec.js
+++ tests/unit/components/item/AwesomeSwiper.spec.js
@@ -6,38 +6,7 @@ import AwesomeSwiper from '../../../../components/item/AwesomeSwiper.vue';
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-const propsData = {
-  media: [
-    {
-      about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119112/10265.119112.original.jpg',
-      thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg' },
-      rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
-    },
-    {
-      about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119200/10265.119200.original.jpg',
-      thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg' },
-      rightsStatement: 'https://creativecommons.org/licenses/by-nd/4.0/'
-    },
-    {
-      about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119203/10265.119203.original.jpg',
-      thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119203%2F10265.119203.original.jpg' },
-      rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
-    },
-    {
-      about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119639/10265.119639.original.jpg',
-      thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119639%2F10265.119639.original.jpg' },
-      rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
-    },
-    {
-      about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119640/10265.119640.original.jpg',
-      thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119640%2F10265.119640.original.jpg' },
-      rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
-    }
-  ],
-  id: '/2020601/https___1914_1918_europeana_eu_contributions_10265'
-};
-
-const factory = () => mount(AwesomeSwiper, {
+const factory = (propsData) => mount(AwesomeSwiper, {
   localVue,
   mocks: {
     $t: (key) => key,
@@ -46,14 +15,78 @@ const factory = () => mount(AwesomeSwiper, {
   propsData
 });
 
+const media = [
+  {
+    about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119112/10265.119112.original.jpg',
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
+  },
+  {
+    about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119200/10265.119200.original.jpg',
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg' },
+    rightsStatement: 'https://creativecommons.org/licenses/by-nd/4.0/'
+  },
+  {
+    about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119203/10265.119203.original.jpg',
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119203%2F10265.119203.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
+  },
+  {
+    about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119639/10265.119639.original.jpg',
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119639%2F10265.119639.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
+  },
+  {
+    about: 'https://europeana1914-1918.s3.amazonaws.com/attachments/119640/10265.119640.original.jpg',
+    thumbnails: { large: 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119640%2F10265.119640.original.jpg' },
+    rightsStatement: 'http://creativecommons.org/licenses/by-sa/3.0/'
+  }
+];
+const selected = 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119112%2F10265.119112.original.jpg';
+// const nonSelected = 'https://api.europeana.eu/api/v2/thumbnail-by-url.json?size=w400&type=IMAGE&uri=https%3A%2F%2Feuropeana1914-1918.s3.amazonaws.com%2Fattachments%2F119200%2F10265.119200.original.jpg';
+const id = '/2020601/https___1914_1918_europeana_eu_contributions_10265';
+const europeanaIdentifier = '/2020601/https___1914_1918_europeana_eu_contributions_10265';
+
 describe('components/item/AwesomeSwiper', () => {
-  describe('when the slide changes', () => {
-    it('shows a new image', () => {
-      const wrapper = factory();
-
-      // wrapper.attributes().href.should.eq('proxied');
-      wrapper.findAll('img').length.should.eq(1);
+  context('when the swiper loads', () => {
+    it('shows a five images', () => {
+      const wrapper = factory({ media, id, europeanaIdentifier });
+      // console.log(wrapper.attributes());
+      wrapper.findAll('img').length.should.eq(5);
     });
-  });
+    it('has an identifier', () => {
+      const wrapper = factory({ media, id, europeanaIdentifier });
+      wrapper.attributes().id.should.eq('/2020601/https___1914_1918_europeana_eu_contributions_10265');
+    });
 
+    it('marks the selected media item', () => {
+      const wrapper = factory({ media, id, europeanaIdentifier, selected });
+      console.log(wrapper.attributes());
+      // console.log(wrapper.find('.swiper-slide-active'));
+      // const selectedImg = wrapper.find(`.swiper-slide-active img[src="${selected}"]`);
+
+      // selectedImg.find().parent('.swiper-slide').classes().should.include('swiper-slide-active');
+    });
+
+    // describe('clicking on a non-selected thumbnail', () => {
+    //   it('makes it the selected one', () => {
+    //     const wrapper = factory({ media, selected, europeanaIdentifier });
+
+    //     const nonSelectedImg = wrapper.find(`button[data-about="${nonSelected}"]`);
+
+    //     nonSelectedImg.classes().should.not.include('selected');
+    //     nonSelectedImg.trigger('click');
+    //     nonSelectedImg.classes().should.include('selected');
+    //   });
+
+    //   it('emits a `select` event with the item URI', () => {
+    //     const wrapper = factory({ media, selected });
+
+    //     const nonSelectedImg = wrapper.find(`button[data-about="${nonSelected}"]`);
+    //     nonSelectedImg.trigger('click');
+
+    //     wrapper.emitted('select').should.deep.eq([[nonSelected]]);
+    //   });
+    // });
+  });
 });
