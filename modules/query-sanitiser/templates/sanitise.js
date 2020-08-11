1051c9e4a3e2ad36428eb645220677a2d854a780
Richard Doe <richard.doe@rwdit.net>
2020-08-11 14:33:15 +0100

    Query sanitiser module & store

---
+++ modules/query-sanitiser/templates/sanitise.js
@@ -0,0 +1,21 @@
+/**
+ * Page to request, from an API endpoint (Europeana/Contentful/?)
+ * If parameter is not present, returns default of page 1.
+ * If parameter is present, and represents a positive integer, return it
+ * typecast to Number.
+ * Otherwise, parameter is invalid for page number, and return `null`.
+ * @param {string} queryPage `page` query parameter from URL
+ * @return {?number}
+ */
+export const page = (unsanitised) => {
+  let sanitised = null;
+
+  if (unsanitised === undefined || unsanitised === null) {
+    sanitised = 1;
+  } else {
+    const pageAsNumber = Number(unsanitised);
+    if (Number.isInteger(pageAsNumber) && pageAsNumber > 0) sanitised = pageAsNumber;
+  }
+
+  return sanitised;
+};
--- pages/galleries/index.vue
+++ pages/galleries/index.vue
@@ -51,12 +51,12 @@
       PaginationNav: () => import('../../components/generic/PaginationNav')
     },
     middleware: 'sanitisePageQuery',
-    asyncData({ query, error, app }) {
+    asyncData({ query, error, app, store }) {
       const variables = {
         locale: app.i18n.isoLocale(),
         preview: query.mode === 'preview',
         limit: PER_PAGE,
-        skip: (app.$page - 1) * PER_PAGE
+        skip: (store.state.sanitised.page - 1) * PER_PAGE
       };
 
       return app.$contentful.query('galleryFoyerPage', variables)
@@ -65,7 +65,7 @@
           return {
             galleries: data.imageGalleryCollection.items,
             total: data.imageGalleryCollection.total,
-            page: app.$page,
+            page: store.state.sanitised.page,
             perPage: PER_PAGE
           };
         })
