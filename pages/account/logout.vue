1ff1a356d5a35d5326bfc3d8f629b85f34b17b76
Spyros Bekiaris <spirosbekos@gmail.com>
2020-08-25 16:06:22 +0300

    Force possibly other open tabs of the portal to refresh after logout

--- pages/account/logout.vue
+++ pages/account/logout.vue
@@ -11,6 +11,8 @@
       ({ app }) => {
         const path = app.$auth.strategies.keycloak.options.end_session_endpoint;
         app.$goto(`${path}?redirect_uri=${encodeURIComponent(window.location.origin)}`);
+        // Trigger event to force possibly other open tabs of the portal to refresh after logout
+        localStorage.setItem('logout-event', `logout-${Math.random()}`);
       }
     ]
   };
