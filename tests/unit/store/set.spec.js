dfe9d2fb2447cbf3d42ea43ccb81d40e8bea699c
Richard Doe <richard.doe@rwdit.net>
2020-08-28 11:08:16 +0100

    Unit test set store

---
+++ tests/unit/store/set.spec.js
@@ -0,0 +1,193 @@
+import * as store from '../../../store/set';
+// import apiConfig from '../../../modules/apis/defaults';
+// import axios from 'axios';
+// import nock from 'nock';
+import sinon from 'sinon';
+
+// axios.defaults.adapter = require('axios/lib/adapters/http');
+
+// const baseRequest = nock(apiUrl).get(apiEndpoint);
+// const defaultResponse = { success: true, items: [], totalResults: 123456 };
+
+describe('store/set', () => {
+  describe('getters', () => {
+    describe('isLiked()', () => {
+      it('is `true` if likedItemIds state includes item ID', () => {
+        const itemId = '/123/ghi';
+        const state = {
+          likedItemIds: ['/123/abc', '/123/def', '/123/ghi']
+        };
+
+        const isLiked = store.getters.isLiked(state)(itemId);
+
+        isLiked.should.be.true;
+      });
+
+      it('is `false` if likedItemIds state does not include item ID', () => {
+        const itemId = '/123/ghi';
+        const state = {
+          likedItemIds: ['/123/abc', '/123/def']
+        };
+
+        const isLiked = store.getters.isLiked(state)(itemId);
+
+        isLiked.should.be.false;
+      });
+    });
+  });
+
+  describe('actions', () => {
+    const commit = sinon.spy();
+    const dispatch = sinon.spy();
+    const setId = 'http://data.europeana.eu/set/123';
+    const itemId = '/123/ghi';
+    const userId = 'a-b-c-d-e';
+    const likes = {
+      id: setId,
+      items: []
+    };
+
+    beforeEach(() => {
+      commit.resetHistory();
+      dispatch.resetHistory();
+      store.actions.$sets = {};
+      store.actions.$auth = {};
+    });
+
+    describe('reset()', () => {
+      const resetCommits = {
+        setLikesId: null,
+        setLikedItems: [],
+        setCreations: []
+      };
+
+      for (const commitName in resetCommits) {
+        const commitValue = resetCommits[commitName];
+        it(`commits "${commitName}" with ${commitValue}`, ()  => {
+          store.actions.reset({ commit });
+
+          commit.should.have.been.calledWith(commitName, commitValue);
+        });
+      }
+    });
+
+    describe('like()', () => {
+      it('adds to likes set via $sets, then commits with "like"', () => {
+        store.actions.$sets.modifyItems = sinon.stub().resolves({});
+        const state = { likesId: setId };
+
+        store.actions.like({ commit, state }, itemId);
+
+        store.actions.$sets.modifyItems.should.have.been.calledWith('add', state.likesId, itemId);
+        commit.should.have.been.calledWith('like', itemId);
+      });
+    });
+
+    describe('unlike()', () => {
+      it('removes from likes set via $sets, then commits with "unlike"', () => {
+        store.actions.$sets.modifyItems = sinon.stub().resolves({});
+        const state = { likesId: setId };
+
+        store.actions.unlike({ commit, state }, itemId);
+
+        store.actions.$sets.modifyItems.should.have.been.calledWith('delete', state.likesId, itemId);
+        commit.should.have.been.calledWith('unlike', itemId);
+      });
+    });
+
+    describe('addItem()', () => {
+      it('adds to set via $sets, then dispatches "refreshCreation"', async() => {
+        store.actions.$sets.modifyItems = sinon.stub().resolves({});
+        const state = {};
+
+        await store.actions.addItem({ dispatch, state }, { setId, itemId });
+
+        store.actions.$sets.modifyItems.should.have.been.calledWith('add', setId, itemId);
+        dispatch.should.have.been.calledWith('refreshCreation', setId);
+      });
+
+      context('when set is active', () => {
+        it('dispatches "fetchActive"', async() => {
+          store.actions.$sets.modifyItems = sinon.stub().resolves({});
+          const state = { active: { id: setId } };
+
+          await store.actions.addItem({ dispatch, state }, { setId, itemId });
+
+          dispatch.should.have.been.calledWith('fetchActive', setId);
+        });
+      });
+    });
+
+    describe('removeItem()', () => {
+      it('removes from set via $sets, then dispatches "refreshCreation"', async() => {
+        store.actions.$sets.modifyItems = sinon.stub().resolves({});
+        const state = {};
+
+        await store.actions.removeItem({ dispatch, state }, { setId, itemId });
+
+        store.actions.$sets.modifyItems.should.have.been.calledWith('delete', setId, itemId);
+        dispatch.should.have.been.calledWith('refreshCreation', setId);
+      });
+
+      context('when set is active', () => {
+        it('dispatches "fetchActive"', async() => {
+          store.actions.$sets.modifyItems = sinon.stub().resolves({});
+          const state = { active: { id: setId } };
+
+          await store.actions.removeItem({ dispatch, state }, { setId, itemId });
+
+          dispatch.should.have.been.calledWith('fetchActive', setId);
+        });
+      });
+    });
+
+    // TODO: rename to indicate setting of ID only?
+    describe('setLikes()', () => {
+      it('gets likes set via $sets, then commits ID with "setLikesId"', async() => {
+        store.actions.$auth.user = { sub: userId };
+        store.actions.$sets.getLikes = sinon.stub().resolves(setId);
+
+        await store.actions.setLikes({ commit });
+
+        store.actions.$sets.getLikes.should.have.been.calledWith(userId);
+        commit.should.have.been.calledWith('setLikesId', setId);
+      });
+    });
+
+    describe('createLikes()', () => {
+      it('creates likes set via $sets, then commits ID with "setLikesId"', async() => {
+        store.actions.$sets.createLikes = sinon.stub().resolves({ id: setId });
+
+        await store.actions.createLikes({ commit });
+
+        store.actions.$sets.createLikes.should.have.been.calledWith();
+        commit.should.have.been.calledWith('setLikesId', setId);
+      });
+    });
+
+    describe('fetchLikes()', () => {
+      context('without likesId in state', () => {
+        it('does not fetch likes via $sets', async() => {
+          store.actions.$sets.getSet = sinon.stub().resolves(likes);
+          const state = {};
+
+          await store.actions.fetchLikes({ state, commit });
+
+          store.actions.$sets.getSet.should.not.have.been.called;
+        });
+      });
+
+      context('with likesId in state', () => {
+        it('fetches likes via $sets, then commits them with "setLikedItems"', async() => {
+          store.actions.$sets.getSet = sinon.stub().resolves(likes);
+          const state = { likesId: setId };
+
+          await store.actions.fetchLikes({ state, commit });
+
+          store.actions.$sets.getSet.should.have.been.calledWith(setId, sinon.match.any);
+          commit.should.have.been.calledWith('setLikedItems', likes.items);
+        });
+      });
+    });
+  });
+});
