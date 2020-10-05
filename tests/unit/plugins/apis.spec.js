979172f9ec30715eb9c2284f2d3d1752e3f70f17
Richard Doe <richard.doe@rwdit.net>
2020-10-05 10:25:00 +0100

    Update apis plugin unit test

--- tests/unit/plugins/apis.spec.js
+++ tests/unit/plugins/apis.spec.js
@@ -1,21 +1,38 @@
-import * as store from '../../../store/apis';
+import plugin from '../../../plugins/apis';
 
-describe('store/apis', () => {
-  describe('mutations', () => {
-    describe('readUrlsFromRequestHeaders', () => {
-      it('sets URLs for APIs from request headers', () => {
-        const headers = {
-          'x-europeana-annotation-api-url': 'https://annotation.example.org',
-          'x-europeana-entity-api-url': 'https://entity.example.org',
-          'x-europeana-record-api-url': 'https://record.example.org'
-        };
-        const state = store.state();
+import sinon from 'sinon';
 
-        store.mutations.readUrlsFromRequestHeaders(state, headers);
+describe('plugins/apis', () => {
+  describe('store module', () => {
+    let storeModule;
 
-        state.annotation.url.should.eql('https://annotation.example.org');
-        state.entity.url.should.eql('https://entity.example.org');
-        state.record.url.should.eql('https://record.example.org');
+    before(async() => {
+      await plugin({
+        app: {},
+        store: {
+          registerModule: sinon.spy((name, module) => {
+            storeModule = module;
+          })
+        }
+      });
+    });
+
+    describe('mutations', () => {
+      describe('readUrlsFromRequestHeaders', () => {
+        it('sets URLs for APIs from request headers', () => {
+          const headers = {
+            'x-europeana-annotation-api-url': 'https://annotation.example.org',
+            'x-europeana-entity-api-url': 'https://entity.example.org',
+            'x-europeana-record-api-url': 'https://record.example.org'
+          };
+          const state = storeModule.state();
+
+          storeModule.mutations.init(state, { req: { headers } });
+
+          state.annotation.baseURL.should.eql('https://annotation.example.org');
+          state.entity.baseURL.should.eql('https://entity.example.org');
+          state.record.baseURL.should.eql('https://record.example.org');
+        });
       });
     });
   });
