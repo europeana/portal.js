adb3247163c48f082e65a10954839ae26707bad8
Pixelomo <sutherland_a@me.com>
2020-10-13 21:07:44 +0900

    Blog refactored for new model (#841)
    * Blog refactored for new model
    
    * Moved blog/exhibit css to shared text.scss
    
    * Combine blog and exhibitions head markup in to Head comp
    
    * CTA added, images fixed, styling fixed
    
    * Remove /deep/
    
    * Overflow hidden
    
    * Refactor button and title spacing
    
    * Fix titles, cta, overflow
    
    * Blog spacing and chapter numbers
    
    Co-authored-by: mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>

---
+++ components/authored/AuthoredHead.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-container>
+    <b-row class="justify-content-center">
+      <b-col
+        cols="12"
+        class="col-lg-8 pt-large mb-4"
+      >
+        <article>
+          <div class="title">
+            <h2
+              v-if="exhibitionTitle"
+              class="subtitle mb-0"
+            >
+              {{ exhibitionTitle }}
+            </h2>
+            <h1 data-qa="title">
+              {{ title }}
+            </h1>
+            <p
+              v-if="description"
+              class="lead"
+            >
+              {{ description }}
+            </p>
+          </div>
+        </article>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-center">
+      <b-col
+        cols="12"
+        class="col-lg-8"
+      >
+        <ImageWithAttribution
+          v-if="heroImage"
+          :src="heroImage.url"
+          :image-content-type="heroImage.contentType"
+          :rights-statement="hero.license"
+          :attribution="hero"
+          hero
+        />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'AuthoredHead',
+
+    components: {
+      ImageWithAttribution: () => import('../../components/generic/ImageWithAttribution')
+    },
+
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+
+      description: {
+        type: String,
+        default: ''
+      },
+
+      exhibitionTitle: {
+        type: String,
+        default: ''
+      },
+
+      hero: {
+        type: Object,
+        default: null
+      }
+    },
+
+    computed: {
+      heroImage() {
+        return this.hero ? this.hero.image : null;
+      }
+    }
+  };
+</script>
