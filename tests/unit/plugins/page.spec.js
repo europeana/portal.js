5eece73f2909318461828f0559b9e45469919efb
Richard Doe <richard.doe@rwdit.net>
2020-11-23 10:14:54 +0000

    Page plugin with pageHeadTitle helper

---
+++ tests/unit/plugins/page.spec.js
@@ -0,0 +1,18 @@
+import pagePlugin from '../../../plugins/page';
+
+describe('pageHeadTitle', () => {
+  it('appends site name to supplied page title', () => {
+    const pageTitle = 'About us';
+    const siteName = 'Europeana';
+    const expected = 'About us | Europeana';
+
+    let pageHeadTitle;
+    pagePlugin(
+      { $config: { app: { siteName } } },
+      (name, fn) => pageHeadTitle = fn
+    );
+    const pageTitleWithSiteName = pageHeadTitle(pageTitle);
+
+    pageTitleWithSiteName.should.eq(expected);
+  });
+});
