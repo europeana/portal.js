ad61cdebc096aad4a16f1cb3546211e4ab80984e
LeoniePeters <leoniepeters@gmail.com>
2020-11-30 13:44:00 +0100

    Unit test for vue announcer in default layout

---
+++ tests/unit/layouts/default.spec.js
@@ -0,0 +1,41 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
+import Vuex from 'vuex';
+// import sinon from 'sinon';
+
+import layout from '../../../layouts/default';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: { breadcrumb: {} }
+});
+
+const factory = () => shallowMount(layout, { localVue,
+  store,
+  data() {
+    return {
+      enableAnnouncer: true
+    };
+  },
+  mocks: {
+    $t: key => key
+  },
+  stubs: {
+    CookieDisclaimer: true,
+    VueAnnouncer: { template: '<div id="announcer" aria-live="polite"></div>' },
+    nuxt: true,
+    PageHeader: true,
+    PageFooter: true
+  } });
+
+describe('layouts/default.vue', () => {
+  describe('VueAnnouncer', () => {
+    it('is enabled', () => {
+      const wrapper = factory();
+      wrapper.find('#announcer').exists().should.equal(true);
+    });
+  });
+});
