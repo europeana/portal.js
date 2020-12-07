ac720a649b56c6e9d2c7926f83f50503d68b7f44
Richard Doe <richard.doe@rwdit.net>
2020-12-07 08:44:26 +0000

    Post-process search hits: coerce to before/after/match

--- pages/iiif/index.vue
+++ pages/iiif/index.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+  import uniq from 'lodash/uniq';
+
   export default {
     layout: 'minimal',
 
@@ -17,9 +19,11 @@
     data() {
       return {
         manifest: null,
-        MIRADOR_BUILD_PATH: 'https://unpkg.com/@europeana/mirador@3.0.0-beta.9.2/dist',
+        MIRADOR_BUILD_PATH: 'https://unpkg.com/mirador@3.0.0/dist',
         page: null,
-        uri: null
+        uri: null,
+        mirador: null,
+        miradorStoreManifestJsonUnsubscriber: () => {}
       };
     },
 
@@ -56,6 +60,9 @@
           },
           workspaceControlPanel: {
             enabled: false
+          },
+          requests: {
+            postprocessors: [this.postprocessMiradorRequest]
           }
         };
 
@@ -65,23 +72,138 @@
 
     mounted() {
       this.$nextTick(() => {
-        const mirador = Mirador.viewer(this.miradorViewerOptions); // eslint-disable-line no-undef
-
-        mirador.store.subscribe(() => {
-          const miradorWindow = Object.values(mirador.store.getState().windows)[0]; // only takes one window into account at the moment
-          if (!this.manifest) {
-            this.manifest = mirador.store.getState().manifests[miradorWindow.manifestId].json;
-          }
-
-          if (miradorWindow.canvasId !== this.page) {
-            this.page = miradorWindow.canvasId;
-            this.fetchImageData(this.uri, this.page);
-          }
-        });
+        this.mirador = window.Mirador.viewer(this.miradorViewerOptions);
+        this.miradorStoreManifestJsonUnsubscriber = this.mirador.store.subscribe(this.miradorStoreManifestJsonListener);
       });
     },
 
     methods: {
+      miradorStoreManifestJsonListener() {
+        const miradorWindow = Object.values(this.mirador.store.getState().windows)[0]; // only takes one window into account at the moment
+        if (miradorWindow) {
+          const miradorManifest = this.mirador.store.getState().manifests[miradorWindow.manifestId];
+          if (miradorManifest) {
+            this.manifest = miradorManifest.json;
+            if (miradorWindow.canvasId !== this.page) {
+              this.page = miradorWindow.canvasId;
+              this.fetchImageData(this.uri, this.page);
+            }
+          }
+        }
+      },
+
+      postprocessMiradorRequest(url, action) {
+        switch (action.type) {
+        case 'mirador/RECEIVE_ANNOTATION':
+          this.postprocessMiradorAnnotation(url, action);
+          break;
+        case 'mirador/RECEIVE_SEARCH':
+          this.postprocessMiradorSearch(url, action);
+          break;
+        }
+      },
+
+
+      postprocessMiradorSearch(url, action) {
+        this.coerceSearchHitsToBeforeAfterMatch(action.searchJson);
+      },
+
+      // Hack to flatten oa:TextQuoteSelector hit selectors to before/after/match
+      // hits, as Mirador 3.0.0 does not support oa:TextQuoteSelector style.
+      coerceSearchHitsToBeforeAfterMatch(searchJson) {
+        const hits = [];
+
+        for (const hit of (searchJson.hits || [])) {
+          if (hit.selectors) {
+            for (const selector of hit.selectors) {
+              hits.push({
+                '@type': hit['@type'],
+                annotations: hit.annotations,
+                before: selector.prefix,
+                after: selector.suffix,
+                match: selector.exact
+              });
+            }
+          } else {
+            hits.push(hit);
+          }
+        }
+
+        searchJson.hits = hits;
+      },
+
+      postprocessMiradorAnnotation(url, action) {
+        this.filterAnnotationResources(action.annotationJson);
+        this.coerceAnnotationToCanvasId(action.annotationJson);
+        this.dereferenceAnnotationResources(action.annotationJson);
+      },
+
+      // Hack to force `on` attribute to canvas ID
+      //
+      // TODO: remove when API output updated to use canvas ID
+      coerceAnnotationToCanvasId(annotationJson) {
+        annotationJson.resources = annotationJson.resources.map((resource) => {
+          const coercedResource = Object.assign({}, resource);
+          coercedResource.on = [].concat(coercedResource.on);
+          if (coercedResource.on[0].includes('xywh=')) {
+            coercedResource.on[0] = coercedResource.on[0].replace(/^[^#]+/, this.page); // replace up to hash
+          }
+          return coercedResource;
+        });
+      },
+
+      // Filter to line-level annotations, and only those with a `char` fragment
+      // selector.
+      filterAnnotationResources(annotationJson) {
+        annotationJson.resources = annotationJson.resources.filter(
+          resource => (resource.dcType === 'Line') && (/char=(\d+),(\d+)$/.test(resource.resource['@id'])),
+        );
+      },
+
+      fetchAnnotationResourcesFulltext(annotationJson) {
+        const urls = annotationJson.resources
+          .filter(resource => !resource.resource.chars && resource.resource['@id'])
+          .map(resource => resource.resource['@id'].split('#')[0]);
+
+        const fulltext = {};
+
+        // TODO: error handling
+        const fetches = uniq(urls).map(url => fetch(url)
+          .then(response => response.json())
+          .then((response) => {
+            if (response.type === 'FullTextResource') fulltext[url] = response.value;
+          }));
+
+        return Promise.all(fetches).then(() => fulltext);
+      },
+
+      async dereferenceAnnotationResources(annotationJson) {
+        const fulltext = await this.fetchAnnotationResourcesFulltext(annotationJson);
+
+        for (const resource of annotationJson.resources) {
+          if (resource.resource.chars || !resource.resource['@id']) {
+            continue;
+          }
+
+          const url = resource.resource['@id'].split('#')[0];
+          if (!fulltext[url]) continue;
+
+          const fragment = resource.resource['@id'].split('#')[1];
+
+          resource.resource.chars = fulltext[url];
+
+          if (fragment) {
+            const charMatch = fragment.match(/char=(\d+),(\d+)$/);
+            if (charMatch) {
+              resource.resource.chars = resource.resource.chars.slice(
+                Number(charMatch[1]),
+                Number(charMatch[2]) + 1,
+              );
+            }
+          }
+        }
+      },
+
       fetchImageData(url, pageId) {
         if (!this.manifest) return;
 
