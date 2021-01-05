1dbacddce0c81ccc3e67a9254ed3686ba1ba5df7
rwd <richard.doe@rwdit.net>
2021-01-05 15:20:20 +0000

    Unit test login/logout redirection

---
+++ tests/unit/pages/account/logout.spec.js
@@ -0,0 +1,69 @@
+import { shallowMountNuxt } from '../../utils';
+import sinon from 'sinon';
+
+import page from '../../../../pages/account/logout';
+
+global.localStorage = {
+  setItem: sinon.spy()
+};
+
+describe('pages/account/logout.vue', () => {
+  describe('beforeRouteEnter', () => {
+    it('stores the previous page full path for auth redirection', () => {
+      const authStorageSetUniversal = sinon.spy();
+      const wrapper = shallowMountNuxt(page, {
+        mocks: {
+          $auth: {
+            loginWith: sinon.spy(),
+            logout: sinon.spy(),
+            $storage: {
+              getUniversal: sinon.spy(),
+              setUniversal: authStorageSetUniversal
+            },
+            strategies: {
+              keycloak: {
+                options: {
+                  'end_session_endpoint': 'https://auth.example.org/logout'
+                }
+              }
+            }
+          },
+          $goto: sinon.spy()
+        }
+      });
+
+      const from = { fullPath: '/eu/item/123/def' };
+      const next = sinon.stub().yields(wrapper.vm);
+      page.beforeRouteEnter.call(wrapper.vm, null, from, next);
+
+      next.should.have.been.called;
+      authStorageSetUniversal.should.have.been.calledWith('redirect', from.fullPath);
+    });
+  });
+
+  describe('mounted', () => {
+    it('redirects to the keycloak end session endpoint', () => {
+      const wrapper = shallowMountNuxt(page, {
+        mocks: {
+          $auth: {
+            loginWith: sinon.spy(),
+            logout: sinon.spy(),
+            $storage: {
+              getUniversal: sinon.stub().withArgs('redirect').returns('/about-us')
+            },
+            strategies: {
+              keycloak: {
+                options: {
+                  'end_session_endpoint': 'https://auth.example.org/logout'
+                }
+              }
+            }
+          },
+          $goto: sinon.spy()
+        }
+      });
+
+      wrapper.vm.$goto.should.have.been.calledWith('https://auth.example.org/logout?redirect_uri=http%3A%2F%2Flocalhost%2Fabout-us');
+    });
+  });
+});
