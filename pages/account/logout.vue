9429b1c5fdb48ea7b5d23997f99dc999b7d5ebb5
rwd <richard.doe@rwdit.net>
2021-01-05 14:29:19 +0000

    Redirect back to previous page after logout

--- pages/account/logout.vue
+++ pages/account/logout.vue
@@ -5,11 +5,20 @@
 <script>
   export default {
     layout: 'minimal',
+
     mounted() {
       this.$auth.logout();
-      const path = this.$auth.strategies.keycloak.options.end_session_endpoint;
       localStorage.setItem('logout-event', `logout-${Math.random()}`);
-      this.$goto(`${path}?redirect_uri=${encodeURIComponent(window.location.origin)}`);
+
+      const path = this.$auth.strategies.keycloak.options.end_session_endpoint;
+      const redirect = window.location.origin + this.$auth.$storage.getUniversal('redirect');
+      this.$goto(`${path}?redirect_uri=${encodeURIComponent(redirect)}`);
+    },
+
+    beforeRouteEnter(to, from, next) {
+      next(vm => {
+        vm.$auth.$storage.setUniversal('redirect', from.fullPath);
+      });
     }
   };
   </script>
