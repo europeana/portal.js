5764179cc114a2ba348f2259d536caf77e6a6c70
Alan Sutherland <sutherland_a@me.com>
2021-01-14 22:54:09 +0900

    added additional cookies in groups

--- plugins/klaro-config.js
+++ plugins/klaro-config.js
@@ -10,6 +10,10 @@ export const klaroConfig = {
           title: 'Performance optimization',
           description: 'These services process personal information to optimize the service that this website offers.'
         },
+        marketing: {
+          title: 'Marketing',
+          description: 'These services process personal information to help us better understand user behavior by tracking user flows and engagement.'
+        },
         service: {
           title: 'Service provision',
           description: 'These services are essential for the correct functioning of this website. You cannot disable them here as the service would not work correctly otherwise.'
@@ -19,14 +23,78 @@ export const klaroConfig = {
   },
   services: [
     {
-      name: 'i18n',
+      name: 'searchResultsView',
       purposes: ['performance'],
       cookies: [
-        'i18n_locale_code'
+        'searchResultsView'
       ],
       translations: {
         en: {
-          title: 'Language code'
+          title: 'Search results view'
+        }
+      }
+    },
+    {
+      name: 'debugSettings',
+      purposes: ['performance'],
+      cookies: [
+        'debugSettings'
+      ],
+      translations: {
+        en: {
+          title: 'Debug toggle'
+        }
+      }
+    },
+    // Jira service desk
+    // {
+    //   name: '',
+    //   purposes: ['performance'],
+    //   cookies: [
+    //     ''
+    //   ],
+    //   translations: {
+    //     en: {
+    //       title: 'Jira service desk'
+    //     }
+    //   }
+    // },
+    {
+      name: 'Google Analytics',
+      purposes: ['marketing'],
+      cookies: [
+        '_ga',
+        '_gid',
+        '_gat_UA-12776629-19'
+      ],
+      translations: {
+        en: {
+          title: 'Google Analytics'
+        }
+      }
+    },
+    {
+      name: 'Hotjar',
+      purposes: ['marketing'],
+      cookies: [
+        '_hjid',
+        '_hjTLDTest'
+      ],
+      translations: {
+        en: {
+          title: 'Hotjar'
+        }
+      }
+    },
+    {
+      name: 'Google Optimize',
+      purposes: ['marketing'],
+      cookies: [
+        '?'
+      ],
+      translations: {
+        en: {
+          title: 'Google Optimize'
         }
       }
     },
@@ -42,8 +110,39 @@ export const klaroConfig = {
           title: 'Cloudflare'
         }
       }
+    },
+    {
+      name: 'i18n',
+      purposes: ['service'],
+      cookies: [
+        'i18n_locale_code'
+      ],
+      required: true,
+      translations: {
+        en: {
+          title: 'Language code'
+        }
+      }
+    },
+    {
+      name: 'auth.strategy',
+      purposes: ['service'],
+      cookies: [
+        'auth.strategy'
+      ],
+      required: true,
+      translations: {
+        en: {
+          title: 'Auth Strategy'
+        }
+      }
     }
   ],
   mustConsent: true,
-  acceptAll: true
+  acceptAll: true,
+  callback: (consent, service) => {
+    console.log(
+      'User consent for service ' + service.name + ': consent=' + consent
+    );
+  }
 };
