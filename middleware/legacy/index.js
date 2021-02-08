0ec658a0bc23db7ccd57df3774490db1f94a7544
rwd <richard.doe@rwdit.net>
2021-02-08 14:46:45 +0000

    Handle error statuses too

--- middleware/legacy/index.js
+++ middleware/legacy/index.js
@@ -1,6 +1,8 @@
 // This middleware is responsible for handling redirects for legacy URLs used on
 // previous versions of the Europeana Portal.
 
+import http from 'http';
+
 import entity from './rules/entity';
 import exhibitions from './rules/exhibitions';
 import explore from './rules/explore';
@@ -28,19 +30,26 @@ function stringifyPathChunks(chunks) {
   return chunks.filter((chunk) => typeof chunk !== undefined).join('');
 }
 
-export default ({ redirect, route, query }) => {
+export default ({ redirect, route, query, error }) => {
   if (/^\/[a-z]{2}\/portal(\/$|$)/.test(route.path)) return redirect(302, route.path.match(/^(\/[a-z]{2})/)[1]);
   if (!/^\/portal(\/|$)/.test(route.path)) return;
 
   for (const rule of rules) {
-    const redirectRoute = rule(route, query);
+    const legacyRoute = rule(route, query);
 
-    if (redirectRoute) {
-      if (Array.isArray(redirectRoute.path)) redirectRoute.path = stringifyPathChunks(redirectRoute.path);
-      if (!redirectRoute.query && query) redirectRoute.query = query;
-      if (!redirectRoute.status) redirectRoute.status = 301;
+    if (legacyRoute) {
+      if (Array.isArray(legacyRoute.path)) legacyRoute.path = stringifyPathChunks(legacyRoute.path);
+      if (!legacyRoute.query && query) legacyRoute.query = query;
+      if (!legacyRoute.status) legacyRoute.status = 301;
 
-      return redirect(redirectRoute.status, redirectRoute.path, redirectRoute.query);
+      if ((legacyRoute.status >= 300) && (legacyRoute.status < 400)) {
+        return redirect(legacyRoute.status, legacyRoute.path, legacyRoute.query);
+      } else {
+        return error({
+          message: `${legacyRoute.status} ${http.STATUS_CODES[legacyRoute.status]}`,
+          statusCode: legacyRoute.status
+        });
+      }
     }
   }
 };
