45e92990859a392c0ea1d3a7a77483379a5bc715
rwd <richard.doe@rwdit.net>
2021-02-24 11:52:10 +0000

    Rewrite patch in JS, no sed

--- tests/unit/patch-istanbul-lib-source-maps.js
+++ tests/unit/patch-istanbul-lib-source-maps.js
@@ -5,6 +5,11 @@
 // Courtesy of: https://gist.github.com/lsapan/3bfd0ffc0fb3d4a036fce84f6eea142e
 // See: https://github.com/istanbuljs/istanbuljs/issues/464
 
-const { execSync } = require('child_process');
+const fs = require('fs');
 
-execSync('sed -i "s/source: pathutils.relativeTo(start.source, origFile),/source: origFile,/" node_modules/istanbul-lib-source-maps/lib/get-mapping.js');
+const path = 'node_modules/istanbul-lib-source-maps/lib/get-mapping.js';
+const find = 'source: pathutils.relativeTo(start.source, origFile),';
+const replace = 'source: origFile,';
+
+const original = fs.readFileSync(path, 'utf8');
+if (original.includes(find)) fs.writeFileSync(path, original.replace(find, replace));
