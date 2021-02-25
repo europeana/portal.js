57037414916fd1cc84eb677f7196fa90a550a594
Lutz Biedinger <lutz.biedinger@gmail.com>
2021-02-25 16:25:06 +0100

    promise for snapshot runner

--- tests/features/support/step-runners.js
+++ tests/features/support/step-runners.js
@@ -277,7 +277,7 @@ module.exports = {
     // this allows any JS based resizing/loading/animations from previous steps to finish.
     await this.waitSomeSeconds(1);
 
-    await client.percySnapshot(pageName);
+    return client.percySnapshot(pageName);
   },
   async hrefLangTags() {
     await client.expect.element('link[rel=alternate]').to.be.present;
