3cdd29662fca02a3adbd35f21cd57d85e71d4068
Richard Doe <richard.doe@rwdit.net>
2021-03-03 11:10:49 +0000

    Migrate from @percy/agent to @percy/cli (#1009)
    * Migrate from @percy/agent to @percy/cli
    
    * Tell Percy to use local Chromium
    
    * Revert CI job to run visual tests after e2e

--- docker/stack/nightwatch/percy.js
+++ docker/stack/nightwatch/percy.js
@@ -1,16 +1,10 @@
-// Config file for Percy
-// Doc: https://docs.percy.io/docs/sdk-configuration
 module.exports = {
-  'version': 1,
-
-  'agent': {
-    'asset-discovery': {
-      'allowed-hostnames': [
-        'fonts.gstatic.com',
-        'images.ctfassets.net',
-        'unpkg.com'
-      ]
-    },
-    'network-idle-timeout': 1000
+  version: 2,
+  discovery: {
+    allowedHostnames: [
+      'fonts.gstatic.com',
+      'images.ctfassets.net',
+      'unpkg.com'
+    ]
   }
-};
+}
