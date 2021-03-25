045e36656c5231dd7ca22fab499162442f84d4c1
Richard Doe <richard.doe@rwdit.net>
2021-03-25 09:33:46 +0000

    Intentional duplication

--- tests/unit/plugins/europeana/auth.spec.js
+++ tests/unit/plugins/europeana/auth.spec.js
@@ -104,6 +104,82 @@ describe('plugins/europeana/auth', () => {
         });
       });
     });
+  });
+
+  describe('keycloakResponseErrorHandler', () => {
+    context('when response status is 401', () => {
+      const error = new Error(401);
+      error.response = { status: 401 };
+      error.config = {
+        headers: {}
+      };
+
+      context('and the user is logged in with a refresh token', () => {
+        const ctx = mockContext({
+          $auth: {
+            getRefreshToken: () => 'token',
+            loggedIn: true
+          }
+        });
+
+        it('attempts to refresh the access token', async() => {
+          await keycloakResponseErrorHandler(ctx, error);
+
+          ctx.$auth.request.should.have.been.called;
+        });
+
+        context('when it has refreshed the access token', () => {
+          const ctx = mockContext({
+            $auth: {
+              getRefreshToken: () => 'token',
+              loggedIn: true,
+              request: sinon.stub().resolves({
+                accessToken: 'new'
+              })
+            },
+            $axios: {
+              request: sinon.spy()
+            }
+          });
+
+          it('stores the new access token', async() => {
+            await keycloakResponseErrorHandler(ctx, error);
+
+            ctx.$auth.setToken.should.have.been.calledWith('strategy', 'new');
+            ctx.$auth.strategy['_setToken'].should.have.been.calledWith('new');
+          });
+
+          it('retries the original request', async() => {
+            await keycloakResponseErrorHandler(ctx, error);
+
+            ctx.$axios.request.should.have.been.called;
+          });
+        });
+
+        context('when it could not refresh the access token', () => {
+          it('redirects to the login URL', async() => {
+            await keycloakResponseErrorHandler(ctx, error);
+
+            ctx.redirect.should.have.been.calledWith('http://example.org/login');
+          });
+        });
+      });
+
+      context('but the user is not logged in with a refresh token', () => {
+        const ctx = mockContext({
+          $auth: {
+            getRefreshToken: () => null,
+            loggedIn: false
+          }
+        });
+
+        it('redirects to the login URL', async() => {
+          await keycloakResponseErrorHandler(ctx, error);
+
+          ctx.redirect.should.have.been.calledWith('http://example.org/login');
+        });
+      });
+    });
 
     context('when response status is not 401', () => {
       it('rejects it', () => {
