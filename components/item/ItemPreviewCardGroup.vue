76ddf469eb8d88ef73c447d2aa0898a9692c29e7
Leonie Peters <leoniepeters@gmail.com>
2021-03-26 11:00:38 +0100

    Add transition for smoother resize

--- components/item/ItemPreviewCardGroup.vue
+++ components/item/ItemPreviewCardGroup.vue
@@ -5,7 +5,7 @@
     <client-only>
       <div
         v-masonry
-        transition-duration="0"
+        transition-duration="0.1"
         item-selector=".card"
         horizontal-order="true"
         column-width=".masonry-container .card"
