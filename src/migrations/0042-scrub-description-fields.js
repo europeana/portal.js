11def54917f0d66977535c6a0df4762a3cbf0e1c
Leonie Peters <leoniepeters@gmail.com>
2021-04-12 10:31:36 +0200

    Use search queries in getAssets and add check to publish only when status is published

--- src/migrations/0042-scrub-description-fields.js
+++ src/migrations/0042-scrub-description-fields.js
@@ -6,22 +6,26 @@ const client = contentful.createClient({
   accessToken: process.env.CTF_CMA_ACCESS_TOKEN
 });
 
+const scrubBeforeDate = new Date('2 March 2021');
+
 client.getSpace(process.env.CTF_SPACE_ID)
   .then((space) => space.getEnvironment(process.env.CTF_ENVIRONMENT_ID))
   // instead of the forEach we might be able to insert a query
   // as parameter to getAssets to filter the assets
-  .then((environment) => environment.getAssets({ limit: 1000 }))
+  .then((environment) => environment.getAssets({
+    limit: 1000,
+    'sys.createdAt[lt]': scrubBeforeDate,
+    'fields.description[exists]': true
+  }))
   .then((response) => {
     response.items.forEach(asset => {
-      const createdAt = new Date(asset.sys.createdAt);
-      const scrubBeforeDate = new Date('2 March 2021');
-      if (createdAt < scrubBeforeDate && asset.fields.description) {
-        asset.fields.description = undefined;
-        asset.update();
+      asset.fields.description = undefined;
+      asset.update();
+      if (!!asset.sys.publishedVersion &&
+    asset.sys.version === asset.sys.publishedVersion + 1) {
         asset.publish();
-        console.log(`Asset ${asset.sys.id} updated.`);
       }
+      console.log(`Asset ${asset.sys.id} updated.`);
     });
   })
-
   .catch(console.error);
