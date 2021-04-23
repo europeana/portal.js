d204f6f27fba935dad0e4e87df65a0c9c7a123bd
Leonie Peters <leoniepeters@gmail.com>
2021-04-23 15:03:12 +0200

    Redirect to keycloack or portal depending on origin

--- src/pages/account/login.vue
+++ src/pages/account/login.vue
@@ -6,14 +6,29 @@
   export default {
     layout: 'minimal',
 
-    created() {
-      this.$auth.$storage.setUniversal('portalLoggingIn', true);
-      this.$auth.loginWith('keycloak');
+    data() {
+      return {
+        fromPortal: true
+      };
+    },
+
+    mounted() {
+      if (this.fromPortal) {
+        this.$auth.$storage.setUniversal('portalLoggingIn', true);
+        this.$auth.loginWith('keycloak');
+      } else {
+        const redirect = window.location.origin + this.$auth.$storage.getUniversal('redirect');
+        this.$goto(redirect);
+      }
     },
 
     beforeRouteEnter(to, from, next) {
       next(vm => {
-        vm.$auth.$storage.setUniversal('redirect', from.fullPath);
+        if (from.name) {
+          vm.$auth.$storage.setUniversal('redirect', from.fullPath);
+        } else {
+          vm.fromPortal = false;
+        }
       });
     }
   };
