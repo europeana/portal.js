24e596567f3e758dfdf9af4755421f4f118b3d4e
Richard Doe <richard.doe@rwdit.net>
2021-05-07 10:54:16 +0100

    Pass referrer to login

--- src/pages/account/login.vue
+++ src/pages/account/login.vue
@@ -8,7 +8,12 @@
 
     created() {
       this.$auth.$storage.setUniversal('portalLoggingIn', true);
-      this.$auth.loginWith('keycloak', { params: { 'ui_locales': this.$i18n.locale } });
+      this.$auth.loginWith('keycloak', {
+        params: {
+          'referrer_uri': this.$config.app.baseUrl,
+          'ui_locales': this.$i18n.locale
+        }
+      });
     },
 
     beforeRouteEnter(to, from, next) {
