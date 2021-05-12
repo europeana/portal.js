f50277525131d2fbfe329fb5dc7d47a7f0e43041
Richard Doe <richard.doe@rwdit.net>
2021-05-12 10:17:15 +0100

    Do not include referrer params
    As Keycloak login does not observe them

--- src/pages/account/login.vue
+++ src/pages/account/login.vue
@@ -10,8 +10,6 @@
       this.$auth.$storage.setUniversal('portalLoggingIn', true);
       this.$auth.loginWith('keycloak', {
         params: {
-          'referrer': this.$auth.strategy.options.client_id,
-          'referrer_uri': this.$config.app.baseUrl,
           'ui_locales': this.$i18n.locale
         }
       });
