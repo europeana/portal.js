f08531bfaef1245a29e2d93fe408ff9db1fbf604
Richard Doe <richard.doe@rwdit.net>
2021-07-01 13:02:44 +0100

    Include /_api prefix

--- bin/api.js
+++ bin/api.js
@@ -1,4 +1,9 @@
-import app from '../src/server-middleware/api';
+import express from 'express';
+const app = express();
+
+import api from '../src/server-middleware/api';
+app.use('/_api', api);
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Listening on: http://localhost:${port}/`);
