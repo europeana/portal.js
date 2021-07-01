46797805e9eeee5f3967190e32ba9ecde30e8346
Leonie Peters <leoniepeters@gmail.com>
2021-07-01 09:33:07 +0200

    Clean up commented out code and remove unnecessary return

--- src/plugins/hotjar.client.js
+++ src/plugins/hotjar.client.js
@@ -4,7 +4,7 @@ export default ({ $config }, inject) => {
   } else if ($config.app.features.klaro) {
     inject('initHotjar', init);
   } else {
-    return init();
+    init();
   }
 
   function init() {
@@ -15,10 +15,6 @@ export default ({ $config }, inject) => {
       h['_hjSettings'] = { hjid: $config.hotjar.id, hjsv: $config.hotjar.sv };
       a = o.getElementsByTagName('head')[0];
       r = o.createElement('script'); r.async = 1;
-      // r.setAttribute('type', 'text/javascript');
-      // r.setAttribute('data-src', t + h['_hjSettings'].hjid + j + h['_hjSettings'].hjsv);
-      // r.setAttribute('data-name', 'hotjar');
-      // r.setAttribute('data-type', 'text/javascript');
       r.setAttribute('src', t + h['_hjSettings'].hjid + j + h['_hjSettings'].hjsv);
       a.appendChild(r);
     })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
