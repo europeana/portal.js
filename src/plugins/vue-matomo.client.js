34f2a60b731811bdbb70479a375fc283ba50c44b
Richard Doe <richard.doe@rwdit.net>
2021-07-05 11:42:10 +0100

    Site search handler
    Using forked version of vue-matomo pending upstream PR

--- src/plugins/vue-matomo.client.js
+++ src/plugins/vue-matomo.client.js
@@ -1,15 +1,38 @@
 import Vue from 'vue';
-import VueMatomo from 'vue-matomo';
+import VueMatomo from '@europeana/vue-matomo';
+
+export const trackSiteSearch = (to) => {
+  let siteSearch = null;
+
+  if (Object.keys(to.query).includes('query')) {
+    // register a site search, treating any collection filter as the category
+    // and passing *:* to Matomo for empty query
+    const keyword = to.query.query === '' ? '*:*' : to.query.query;
+
+    let category;
+    if (to.name.startsWith('collections-type-all')) {
+      category = `${to.params.type} ${to.params.pathMatch}`;
+    }
+
+    siteSearch = {
+      keyword,
+      category
+    };
+  }
+
+  return siteSearch;
+};
 
 export default ({ app, $config: { matomo: { host, siteId } } }) => {
   if (!host || !siteId) {
     return;
   }
 
+  // Docs: https://github.com/AmazingDreams/vue-matomo#readme
   Vue.use(VueMatomo, {
-    // Docs: https://github.com/AmazingDreams/vue-matomo#readme
     router: app.router,
     host,
-    siteId
+    siteId,
+    trackSiteSearch
   });
 };
