564c41c6bd345238cc9be39579879f339e4b7452
Leonie Peters <leoniepeters@gmail.com>
2021-07-19 11:43:57 +0200

    Only use en prefLabel and filter out

--- src/server-middleware/api/entities/organisations.js
+++ src/server-middleware/api/entities/organisations.js
@@ -1,6 +1,6 @@
 const cacher = require('../../../cachers/entities/organisations');
 import { errorHandler } from '../';
-import { langMapValueForLocale } from '../../../plugins/europeana/utils';
+// import { langMapValueForLocale } from '../../../plugins/europeana/utils';
 import { getEntitySlug } from '../../../plugins/europeana/entity';
 
 export default (options = {}) => (req, res) => {
@@ -16,8 +16,10 @@ export default (options = {}) => (req, res) => {
       return Object.keys(body).map(id => ({
         id,
         slug: getEntitySlug(id, body[id].prefLabel.en),
-        prefLabel: langMapValueForLocale(body[id].prefLabel, req.query.locale || 'en').values[0]
-      }));
+        // For now only get English labels.
+        // prefLabel: langMapValueForLocale(body[id].prefLabel, req.query.locale || 'en').values[0]
+        prefLabel: body[id].prefLabel.en
+      })).filter(organisation => organisation.prefLabel);
     })
     .then(localised => res.json(localised))
     .catch(error => errorHandler(res, error));
