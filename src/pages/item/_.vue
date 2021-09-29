feffc89b604537d11ff714cbcea8502fa9be0b50
Richard Doe <richard.doe@rwdit.net>
2021-09-29 14:43:42 +0100

    fix: use dc:type for similar items query, not dc:title

--- src/pages/item/_.vue
+++ src/pages/item/_.vue
@@ -341,7 +341,7 @@
 
         const dataSimilarItems = {
           dcSubject: this.getSimilarItemsData(this.coreFields.dcSubject),
-          dcType: this.getSimilarItemsData(this.title), // TODO: Evaluate why title is being set as type here.
+          dcType: this.getSimilarItemsData(this.coreFields.dcType),
           dcCreator: this.getSimilarItemsData(this.coreFields.dcCreator),
           edmDataProvider: this.getSimilarItemsData(this.fields.edmDataProvider)
         };
