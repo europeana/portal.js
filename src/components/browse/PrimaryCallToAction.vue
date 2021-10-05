6c4e314b54c77f54af97449f3ffb45c6e34e77ca
Leonie Peters <leoniepeters@gmail.com>
2021-10-05 14:30:53 +0200

    Add bottom margin and clean up styles

--- src/components/browse/PrimaryCallToAction.vue
+++ src/components/browse/PrimaryCallToAction.vue
@@ -8,7 +8,7 @@
     <SmartLink
       :destination="link.url"
       data-qa="call to action"
-      class="btn btn-cta btn-primary d-inline-flex align-items-center"
+      class="btn btn-cta btn-primary"
     >
       {{ link.text }}
     </SmartLink>
@@ -45,18 +45,12 @@
   .primary-cta {
     background-color: $bodygrey;
     padding: 1.5rem calc((50vw - 50%) / 2);
-    margin: 0 calc((-50vw + 50%) / 2);
+    margin: 0 calc((-50vw + 50%) / 2) 2rem;
     max-width: calc(100% + 100px);
     @media (min-width: $bp-large) {
       padding: 1.5rem 50px;
-      margin: 0 -50px;
+      margin: 0 -50px 2rem;
     }
-    // padding: 1.5rem;
-    // margin: 0 -1.5rem;
-    // @media (min-width: $bp-medium) {
-    //   padding: 1.5rem 50px;
-    //   margin: 0 -50px;
-    // }
     .btn-primary.btn-cta {
       margin-bottom: 0;
     }
