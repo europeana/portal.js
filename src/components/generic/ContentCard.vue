6b89180eb3e2ab008136d92cc05bcac1982cf98a
Richard Doe <richard.doe@rwdit.net>
2021-11-12 09:11:50 +0000

    fix: prevent hydration error on hit highlights (#1289)

--- src/components/generic/ContentCard.vue
+++ src/components/generic/ContentCard.vue
@@ -63,30 +63,32 @@
         >
           {{ $d(new Date(datetime), 'short') }}
         </time>
-        <template v-if="hitsText">
-          <b-card-text
-            text-tag="div"
-            data-qa="highlighted search term"
-          >
-            <p>{{ hitsText.prefix }}<strong class="has-text-highlight">{{ hitsText.exact }}</strong>{{ hitsText.suffix }}</p>
-          </b-card-text>
-        </template>
+        <b-card-text
+          v-if="hitsText"
+          text-tag="div"
+          data-qa="highlighted search term"
+        >
+          <p>
+            {{ hitsText.prefix }}
+            <strong class="has-text-highlight">
+              {{ hitsText.exact }}
+            </strong>
+            {{ hitsText.suffix }}
+          </p>
+        </b-card-text>
         <template v-if="displayTexts.length > 0">
-          <template
+          <b-card-text
             v-for="(text, index) in displayTexts"
+            :key="index"
+            :lang="text.code"
+            text-tag="div"
           >
-            <b-card-text
-              :key="index"
-              :lang="text.code"
-              text-tag="div"
-            >
-              <!-- eslint-disable vue/no-v-html -->
-              <p
-                v-html="cardText(text.values)"
-              />
-              <!-- eslint-enable vue/no-v-html -->
-            </b-card-text>
-          </template>
+            <!-- eslint-disable vue/no-v-html -->
+            <p
+              v-html="cardText(text.values)"
+            />
+            <!-- eslint-enable vue/no-v-html -->
+          </b-card-text>
         </template>
       </b-card-body>
     </SmartLink>
