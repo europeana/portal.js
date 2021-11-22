f1b24d10aa53af67859a27a6459d12f42a7be693
Richard Doe <richard.doe@rwdit.net>
2021-11-22 15:40:45 +0000

    chore: update stylelint to v14

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: 'stylelint-config-recommended',
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue'
+  ],
   rules: {
     'at-rule-no-unknown': null,
     'block-closing-brace-newline-after': 'always',
