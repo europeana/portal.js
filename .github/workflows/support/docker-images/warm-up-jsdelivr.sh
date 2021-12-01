e7d1d79ff5600f55699e75554c0f3bb522df261c
Richard Doe <richard.doe@rwdit.net>
2021-12-01 14:35:12 +0000

    chore: retry each URL

--- .github/workflows/support/docker-images/warm-up-jsdelivr.sh
+++ .github/workflows/support/docker-images/warm-up-jsdelivr.sh
@@ -4,34 +4,32 @@ version=$(npm view @europeana/portal version)
 
 apk add --no-cache curl
 
-# Give jsDelivr time to fetch the new version
-url="https://cdn.jsdelivr.net/npm/@europeana/portal@${version}/"
-cached=false
-attempt=0
-while [[ ${cached} == "false" ]]; do
-  attempt=$(( attempt + 1 ))
-  if [[ ${attempt} -gt 12 ]]; then
-    echo "ERROR: Failed to detect version available on jsDelivr after 12 attempts."
-    exit 1
-  fi
+warm_up_url () {
+  url=$1
+  cached=false
+  attempt=0
+  while [[ ${cached} == "false" ]]; do
+    attempt=$(( attempt + 1 ))
+    if [[ ${attempt} -gt 12 ]]; then
+      echo "ERROR: Failed to detect version available on jsDelivr after 12 attempts."
+      exit 1
+    fi
 
-  echo "Checking for version availability on jsDelivr, attempt #${attempt}"
-  echo ${url}
-  curl --fail --silent -o /dev/null -I "${url}"
+    echo "Checking for version availability on jsDelivr, attempt #${attempt}"
+    echo ${url}
+    curl --fail --silent -o /dev/null -I "${url}"
 
-  if [[ "$?" == "0" ]]; then
-    echo "OK"
-    echo
-    cached=true
-  else
-    sleep 5
-  fi
-done
+    if [[ "$?" == "0" ]]; then
+      echo "OK"
+      echo
+      cached=true
+    else
+      sleep 5
+    fi
+  done
+}
 
-set -e
 for file in $(find .nuxt/dist/client -type f | sort); do
   url="https://cdn.jsdelivr.net/npm/@europeana/portal@${version}/${file}"
-  echo ${url}
-
-  curl --fail --silent -o /dev/null -I "${url}"
+  warm_up_url ${url}
 done
