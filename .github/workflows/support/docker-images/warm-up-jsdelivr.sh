afc8578ec4fc36aeb6caa7cce0928eb1ab4f1286
Richard Doe <richard.doe@rwdit.net>
2021-12-01 13:43:05 +0000

    chore: move jsDeliver availability detection to script

--- .github/workflows/support/docker-images/warm-up-jsdelivr.sh
+++ .github/workflows/support/docker-images/warm-up-jsdelivr.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
-set -e
-
 version=$(npm view @europeana/portal version)
 
+apk add --no-cache curl
+
+# Give jsDelivr time to fetch the new version
+url="https://cdn.jsdelivr.net/npm/@europeana/portal@${version}"
+cached=false
+attempt=0
+while [[ ${cached} == "false" ]]; do
+  attempt=$(( attempt + 1 ))
+  if [[ ${attempt} -gt 12 ]]; then
+    echo "ERROR: Failed to detect version available on jsDelivr after 12 attempts."
+    exit 1
+  fi
+
+  echo "Checking for version availability on jsDelivr, attempt #${attempt}"
+  curl --fail --silent -o /dev/null -I "${url}"
+
+  if [[ "$?" == "0" ]]; then
+    echo "OK"
+    echo
+    cached=true
+  else
+    sleep 5
+  fi
+done
+
+set -e
 for file in $(find .nuxt/dist/client -type f | sort); do
   url="https://cdn.jsdelivr.net/npm/@europeana/portal@${version}/${file}"
   echo ${url}
-  wget -q ${url}
+
+  curl --fail --silent -o /dev/null -I "${url}"
 done
