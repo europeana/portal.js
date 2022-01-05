14d5f69c7501e48a3c7651460802a6b0c7593882
Richard Doe <richard.doe@rwdit.net>
2022-01-05 09:15:52 +0000

    test: add unit tests for server middlewares

---
+++ tests/unit/server-middleware/referrer-policy.spec.js
@@ -0,0 +1,24 @@
+import sinon from 'sinon';
+import middleware from '@/server-middleware/referrer-policy.js';
+
+describe('server-middleware/referrer-policy', () => {
+  const res = { setHeader: sinon.spy() };
+  const next = sinon.spy();
+
+  beforeEach(() => {
+    res.setHeader.resetHistory();
+    next.resetHistory();
+  });
+
+  it('sets Referrer-Policy response header to "strict-origin-when-cross-origin"', () => {
+    middleware(null, res, next);
+
+    expect(res.setHeader.calledWith('Referrer-Policy', 'strict-origin-when-cross-origin'));
+  });
+
+  it('calls next', () => {
+    middleware(null, res, next);
+
+    expect(next.called);
+  });
+});
