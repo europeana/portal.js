6fcc4f240300640a1933055c9d354d2a34ca23de
Lutz Biedinger <lutz.biedinger@gmail.com>
2022-01-07 15:50:48 +0100

    specs for RelatedCollection chips

--- tests/unit/components/generic/RelatedCollections.spec.js
+++ tests/unit/components/generic/RelatedCollections.spec.js
@@ -1,7 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import BootstrapVue from 'bootstrap-vue';
 import Vuex from 'vuex';
-import sinon from 'sinon';
 
 import RelatedCollections from '@/components/generic/RelatedCollections.vue';
 
@@ -9,7 +8,6 @@ const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 localVue.use(Vuex);
 
-const $path = sinon.stub();
 const factory = (options = {}) => {
   return mount(RelatedCollections, {
     localVue,
@@ -22,7 +20,7 @@ const factory = (options = {}) => {
         $i18n: { locale: 'en' },
         $t: () => {},
         $fetch: () => {},
-        $path,
+        $path: (args) => { return `${args.params.type} - ${args.params.pathMatch}`},
         $link: {
           to: route => route,
           href: () => null
@@ -44,21 +42,32 @@ const store = (options = {}) => {
 
 const relatedChips = [
   {
-    id: 'http://data.europeana.eu/concept/base/190',
-    prefLabel: {
-      en: 'Art'
-    }
+    description: 'This is a scpecially curated chip!',
+    identifier: 'http://data.europeana.eu/agent/base/123',
+    image: 'imageUrlItem1',
+    name: 'Entity from contentful',
+    slug: '123-entity-from-contentful'
   },
   {
     id: 'http://data.europeana.eu/concept/base/194',
     prefLabel: {
       en: 'Visual arts'
+    },
+    isShownBy: {
+      id: 'item2isShownById',
+      source: 'http://data.europeana.eu/item/123/XYZ',
+      thumbnail:'thumbnailUrlItem2',
+      type:'WebResource'
     }
   },
   {
-    id: 'http://data.europeana.eu/concept/base/96',
+    id: 'http://data.europeana.eu/organzation/base/1',
+    logo: {
+      id: 'http://www.wikimedia.org/wiki/Special:FilePath/logoUrlItem3.jpg',
+      source: 'www.wikimedia.org/wiki/Special:FilePath/logoUrlItem3',
+    },
     prefLabel: {
-      en: 'Art Nouveau'
+      en: 'Europeana'
     }
   },
   {
@@ -80,7 +89,7 @@ describe('components/generic/RelatedCollections', () => {
     });
 
     it('contains four related chips', () => {
-      const chips = wrapper.findAll('span.badge');
+      const chips = wrapper.findAll('a.badge');
       expect(chips.length).toBe(4);
     });
   });
@@ -92,6 +101,49 @@ describe('components/generic/RelatedCollections', () => {
     it('related collections does not render', () => {
       const relatedCollections = wrapper.find('[data-qa="related collections"]');
       expect(relatedCollections.exists()).toBe(false);
+    });
+  });
+
+  describe('#linkGen', () => {
+    describe('when the item has an identifier/it is a curated chip from contenful', () => {
+      it('uses the identifier and name for the slug', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.linkGen(relatedChips[0])).toEqual('person - 123-entity-from-contentful');
+      });
+    });
+
+    describe('when the item has an id/it is a Europeana entity from a search request', () => {
+      it('uses the id and the prefLabel for the name', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.linkGen(relatedChips[1])).toEqual('topic - 194-visual-arts');
+      });
+    });
+  });
+
+  describe('#imageUrl', ()=> {
+    describe('when the item has an image attribute', () => {
+      it('uses the image at a width of 200', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.imageUrl(relatedChips[0])).toEqual('imageUrlItem1&size=w200');
+      });
+    });
+    describe('when the item has an isShownBy with a thumbnail', () => {
+      it('uses the thumbnail at a width of 200', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.imageUrl(relatedChips[1])).toEqual('thumbnailUrlItem2&size=w200');
+      });
+    });
+    describe('when the item has a logo attribute', () => {
+      it('uses the logo URL from wikimedia at a size of 28', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.imageUrl(relatedChips[2])).toEqual('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/logoUrlItem3.jpg/28px-logoUrlItem3.jpg');
+      });
+    });
+    describe('when there is no relevant attribute', () => {
+      it('returns null', () => {
+        const wrapper = factory();
+        expect(wrapper.vm.imageUrl(relatedChips[3])).toBeNull();
+      });
     });
   });
 });
