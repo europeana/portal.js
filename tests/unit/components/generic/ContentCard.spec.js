c02224b0f999c633e80980d7e281c647e61c604e
Richard Doe <richard.doe@rwdit.net>
2022-01-07 11:51:30 +0000

    test: increase ContentCard test coverage

--- tests/unit/components/generic/ContentCard.spec.js
+++ tests/unit/components/generic/ContentCard.spec.js
@@ -23,22 +23,41 @@ const factory = () => mount(ContentCard, {
       locale: 'en'
     },
     $path: () => '',
-    $t: (key) => key,
+    $t: (key) => {
+      if (key === 'formatting.ellipsis') {
+        return '…';
+      } else if (key === 'formatting.listSeperator') {
+        return ';';
+      } else {
+        return key;
+      }
+    },
     $tc: (key) => key,
     $store
   }
 });
 
 describe('components/generic/ContentCard', () => {
-  it('has a description', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ texts: ['The Milkmaid by Vermeer'] });
 
-    const description =  wrapper.find('[data-qa="content card"] .card-body');
-    expect(description.text()).toBe('The Milkmaid by Vermeer');
+  describe('card title', () => {
+    it('is displayed as-is if a string', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ title: 'The Milkmaid by Vermeer' });
+
+      const title =  wrapper.find('[data-qa="content card"] .card-title');
+      expect(title.text()).toBe('The Milkmaid by Vermeer');
+    });
+
+    it('is localised if a LangMap', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ title: { en: 'Art', es: 'Arte'} });
+
+      const title =  wrapper.find('[data-qa="content card"] .card-title');
+      expect(title.text()).toBe('Art');
+    });
   });
 
-  describe('display labels', () => {
+  describe('card subtitle', () => {
     const tests = [
       {
         type: 'blog',
@@ -85,19 +104,19 @@ describe('components/generic/ContentCard', () => {
             const wrapper = factory();
             await wrapper.setProps({ url });
 
-            const label =  wrapper.find('[data-qa="content card"] .card-subtitle');
+            const subtitle =  wrapper.find('[data-qa="content card"] .card-subtitle');
             if (test.type === 'collections') {
               if (wrapper.vm.themes.some(theme => (typeof url === 'string' && url.includes(theme))
               || theme === url.params?.pathMatch)) {
                 // TO DO remove when thematic collections topics get there own 'theme' type
-                expect(label.text()).toBe('cardLabels.theme');
+                expect(subtitle.text()).toBe('cardLabels.theme');
               } else {
-                expect(label.text()).toBe(`cardLabels.${test.urls[1].params.type}`);
+                expect(subtitle.text()).toBe(`cardLabels.${test.urls[1].params.type}`);
               }
             } else if (test.type === 'blog') {
-              expect(label.text()).toBe('blog.posts');
+              expect(subtitle.text()).toBe('blog.posts');
             } else {
-              expect(label.text()).toBe(`${test.type}.${test.type}`);
+              expect(subtitle.text()).toBe(`${test.type}.${test.type}`);
             }
           });
         }
@@ -105,76 +124,132 @@ describe('components/generic/ContentCard', () => {
     }
   });
 
-  it('has a creator and institution', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ texts: ['Edvard Munch', 'Munchmuseet (The Munch Museum)'] });
+  describe('card body', () => {
+    it('displays the texts', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ texts: ['The Milkmaid by Vermeer'] });
 
-    const description =  wrapper.find('[data-qa="content card"] .card-body');
-    expect(description.text()).toContain('Edvard Munch');
-    expect(description.text()).toContain('Munchmuseet');
-  });
-
-  it('has a link', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ url: 'https://example.org' });
-
-    const link =  wrapper.find('[data-qa="content card"] .card-link');
-    expect(link.attributes().href).toBe('https://example.org');
-  });
-
-  it('has an image', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ imageUrl: 'https://example.org' });
-
-    const card =  wrapper.find('[data-qa="content card"] .card-img img');
-    expect(card).toBeDefined();
-  });
-
-  it('has an optimised image', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ imageUrl: '//images.ctfassets.net/example/example.jpg', imageContentType: 'image/jpeg' });
-
-    expect(wrapper.vm.optimisedImageUrl).toContain('fm=jpg&fl=progressive&q=50');
-  });
-
-  it('has an optimised image with max width', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({
-      imageUrl: '//images.ctfassets.net/example/example.jpg',
-      imageContentType: 'image/jpeg',
-      imageOptimisationOptions: { width: 510 }
+      const body =  wrapper.find('[data-qa="content card"] .card-body');
+      expect(body.text()).toBe('The Milkmaid by Vermeer');
     });
 
-    expect(wrapper.vm.optimisedImageUrl).toContain('fm=jpg&fl=progressive&q=50&w=510');
-  });
+    it('localises LangMap texts', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ texts: [{ en: 'Art', es: 'Arte'}] });
 
-  it('has no image and is of variant mini', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ imageUrl: null, variant: 'mini' });
-
-    const image =  wrapper.find('[data-qa="content card"] .card-img');
-    expect(image.exists()).toBe(false);
-  });
-
-  it('has has an image and is of variant mini', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({ imageUrl: 'https://example.org', variant: 'mini' });
-
-    const image =  wrapper.find('[data-qa="content card"] .card-img');
-    expect(image.exists()).toBe(true);
-  });
-
-  it('highlights the search term if found', async() => {
-    const wrapper = factory();
-    await wrapper.setProps({
-      hitsText: {
-        prefix: 'The quick brown ',
-        exact: 'fox',
-        suffix: ' jumps over the lazy dog'
-      }
+      const body =  wrapper.find('[data-qa="content card"] .card-body');
+      expect(body.text()).toBe('Art');
     });
 
-    const description =  wrapper.find('[data-qa="highlighted search term"] strong');
-    expect(description.text()).toBe('fox');
+    it('truncates long texts', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ texts: [
+        '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
+      ] });
+
+      const body =  wrapper.find('[data-qa="content card"] .card-body');
+      expect(body.text().length).toBe(256);
+      expect(body.text().endsWith('…')).toBe(true);
+    });
+
+    it('optionally limits number of values per text to display', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ texts: [['A', 'B', 'C', 'D', 'E', 'F']], limitValuesWithinEachText: 3 });
+
+      const body =  wrapper.find('[data-qa="content card"] .card-body');
+      expect(body.text()).toBe('A; B; C; …');
+    });
+
+    it('has a creator and institution', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ texts: ['Edvard Munch', 'Munchmuseet (The Munch Museum)'] });
+
+      const body =  wrapper.find('[data-qa="content card"] .card-body');
+      expect(body.text()).toContain('Edvard Munch');
+      expect(body.text()).toContain('Munchmuseet');
+    });
+  });
+
+  describe('card link', () => {
+    it('may have a link', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ url: 'https://example.org' });
+
+      const link =  wrapper.find('[data-qa="content card"] .card-link');
+      expect(link.attributes().href).toBe('https://example.org');
+    });
+  });
+
+  describe('card image', () => {
+    it('may have an image', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ imageUrl: 'https://example.org' });
+
+      const image =  wrapper.find('[data-qa="content card"] .card-img img');
+      expect(image).toBeDefined();
+      expect(image.attributes('src')).toBe('https://example.org');
+    });
+
+    it('may have an optimised image', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ imageUrl: '//images.ctfassets.net/example/example.jpg', imageContentType: 'image/jpeg' });
+
+      expect(wrapper.vm.optimisedImageUrl).toContain('fm=jpg&fl=progressive&q=50');
+    });
+
+    it('may have an optimised image with max width', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({
+        imageUrl: '//images.ctfassets.net/example/example.jpg',
+        imageContentType: 'image/jpeg',
+        imageOptimisationOptions: { width: 510 }
+      });
+
+      expect(wrapper.vm.optimisedImageUrl).toContain('fm=jpg&fl=progressive&q=50&w=510');
+    });
+
+    it('may have no image and is of variant mini', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ imageUrl: null, variant: 'mini' });
+
+      const image =  wrapper.find('[data-qa="content card"] .card-img');
+      expect(image.exists()).toBe(false);
+    });
+
+    it('may have an image and is of variant mini', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ imageUrl: 'https://example.org', variant: 'mini' });
+
+      const image =  wrapper.find('[data-qa="content card"] .card-img');
+      expect(image.exists()).toBe(true);
+    });
+
+    it('removes the image if the image media is not found', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ imageUrl: 'https://example.org', variant: 'mini' });
+
+      let image = wrapper.find('[data-qa="content card"] .card-img img');
+      expect(image.exists()).toBe(true);
+      await image.trigger('error');
+
+      image = wrapper.find('[data-qa="content card"] .card-img img');
+      expect(image.exists()).toBe(false);
+    });
+  });
+
+  describe('highlighted search term', () => {
+    it('highlights the search term if found', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({
+        hitsText: {
+          prefix: 'The quick brown ',
+          exact: 'fox',
+          suffix: ' jumps over the lazy dog'
+        }
+      });
+
+      const body =  wrapper.find('[data-qa="highlighted search term"] strong');
+      expect(body.text()).toBe('fox');
+    });
   });
 });
