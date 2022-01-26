9c4d3b55821f6cd05155c1ccfb034c9a939b9f0d
Richard Doe <richard.doe@rwdit.net>
2022-01-26 09:36:42 +0000

    refactor: show selected filters without fields

--- src/components/search/SideFacetDropdown.vue
+++ src/components/search/SideFacetDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!fetched || fields.length > 0">
+  <div v-if="!fetched || fields.length > 0 || selectedFilters[this.name].length > 0">
     <label
       class="facet-label"
     >{{ facetName }}</label>
@@ -13,6 +13,7 @@
       :variant="dropdownVariant"
       class="facet-dropdown side-facet"
       :data-type="type"
+      :disabled="fetched && fields.length === 0"
       data-qa="search facet"
       block
       @hidden="hiddenDropdown"
