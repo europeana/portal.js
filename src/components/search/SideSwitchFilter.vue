4275370e9ad76b468c8f3f02618bbd05f8f1fb44
Richard Doe <richard.doe@rwdit.net>
2022-02-04 10:24:36 +0000

    refactor: add default value prop
    Side switch filter is only resettable if its value is not the default.

--- src/components/search/SideSwitchFilter.vue
+++ src/components/search/SideSwitchFilter.vue
@@ -75,6 +75,16 @@
       },
 
       /**
+       * Value to consider default for the switch
+       *
+       * If the switch is set to its default value, it is not resettable.
+       */
+      defaultValue: {
+        type: String,
+        default: 'unchecked'
+      },
+
+      /**
        * Text for the switch label
        */
       label: {
@@ -111,7 +121,7 @@
       init() {
         this.$store.dispatch('search/setResettableFilter', {
           name: this.name,
-          selected: this.localValue
+          selected: (this.localValue === this.defaultValue) ? null : this.localValue
         });
       }
     }
