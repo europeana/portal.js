edb558dda1b5c831628d40e005755b89eb794cd0
Richard Doe <richard.doe@rwdit.net>
2022-02-11 11:07:32 +0000

    feat: permissive CORS on API server middleware

--- src/server-middleware/api/index.js
+++ src/server-middleware/api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import defu  from 'defu';
 import apm from 'elastic-apm-node';
 import logging from '../logging.js';
 
 const app = express();
 app.disable('x-powered-by'); // Security: do not disclose technology fingerprints
+app.use(cors());
 app.use(express.json());
 app.use(logging);
 
