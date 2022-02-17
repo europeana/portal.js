e12a1f6f67632aa63c5060eb511eb6b9876dd15e
Leonie Peters <leoniepeters@gmail.com>
2022-02-17 17:05:08 +0100

    Add unit test for uncovered cases

--- tests/unit/components/browse/InfoCardSection.spec.js
+++ tests/unit/components/browse/InfoCardSection.spec.js
@@ -7,63 +7,78 @@ localVue.use(BootstrapVue);
 
 const factory = () => shallowMount(InfoCardSection, {
   localVue
-  // mocks: {
-  //   $t: () => {}
-  // }
 });
 
-const dummySection = {
+const hasPartCollection = {
+  items: [
+    {
+      url: { name: 'dummyURLone' },
+      info: '100,000',
+      label: 'IMAGES',
+      image: 'ic-image'
+    },
+    {
+      url: { name: 'dummyURLtwo' },
+      info: '50,000',
+      label: 'VIDEOS',
+      image: 'ic-video'
+    },
+    {
+      url: { name: 'dummyURLthree' },
+      info: '2,000',
+      label: '3D',
+      image: 'ic-3d'
+    }
+  ]
+};
+
+const typeCountsSection = {
   type: 'items/type-counts',
-  hasPartCollection: {
-    items: [
-      {
-        url: { name: 'dummyURLone' },
-        info: '100,000',
-        label: 'IMAGES',
-        image: 'ic-image'
-      },
-      {
-        url: { name: 'dummyURLtwo' },
-        info: '50,000',
-        label: 'VIDEOS',
-        image: 'ic-video'
-      },
-      {
-        url: { name: 'dummyURLthree' },
-        info: '2,000',
-        label: '3D',
-        image: 'ic-3d'
-      }
-    ]
-  }
+  hasPartCollection
+};
+
+const otherTypeSection = {
+  hasPartCollection
 };
 
 describe('components/browse/InfoCardSection', () => {
   it('shows a section with cards', async() => {
     const wrapper = factory();
-    await wrapper.setProps({ section: dummySection });
+    await wrapper.setProps({ section: typeCountsSection });
 
     expect(wrapper.findAll('[data-qa="section group"]').length).toBe(1);
   });
   it('shows a card for each element', async() => {
     const wrapper = factory();
-    await wrapper.setProps({ section: dummySection });
+    await wrapper.setProps({ section: typeCountsSection });
     expect(wrapper.findAll('infocard-stub').length).toBe(3);
   });
   describe('cardGroupClass', () => {
     it('is three columns for mediaType counts', async() => {
       const wrapper = factory();
-      await wrapper.setProps({ section: dummySection });
+      await wrapper.setProps({ section: typeCountsSection });
 
       expect(wrapper.vm.cardGroupClass).toBe('card-deck-3-cols');
+    });
+    it('is four columns for other types', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ section: otherTypeSection });
+
+      expect(wrapper.vm.cardGroupClass).toBe('card-deck-4-cols');
     });
   });
   describe('cardVariant', () => {
     it('is dark for mediaType counts', async() => {
       const wrapper = factory();
-      await wrapper.setProps({ section: dummySection });
+      await wrapper.setProps({ section: typeCountsSection });
 
       expect(wrapper.vm.cardVariant).toBe('dark');
+    });
+    it('is default for other types', async() => {
+      const wrapper = factory();
+      await wrapper.setProps({ section: otherTypeSection });
+
+      expect(wrapper.vm.cardVariant).toBe('default');
     });
   });
 });
