70bee64b2a68e5d7fc09cbb3d113be602524e87f
Richard Doe <richard.doe@rwdit.net>
2022-02-21 08:53:31 +0000

    refactor: add TODO note re use of prop

--- src/components/search/FacetFieldLabel.vue
+++ src/components/search/FacetFieldLabel.vue
@@ -86,6 +86,8 @@
         return themes.find(theme => theme.qf === this.collection);
       },
 
+      // TODO: move the label replacement pattern to a prop instead. and do
+      //       the same for media type?
       themeSpecificFieldLabelPattern() {
         return (this.theme?.facets || []).find((facet) => facet.field === this.facetName)?.label;
       }
