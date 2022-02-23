bc4768ce9998a7d9ed9468bbb42c904ea74b27b0
Richard Doe <richard.doe@rwdit.net>
2022-02-23 10:02:02 +0000

    chore: read version from pkg-versions.js
    As npm is not present in the distroless image.

--- bin/sonar-project-version.js
+++ bin/sonar-project-version.js
@@ -1,12 +1,14 @@
 import propertiesReader from 'properties-reader';
 
+import versions from '../pkg-versions.js';
+
 const versionSonarProjectProperties = async() => {
   const sonarProjectPropertiesFilePath = new URL('../sonar-project.properties', import.meta.url);
   const sonarProjectProperties = propertiesReader(sonarProjectPropertiesFilePath,
     'utf-8',
     { writer: { saveSections: false } }
   );
-  sonarProjectProperties.set('sonar.projectVersion', process.env.npm_package_version);
+  sonarProjectProperties.set('sonar.projectVersion', versions['@europeana/portal']);
   await sonarProjectProperties.save(sonarProjectPropertiesFilePath);
 };
 
