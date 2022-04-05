c22ae9584f5c7a899091666d5442a967d4b27e5a
Richard Doe <richard.doe@rwdit.net>
2022-04-05 14:47:17 +0100

    feat: optional expiration date for feature notification (#1484)
    * feat: add version & expiration options
    
    * refactor: read expiration date from env
    
    * test: spec featureNotificationExpiration
    
    * test: spec use of new feature expiration

--- src/features/index.js
+++ src/features/index.js
@@ -3,6 +3,11 @@ import featureToggles from './toggles.js';
 
 export const featureIsEnabled = (value) => Boolean(Number(value));
 
+export const featureNotificationExpiration = (value) => {
+  const date = new Date(value);
+  return date.toString() === 'Invalid Date' ? null : date;
+};
+
 export default () => featureToggles
   .reduce((memo, featureToggle) => {
     const envKey = `ENABLE_${decamelize(featureToggle.name).toUpperCase()}`;
