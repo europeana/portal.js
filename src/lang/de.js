f4fc74687802bfd431bf25ccf105592fbc3a4d34
Lutz Biedinger <lutz.biedinger@gmail.com>
2022-04-05 13:37:37 +0200

    update German translations.

--- src/lang/de.js
+++ src/lang/de.js
@@ -486,7 +486,7 @@ export default {
         "restricted": "Ja, mit Bedingungen",
         "uncategorized": "Nicht kategorisiert"
       },
-      "select": "Wählen Sie aus, ob die Artikel wiederverwendbar sind"
+      "select": "Wählen Sie aus, ob die Objekte wiederverwendbar sind"
     },
     "TYPE": {
       "name": "Art des Mediums",
@@ -545,7 +545,7 @@ export default {
         "3": "Hohe Qualität & wiederverwendbar mit Bedingungen",
         "4": "Hohe Qualität & frei wiederverwendbar"
       },
-      "select": "Artikelqualitäten auswählen"
+      "select": "Objektqualitäten auswählen"
     },
     "moreOptions": "Suchen, um {0} mehr {1} zu finden.",
     "proxy_dc_format": {
