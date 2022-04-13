1002e4c15a7d1efd51af2ecfcd1f7ee8a7020cf0
Richard Doe <richard.doe@rwdit.net>
2022-04-13 10:13:47 +0100

    fix: set collection label in fetch (#1504)
    * fix: set collection label in fetch
    
    * test: spec storing collection label
    
    * fix: observe changes to pageTitle
    
    * fix: set collection label after all other content
    
    * fix: hide collection search option if no label

--- tests/unit/pages/collections/type/_.spec.js
+++ tests/unit/pages/collections/type/_.spec.js
@@ -137,6 +137,14 @@ describe('pages/collections/type/_', () => {
       expect(wrapper.vm.$apis.entity.get.calledWith(topicEntity.type, topicEntity.pathMatch)).toBe(true);
     });
 
+    it('stores the collection label in search store', async() => {
+      const wrapper = factory(topicEntity);
+
+      await wrapper.vm.fetch();
+
+      expect(wrapper.vm.$store.commit.calledWith('search/setCollectionLabel', 'Topic')).toBe(true);
+    });
+
     describe('collection page', () => {
       const requestMade = async(curatedEntities) => {
         const wrapper = factory(topicEntity);
