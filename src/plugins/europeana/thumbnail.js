38a372d26534841328b791f24c86597c2ed73928
Richard Doe <richard.doe@rwdit.net>
2022-04-26 09:58:18 +0100

    feat: support Thumbnail API v3

--- src/plugins/europeana/thumbnail.js
+++ src/plugins/europeana/thumbnail.js
@@ -1,11 +1,14 @@
 /**
  * @file Functions for working with the Europeana Thumbnail API
- * @see https://pro.europeana.eu/resources/apis/record#thumbnails
+ * @see https://pro.europeana.eu/page/record#thumbnails
  */
+
+import md5 from 'md5';
 
 import { apiConfig } from './utils.js';
 import { BASE_URL as EUROPEANA_DATA_URL } from './data.js';
 
+// TODO: switch to v3 when v2 support is deprecated
 export const BASE_URL = 'https://api.europeana.eu/thumbnail/v2';
 
 export const thumbnailTypeForMimeType = (mimeType) => {
@@ -34,44 +37,82 @@ export const thumbnailTypeForMimeType = (mimeType) => {
 
 export default (context = {}) => {
   const config = apiConfig(context.$config, 'thumbnail');
+  const baseUrl = config.url || BASE_URL;
 
-  const media = (uri, { size, type } = {}) => {
-    const apiUrl = new URL(`${config.url || BASE_URL}/url.json`);
-
-    apiUrl.searchParams.set('uri', uri);
-
+  // TODO: remove `type` when v2 support is deprecated
+  const media = (uri, { hash, size, type } = {}) => {
     if (!size) {
       size = 200;
     }
-    apiUrl.searchParams.set('size', (typeof size === 'number' ? `w${size}` : size));
 
-    if (type) {
-      apiUrl.searchParams.set('type', type);
-    }
+    // TODO: remove when v2 support is deprecated
+    const v2 = () => {
+      if (!uri) {
+        return null;
+      }
 
-    return apiUrl.toString();
+      const apiUrl = new URL(`${baseUrl}/url.json`);
+
+      apiUrl.searchParams.set('uri', uri);
+
+      apiUrl.searchParams.set('size', (`${size}`.startsWith('w') ? size : `w${size}`));
+
+      if (type) {
+        apiUrl.searchParams.set('type', type);
+      }
+
+      return apiUrl.toString();
+    };
+
+    const v3 = () => {
+      if (!hash && uri) {
+        hash = md5(uri);
+      }
+      return `${baseUrl}/${size}/${hash}`;
+    };
+
+    return baseUrl.endsWith('/v3') ? v3() : v2();
   };
 
+  // TODO: remove when v2 support is deprecated
   const generic = (itemId, { size, type } = {}) => {
     const uri = `${EUROPEANA_DATA_URL}/item${itemId}`;
     return media(uri, { size, type });
   };
 
-  const edmPreview = (url, { size, type } = {}) => {
-    if (!url) {
+  // TODO: remove `type` when v2 support is deprecated
+  const edmPreview = (thumbnailApiUrl, { size, type } = {}) => {
+    if (!thumbnailApiUrl) {
       return null;
     }
 
-    const edmPreviewUrl = new URL(url);
+    const edmPreviewUrl = new URL(thumbnailApiUrl);
 
-    if (!size) {
-      size = edmPreviewUrl.searchParams.get('size');
-    }
-    if (!type) {
-      type = edmPreviewUrl.searchParams.get('type');
-    }
+    // TODO: remove when v2 support is deprecated
+    const v2 = () => {
+      if (!size) {
+        const sizeParam = edmPreviewUrl.searchParams.get('size');
+        if (sizeParam) {
+          size = sizeParam.replace('w', '');
+        }
+      }
+      if (!type) {
+        type = edmPreviewUrl.searchParams.get('type');
+      }
 
-    return media(edmPreviewUrl.searchParams.get('uri'), { size, type });
+      return media(edmPreviewUrl.searchParams.get('uri'), { size, type });
+    };
+
+    const v3 = () => {
+      const sizeAndHash = edmPreviewUrl.pathname.slice(edmPreviewUrl.pathname.indexOf('/v3/') + 4).split('/');
+      if (!size) {
+        size = sizeAndHash[0];
+      }
+      const hash = sizeAndHash[1];
+      return media(null, { hash, size });
+    };
+
+    return (edmPreviewUrl.pathname.includes('/v3/') ? v3() : v2());
   };
 
   return {
