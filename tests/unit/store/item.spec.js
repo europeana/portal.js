e8ef2f90cc3ed3a3c4afde28c34ba5dc3a04b27f
Richard Doe <richard.doe@rwdit.net>
2022-04-28 13:09:38 +0100

    test: remove redundant store specs

--- tests/unit/store/item.spec.js
+++ tests/unit/store/item.spec.js
@@ -22,56 +22,8 @@ const annotations = [
   }
 ];
 
-const featuredSetPins = {
-  'entityIdWithPin': ['/123/abc', '456/def'],
-  'entityIdWithoutPin': []
-};
-const featuredSetIds = {
-  'entityIdOne': '123',
-  'entityIdTwo': '124'
-};
-
 describe('store/item', () => {
   describe('getters', () => {
-    describe('pinnedTo()', () => {
-      describe('when there are featuredSetPins', () => {
-        const state = {
-          id: '/123/abc',
-          featuredSetPins
-        };
-        describe('when asking for an entity that has the pin', () => {
-          it('returns true', () => {
-            expect(store.getters.pinnedTo(state)('entityIdWithPin')).toBe(true);
-          });
-        });
-
-        describe('when asking for an entity that DOES not have the pin', () => {
-          it('returns false', () => {
-            expect(store.getters.pinnedTo(state)('entityIdWithoutPin')).toBe(false);
-          });
-        });
-      });
-    });
-
-    describe('id()', () => {
-      describe('when there is an item id', () => {
-        const state = {
-          id: '/123/abc'
-        };
-        it('returns the id', () => {
-          expect(store.getters.id(state)).toBe('/123/abc');
-        });
-      });
-      describe('when there is NO item id', () => {
-        const state = {
-          id: null
-        };
-        it('returns the id', () => {
-          expect(store.getters.id(state)).toBeNull();
-        });
-      });
-    });
-
     describe('annotationsByMotivation()', () => {
       describe('when there are annotations', () => {
         const state = {
@@ -102,25 +54,14 @@ describe('store/item', () => {
 
         await store.actions.reset({ commit });
 
-        expect(commit.calledWith('setId', { value: null })).toBe(true);
         expect(commit.calledWith('setAnnotations', [])).toBe(true);
         expect(commit.calledWith('setRelatedEntities', [])).toBe(true);
-        expect(commit.calledWith('setAllRelatedEntities', [])).toBe(true);
-        expect(commit.calledWith('setFeaturedSetIds', {})).toBe(true);
-        expect(commit.calledWith('setFeaturedSetPins', {})).toBe(true);
         expect(commit.calledWith('setSimilarItems', [])).toBe(true);
       });
     });
   });
 
   describe('mutations', () => {
-    describe('setId()', () => {
-      it('sets the id state', () => {
-        const state = { id: null };
-        store.mutations.setId(state, 'abc/123');
-        expect(state.id).toEqual('abc/123');
-      });
-    });
     describe('setAnnotations()', () => {
       it('sets the annotations state', () => {
         const state = { annotations: [] };
@@ -134,49 +75,6 @@ describe('store/item', () => {
         const state = { relatedEntities: [] };
         store.mutations.setRelatedEntities(state, relatedEntities);
         expect(state.relatedEntities).toEqual(relatedEntities);
-      });
-    });
-    describe('setAllRelatedEntities()', () => {
-      it('sets the allRelatedEntities state', () => {
-        const allRelatedEntities = ['all related'];
-        const state = { allRelatedEntities: [] };
-        store.mutations.setAllRelatedEntities(state, allRelatedEntities);
-        expect(state.allRelatedEntities).toEqual(allRelatedEntities);
-      });
-    });
-    describe('setFeaturedSetIds()', () => {
-      it('sets the featuredSetIds state', () => {
-        const state = { featuredSetIds: {} };
-        store.mutations.setFeaturedSetIds(state, featuredSetIds);
-        expect(state.featuredSetIds).toEqual(featuredSetIds);
-      });
-    });
-    describe('addToFeaturedSetIds()', () => {
-      it('sets the featuredset state', async() => {
-        const state = { featuredSetIds: {} };
-        await store.mutations.addToFeaturedSetIds(state, { entityUri: 'entityIdThree', setId: '125' });
-        expect(state.featuredSetIds['entityIdThree']).toEqual('125');
-      });
-    });
-    describe('setFeaturedSetPins()', () => {
-      it('sets the featuredSetPins state', () => {
-        const state = { featuredSetPins: {} };
-        store.mutations.setFeaturedSetPins(state, featuredSetPins);
-        expect(state.featuredSetPins).toEqual(featuredSetPins);
-      });
-    });
-    describe('addToFeaturedSetPins()', () => {
-      it('sets the featuredset state', async() => {
-        const state = { featuredSetPins: {} };
-        await store.mutations.addToFeaturedSetPins(state, { entityUri: 'entityIdThree', pins: [] });
-        expect(state.featuredSetPins['entityIdThree']).toEqual([]);
-      });
-    });
-    describe('addPinToFeaturedSetPins()', () => {
-      it('sets the featuredset state', async() => {
-        const state = { featuredSetPins };
-        await store.mutations.addPinToFeaturedSetPins(state, { entityUri: 'entityIdWithoutPin', pin: '/123/abc' });
-        expect(state.featuredSetPins['entityIdWithoutPin'].includes('/123/abc')).toEqual(true);
       });
     });
     describe('setSimilarItems()', () => {
