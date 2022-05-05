2d04140c54ba447390c998996d91bd2b12dbbec4
Richard Doe <richard.doe@rwdit.net>
2022-05-05 12:12:01 +0100

    test: spec entryUrl and entry sort/limit

--- tests/unit/components/related/RelatedEditorial.spec.js
+++ tests/unit/components/related/RelatedEditorial.spec.js
@@ -32,7 +32,8 @@ const factory = ({ propsData, mocks } = {})  => shallowMountNuxt(RelatedEditoria
     },
     $t: (key) => key,
     ...mocks
-  }
+  },
+  stubs: ['b-card-group']
 });
 
 describe('components/related/RelatedEditorial', () => {
@@ -54,6 +55,39 @@ describe('components/related/RelatedEditorial', () => {
           limit: 4
         })).toBe(true);
       });
+
+      it('stores 4 most recent entries', async() => {
+        const primaryImageOfPage = { image: {} };
+        const contentfulQueryResponse = {
+          data: {
+            data: {
+              blogPostingCollection: {
+                items: [
+                  { identifier: 'blog-1', datePublished: '2022-04-30T00:00:00.000+00:00', primaryImageOfPage },
+                  { identifier: 'blog-2', datePublished: '2022-04-20T00:00:00.000+00:00', primaryImageOfPage },
+                  { identifier: 'blog-3', datePublished: '2022-04-10T00:00:00.000+00:00', primaryImageOfPage }
+                ]
+              },
+              exhibitionPageCollection: {
+                items: [
+                  { identifier: 'exhibition-1', datePublished: '2022-04-25T00:00:00.000+00:00', primaryImageOfPage },
+                  { identifier: 'exhibition-2', datePublished: '2022-04-24T00:00:00.000+00:00', primaryImageOfPage },
+                  { identifier: 'exhibition-3', datePublished: '2022-04-05T00:00:00.000+00:00', primaryImageOfPage }
+                ]
+              }
+            }
+          }
+        };
+
+        const wrapper = factory({ propsData: { entityUri } });
+        wrapper.vm.$contentful.query.resolves(contentfulQueryResponse);
+
+        await wrapper.vm.fetch();
+
+        expect(wrapper.vm.related.map(entry => entry.identifier)).toEqual([
+          'blog-1', 'exhibition-1', 'exhibition-2', 'blog-2'
+        ]);
+      });
     });
 
     describe('when no entity URI is supplied', () => {
@@ -65,6 +99,32 @@ describe('components/related/RelatedEditorial', () => {
         await wrapper.vm.fetch();
 
         expect(wrapper.vm.$contentful.query.called).toBe(false);
+      });
+    });
+  });
+
+  describe('methods', () => {
+    describe('entryUrl', () => {
+      it('prefixes BlogPosting entries with /blog', () => {
+        const wrapper = factory();
+
+        const entryUrl = wrapper.vm.entryUrl({
+          '__typename': 'BlogPosting',
+          identifier: 'interesting'
+        });
+
+        expect(entryUrl).toBe('/blog/interesting');
+      });
+
+      it('prefixes ExhibitionPage entries with /exhibitions', () => {
+        const wrapper = factory();
+
+        const entryUrl = wrapper.vm.entryUrl({
+          '__typename': 'ExhibitionPage',
+          identifier: 'educational'
+        });
+
+        expect(entryUrl).toBe('/exhibitions/educational');
       });
     });
   });
