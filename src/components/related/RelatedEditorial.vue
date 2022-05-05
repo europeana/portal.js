c52706dc9009824194975fc6a3ad6dee7e296a28
Richard Doe <richard.doe@rwdit.net>
2022-05-05 11:28:28 +0100

    refactor: simplify template

--- src/components/related/RelatedEditorial.vue
+++ src/components/related/RelatedEditorial.vue
@@ -1,17 +1,36 @@
 <template>
-  <ContentCardSection
-    :section="contentCardSection"
-  />
+  <section
+    v-show="related.length > 0"
+    class="row mb-5"
+  >
+    <div class="col-12">
+      <h2>{{ $t('editorialContentYouMayLike') }}</h2>
+      <b-card-group
+        class="card-deck-4-cols"
+        deck
+      >
+        <ContentCard
+          v-for="(entry, index) in related"
+          :key="index"
+          :title="entry.name"
+          :url="entryUrl(entry)"
+          :image-url="entry.primaryImageOfPage.image.url"
+          :image-content-type="entry.primaryImageOfPage.image.contentType"
+          variant="mini"
+        />
+      </b-card-group>
+    </div>
+  </section>
 </template>
 
 <script>
-  import ContentCardSection from '../browse/ContentCardSection';
+  import ContentCard from '../generic/ContentCard';
 
   export default {
     name: 'RelatedEditorial',
 
     components: {
-      ContentCardSection
+      ContentCard
     },
 
     props: {
@@ -28,6 +47,12 @@
     },
 
     async fetch() {
+      // TODO: in future, this component may instead make other queries, e.g
+      //       to search editorial content by title
+      if (!this.entityUri) {
+        return;
+      }
+
       const variables = {
         entityUri: this.entityUri,
         locale: this.$i18n.isoLocale(),
@@ -43,37 +68,17 @@
         .slice(0, 4);
     },
 
-    computed: {
-      // Adapt response to format expected by ContentCardSection
-      // TODO: refactor when design is implemented (EC-5798)
-      contentCardHasPartCollection() {
-        return this.related.map(entry => {
-          let urlPrefix;
-          if (entry['__typename'] === 'BlogPosting') {
-            urlPrefix = '/blog';
-          } else if (entry['__typename'] === 'exhibitionPage') {
-            urlPrefix = '/exhibitions';
-          }
-          const url = `${urlPrefix}/${entry.identifier}`;
+    methods: {
+      entryUrl(entry) {
+        let urlPrefix;
 
-          return {
-            '__typename': entry['__typename'],
-            '__variant': 'mini',
-            name: entry.name,
-            image: entry.primaryImageOfPage.image,
-            url
-          };
-        });
-      },
+        if (entry['__typename'] === 'BlogPosting') {
+          urlPrefix = '/blog';
+        } else if (entry['__typename'] === 'ExhibitionPage') {
+          urlPrefix = '/exhibitions';
+        }
 
-      contentCardSection() {
-        return {
-          '__typename': 'CardGroup',
-          hasPartCollection: {
-            items: this.contentCardHasPartCollection
-          },
-          headline: this.$t('editorialContentYouMayLike')
-        };
+        return `${urlPrefix}/${entry.identifier}`;
       }
     }
   };
