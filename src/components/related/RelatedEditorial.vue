d7cac6e7aa47481f85e5bf80d28824dca3ec9bff
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2022-05-10 16:06:25 +0200

    Added styleguide examples

--- src/components/related/RelatedEditorial.vue
+++ src/components/related/RelatedEditorial.vue
@@ -44,12 +44,19 @@
       entityUri: {
         type: String,
         default: null
+      },
+      /**
+       * URI of the entity
+      */
+      relatedEditorial: {
+        type: Array,
+        default: () => []
       }
     },
 
     data() {
       return {
-        related: []
+        related: this.relatedEditorial
       };
     },
 
@@ -92,9 +99,68 @@
 </script>
 
 <docs lang="md">
-  Related editorial content
+  Related editorial content: 4 entries
   ```jsx
   <RelatedEditorial
     entity-uri="http://data.europeana.eu/concept/base/190"
+    :related-editorial="[{'__typename': 'BlogPosting',
+      'name': 'Landscapes from the Soul: testing a longer title and even longer and some more characters',
+      'identifier': 'landscapes-from-the-soul',
+      'primaryImageOfPage': {
+        'image': {
+          'url': 'https://images.ctfassets.net/i01duvb6kq77/6WDayOZJdHkYiiNmQTjtXb/2c0e57e1d7fc2207ebe5c0cba2fc2a49/17.Hu_Chi-Chung_Sans_titre_1969__Huile_sur_sable_sur_toile__80_x_116_5cm.jpg',
+          'contentType': 'image/jpeg'
+        }
+      }
+    },
+    {
+      '__typename': 'BlogPosting',
+      'name': 'Jesuits in China, Part 2',
+      'identifier': 'jesuits-in-china-part-2',
+      'primaryImageOfPage': {
+        'image': {
+          'url': 'https://images.ctfassets.net/i01duvb6kq77/6nhR29jb4UlVIfqX6beO6r/b934723593fe85f2c41864363a60a842/2024909_photography_ProvidedCHO_United_Archives_p_01753921_jpg.jpeg',
+          'contentType': 'image/jpeg'
+        }
+      }
+    },
+    {
+      '__typename': 'BlogPosting',
+      'name': 'Wifredo Lam: disturbing the dreams of the exploiters',
+      'identifier': 'wifredo-lam-disturbing-the-dreams-of-the-exploiters',
+      'primaryImageOfPage': {
+        'image': {
+          'url': 'https://images.ctfassets.net/i01duvb6kq77/3SXe6aCZjJdYHQVj0vYwKM/e2642c8ceaa49bc10b12e31dbf6301ff/feature_wifredolam.jpg',
+          'contentType': 'image/jpeg'
+        }
+      }
+    },
+    {
+      '__typename': 'BlogPosting',
+      'name': 'Vitalism: art celebrating sport, bodies & nature',
+      'identifier': 'vitalism-art-celebrating-sport-bodies-and-nature',
+      'primaryImageOfPage': {
+        'image': {
+          'url': 'https://images.ctfassets.net/i01duvb6kq77/16tOTH1YCcdO24iQJ4EYzK/e17a53c85ed0751533a9821933372954/feature_vitalism.jpg',
+          'contentType': 'image/jpeg'
+        }
+      }
+    }]"
+  />
+  ```
+  Related editorial content: 1 entry
+  ```jsx
+  <RelatedEditorial
+    entity-uri="http://data.europeana.eu/concept/base/190"
+    :related-editorial="[{'__typename': 'BlogPosting',
+      'name': 'Landscapes from the Soul: testing a longer title and even longer and some more characters',
+      'identifier': 'landscapes-from-the-soul',
+      'primaryImageOfPage': {
+        'image': {
+          'url': 'https://images.ctfassets.net/i01duvb6kq77/6WDayOZJdHkYiiNmQTjtXb/2c0e57e1d7fc2207ebe5c0cba2fc2a49/17.Hu_Chi-Chung_Sans_titre_1969__Huile_sur_sable_sur_toile__80_x_116_5cm.jpg',
+          'contentType': 'image/jpeg'
+        }
+      }
+    }]"
   />
 </docs>
