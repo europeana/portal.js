0ada0bb64b15d5582f43cc03d0498e2ea0504b4e
Richard Doe <richard.doe@rwdit.net>
2022-05-18 15:07:32 +0200

    test: spec get set with minimal items

--- tests/unit/plugins/europeana/set.spec.js
+++ tests/unit/plugins/europeana/set.spec.js
@@ -1,12 +1,12 @@
 import nock from 'nock';
+import sinon from 'sinon';
 
-import set, { BASE_URL } from '@/plugins/europeana/set';
-
-const $config = { europeana: { apis: { set: { key: 'apikey' } } } };
+import plugin, { BASE_URL } from '@/plugins/europeana/set';
+import { BASE_URL as RECORD_BASE_URL } from '@/plugins/europeana/record';
 
 const setId = '1234';
 const itemId = '/123/abc';
-
+const $config = { europeana: { apis: { set: { key: 'apikey' } } } };
 const likesResponse = {
   '@context': 'http://www.europeana.eu/schemas/context/collection.jsonld',
   id: 'http://data.europeana.eu/set/1234',
@@ -53,32 +53,106 @@ const setsResponse = [
   }
 ];
 
-describe('describe./@/plugins/europeana/set', () => {
+describe('@/plugins/europeana/set', () => {
   afterEach(() => {
     nock.cleanAll();
+    sinon.resetHistory();
   });
 
   describe('get()', () => {
+    const setId = '1';
+    const setGetResponse = {
+      id: 'http://data.europeana.eu/set/1',
+      type: 'Collection',
+      items: [
+        'http://data.europeana.eu/item/123/abc',
+        'http://data.europeana.eu/item/123/def'
+      ]
+    };
+    const recordSearchResponse = {
+      items: [
+        { id: '/123/abc', prefLabel: { en: ['ABC'] } }
+      ]
+    };
+
     it('gets the set data', async() => {
-      const setId = '1';
       nock(BASE_URL)
         .get(`/${setId}`)
         .query(true)
-        .reply(200, setsResponse[0]);
+        .reply(200, setGetResponse);
 
-      const response = await set({ $config }).get(setId);
-      expect(response.items).toEqual(['item-1', 'item-2']);
+      const response = await plugin({ $config }).get(setId);
+      expect(response.items).toEqual(['http://data.europeana.eu/item/123/abc', 'http://data.europeana.eu/item/123/def']);
     });
 
     it('includes the axios default params', async() => {
-      const setId = '1';
       nock(BASE_URL)
         .get(`/${setId}`)
         .query(query => query.wskey === 'apikey')
-        .reply(200);
+        .reply(200, setGetResponse);
 
-      await set({ $config }).get(setId);
+      await plugin({ $config }).get(setId);
       expect(nock.isDone()).toBe(true);
+    });
+
+    describe('options', () => {
+      describe('withMinimalItems', () => {
+        const context = {
+          $config,
+          i18n: { locale: 'nl', t: key => key },
+          $apis: { record: { find: sinon.stub().resolves(recordSearchResponse) } }
+        };
+        beforeEach(() => {
+          nock(BASE_URL)
+            .get(`/${setId}`)
+            .query(true)
+            .reply(200, setGetResponse);
+        });
+
+        describe('when set to `true`', () => {
+          const options = { withMinimalItems: true };
+
+          it('requests 100 minimal profile items from the Record API', async() => {
+            await plugin(context).get(setId, {}, options);
+
+            expect(context.$apis.record.find.calledWith(setGetResponse.items, {
+              profile: 'minimal',
+              rows: 100
+            })).toBe(true);
+          });
+
+          it('stores the found items on the set', async() => {
+            const set = await plugin(context).get(setId, {}, options);
+
+            expect(set.items[0]).toEqual(recordSearchResponse.items[0]);
+          });
+
+          it('stores a dcTitleLangAware warning for items not found', async() => {
+            const set = await plugin(context).get(setId, {}, options);
+
+            expect(set.items[1]).toEqual({
+              id: '/123/def',
+              dcTitleLangAware: { nl: ['record.status.unpublished'] }
+            });
+          });
+        });
+
+        describe('when set to `false` (by default)', () => {
+          const options = {};
+
+          it('does not request items from the Record API', async() => {
+            await plugin(context).get(setId, {}, options);
+
+            expect(context.$apis.record.find.called).toBe(false);
+          });
+
+          it('leaves the item URIs on the set', async() => {
+            const set = await plugin(context).get(setId, {}, options);
+
+            expect(set.items).toEqual(setGetResponse.items);
+          });
+        });
+      });
     });
   });
 
@@ -89,7 +163,7 @@ describe('describe./@/plugins/europeana/set', () => {
         .query(query => query.query === 'creator:auth-user-sub type:BookmarkFolder')
         .reply(200, searchResponse);
 
-      const response = await set({ $config }).getLikes('auth-user-sub');
+      const response = await plugin({ $config }).getLikes('auth-user-sub');
       expect(response).toBe('http://data.europeana.eu/set/163');
     });
   });
@@ -101,7 +175,7 @@ describe('describe./@/plugins/europeana/set', () => {
         .query(true)
         .reply(200, likesResponse);
 
-      const response = await set({ $config }).createLikes();
+      const response = await plugin({ $config }).createLikes();
       expect(response.id).toBe('http://data.europeana.eu/set/1234');
     });
   });
@@ -112,7 +186,7 @@ describe('describe./@/plugins/europeana/set', () => {
         .put(`/${setId}${itemId}`)
         .query(true)
         .reply(200, likesResponse);
-      const response =  await set({ $config }).modifyItems('add', setId, itemId);
+      const response =  await plugin({ $config }).modifyItems('add', setId, itemId);
       expect(response.id).toBe('http://data.europeana.eu/set/1234');
     });
   });
@@ -124,7 +198,7 @@ describe('describe./@/plugins/europeana/set', () => {
         .query(true)
         .reply(204);
 
-      await set({ $config }).deleteSet(setId);
+      await plugin({ $config }).deleteSet(setId);
       expect(nock.isDone()).toBe(true);
     });
   });
