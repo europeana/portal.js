337debb8230a796315b8cba2736a0533a7e01eb6
Richard Doe <richard.doe@rwdit.net>
2022-06-22 08:49:15 +0100

    refactor: item page fetch hook (#1569)
    * refactor: item page fetch hook
    
    * chore: lint
    
    * refactor: move code into fetch hook
    
    * test: spec ItemRecommendedItems fetch
    
    * test: spec handling of null metadata
    
    * refactor: LangMap util never returns falsy
    
    * refactor: rename to ItemRecommendations
    
    * style: remove unexpected negated condition
    
    * test: spec item page fetch and annotations computations
    
    * Unit test conditionally rendered components and dynamic imports
    
    * Remove unused relatedEntities
    
    * Remove unneeded test
    
    * Mock recommended items to actually render the template in unit test
    
    * test: use $fetchState
    
    * chore: tweak size limits
    
    Co-authored-by: Leonie Peters <leoniepeters@gmail.com>

---
+++ tests/unit/components/item/ItemRecommendations.spec.js
@@ -0,0 +1,68 @@
+import { createLocalVue } from '@vue/test-utils';
+import { shallowMountNuxt } from '../../utils';
+import ItemRecommendations from '@/components/item/ItemRecommendations.vue';
+
+import sinon from 'sinon';
+const localVue = createLocalVue();
+
+const recommendedItems = [{ id: 'item001' }];
+
+const factory = ({ propsData = {}, mocks = {} } = {}) => shallowMountNuxt(ItemRecommendations, {
+  localVue,
+  propsData,
+  mocks: {
+    $apis: {
+      recommendation: {
+        recommend: sinon.stub().resolves({ items: recommendedItems })
+      },
+      record: {
+        search: sinon.stub().resolves({ items: recommendedItems })
+      }
+    },
+    $auth: {},
+    $fetchState: {},
+    $i18n: { locale: 'en' },
+    $t: key => key,
+    ...mocks
+  },
+  stubs: ['b-row', 'b-col']
+});
+
+describe('components/item/ItemRecommendations', () => {
+  describe('fetch', () => {
+    describe('when user is logged in', () => {
+      const mocks = { $auth: { loggedIn: true } };
+      it('queries the Recommendation API by the item ID', async() => {
+        const propsData = { identifier: '/123/abc' };
+        const wrapper = factory({ propsData, mocks });
+
+        await wrapper.vm.fetch();
+
+        expect(wrapper.vm.$apis.recommendation.recommend.calledWith('record', propsData.identifier)).toBe(true);
+      });
+    });
+
+    describe('when user is not logged in', () => {
+      const mocks = { $auth: { loggedIn: false } };
+      it('queries the Record API by the metadata', async() => {
+        const propsData = {
+          identifier: '/123/abc',
+          dcSubject: { en: ['whale'] },
+          dcType: { def: ['image'] },
+          dcCreator: null,
+          edmDataProvider: { def: ['Europeana Foundation'] }
+        };
+        const wrapper = factory({ propsData, mocks });
+
+        await wrapper.vm.fetch();
+
+        expect(wrapper.vm.$apis.record.search.calledWith({
+          query: '(what:("whale" OR "image")^0.8 OR DATA_PROVIDER:("Europeana Foundation")^0.2) NOT europeana_id:"/123/abc"',
+          rows: 4,
+          profile: 'minimal',
+          facet: ''
+        })).toBe(true);
+      });
+    });
+  });
+});
