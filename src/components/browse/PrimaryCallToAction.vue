3cfb6aa27d37da6e2f66792b4019b6e203ebe443
Leonie Peters <leoniepeters@gmail.com>
2022-06-27 17:54:27 +0200

    Add Primary call to action to browse page

--- src/components/browse/PrimaryCallToAction.vue
+++ src/components/browse/PrimaryCallToAction.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="primary-cta">
+  <div class="primary-cta text-center">
     <RichText
       :text="text"
       :rich-text-is-card="false"
-      class="primary-cta-rich-text"
+      class="primary-cta-rich-text text-left"
     />
     <SmartLink
       :destination="link.url"
@@ -43,18 +43,28 @@
   @import '@/assets/scss/variables';
 
   .primary-cta {
-    background-color: $bodygrey;
+    background-color: $white;
     padding: 1.5rem calc((50vw - 50%) / 2);
     margin: 0 calc((-50vw + 50%) / 2) 2rem;
-    max-width: calc(100% + 100px);
 
     @media (min-width: $bp-large) {
       padding: 1.5rem 50px;
-      margin: 0 -50px 2rem;
+      margin: 0 auto 2rem;
+      max-width: calc(66.667% + 100px);
     }
 
     .btn-primary.btn-cta {
       margin-bottom: 0;
+    }
+  }
+
+  .text-page .primary-cta {
+    background-color: $bodygrey;
+    max-width: calc(100% + 100px);
+
+    @media (min-width: $bp-large) {
+      margin: 0 -50px 2rem;
+      max-width: calc(100% + 100px);
     }
   }
 </style>
