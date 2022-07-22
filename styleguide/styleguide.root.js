dec501bfdd09862b139fdd6e947eb95350b10528
Leonie Peters <leoniepeters@gmail.com>
2022-07-22 16:45:27 +0200

    Clean up styleguide plugin and store mocks

--- styleguide/styleguide.root.js
+++ styleguide/styleguide.root.js
@@ -1,70 +1,89 @@
 import Vue from 'vue';
-// import Vuex from 'vuex';
+import Vuex from 'vuex';
 import BootstrapVue from 'bootstrap-vue';
 import { VueMasonryPlugin } from 'vue-masonry';
 // import VueRouter from 'vue-router';
 import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
-// Vue.use(Vuex);
+Vue.use(Vuex);
 Vue.use(BootstrapVue);
 Vue.use(VueMasonryPlugin);
 // Vue.use(VueRouter);
+
 Vue.directive('visible-on-scroll', () => {});
 
-Vue.prototype.$path = () => {
-  return '/';
-};
-Vue.prototype.$route = {
-  query: {}
-},
-Vue.prototype.$link = {
-  to: route => route,
-  href: () => null
-};
-Vue.prototype.$store = {
-  state: {
-    auth: { loggedIn: false },
-    entity: { pinned: [] },
-    search: {
-      liveQueries: [],
-      showSearchBar: false,
-      allThemes: []
+Object.assign(Vue.prototype, {
+  $apis:
+  {
+    entity: {
+      suggest(query) {
+        return Promise.resolve([
+          { prefLabel: { en: `suggestion for ${query}` } }
+        ]);
+      },
+      imageUrl: (entity) => entity.image?.url
+    }
+  },
+  $auth: { $storage: { setUniversal: () => {} }, loginWith: () => {} },
+  $fetchState: {},
+  $link: {
+    to: route => route,
+    href: () => null
+  },
+  $path: () => {
+    return '/';
+  },
+  $route: { query: {} }
+});
+
+const store = new Vuex.Store({
+  modules: {
+    auth: {
+      state: { loggedIn: false }
     },
-    set: { liked: [] }
-  },
-  getters: {
-    'entity/isPinned': () => {},
-    'search/activeView': () => {},
-    'search/formatFacetFieldLabel': (name, value) => value,
-    'set/isLiked': () => {}
-  },
-  mutations: {
-    'search/setShowSearchBar': (state, value) => state.search.showSearchBar = value
-  },
-  commit: () => {},
-  dispatch: () => {}
-};
-Vue.prototype.$auth = { $storage: { setUniversal: () => {} }, loginWith: () => {} };
-Vue.prototype.$fetchState = {};
-Vue.prototype.$apis = {
-  entity: {
-    suggest(query) {
-      return Promise.resolve([
-        { prefLabel: { en: `suggestion for ${query}` } }
-      ]);
+    breadcrumb: {
+      namespaced: true
+    },
+    debug: {
+      namespaced: true,
+      getters: {
+        settings: () => {
+          return {};
+        }
+      }
+    },
+    entity: {
+      namespaced: true,
+      state: { pinned: [] },
+      getters: {
+        isPinned: () => {}
+      }
+    },
+    search: {
+      namespaced: true,
+      state: {
+        liveQueries: [],
+        showSearchBar: false,
+        allThemes: []
+      },
+      getters: {
+        activeView: () => {},
+        formatFacetFieldLabel: (name, value) => value
+      },
+      mutations: {
+        setShowSearchBar: (state, value) => state.search.showSearchBar = value
+      }
+    },
+    set: {
+      namespaced: true,
+      state: { liked: [] },
+      getters: {
+        isLiked: () => {}
+      }
     }
   }
-};
-
-// TODO: properly import store modules needed for components that use them
-// const store = new Vuex.Store({
-//   getters: {
-//     'debug/settings': state => {
-//       return {}
-//     }
-//   }
-// })
+});
 
 import messages from '@/lang/en';
 import '@/plugins/vue-filters';
@@ -80,6 +99,7 @@ export default previewComponent => {
   // https://vuejs.org/v2/guide/render-function.html
   return {
     i18n,
+    store,
     render(createElement) {
       return createElement(previewComponent);
     }
