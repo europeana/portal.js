d39aec36279349c2f3441b88f450653ecc451e97
Richard Doe <richard.doe@rwdit.net>
2022-08-18 12:59:14 +0100

    1.78.0

--- pkg-versions.js
+++ pkg-versions.js
@@ -7,7 +7,7 @@
 // NOTE: This file is auto-updated by ./bin/pkg-versions.js
 
 export default {
-  '@europeana/portal': '1.77.0',
+  '@europeana/portal': '1.78.0',
   '@nuxt/core': '2.15.8',
   'bootstrap': '4.6.1',
   'bootstrap-vue': '2.21.2'
