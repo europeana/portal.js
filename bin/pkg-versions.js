35de503677eca27417eadc2d86ff8e9d98c66986
Richard Doe <richard.doe@rwdit.net>
2022-08-25 15:47:49 +0100

    chore: updates paths

--- bin/pkg-versions.js
+++ bin/pkg-versions.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import versions from '../pkg-versions.js';
 
-const versionsFile = new URL('../pkg-versions.js', import.meta.url);
+const versionsFile = new URL('../packages/portal/pkg-versions.js', import.meta.url);
 
 let versionsSrc = fs.readFileSync(versionsFile, { encoding: 'utf8' });
 
 for (const pkg in versions) {
   let pkgJsonFile;
   if (pkg === '@europeana/portal') {
-    pkgJsonFile = new URL('../package.json', import.meta.url);
+    pkgJsonFile = new URL('../packages/portal/package.json', import.meta.url);
   } else {
     pkgJsonFile = new URL(`../node_modules/${pkg}/package.json`, import.meta.url);
   }
