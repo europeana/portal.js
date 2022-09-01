630bf4f6cd0eaf710b83765177bcf8b2e9ccbdd2
Richard Doe <richard.doe@rwdit.net>
2022-09-01 14:02:26 +0100

    chore: check for env var

--- src/migrations/0082-update-browsePage-staticPage-identifier-help-text.cjs
+++ src/migrations/0082-update-browsePage-staticPage-identifier-help-text.cjs
@@ -1,4 +1,9 @@
 module.exports = function(migration) {
+  if (!process.env.SLUG_VALIDATION_APP_ID) {
+    console.log('No app ID specified in SLUG_VALIDATION_APP_ID; aborting.');
+    process.exit(1);
+  }
+
   // Removes reference to "home" as the URL slug for the homepage.
   const helpText = 'Do not include a leading slash. Should be unique for both browse and static pages';
 
