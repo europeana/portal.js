aebd2b5bd78d7c2bb3f6501f1b2e3e7b4d61909f
Richard Doe <richard.doe@rwdit.net>
2022-09-06 08:24:07 +0100

    test: spec gallery error handling

--- tests/unit/pages/galleries/_set.spec.js
+++ tests/unit/pages/galleries/_set.spec.js
@@ -82,6 +82,11 @@ const factory = (options = {}) => shallowMountNuxt(page, {
       thumbnail: {
         edmPreview: () => ''
       }
+    },
+    $nuxt: {
+      context: {
+        res: {}
+      }
     }
   },
   stubs: ['SetRecommendations']
@@ -97,6 +102,43 @@ describe('SetPage', () => {
       await wrapper.vm.fetch();
 
       expect(storeDispatch.calledWith('set/fetchActive', '123')).toBe(true);
+    });
+
+    describe('on errors', () => {
+      it('set the status code on SSRs', async() => {
+        const wrapper = factory();
+        process.server = true;
+        wrapper.vm.$store.dispatch = sinon.stub().throws(() => new Error('Internal Server Error'));
+
+        let error;
+        try {
+          await wrapper.vm.fetch();
+        } catch (e) {
+          error = e;
+        }
+
+        expect(wrapper.vm.$nuxt.context.res.statusCode).toBe(500);
+        expect(error.message).toBe('Internal Server Error');
+      });
+
+      it('displays an illustrated error message for 403 status', async() => {
+        const unauthorisedError = { statusCode: 403, message: 'Unauthorised' };
+        const wrapper = factory();
+        process.server = true;
+        wrapper.vm.$store.dispatch = sinon.stub().throws(() => unauthorisedError);
+        wrapper.vm.$fetchState.error = unauthorisedError;
+
+        let error;
+        try {
+          await wrapper.vm.$fetch();
+        } catch (e) {
+          error = e;
+        }
+
+        expect(wrapper.vm.$nuxt.context.res.statusCode).toBe(403);
+        expect(error.titlePath).toBe('errorMessage.galleryUnauthorised.title');
+        expect(error.illustrationSrc).toBe('il-gallery-unauthorised.svg');
+      });
     });
   });
 
