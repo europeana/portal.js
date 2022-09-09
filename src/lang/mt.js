2754176c522486a644ab258f67d94ffd4ec2f15b
Richard Doe <richard.doe@rwdit.net>
2022-09-09 09:05:49 +0100

    chore: unescape /

--- src/lang/mt.js
+++ src/lang/mt.js
@@ -6,7 +6,7 @@ export default {
     "likes": "Likes",
     "linkAccount": "Il-Kont Tiegħi",
     "linkLogin": "Illoggja",
-    "linkLoginJoin": "Idħol\/Ingħaqad",
+    "linkLoginJoin": "Idħol/Ingħaqad",
     "linkLogout": "Oħroġ",
     "myProfile": "Il-profil Tiegħi",
     "notifications": {
@@ -201,7 +201,7 @@ export default {
         "title": "Wisq oġġetti pinned"
       },
       "pinned": "L-item ġie mwaħħal. Issa tinsab fin-naħa ta' fuq tal-ewwel paġna ta' din il-ġabra. Jista' jieħu sa 24 siegħa biex jidher għal kulħadd.",
-      "select": "Agħżel entità relatata biex tippinja\/unpin l-oġġett fuq\/minnha.",
+      "select": "Agħżel entità relatata biex tippinja/unpin l-oġġett fuq/minnha.",
       "unpin": "Dan l-oġġett se jieqaf jidher fin-naħa ta' fuq tal-kollezzjoni \" {entity} \". Aħna ninnotifikawk meta din il-bidla tkun viżibbli fuq il-paġna tal-ġbir.",
       "unpinned": "L-oġġett ma tpoġġiex. M’għadux fil-quċċata tal-ewwel paġna ta ’din il-kollezzjoni. Jista 'jieħu sa 24 siegħa biex jisparixxi għal kulħadd."
     }
@@ -507,7 +507,7 @@ export default {
     "MIME_TYPE": {
       "name": "Format tal-fajl | Formati tal-fajl",
       "options": {
-        "text\/plain": "Test bla format"
+        "text/plain": "Test bla format"
       },
       "select": "Agħżel il-formati tal-fajl"
     },
@@ -795,7 +795,7 @@ export default {
     "services": {
       "abTest": {
         "description": "Jgħin biex intejbu l-websajt tagħna billi kultant napplikaw bidliet sperimentali.",
-        "title": "Ittestjar A \/ B"
+        "title": "Ittestjar A / B"
       },
       "auth-strategy": {
         "description": "Jiftakar l-istrateġija ta' awtorizzazzjoni biex tużaha biex tilloggja.",
@@ -827,7 +827,7 @@ export default {
       },
       "searchResultsView": {
         "description": "Tiftakar jekk tippreferi tara r-riżultati tat-tfittxija f'lista jew viżjoni tal-grilja.",
-        "title": "Veduta tar-riżultat tat-tfittxija (lista \/ gradilja)"
+        "title": "Veduta tar-riżultat tat-tfittxija (lista / gradilja)"
       }
     }
   },
