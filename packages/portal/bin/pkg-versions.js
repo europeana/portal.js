1a05847bf7768dfadc60b45e23f9b466373423aa
Richard Doe <richard.doe@rwdit.net>
2022-09-13 08:27:13 +0100

    chore: pkg-specific versioning

--- packages/portal/bin/pkg-versions.js
+++ packages/portal/bin/pkg-versions.js
@@ -1,16 +1,18 @@
 import fs from 'fs';
-import versions from '../packages/portal/pkg-versions.js';
+import versions from '../pkg-versions.js';
 
-const versionsFile = new URL('../packages/portal/pkg-versions.js', import.meta.url);
+console.log(process.env.npm_package_version);
+
+const versionsFile = new URL('../pkg-versions.js', import.meta.url);
 
 let versionsSrc = fs.readFileSync(versionsFile, { encoding: 'utf8' });
 
 for (const pkg in versions) {
   let pkgJsonFile;
   if (pkg === '@europeana/portal') {
-    pkgJsonFile = new URL('../packages/portal/package.json', import.meta.url);
+    pkgJsonFile = new URL('../package.json', import.meta.url);
   } else {
-    pkgJsonFile = new URL(`../node_modules/${pkg}/package.json`, import.meta.url);
+    pkgJsonFile = new URL(`../../../node_modules/${pkg}/package.json`, import.meta.url);
   }
   const version = JSON.parse(fs.readFileSync(pkgJsonFile, { encoding: 'utf8' })).version;
 
