517a795d8c8ea650c8866458c4f80db7c70a087a
lbiedinger <lutz.biedinger@gmail.com>
2022-09-27 20:19:57 +0200

    wip style on radio buttons

--- packages/portal/src/components/search/ViewToggles.vue
+++ packages/portal/src/components/search/ViewToggles.vue
@@ -3,27 +3,26 @@
     <b-form-group
       align="right"
     >
-      <b-form-radio
-        v-for="view in views"
+      <b-form-radio-group
         v-model="value"
-        :key="view"
-        :value="view"
-        :active="value === view"
-        :data-qa="`search ${view} view toggle`"
-        class="pl-3"
+        buttons
       >
-        <span
-          :class="view"
-          class="icon-view-toggle"
-          :title="$t(`searchViews.${view}`)"
+        <b-form-radio
+          v-for="view in views"
+          :key="view"
+          :value="view"
+          :data-qa="`search ${view} view toggle`"
+          class="pl-3"
         >
-          {{ view }}
-        </span>
-      </b-form-radio>
+          <span
+            :class="view"
+            class="icon-view-toggle"
+            :title="$t(`searchViews.${view}`)"
+          >
+          </span>
+        </b-form-radio>
+      </b-form-radio-group>
     </b-form-group>
-    <div class="mt-3">
-      value: <strong>{{ value }}</strong>
-    </div>
   </div>
 </template>
 
@@ -53,7 +52,7 @@
     },
     watch: {
       value() {
-        this.$router.push({ ...this.$route, ...{  query: { ...this.$route.query, ...{ view: this.value } } } });
+        this.$goto({ ...this.$route, ...{  query: { ...this.$route.query, ...{ view: this.value } } } });
 
         this.$cookies && this.$cookies.set('searchResultsView', this.value);
 
@@ -63,11 +62,11 @@
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import '@/assets/scss/variables';
   @import '@/assets/scss/icons';
 
-  .nav-link {
+  .btn-group-toggle {
     padding: 0;
     position: relative;
     text-decoration: none;
@@ -92,41 +91,44 @@
       }
     }
 
-    &::before {
-      background: $white;
-      border-radius: 50%;
-      box-sizing: border-box;
-      content: '';
-      display: block;
-      opacity: 0;
-      position: absolute;
-      transform: scale(0);
-      transition-duration: 0.15s;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-      transition-property: transform, opacity;
-      bottom: -10px;
-      left: -10px;
-      right: -10px;
-      top: -10px;
-      z-index: -1;
-    }
+    label.btn {
+      line-height: 1;
+      background: none;
+      border: 0;
 
-    &:hover::before {
-      opacity: 1;
-      transform: scale(1);
-    }
+      input {
+        display: none;
+      }
+      // &::before {
+      //   background: $white;
+      //   border-radius: 50%;
+      //   box-sizing: border-box;
+      //   content: '';
+      //   display: block;
+      //   opacity: 0;
+      //   position: absolute;
+      //   transform: scale(0);
+      //   transition-duration: 0.15s;
+      //   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      //   transition-property: transform, opacity;
+      //   bottom: -10px;
+      //   left: -10px;
+      //   right: -10px;
+      //   top: -10px;
+      //   z-index: -1;
+      // }
+      //
+      // &:hover::before {
+      //   opacity: 1;
+      //   transform: scale(1);
+      //   box-shadow: none;
+      // }
 
-    &:hover .icon-view-toggle,
-    &.active .icon-view-toggle {
-      color: $greyblack;
-    }
-
-    &.active {
-      cursor: default;
-
-      &::before {
-        opacity: 0;
-        transform: scale(0);
+      &.active {
+        background: none;
+        .icon-view-toggle {
+          color: $greyblack;
+        }
       }
     }
   }
