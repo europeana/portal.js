2262a900ae9593934446e99e835afa08150e038c
Richard Doe <richard.doe@rwdit.net>
2022-10-06 09:33:51 +0100

    test: spec reuse of agents

--- packages/portal/tests/unit/modules/http/templates/plugin.axios.server.spec.js
+++ packages/portal/tests/unit/modules/http/templates/plugin.axios.server.spec.js
@@ -11,12 +11,27 @@ describe('modules/http/templates/plugin.axios.server', () => {
       expect(axios.defaults.httpsAgent.keepAlive).toBe(true);
     });
 
-    it('reuses agents in axios requests', () => {
-      const axios1 = axios.create();
-      const axios2 = axios.create();
+    it('instantiates single agents to reuse', () => {
+      plugin();
+      const httpAgent1 = axios.defaults.httpAgent;
+      const httpsAgent1 = axios.defaults.httpsAgent;
 
-      expect(axios1.defaults.httpAgent).toBe(axios2.defaults.httpAgent);
-      expect(axios1.defaults.httpsAgent).toBe(axios2.defaults.httpsAgent);
+      plugin();
+      const httpAgent2 = axios.defaults.httpAgent;
+      const httpsAgent2 = axios.defaults.httpsAgent;
+
+      expect(httpAgent1).toBe(httpAgent2);
+      expect(httpsAgent1).toBe(httpsAgent2);
+    });
+
+    it('reuses agents in axios requests', () => {
+      plugin();
+
+      const axiosInstance1 = axios.create();
+      const axiosInstance2 = axios.create();
+
+      expect(axiosInstance1.defaults.httpAgent).toBe(axiosInstance2.defaults.httpAgent);
+      expect(axiosInstance1.defaults.httpsAgent).toBe(axiosInstance2.defaults.httpsAgent);
     });
   });
 });
