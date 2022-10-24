b0c021f6761404a566c697c2037fe1b624482892
Richard Doe <richard.doe@rwdit.net>
2022-10-24 10:59:29 +0100

    v1.88.0

--- packages/portal/pkg-versions.js
+++ packages/portal/pkg-versions.js
@@ -9,7 +9,7 @@
 // TODO: it should also be auto-updated when running npm install/update/etc
 
 export default {
-  '@europeana/portal': '1.87.0',
+  '@europeana/portal': '1.88.0',
   '@nuxt/core': '2.15.8',
   'bootstrap': '4.6.2',
   'bootstrap-vue': '2.22.0'
