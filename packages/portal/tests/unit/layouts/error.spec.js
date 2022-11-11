ab0347d2329c3889ca1ab7255bacd4d3cb044713
Richard Doe <richard.doe@rwdit.net>
2022-11-11 13:41:21 +0000

    test: spec error layout changes

---
+++ packages/portal/tests/unit/layouts/error.spec.js
@@ -0,0 +1,71 @@
+import { createLocalVue } from '@vue/test-utils';
+import { shallowMountNuxt } from '../utils';
+import BootstrapVue from 'bootstrap-vue';
+
+import layout from '@/layouts/error';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const factory = ({ propsData = {}, data = {} } = {}) => shallowMountNuxt(layout, {
+  localVue,
+  propsData,
+  data() {
+    return { ...data };
+  },
+  mocks: {
+    $config: {
+      app: {
+        siteName: 'Europeana'
+      }
+    },
+    $store: {
+      state: {
+        pageMeta: {
+          data: {}
+        }
+      }
+    },
+    $t: (key) => key
+  }
+});
+
+describe('layouts/error.vue', () => {
+  const error = { message: 'Not Found' };
+
+  describe('template', () => {
+    it('includes error message text', () => {
+      const wrapper = factory({ propsData: { error } });
+
+      const text = wrapper.text();
+
+      expect(text.includes(error.message)).toBe(true);
+    });
+  });
+
+  describe('head', () => {
+    describe('title', () => {
+      it('uses pageMeta from store if present', () => {
+        const pageMeta = {
+          title: 'Item not found'
+        };
+        const wrapper = factory({ propsData: { error } });
+        wrapper.vm.$store.state.pageMeta.data = pageMeta;
+
+        const headTitle = wrapper.vm.head().title;
+
+        expect(headTitle).toBe('Item not found | Europeana');
+      });
+
+      it('falls back to "Error" (translated)', () => {
+        const pageMeta = {};
+        const wrapper = factory({ propsData: { error } });
+        wrapper.vm.$store.state.pageMeta.data = pageMeta;
+
+        const headTitle = wrapper.vm.head().title;
+
+        expect(headTitle).toBe('error | Europeana');
+      });
+    })
+  })
+});
