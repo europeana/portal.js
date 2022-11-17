e2eb4fb20271e6f14fd7ddcbc3c33c7f660d26b5
Richard Doe <richard.doe@rwdit.net>
2022-11-17 14:18:18 +0000

    chore: remove console logging

--- packages/portal/src/middleware/no-ssr-cookies.js
+++ packages/portal/src/middleware/no-ssr-cookies.js
@@ -1,7 +1,5 @@
 export default ({ res }) => {
   if (process.server && res) {
-    console.log('no-ssr-cookies', res.getHeaders())
-    res.removeHeader('set-cookie')
-    console.log('no-ssr-cookies', res.getHeaders())
+    res.removeHeader('set-cookie');
   }
 };
