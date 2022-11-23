6022ebe0ee8be5e9eceea776c20a0ba33cc5ae27
lbiedinger <lutz.biedinger@gmail.com>
2022-11-23 10:18:15 +0100

    override responsive footer spacing in feedback

--- packages/portal/src/components/feedback/FeedbackWidget.vue
+++ packages/portal/src/components/feedback/FeedbackWidget.vue
@@ -471,6 +471,9 @@
     // This overrides the responsive font-size inherited from the footer.
     @media (min-width: $bp-xxxl) {
       font-size: $font-size-base;
+      .mt-4 {
+        margin-top: 1.5rem !important;
+      }
     }
 
     &.show-feedback-widget {
