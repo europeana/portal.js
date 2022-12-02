e40853ad4d91387e09613ece93b087d052797c06
Richard Doe <richard.doe@rwdit.net>
2022-12-02 08:02:05 +0000

    refactor: fix linting, tidy up, rename computed props

--- packages/portal/src/pages/galleries/_.vue
+++ packages/portal/src/pages/galleries/_.vue
@@ -116,7 +116,7 @@
               </b-row>
               <div class="d-inline-flex collection-buttons">
                 <template
-                  v-if="userCanEdit"
+                  v-if="userCanEditSet"
                 >
                   <b-button
                     class="d-inline-flex align-items-center mr-2"
@@ -291,35 +291,30 @@
         return this.set.creator && typeof this.set.creator === 'string' ? this.set.creator : this.set.creator.id;
       },
       userIsOwner() {
-        return this.$auth.loggedIn && this.$store.state.auth.user &&
-          this.setCreatorId &&
-          this.setCreatorId.endsWith(`/${this.$store.state.auth.user.sub}`);
+        return this.$auth.loggedIn && this.$auth.user &&
+          this.setCreatorId?.endsWith(`/${this.$auth.user.sub}`);
       },
       userIsEntityEditor() {
-        const user = this.$store.state.auth.user;
-        const entitiesEditor = user?.resource_access?.entities?.roles?.includes('editor');
-        const usersetsEditor = user?.resource_access?.usersets?.roles?.includes('editor');
+        const entitiesEditor = this.$auth.user?.resource_access?.entities?.roles?.includes('editor');
+        const usersetsEditor = this.$auth.user?.resource_access?.usersets?.roles?.includes('editor');
         return entitiesEditor && usersetsEditor;
       },
       userIsPublisher() {
-        const user = this.$store.state.auth.user;
+        const user = this.$auth.user;
         const publisher = user?.resource_access?.usersets?.roles?.includes('publisher');
         return !!publisher;
       },
-      userCanEditRecommendations() {
+      userCanHandleRecommendations() {
         return this.userIsOwner || (this.setIsEntityBestItems && this.userIsEntityEditor);
       },
-      userCanEdit() {
-        return this.userIsOwner || (this.userIsPublisher && this.set.visibility === 'published'));
-      },
-      setIsEntityBestItems() {
-        return this.set.type === 'EntityBestItemsSet';
+      userCanEditSet() {
+        return this.userIsOwner || (this.userIsPublisher && this.set.visibility === 'published');
       },
       setIsEntityBestItems() {
         return this.set.type === 'EntityBestItemsSet';
       },
       displayRecommendations() {
-        return this.enableRecommendations && this.$auth.loggedIn && this.userCanEditRecommendations;
+        return this.enableRecommendations && this.$auth.loggedIn && this.userCanHandleRecommendations;
       },
       enableRecommendations() {
         if (this.setIsEntityBestItems) {
