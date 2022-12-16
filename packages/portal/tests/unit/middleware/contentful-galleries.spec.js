8b7b76172cc227622422f7ab5e8d4583048cfc52
Richard Doe <richard.doe@rwdit.net>
2022-12-16 15:31:34 +0000

    feat: final Set IDs, and 410 Gone for galleries skipped

--- packages/portal/tests/unit/middleware/contentful-galleries.spec.js
+++ packages/portal/tests/unit/middleware/contentful-galleries.spec.js
@@ -6,22 +6,33 @@ describe('middleware/contentful-galleries', () => {
   afterEach(sinon.resetHistory);
 
   const redirect = sinon.spy();
+  const error = sinon.spy();
 
   describe('when route path is for migrated gallery', () => {
     const route = { path: '/en/galleries/yellow' };
 
     it('redirects to path with numeric Set ID', () => {
-      middleware({ route, redirect });
+      middleware({ route, redirect, error });
 
-      expect(redirect.calledWith('/en/galleries/8379-yellow')).toBe(true);
+      expect(redirect.calledWith('/en/galleries/9280-yellow')).toBe(true);
     });
   });
 
-  describe('when route path is not for migrated gallery', () => {
+  describe('when route path is for gallery skipped for migration', () => {
+    const route = { path: '/en/galleries/postcards' };
+
+    it('issues a 410 error', () => {
+      middleware({ route, redirect, error });
+
+      expect(error.calledWith(sinon.match.has('status', 410))).toBe(true);
+    });
+  });
+
+  describe('when route path is not for known gallery', () => {
     const route = { path: '/en/galleries/i-dont-think-so' };
 
-    it('does not redirects', () => {
-      middleware({ route, redirect });
+    it('does not redirect', () => {
+      middleware({ route, redirect, error });
 
       expect(redirect.called).toBe(false);
     });
