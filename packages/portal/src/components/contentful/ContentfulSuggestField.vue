8e5c693231c9715ad92c23355720071022fa2ed7
Richard Doe <richard.doe@rwdit.net>
2023-01-11 14:34:39 +0000

    refactor: extract CTF field suggest into component

---
+++ packages/portal/src/components/contentful/ContentfulSuggestField.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="contentful">
+    <b-form-group>
+      <b-button
+        v-for="val in value"
+        :key="val.id"
+        class="mb-2"
+        @click="removeSelection(val)"
+      >
+        {{ labeller(val) }}
+      </b-button>
+    </b-form-group>
+
+    <b-form>
+      <b-form-group>
+        <b-form-input
+          v-model="searchText"
+          type="search"
+          autocomplete="off"
+          :placeholder="placeholder"
+          @input="handleSearchInput"
+        />
+      </b-form-group>
+      <b-form-group>
+        <b-button
+          v-for="suggestion in suggestions"
+          :key="suggestion.id"
+          class="mb-2"
+          :disabled="isSelected(suggestion)"
+          @click="selectSuggestion(suggestion)"
+        >
+          {{ labeller(suggestion) }}
+        </b-button>
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContentfulSuggestField',
+
+    props: {
+      suggester: {
+        type: Function,
+        required: true
+      },
+
+      resolver: {
+        type: Function,
+        required: true
+      },
+
+      labeller: {
+        type: Function,
+        default: (val) => val
+      },
+
+      placeholder: {
+        type: String,
+        default: 'Search'
+      }
+    },
+
+    data() {
+      return {
+        value: [],
+        searchText: null,
+        suggestions: [],
+        contentfulExtensionSdk: null
+      };
+    },
+
+    watch: {
+      value: 'updateContentfulField'
+    },
+
+    mounted() {
+      window.contentfulExtension.init((sdk) => {
+        this.contentfulExtensionSdk = sdk;
+        if (sdk.location.is(window.contentfulExtension.locations.LOCATION_ENTRY_FIELD)) {
+          sdk.window.startAutoResizer();
+          this.resolveFieldValue();
+        }
+      });
+    },
+
+    methods: {
+      async resolveFieldValue() {
+        const ids = this.contentfulExtensionSdk.field.getValue() || [];
+        if (ids.length > 0) {
+          this.value = await this.resolver(ids);
+        }
+      },
+
+      async handleSearchInput(val) {
+        this.suggestions = await this.suggester(val);
+      },
+
+      isSelected(suggestion) {
+        return this.value.map(val => val.id).includes(suggestion.id);
+      },
+
+      removeSelection(remove) {
+        this.value = this.value.filter(val => val.id !== remove.id);
+      },
+
+      selectSuggestion(select) {
+        this.value = this.value.concat(select);
+      },
+
+      updateContentfulField() {
+        this.contentfulExtensionSdk?.field?.setValue(this.value.map(val => val.id));
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .contentful {
+    button {
+      margin-right: 1rem;
+    }
+  }
+</style>
--- packages/portal/src/pages/contentful/set-suggest/index.vue
+++ packages/portal/src/pages/contentful/set-suggest/index.vue
@@ -1,55 +1,23 @@
 <template>
-  <div class="contentful">
-    <b-form-group>
-      <b-button
-        v-for="val in value"
-        :key="val.id"
-        class="mb-2"
-        @click="removeSelection(val)"
-      >
-        {{ val.title.en || Object.values(val.title)[0] }}
-      </b-button>
-    </b-form-group>
-
-    <b-form>
-      <b-form-group>
-        <b-form-input
-          v-model="searchText"
-          type="search"
-          autocomplete="off"
-          placeholder="Search for published user galleries"
-          @input="suggestSets"
-        />
-      </b-form-group>
-      <b-form-group>
-        <b-button
-          v-for="suggestion in suggestions"
-          :key="suggestion.id"
-          class="mb-2"
-          :disabled="isSelected(suggestion)"
-          @click="selectSuggestion(suggestion)"
-        >
-          {{ suggestion.title.en || Object.values(suggestion.title)[0] }}
-        </b-button>
-      </b-form-group>
-    </b-form>
-  </div>
+  <ContentfulSuggestField
+    :suggester="suggestSets"
+    :resolver="findSets"
+    :labeller="labelSet"
+    placeholder="Search for published user galleries"
+  />
 </template>
 
 <script>
+  import ContentfulSuggestField from '@/components/contentful/ContentfulSuggestField';
+
   export default {
     name: 'ContentfulSetSuggestPage',
 
-    layout: 'contentful',
-
-    data() {
-      return {
-        value: [],
-        searchText: null,
-        suggestions: [],
-        contentfulExtensionSdk: null
-      };
+    components: {
+      ContentfulSuggestField
     },
+
+    layout: 'contentful',
 
     head() {
       return {
@@ -57,65 +25,25 @@
       };
     },
 
-    watch: {
-      value: 'updateContentfulField'
-    },
-
-    mounted() {
-      window.contentfulExtension.init((sdk) => {
-        this.contentfulExtensionSdk = sdk;
-        if (sdk.location.is(window.contentfulExtension.locations.LOCATION_ENTRY_FIELD)) {
-          sdk.window.startAutoResizer();
-          this.findSets();
-        }
-      });
-    },
-
     methods: {
-      isSelected(suggestion) {
-        return this.value.map(val => val.id).includes(suggestion.id);
-      },
-
       async suggestSets(val) {
         const response = await this.$apis.set.search({
           query: val, qf: 'visibility:published', profile: 'standard'
         });
-        this.suggestions = response.data.items || [];
+        return response.data.items || [];
       },
 
-      findSets() {
-        const ids = this.contentfulExtensionSdk.field.getValue() || [];
-        if (ids.length > 0) {
-          // TODO: this is very inefficient, requiring a GET request for each
-          //       linked set, but the Set API does not yet support searching
-          //       by multiple IDs combined with OR. refactor when the API
-          //       supports searching by multiple IDs.
-          Promise.all(ids.map((id) => this.$apis.set.get(id, { profile: 'standard' })))
-            .then((sets) => {
-              this.value = sets;
-            });
-        }
+      findSets(val) {
+        // TODO: this is very inefficient, requiring a GET request for each
+        //       linked set, but the Set API does not yet support searching
+        //       by multiple IDs combined with OR. refactor when the API
+        //       supports searching by multiple IDs.
+        return Promise.all(val.map((id) => this.$apis.set.get(id, { profile: 'standard' })));
       },
 
-      removeSelection(remove) {
-        this.value = this.value.filter(val => val.id !== remove.id);
-      },
-
-      selectSuggestion(select) {
-        this.value = this.value.concat(select);
-      },
-
-      updateContentfulField() {
-        this.contentfulExtensionSdk?.field?.setValue(this.value.map(val => val.id));
+      labelSet(val) {
+        return val.title.en || Object.values(val.title)[0];
       }
     }
   };
 </script>
-
-<style lang="scss" scoped>
-  .contentful {
-    button {
-      margin-right: 1rem;
-    }
-  }
-</style>
