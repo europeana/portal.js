3f718a1d411ebe5c3ed89a9a680717acf5f3c06e
lbiedinger <lutz.biedinger@gmail.com>
2023-02-13 14:00:21 +0100

    file extension for migration

---
+++ packages/contentful/migrations/0098-static-browse-pages-validate-for-themes-slug.cjs
@@ -0,0 +1,21 @@
+module.exports = function(migration) {
+  const browsePage = migration.editContentType('browsePage');
+  const staticPage = migration.editContentType('staticPage');
+
+  [browsePage, staticPage].forEach(page =>
+    page
+      .editField('identifier')
+      .validations([
+        {
+          unique: true
+        },
+        {
+          prohibitRegexp: {
+            pattern: '^(account$|account/|api$|api/|blog$|blog/|collections/|contentful/|debug$|debug/|exhibitions$|exhibitions/|galleries$|galleries/|iiif$|item/|media$|record/|schemas$|schemas/|search$|themes$|themes/)',
+            flags: null
+          },
+          message: 'This URL slug is reserved.'
+        }
+      ])
+  );
+};
