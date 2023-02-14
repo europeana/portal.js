ab64a695bd1a01c2e1226dcd0fe5802ccba8a42c
Lutz Biedinger <lutz.biedinger@gmail.com>
2023-02-14 16:49:58 +0100

    feat: automated theme hub page (#1849)
    * automated theme foyer page WIP
    
    * specs
    
    * migration for updating validation
    
    * file extension for migration
    
    * file extension for migration
    
    * linting
    
    * no scrolling to top
    
    * bump size limits

---
+++ packages/portal/src/pages/themes/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <b-container
+    data-qa="themes"
+  >
+    <ContentHeader
+      :title="pageMeta.title"
+      :description="$t('themes.description')"
+    />
+    <b-row class="flex-md-row pb-5">
+      <b-col cols="12">
+        <b-card-group
+          class="card-deck-4-cols"
+          deck
+          data-qa="theme section"
+        >
+          <ContentCard
+            v-for="theme in themes"
+            :key="theme.identifier"
+            :title="theme.name"
+            :url="{ name: 'themes-all', params: { pathMatch: theme.identifier } }"
+            :image-url="imageUrl(theme.primaryImageOfPage)"
+            :image-content-type="imageContentType(theme.primaryImageOfPage)"
+            :image-optimisation-options="{ width: 510 }"
+            :image-alt="imageAlt(theme.primaryImageOfPage)"
+            :texts="[theme.description]"
+            :show-subtitle="false"
+          />
+        </b-card-group>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  import ContentHeader from '../../components/generic/ContentHeader';
+  import ContentCard from '../../components/generic/ContentCard';
+  import pageMetaMixin from '@/mixins/pageMeta';
+
+  export default {
+    name: 'ThemesIndexPage',
+    components: {
+      ContentHeader,
+      ContentCard
+    },
+    mixins: [pageMetaMixin],
+
+    data() {
+      return {
+        themes: []
+      };
+    },
+
+    async fetch() {
+      const variables = {
+        locale: this.$i18n.isoLocale(),
+        preview: this.$route.query.mode === 'preview'
+      };
+      const response = await this.$contentful.query('themeFoyerPage', variables);
+      this.themes = response.data.data.themePageCollection.items;
+    },
+
+    computed: {
+      pageMeta() {
+        return {
+          title: this.$tc('themes.themes', 2),
+          description: this.$t('themes.description')
+        };
+      }
+    },
+    methods: {
+      imageUrl(image) {
+        return image?.image?.url;
+      },
+      imageContentType(image) {
+        return image?.image?.contentType;
+      },
+      imageAlt(image) {
+        return image?.image?.description || '';
+      }
+    }
+  };
+</script>
