2b9668717b22ff03cf4558f0bb70115f792a4794
Richard Doe <richard.doe@rwdit.net>
2023-02-22 11:10:21 +0000

    fix: silently catch Set API like retrieval errors

--- packages/portal/src/plugins/user-likes.client.js
+++ packages/portal/src/plugins/user-likes.client.js
@@ -1,8 +1,11 @@
 export default async({ $auth, store }) => {
   if ($auth?.loggedIn) {
-    // TODO: assess whether there is a more efficient way to do this with fewer
-    //       API requests
-    await store.dispatch('set/setLikes');
-    await store.dispatch('set/fetchLikes');
+    try {
+      // TODO: assess whether there is a more efficient way to do this with fewer
+      //       API requests
+      await store.dispatch('set/setLikes');
+      await store.dispatch('set/fetchLikes');
+    } catch(e) {
+    }    
   }
 };
