7064836ffe6abb346b2c2c1b81f3754f85a2a4d6
Richard Doe <richard.doe@rwdit.net>
2023-02-22 11:12:20 +0000

    chore: linting

--- packages/portal/src/plugins/user-likes.client.js
+++ packages/portal/src/plugins/user-likes.client.js
@@ -5,7 +5,8 @@ export default async({ $auth, store }) => {
       //       API requests
       await store.dispatch('set/setLikes');
       await store.dispatch('set/fetchLikes');
-    } catch(e) {
-    }    
+    } catch (e) {
+      // Don't cause everything to break if the Set API is down...
+    }
   }
 };
