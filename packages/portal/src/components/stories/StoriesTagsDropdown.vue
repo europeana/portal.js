63dd115ce0b96d3db8f4a3d0e003918c6bfeb230
Leonie Peters <leoniepeters@gmail.com>
2023-02-23 14:51:07 +0100

    Search tags and custom dropdown

--- packages/portal/src/components/stories/StoriesTagsDropdown.vue
+++ packages/portal/src/components/stories/StoriesTagsDropdown.vue
@@ -1,16 +1,35 @@
 <template>
-  <b-dropdown
-    block
-    no-flip
-  >
-    <RelatedCategoryTags
-      v-if="($features.storiesPageAllTags || selectedTags.length > 0) && (displayTags.length > 0)"
-      :tags="displayTags"
-      :selected="selectedTags"
-      :heading="false"
-      class="responsive-font mb-2"
-    />
-  </b-dropdown>
+  <div
+  class="position-relative">
+    <b-form
+      @submit.stop.prevent="() => {}"
+    >
+        <b-form-input
+          :id="'tag-search-input'"
+          ref="search-input"
+          v-model="searchTag"
+          type="text"
+          autocomplete="off"
+          :placeholder="$t('sideFilters.search')"
+          data-qa="tags dropdown search input"
+          @input="activeSearchInput = true"
+          @focus="activeSearchInput = true"
+          @blur="activeSearchInput = false"
+        />
+        <span class="icon-search" />
+    </b-form>
+    <div
+      class="tag-search-dropdown"
+    >
+      <RelatedCategoryTags
+        v-if="($features.storiesPageAllTags || selectedTags.length > 0) && (displayTags.length > 0)"
+        :tags="displayTags"
+        :selected="selectedTags"
+        :heading="false"
+        class="responsive-font mb-2"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,6 +55,8 @@
 
     data() {
       return {
+        activeSearchInput: false,
+        searchTag: '',
         tags: []
       };
     },
@@ -52,12 +73,56 @@
 
     computed: {
       displayTags() {
+        let displayTags;
+        const keyword = this.trimmedKeyword
         if (this.filteredTags) {
-          return this.tags.filter((tag) => this.filteredTags.includes(tag.identifier) || this.selectedTags.includes(tag.identifier));
+          displayTags = this.tags.filter((tag) => this.filteredTags.includes(tag.identifier) || this.selectedTags.includes(tag.identifier));
         } else {
-          return this.tags;
+          displayTags = this.tags;
         }
+
+        if (keyword) {
+          displayTags = displayTags.filter(tag => {
+            const tagLabel = tag.name;
+            const tagNameMatch = tagLabel.toLowerCase().indexOf(keyword) > -1;
+            return tagNameMatch;
+          });
+        }
+         return displayTags;
+      },
+      trimmedKeyword() {
+        return this.searchTag.trim().toLowerCase();
       }
     }
   };
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables';
+
+.tag-search-dropdown {
+  width: 100%;
+  background-color: $white;
+  overflow: hidden;
+  overflow-x: scroll;
+  animation: appear 750ms ease-in-out;
+  position: absolute;
+  z-index: 20;
+  box-shadow: $boxshadow;
+  max-height: 50vh;
+
+  @media (min-width: $bp-xxxl) {
+    font-size: 1vw;
+  }
+}
+
+@keyframes appear {
+  from {
+    max-height: 0;
+  }
+
+  to {
+    max-height: 0vh;
+  }
+}
+</style>
