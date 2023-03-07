73126bb6927de3bb835acc59b75c97e9639066d1
Leonie Peters <leoniepeters@gmail.com>
2023-03-07 14:44:58 +0100

    Clean up console.warn

--- packages/portal/src/plugins/europeana/utils.js
+++ packages/portal/src/plugins/europeana/utils.js
@@ -394,7 +394,6 @@ export const daily = (set, subsetSize) => {
   if (!Array.isArray(set)) {
     return set;
   }
-  console.warn('is an array', set);
 
   const offset = dailyOffset(set.length, subsetSize);
   return set.slice(offset, offset + subsetSize);
