861abe8c6dcbbb4a5d5ae8a79ea34386502ba79c
Richard Doe <richard.doe@rwdit.net>
2023-03-10 10:22:07 +0000

    chore: remove TODO

--- packages/portal/src/plugins/contentful/query.js
+++ packages/portal/src/plugins/contentful/query.js
@@ -1,5 +1,3 @@
-// TODO: function to make $axios request to local server middleware for CTF
-
 export default ({ $axios } = {}) => (alias, variables) => {
   return $axios.post(`/_api/contentful/graphql/${alias}`, variables);
 };
