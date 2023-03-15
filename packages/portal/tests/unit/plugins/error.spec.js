c15527eefd3fafff06922d429b16d32755a2a5b0
Richard Doe <richard.doe@rwdit.net>
2023-03-15 14:58:30 +0000

    test: spec storing error

--- packages/portal/tests/unit/plugins/error.spec.js
+++ packages/portal/tests/unit/plugins/error.spec.js
@@ -19,6 +19,9 @@ const factory = ({ mocks = {} } = {}) => {
         t: (key) => key
       },
       $nuxt: { context: { res: {} } },
+      $store: {
+        commit: sinon.spy()
+      },
       ...mocks
     }
   });
@@ -27,11 +30,14 @@ const factory = ({ mocks = {} } = {}) => {
 };
 
 describe('@/plugins/error', () => {
+  afterEach(sinon.resetHistory);
+
   describe('handleError', () => {
     it('converts number to the equivalent HTTP error', () => {
       const errorOrCode = 400;
+      const $fetchState = { pending: true };
       const wrapper = factory({
-        mocks: { $error: errorPlugin.handleError }
+        mocks: { $error: errorPlugin.handleError, $fetchState }
       });
 
       let error;
@@ -78,6 +84,17 @@ describe('@/plugins/error', () => {
       expect(wrapper.vm.$nuxt.context.res.statusCode).toBe(errorOrCode);
     });
 
+    it('stores the error in Vuex state', () => {
+      const errorOrCode = 404;
+      const wrapper = factory({
+        mocks: { $error: errorPlugin.handleError }
+      });
+
+      wrapper.vm.$error(errorOrCode);
+
+      expect(wrapper.vm.$store.commit.calledWith('error/set', sinon.match.has('statusCode', 404))).toBe(true);
+    });
+
     describe('when scope and status code resolve to a known code', () => {
       const errorOrCode = { statusCode: 403 };
       const scope = 'gallery';
@@ -119,13 +136,34 @@ describe('@/plugins/error', () => {
       });
     });
 
-    describe('when scope and status code do not resolve to a known code', () => {
-      const errorOrCode = { statusCode: 400 };
+    // TODO: is this behaviour we want? if so, re-implement
+    // describe('when scope and status code do not resolve to a known code', () => {
+    //   const errorOrCode = { statusCode: 400 };
+    //
+    //   it('throws the error', () => {
+    //     const wrapper = factory({
+    //       mocks: { $error: errorPlugin.handleError }
+    //     });
+    //
+    //     let error;
+    //     try {
+    //       wrapper.vm.$error(errorOrCode);
+    //     } catch (e) {
+    //       error = e;
+    //     }
+    //
+    //     expect(error).toBe(errorOrCode);
+    //   });
+    // });
 
-      it('throws the error', () => {
+    describe('when Nuxt fetch state is not pending', () => {
+      const $fetchState = { pending: false };
+
+      it('does not throw error', () => {
         const wrapper = factory({
-          mocks: { $error: errorPlugin.handleError }
+          mocks: { $error: errorPlugin.handleError, $fetchState }
         });
+        const errorOrCode = { statusCode: 403 };
 
         let error;
         try {
@@ -134,25 +172,7 @@ describe('@/plugins/error', () => {
           error = e;
         }
 
-        expect(error).toBe(errorOrCode);
-      });
-    });
-
-    describe('when Nuxt fetch state is not pending', () => {
-      const $fetchState = { pending: false };
-
-      it('emits "show-error-modal" event on Vue root, with error as arg', () => {
-        const wrapper = factory({
-          mocks: { $error: errorPlugin.handleError, $fetchState }
-        });
-        sinon.spy(wrapper.vm.$root, '$emit');
-        const error = { statusCode: 403 };
-
-        wrapper.vm.$error(error);
-
-        expect(wrapper.vm.$root.$emit.calledWith(
-          'show-error-modal', error
-        )).toBe(true);
+        expect(error).toBeUndefined();
       });
     });
   });
