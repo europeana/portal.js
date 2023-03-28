28c272a0f607fe4782aa565a5698cede9d9c4d75
Richard Doe <richard.doe@rwdit.net>
2023-03-28 09:36:19 +0100

    feat: always use Recommendation API on item page
    Not just when user is logged in

--- packages/portal/src/components/item/ItemRecommendations.vue
+++ packages/portal/src/components/item/ItemRecommendations.vue
@@ -23,8 +23,6 @@
 </template>
 
 <script>
-  import similarItemsQuery from '@/plugins/europeana/record/similar-items';
-  import { langMapValueForLocale } from  '@/plugins/europeana/utils';
   import ItemPreviewCardGroup from '@/components/item/ItemPreviewCardGroup';
 
   export default {
@@ -38,26 +36,6 @@
       identifier: {
         type: String,
         required: true
-      },
-
-      dcType: {
-        type: [String, Object, Array],
-        default: null
-      },
-
-      dcSubject: {
-        type: [String, Object, Array],
-        default: null
-      },
-
-      dcCreator: {
-        type: [String, Object, Array],
-        default: null
-      },
-
-      edmDataProvider: {
-        type: [String, Object, Array],
-        default: null
       }
     },
 
@@ -68,48 +46,16 @@
     },
 
     async fetch() {
-      let response;
+      const response = await this.$apis.recommendation.recommend('record', this.identifier);
 
-      if (this.$auth.loggedIn) {
-        response = await this.$apis.recommendation.recommend('record', this.identifier);
-        response.items = response.items
-          // Remove any recommendations that are the same as the active item,
-          // because the Recommendation API/Engine is broken.
-          // TODO: remove if/when recommendations become useful.
-          .filter((item) => item.id !== this.identifier)
-          .slice(0, 8);
-      } else {
-        response = await this.$apis.record.search({
-          query: similarItemsQuery(this.identifier, this.similarItemsFields),
-          rows: 4,
-          profile: 'minimal',
-          facet: ''
-        });
-      }
+      response.items = response.items
+        // Remove any recommendations that are the same as the active item,
+        // because the Recommendation API/Engine is broken.
+        // TODO: remove if/when recommendations become useful.
+        .filter((item) => item.id !== this.identifier)
+        .slice(0, 8);
 
       this.items = response.items;
-    },
-
-    computed: {
-      similarItemsFields() {
-        return {
-          dcSubject: this.similarItemsFieldValue(this.dcSubject),
-          dcType: this.similarItemsFieldValue(this.dcType),
-          dcCreator: this.similarItemsFieldValue(this.dcCreator),
-          edmDataProvider: this.similarItemsFieldValue(this.edmDataProvider)
-        };
-      }
-    },
-
-    methods: {
-      similarItemsFieldValue(value) {
-        if (!value) {
-          return null;
-        }
-        return langMapValueForLocale(value, this.$i18n.locale)
-          .values
-          .filter(item => typeof item === 'string');
-      }
     }
   };
 </script>
