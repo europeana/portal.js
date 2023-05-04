d0d171d9a67d684d42a941a3a36727a1d57873de
Richard Doe <richard.doe@rwdit.net>
2023-05-04 12:54:52 +0100

    refactor: use JS media proxy URL format

--- packages/portal/src/plugins/europeana/record.js
+++ packages/portal/src/plugins/europeana/record.js
@@ -1,4 +1,5 @@
 import pick from 'lodash/pick.js';
+import md5 from 'md5';
 import merge from 'deepmerge';
 
 import { apiError, createAxios, reduceLangMapsForLocale, isLangMap } from './utils.js';
@@ -261,20 +262,20 @@ export default (context = {}) => {
         });
     },
 
+    // TODO: move to media-proxy.js
     mediaProxyUrl(mediaUrl, europeanaId, params = {}) {
       if (!params['api_url']) {
-        // TODO: it is not ideal to hard-code "/api" here, but the media proxy
-        //       expects Record API URLs to end thus, i.e. not /record or /api/v2
-        params['api_url'] = new URL(this.$axios.defaults.baseURL).origin + '/api';
+        params['api_url'] = this.$axios.defaults.baseURL;
       }
 
-      const proxyUrl = new URL(context.$config?.europeana?.proxy?.media?.url || EUROPEANA_MEDIA_PROXY_URL);
-      proxyUrl.pathname = europeanaId;
-      proxyUrl.searchParams.append('view', mediaUrl);
+      const proxyUrl = new URL(context.$config?.europeana?.apis?.mediaProxy?.url || EUROPEANA_MEDIA_PROXY_URL);
+      proxyUrl.pathname = `${proxyUrl.pathname}${europeanaId}/${md5(mediaUrl)}`;
 
       for (const name in params) {
         proxyUrl.searchParams.append(name, params[name]);
       }
+
+      console.log('mediaProxyUrl', proxyUrl.toString())
 
       return proxyUrl.toString();
     }
