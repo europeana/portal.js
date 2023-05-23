f0e42ef2f64300ddc443d029b6a731c684d8cb37
Richard Doe <richard.doe@rwdit.net>
2023-05-23 10:31:45 +0100

    test: spec builder

--- packages/portal/tests/unit/components/search/SearchQueryBuilder.spec.js
+++ packages/portal/tests/unit/components/search/SearchQueryBuilder.spec.js
@@ -6,9 +6,14 @@ import BootstrapVue from 'bootstrap-vue';
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-const factory = ({ propsData = {}, mocks = {} } = {}) => shallowMountNuxt(SearchQueryBuilder, {
+const factory = ({ propsData = {}, data = {}, mocks = {} } = {}) => shallowMountNuxt(SearchQueryBuilder, {
   localVue,
   propsData,
+  data() {
+    return {
+      ...data
+    };
+  },
   mocks: {
     $route: {
       query: {}
@@ -20,10 +25,40 @@ const factory = ({ propsData = {}, mocks = {} } = {}) => shallowMountNuxt(Search
 
 describe('components/search/SearchQueryBuilder', () => {
   describe('when there is an advanced search in the route query', () => {
+    const $route = { query: { qa: 'proxy_dc_title:dog' } };
+
+    it('initalises with those rules', () => {
+      const wrapper = factory({ mocks: { $route } });
+
+      const queryRules = wrapper.vm.queryRules;
+
+      expect(queryRules).toEqual([
+        { field: 'proxy_dc_title', modifier: 'contains', term: 'dog' }
+      ]);
+    });
+
     it('emits the "show" event', () => {
-      const wrapper = factory({ mocks: { $route: { query: { qa: 'proxy_dc_title:dog' } } } });
+      const wrapper = factory({ mocks: { $route } });
 
       expect(wrapper.emitted('show')).toEqual([[true]]);
+    });
+  });
+
+  describe('when there are no initial rules', () => {
+    const $route = { query: {} };
+
+    it('has one blank rule', () => {
+      const wrapper = factory({ mocks: { $route } });
+
+      const queryRules = wrapper.vm.queryRules;
+
+      expect(queryRules).toEqual([{}]);
+    });
+
+    it('does not emit the "show" event', () => {
+      const wrapper = factory({ mocks: { $route } });
+
+      expect(wrapper.emitted('show')).toBeUndefined();
     });
   });
 
@@ -35,6 +70,41 @@ describe('components/search/SearchQueryBuilder', () => {
       addButton.trigger('click');
 
       expect(wrapper.vm.queryRules.length).toBe(2);
+    });
+  });
+
+  describe('methods', () => {
+    describe('clearRule', () => {
+      it('removes rule if there are others', async() => {
+        const wrapper = factory();
+
+        await wrapper.setData({ queryRules: [
+          { field: 'proxy_dc_title', modifier: 'contains', term: 'dog' },
+          { field: 'proxy_dc_type', modifier: 'doesNotContain', term: 'photograph' }
+        ] });
+
+        wrapper.vm.clearRule(0);
+
+        expect(wrapper.vm.queryRules.length).toBe(1);
+        expect(wrapper.vm.queryRules[0]).toEqual(
+          { field: 'proxy_dc_type', modifier: 'doesNotContain', term: 'photograph' }
+        );
+      });
+
+      it('leaves rule if there are no others', async() => {
+        const wrapper = factory();
+
+        await wrapper.setData({ queryRules: [
+          { field: 'proxy_dc_title', modifier: 'contains', term: 'dog' }
+        ] });
+
+        wrapper.vm.clearRule(0);
+
+        expect(wrapper.vm.queryRules.length).toBe(1);
+        expect(wrapper.vm.queryRules[0]).toEqual(
+          { field: 'proxy_dc_title', modifier: 'contains', term: 'dog' }
+        );
+      });
     });
   });
 });
