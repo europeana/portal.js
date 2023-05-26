4576e2dea791b05b6eeb2851ad53c5312e03e722
Richard Doe <richard.doe@rwdit.net>
2023-05-26 09:54:33 +0100

    chore: set URL to harp example

--- packages/portal/src/features/notifications.js
+++ packages/portal/src/features/notifications.js
@@ -1,3 +1,3 @@
 export default [
-  { name: 'advancedSearch', url: '/search' }
+  { name: 'advancedSearch', url: '/search?qf=TYPE%3A"IMAGE"&query=harp&view=grid&qa=proxy_dc_description%3Aarched&qa=-proxy_dc_type%3A%2Aschack%2A' }
 ];
