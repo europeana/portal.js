17181688999adf542e569fd772a7c69bf5d57e4b
Richard Doe <richard.doe@rwdit.net>
2023-06-08 12:30:21 +0100

    feat: return to current page after editing profile

--- packages/portal/src/mixins/keycloak.js
+++ packages/portal/src/mixins/keycloak.js
@@ -14,19 +14,30 @@ export default {
         }
       }
 
-      const redirectUrl = new URL(`${window.location.origin}/account/callback`);
+      const redirectUrl = new URL(`${this.$config.app.baseUrl}/account/callback`);
       redirectUrl.searchParams.set('redirect', redirectPath);
+
       return redirectUrl.toString();
     },
 
     keycloakAccountUrl() {
-      const keycloakAccountUrl = new URL(
-        `/realms/${this.$config.keycloak.realm}/account`, this.$config.keycloak.url
-      );
+      const keycloakAccountUrl = new URL(this.$config.keycloak.url);
+
+      keycloakAccountUrl.pathname = `${keycloakAccountUrl.pathname}/realms/${this.$config.keycloak.realm}/account`;
+      if (keycloakAccountUrl.pathname.startsWith('//')) {
+        keycloakAccountUrl.pathname = keycloakAccountUrl.pathname.slice(1);
+      }
+
+      const referrerUri = new URL(this.$config.app.baseUrl);
+      referrerUri.pathname = this.$route.path;
+      referrerUri.search = new URLSearchParams(this.$route.query).toString();
+      referrerUri.hash = this.$route.hash;
+
       keycloakAccountUrl.search = new URLSearchParams({
         referrer: this.$config.keycloak.clientId,
-        'referrer_uri': this.$config.app.baseUrl
+        'referrer_uri': referrerUri.toString()
       }).toString();
+
       return keycloakAccountUrl.toString();
     }
   },
