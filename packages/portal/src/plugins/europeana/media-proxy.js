2582d19a5aa7ccf6aefa65089a5fc0ee885cc39b
lbiedinger <lutz.biedinger@gmail.com>
2023-07-18 14:57:18 +0200

    update media proxy base URL

--- packages/portal/src/plugins/europeana/media-proxy.js
+++ packages/portal/src/plugins/europeana/media-proxy.js
@@ -1,2 +1,1 @@
-// TODO: change to proxy.europeana.eu/media once JS media proxy is live in prod
-export const BASE_URL = 'https://proxy.europeana.eu';
+export const BASE_URL = 'https://proxy.europeana.eu/media';
