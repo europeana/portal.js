bc23a83a003204a043c7de0ce6fb8e2d64ae131f
Leonie Peters <leoniepeters@gmail.com>
2023-08-28 13:27:19 +0200

    Reuse InfoCard and round off counts

--- packages/portal/src/components/landing/LandingAutomatedCardGroup.vue
+++ packages/portal/src/components/landing/LandingAutomatedCardGroup.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    {{ entries }}
+    <h3 class="title text-center">
+      {{ title }}
+    </h3>
+    <div
+      v-if="hasPartCollectionItems.length"
+      class="d-flex flex-wrap justify-content-center"
+    >
+      <InfoCard
+        v-for="(item, index) in hasPartCollectionItems"
+        :key="index"
+        :info="item.info"
+        :label="item.label"
+        class="px-md-3"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+  import InfoCard from '../generic/InfoCard.vue';
+
   const EUROPEANA_NUMBERS = 'Europeana numbers';
 
   export default {
     name: 'LandingAutomatedCardGroup',
+
+    components: {
+      InfoCard
+    },
 
     props: {
       genre: {
@@ -16,54 +36,51 @@
         required: true
       }
     },
-
     data() {
       const data = {
         keys: null,
-        cardType: null,
-        headline: null,
-        contentful: null,
-        galleries: null,
-        daily: false,
+        title: null,
         entries: []
       };
-
       if (this.genre === EUROPEANA_NUMBERS) {
         data.keys = ['matomo/visits', 'items/type-counts', 'collections/organisations/count'];
-        data.headline = this.$i18n.t('automatedCardGroup.europeanaNumbers');
+        data.title = this.$i18n.t('landing.europeanaNumbers');
       }
       return data;
     },
-
     async fetch() {
-      this.keys.forEach(async key => {
-        const cachedData = await this.fetchCachedData(key);
-        this.entries.push({ [key]: cachedData });
-      });
+      if (this.genre === EUROPEANA_NUMBERS) {
+        for (const key of this.keys) {
+          const cachedData = await this.fetchCachedData(key);
+          const entry = {};
+          if (key === 'matomo/visits') {
+            entry.label = 'visits';
+            entry.count = cachedData;
+          }
+          if (key === 'items/type-counts') {
+            entry.label = 'items';
+            entry.count = cachedData.map(data => data.count).reduce((a, b) => a + b);
+          }
+          if (key === 'collections/organisations/count') {
+            entry.label = 'providingInstitutions';
+            entry.count = cachedData;
+          }
+          this.entries.push(entry);
+        }
+      }
     },
-
     computed: {
       hasPartCollectionItems() {
         let items;
-
-        switch (this.genre) {
-        case EUROPEANA_NUMBERS:
+        if (this.genre === EUROPEANA_NUMBERS) {
           items = this.entries?.map(entry => ({
-            __typename: this.cardType,
-            url: this.searchFromType(entry.label),
-            info: this.$i18n.n(entry.count),
-            label: this.$t(`facets.TYPE.options.${entry.label}`),
-            image: this.infoImageFromType(entry.label)
+            info: this.$i18n.n(this.roundedNumber(entry.count)) + ' +',
+            label: this.$t(`landing.counts.${entry.label}`)
           }));
-          break;
-        default:
-          items = [];
         }
-
-        return items;
+        return items || [];
       }
     },
-
     methods: {
       fetchCachedData(key) {
         if (process.server) {
@@ -75,7 +92,32 @@
           return this.$axios.get(`/_api/cache/${key}`, { baseURL: window.location.origin })
             .then((response) => response.data);
         }
+      },
+      roundedNumber(number) {
+        const precision = 2;
+        const numberLength = number.toString().length;
+        // Number is always rounded down as a plus (+) is added to the count string.
+        const floorRoundedNumber =  Math.floor(number / (10 ** (numberLength - precision))) * (10 ** (numberLength - precision));
+        return floorRoundedNumber.toPrecision(precision);
       }
     }
   };
 </script>
+
+<style lang="scss" scoped>
+  @import '@europeana/style/scss/variables';
+
+  h3 {
+    font-family: $font-family-ubuntu;
+    font-size: $font-size-medium;
+    font-weight: 500;
+
+    @media (min-width: $bp-medium) {
+      font-size: $font-size-large;
+    }
+
+    @media (min-width: $bp-4k) {
+      font-size: $font-size-large-4k;
+    }
+  }
+</style>
