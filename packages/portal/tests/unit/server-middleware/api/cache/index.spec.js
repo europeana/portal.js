0a59b72be423b33d5637c9b6f8a189df2e91ce9f
Richard Doe <richard.doe@rwdit.net>
2023-08-30 11:18:58 +0100

    feat: fetch multiple cache entries (#2052)

--- packages/portal/tests/unit/server-middleware/api/cache/index.spec.js
+++ packages/portal/tests/unit/server-middleware/api/cache/index.spec.js
@@ -15,6 +15,7 @@ const cached = JSON.stringify([
   { id: '1' },
   { id: '2' }
 ]);
+const expressReq = { params: [id], query: {} };
 
 const redisClientStub = {
   getAsync: sinon.stub().resolves(cached),
@@ -31,20 +32,20 @@ describe('server-middleware/api/cache/index', () => {
   });
 
   it('fetches from the cache, with app namespace', async() => {
-    await serverMiddleware(id, config)({}, expressResStub);
+    await serverMiddleware(config)(expressReq, expressResStub);
 
     expect(redisClientStub.getAsync.calledWith(`@europeana:portal.js:${id}`)).toBe(true);
   });
 
   it('responds with JSON', async() => {
-    await serverMiddleware(id, config)({}, expressResStub);
+    await serverMiddleware(config)(expressReq, expressResStub);
 
     expect(expressResStub.set.calledWith('Content-Type', 'application/json')).toBe(true);
-    expect(expressResStub.send.calledWith(cached)).toBe(true);
+    expect(expressResStub.send.calledWith({ [id]: JSON.parse(cached) })).toBe(true);
   });
 
   it('quits the Redis connection', async() => {
-    await serverMiddleware(id, config)({}, expressResStub);
+    await serverMiddleware(config)(expressReq, expressResStub);
 
     expect(redisClientStub.quitAsync.called).toBe(true);
   });
