72764e1f013854d55d96337b6bf86d809b91cfdc
Richard Doe <richard.doe@rwdit.net>
2023-09-05 09:07:40 +0100

    chore: update redis pkg to v4 (#2059)
    * chore: update redis pkg to v4
    
    * chore: restore commented error handling
    
    * chore: update Redis TLS options for v4
    
    * chore: redis tls options

--- packages/portal/src/server-middleware/api/cache/index.js
+++ packages/portal/src/server-middleware/api/cache/index.js
@@ -1,22 +1,31 @@
-import { createRedisClient } from '../../../cachers/utils.js';
+import { createClient } from 'redis';
 import { errorHandler } from '../index.js';
 
 const cacheKey = (id) => `@europeana:portal.js:${id.replace(/\//g, ':')}`;
+
+const createRedisClient = (config = {}) => {
+  const redisClient = createClient(config);
+
+  redisClient.on('error', console.error);
+
+  return redisClient;
+};
 
 export const cached = async(ids, config = {}) => {
   if (!config.url) {
     return Promise.reject(new Error('No cache configured.'));
   }
   const redisClient = createRedisClient(config);
+  await redisClient.connect();
 
   const values = {};
   for (const id of [].concat(ids)) {
     const key = cacheKey(id);
-    const value = await redisClient.getAsync(key);
+    const value = await redisClient.get(key);
     values[id] = JSON.parse(value);
   }
 
-  redisClient.quitAsync();
+  redisClient.disconnect();
 
   return values;
 };
