672c02fd41577fa22f533b1ee63a8e86f5b40de1
lbiedinger <lutz.biedinger@gmail.com>
2023-09-14 08:30:08 +0200

    update migration with help text for organisations

--- packages/contentful/migrations/0108-update-person-fields-multilingual.cjs
+++ packages/contentful/migrations/0108-update-person-fields-multilingual.cjs
@@ -8,5 +8,13 @@ module.exports = function(migration) {
   person
     .editField('affiliation')
     .localized(true);
-
+  person.changeFieldControl(
+    'affiliation',
+    'builtin',
+    'singleLine',
+    {
+      helpText:
+        'The English value is the default value for all other locales. An English label must be provided as a fallback, otherwise the organisation will only show when using the language(s) where it is specified.'
+    }
+  );
 };
