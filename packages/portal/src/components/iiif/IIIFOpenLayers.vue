9cee4afc2b41cad4889ec4f2dddb2e1eaa74434c
Richard Doe <richard.doe@rwdit.net>
2023-09-18 17:33:31 +0100

    feat: load thumbnail until click

--- packages/portal/src/components/iiif/IIIFOpenLayers.vue
+++ packages/portal/src/components/iiif/IIIFOpenLayers.vue
@@ -2,7 +2,7 @@
   <div
     id="iiif-open-layers"
     class="iiif-viewer-inner-wrapper h-100 d-flex flex-column"
-  ></div>
+  />
 </template>
 
 <script>
@@ -38,15 +38,18 @@
         imageInfo: null,
         manifest: null,
         map: null,
-        resource: null
-      }
+        resource: null,
+        thumbnail: null
+      };
     },
 
     async fetch() {
       const manifestResponse = await axios.get(this.uri);
       this.manifest = manifestResponse.data;
 
-      this.resource = this.manifest.sequences?.[0]?.canvases?.[0]?.images?.[0]?.resource;
+      const canvas = this.manifest.sequences?.[0]?.canvases?.[0];
+      this.resource = canvas?.images?.[0]?.resource;
+      this.thumbnail = canvas?.thumbnail;
 
       const serviceId = this.resource?.service?.['@id'];
       if (serviceId) {
@@ -57,79 +60,108 @@
     },
 
     watch: {
-      resource: {
+      thumbnail: {
         deep: true,
-        handler: 'renderImage'
+        handler: 'renderThumbnail'
       }
     },
 
     mounted() {
-      this.renderImage();
+      this.renderThumbnail();
     },
 
     methods: {
-      async renderImage() {
+      async drawMap() {
         const { default: Map } = await import('ol/Map.js');
-        const { default: View } = await import('ol/View.js');
-
         if (!this.map) {
           this.map = new Map({
+            controls: [],
             layers: [],
-            target: 'iiif-open-layers',
+            target: 'iiif-open-layers'
           });
+
+          this.map.getInteractions().forEach((interaction) => interaction.setActive(false));
         }
+      },
+
+      async renderThumbnail() {
+        if (!this.thumbnail) {
+          return;
+        }
+        await this.renderStaticImage(this.thumbnail['@id'], 400, 400);
+        this.map.on('singleclick', this.renderImage);
+      },
+
+      async renderIIIFImage() {
+        // IIIF Image API
+        // https://openlayers.org/en/latest/examples/iiif.html
+        const { default: View } = await import('ol/View.js');
+        const { default: IIIF } = await import('ol/source/IIIF.js');
+        const { default: IIIFInfo } = await import('ol/format/IIIFInfo.js');
+        const { default: TileLayer } = await import('ol/layer/Tile.js');
+
+        await this.drawMap();
+
+        const layer = new TileLayer();
+        this.map.setLayers([layer]);
+
+        const options = new IIIFInfo(this.imageInfo).getTileSourceOptions();
+        options.zDirection = -1;
+        const iiifTileSource = new IIIF(options);
+        layer.setSource(iiifTileSource);
+        this.map.setView(
+          new View({
+            resolutions: iiifTileSource.getTileGrid().getResolutions(),
+            extent: iiifTileSource.getTileGrid().getExtent(),
+            constrainOnlyCenter: true
+          })
+        );
+        this.map.getView().fit(iiifTileSource.getTileGrid().getExtent());
+      },
+
+      async renderStaticImage(url, width, height) {
+        // Static image
+        // https://openlayers.org/en/latest/examples/static-image.html
+        const { default: View } = await import('ol/View.js');
+        const { default: ImageLayer } = await import('ol/layer/Image.js');
+        const { default: Projection } = await import('ol/proj/Projection.js');
+        const { default: Static } = await import('ol/source/ImageStatic.js');
+        const { getCenter } = await import('ol/extent.js');
+
+        await this.drawMap();
+
+        const extent = [0, 0, width, height];
+        const projection = new Projection({
+          code: 'static', // necessary?
+          units: 'pixels',
+          extent
+        });
+        const layer = new ImageLayer({
+          source: new Static({
+            url: this.resource['@id'],
+            projection,
+            imageExtent: extent
+          })
+        });
+        this.map.setLayers([layer]);
+        this.map.setView(new View({
+          projection,
+          center: getCenter(extent),
+          zoom: 1,
+          maxZoom: 8
+        }));
+      },
+
+      async renderImage() {
+        await this.drawMap();
 
         if (this.imageInfo) {
-          // IIIF Image API
-          // https://openlayers.org/en/latest/examples/iiif.html
-          const { default: IIIF } = await import('ol/source/IIIF.js')
-          const { default: IIIFInfo } = await import('ol/format/IIIFInfo.js');
-          const { default: TileLayer } = await import('ol/layer/Tile.js');
-
-          const layer = new TileLayer();
-          this.map.setLayers([layer]);
-
-          const options = new IIIFInfo(this.imageInfo).getTileSourceOptions();
-          options.zDirection = -1;
-          const iiifTileSource = new IIIF(options);
-          layer.setSource(iiifTileSource);
-          this.map.setView(
-            new View({
-              resolutions: iiifTileSource.getTileGrid().getResolutions(),
-              extent: iiifTileSource.getTileGrid().getExtent(),
-              constrainOnlyCenter: true,
-            })
-          );
-          this.map.getView().fit(iiifTileSource.getTileGrid().getExtent());
+          this.renderIIIFImage();
         } else if (this.resource?.['@id']) {
-          // Static image
-          // https://openlayers.org/en/latest/examples/static-image.html
-          const { default: ImageLayer } = await import('ol/layer/Image.js');
-          const { default: Projection } = await import('ol/proj/Projection.js');
-          const { default: Static } = await import('ol/source/ImageStatic.js');
-          const { getCenter } = await import('ol/extent.js');
-
-          const extent = [0, 0, this.resource.width, this.resource.height];
-          const projection = new Projection({
-            code: 'static', // necessary?
-            units: 'pixels',
-            extent: extent
-          });
-          const layer = new ImageLayer({
-            source: new Static({
-              url: this.resource['@id'],
-              projection: projection,
-              imageExtent: extent
-            }),
-          });
-          this.map.setLayers([layer]);
-          this.map.setView(new View({
-            projection: projection,
-            center: getCenter(extent),
-            zoom: 2,
-            maxZoom: 8
-          }));
+          this.renderStaticImage(this.resource['@id'], this.resource.width, this.resource.height);
         }
+
+        this.map.getInteractions().forEach((interaction) => interaction.setActive(true));
       }
     }
   };
