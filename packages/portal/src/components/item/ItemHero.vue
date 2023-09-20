d14436f0a4c985ed08e5235d8fc57c48dca51e4b
Richard Doe <richard.doe@rwdit.net>
2023-09-20 14:59:11 +0100

    chore: remove client-only

--- packages/portal/src/components/item/ItemHero.vue
+++ packages/portal/src/components/item/ItemHero.vue
@@ -5,15 +5,13 @@
       class="iiif-viewer-wrapper d-flex flex-column"
     >
       <slot name="item-language-selector" />
-      <client-only>
-        <IIIFOpenLayers
-          :uri="iiifPresentationManifest"
-          :search-query="$nuxt.context.from ? $nuxt.context.from.query.query : ''"
-          :aria-label="$t('actions.viewDocument')"
-          :item-id="identifier"
-          :provider-url="providerUrl"
-        />
-      </client-only>
+      <IIIFOpenLayers
+        :uri="iiifPresentationManifest"
+        :search-query="$nuxt.context.from ? $nuxt.context.from.query.query : ''"
+        :aria-label="$t('actions.viewDocument')"
+        :item-id="identifier"
+        :provider-url="providerUrl"
+      />
     </div>
     <ItemMediaSwiper
       v-else
