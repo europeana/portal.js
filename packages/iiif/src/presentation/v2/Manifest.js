af4790deb0d0a313aa9e0b3e1f0b1884c0ad2fcb
Richard Doe <richard.doe@rwdit.net>
2023-10-03 08:27:18 +0100

    chore: remove commented code

--- packages/iiif/src/presentation/v2/Manifest.js
+++ packages/iiif/src/presentation/v2/Manifest.js
@@ -9,24 +9,4 @@ export default class IIIFPresentationV2Manifest extends Base {
     const canvases = sequences.map((seq) => seq.canvases).flat();
     this.canvases = canvases.map((cnv) => new Canvas(cnv));
   }
-
-  // get paintings() {
-  //   return this.canvas.images?.filter((image) => image.motivation === 'sc:painting') || [];
-  // }
-  //
-  // get image() {
-  //   return this.painting?.resource;
-  // }
-  //
-  // get imageService() {
-  //   return this.image?.service;
-  // }
-  //
-  // get thumbnail() {
-  //   return this.canvas?.thumbnail;
-  // }
-  //
-  // get annotationsUrl() {
-  //   return this.canvas?.otherContent?.[0];
-  // }
 }
