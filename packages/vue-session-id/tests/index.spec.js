8b436994040df6eaebd893369c232cb7e678b65d
Richard Doe <richard.doe@rwdit.net>
2023-10-19 14:50:00 +0100

    chore: linting

--- packages/vue-session-id/tests/index.spec.js
+++ packages/vue-session-id/tests/index.spec.js
@@ -1,5 +1,3 @@
-import sinon from 'sinon';
-
 import VueSessionId from '@/index.js';
 
 class Vue {}
@@ -7,11 +5,9 @@ class Vue {}
 describe('VueSessionId', () => {
   describe('install', () => {
     it('adds property $sessionId to Vue prototype', () => {
-      // const vue = sinon.spy();
-      // const vue = new Vue;
       VueSessionId.install(Vue);
 
       expect(Vue.prototype.$sessionId).toBeDefined();
-    })
+    });
   });
 });
