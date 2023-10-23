e8aa1036099191e32af8f53b2797e1d8485dc11c
Richard Doe <richard.doe@rwdit.net>
2023-10-23 11:02:40 +0100

    chore: revert unused change

--- packages/portal/src/mixins/canonicalUrl.js
+++ packages/portal/src/mixins/canonicalUrl.js
@@ -1,11 +1,10 @@
 export default {
   methods: {
-    canonicalUrl({ fullPath = true, locale = true, route = null } = {}) {
-      route = route || this.$route;
-      let path = fullPath ? route.fullPath : route.path;
+    canonicalUrl({ locale = true, fullPath = true } = {}) {
+      let path = fullPath ? this.$route.fullPath : this.$route.path;
 
       if (!locale) {
-        if (route.path === `/${this.$i18n.locale}`) {
+        if (this.$route.path === `/${this.$i18n.locale}`) {
           path = path.replace(this.$i18n.locale, '');
         } else if (path.startsWith(`/${this.$i18n.locale}/`)) {
           path = path.slice(3);
