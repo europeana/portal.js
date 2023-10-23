4d02d5f9c851b285d95f69dae1add6bdbbc3d4c2
Richard Doe <richard.doe@rwdit.net>
2023-10-23 10:53:31 +0100

    refactor: move error handler to utils file

--- packages/portal/src/server-middleware/api/index.js
+++ packages/portal/src/server-middleware/api/index.js
@@ -58,16 +58,4 @@ app.get('/version', version);
 
 app.all('/*', (req, res) => res.sendStatus(404));
 
-export const errorHandler = (res, error) => {
-  let status = error.status || 500;
-  let message = error.message;
-
-  if (error.response) {
-    status = error.response.status;
-    message = error.response.data.errorMessage;
-  }
-
-  res.status(status).set('Content-Type', 'text/plain').send(message);
-};
-
 export default app;
