5cf9fc2a09a90b08959841c126b3235f186d3645
Richard Doe <richard.doe@rwdit.net>
2023-10-30 08:59:53 +0000

    feat: weighted item interactions
    add > like > download > view

--- packages/portal/src/server-middleware/api/events/trending.js
+++ packages/portal/src/server-middleware/api/events/trending.js
@@ -12,13 +12,31 @@ export default (config = {}) => {
       }
 
       const selectObjectResult = await pg.query(`
-        SELECT o.uri, COUNT(a.id) AS num
-        FROM events.actions a
-        LEFT JOIN events.objects o ON a.object_id=o.id
-        WHERE a.occurred_at > (current_date - INTERVAL '1 day')
-        AND o.uri LIKE '%/item/%'
-        GROUP BY o.uri
-        ORDER BY num DESC
+        SELECT uri,
+               SUM(action_score) score
+        FROM
+          (SELECT uri,
+                  num * action_weight AS action_score
+           FROM
+             (SELECT o.uri,
+                     at.name,
+                     COUNT(a.id) AS num,
+                     CASE
+                         WHEN at.name = 'view' THEN 1
+                         WHEN at.name = 'download' THEN 2
+                         WHEN at.name = 'like' THEN 4
+                         WHEN at.name = 'add' THEN 6
+                         ELSE 0
+                     END AS action_weight
+              FROM events.actions a
+              LEFT JOIN events.objects o ON a.object_id = o.id
+              LEFT JOIN events.action_types AT ON a.action_type_id = at.id
+              WHERE a.occurred_at > (CURRENT_DATE - INTERVAL '1 day')
+                AND o.uri LIKE '%/item/%'
+              GROUP BY o.uri,
+                       at.name) AS action_counts) AS action_scores
+        GROUP BY uri
+        ORDER BY score DESC
         LIMIT 10
         `
       );
