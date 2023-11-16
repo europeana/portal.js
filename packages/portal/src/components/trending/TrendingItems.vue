fcebbb9e3d8e87410e5f31594d3474a6794d95f7
Leonie Peters <leoniepeters@gmail.com>
2023-11-16 15:07:32 +0100

    Add trending items mosaic to automated card group

---
+++ packages/portal/src/components/trending/TrendingItems.vue
@@ -0,0 +1,62 @@
+<template>
+  <section
+    v-if="items"
+    class="browse-section row mb-5"
+    data-qa="browse section"
+  >
+    <div class="col-12 col-lg-6">
+      <h2
+        class="card-group-title"
+      >
+        {{ headline }}
+      </h2>
+    </div>
+    <div class="col-12">
+      <ItemPreviewCardMosaic
+        :items="items"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios';
+  import { recordIdFromUrl } from '@/plugins/europeana/record.js';
+  import ItemPreviewCardMosaic from '@/components/item/ItemPreviewCardMosaic';
+
+  export default {
+    name: 'TrendingItems',
+
+    components: {
+      ItemPreviewCardMosaic
+    },
+
+    props: {
+      headline: {
+        type: String,
+        default: null
+      }
+    },
+
+    data() {
+      return {
+        items: null
+      };
+    },
+
+    async fetch() {
+      const trendsResponse = await axios.create({
+        baseURL: this.$config.app.baseUrl
+      }).get('/_api/events/trending');
+
+      const itemIds = trendsResponse.data.items.map((item) => recordIdFromUrl(item.uri));
+
+      const findResponse = await this.$apis.record.find(itemIds);
+
+      // reorder based on trending
+      this.items = itemIds
+        .map((id) => findResponse.items.find((item) => item.id === id))
+        .filter((item) => !!item);
+    }
+  };
+</script>
