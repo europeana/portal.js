60e76280c4cb1edc417c9547708c145bbb71c9e3
Richard Doe <richard.doe@rwdit.net>
2023-11-17 10:19:18 +0000

    refactor: typecast view count to number

--- packages/portal/src/server-middleware/api/events/views.js
+++ packages/portal/src/server-middleware/api/events/views.js
@@ -17,15 +17,15 @@ export default (config = {}) => {
           COUNT(*)
         FROM events.actions a
         LEFT JOIN events.objects o ON a.object_id=o.id
-        LEFT JOIN events.action_types AT ON a.action_type_id=at.id
+        LEFT JOIN events.action_types at ON a.action_type_id=at.id
         WHERE a.occurred_at > (CURRENT_TIMESTAMP - INTERVAL '30 days')
           AND o.uri LIKE $1
-          AND AT.name LIKE 'view'
+          AND at.name='view'
         `,
       [url]
       );
 
-      res.json({ viewCount: selectResult.rows[0].count });
+      res.json({ viewCount: Number(selectResult.rows[0].count) });
     } catch (e) {
       console.error(e);
       res.sendStatus(500);
