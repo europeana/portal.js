311f1de9730052c3da108d9610b79ff29497fc5f
Richard Doe <richard.doe@rwdit.net>
2024-01-05 11:25:20 +0000

    chore: isolate styleguide from workspaces (#2169)
    * chore: isolate styleguide from workspaces
    
    * chore: restore package lock

--- styleguide/styleguide.config.cjs
+++ styleguide/styleguide.config.cjs
@@ -3,30 +3,47 @@ const { getWebpackConfig } = require('nuxt');
 const bootstrapVersion = require('bootstrap/package.json').version;
 const bootstrapVueVersion = require('bootstrap-vue/package.json').version;
 
+const FILTERED_PLUGINS = [
+	'WebpackBarPlugin',
+	'HtmlWebpackPlugin',
+	'VueSSRClientPlugin',
+	'HotModuleReplacementPlugin',
+	'FriendlyErrorsWebpackPlugin'
+];
+
 /** @type import("vue-styleguidist").Config */
 module.exports = async() => {
   // get the webpack config directly from nuxt
   const nuxtWebpackConfig = await getWebpackConfig('client', {
     for: process.env.NODE_ENV === 'production' ? 'build' : 'dev',
-    rootDir: '../portal'
+    rootDir: '../packages/portal'
   });
 
-  const webpackConfig = {
-    module: {
-      rules: [
-        ...nuxtWebpackConfig.module.rules.filter(
-          // remove the eslint-loader
-          a => a.loader !== 'eslint-loader'
-        )
-      ]
-    },
-    resolve: {
-      ...nuxtWebpackConfig.resolve
-    },
-    plugins: [
-      ...nuxtWebpackConfig.plugins
-    ]
-  };
+	const webpackConfig = {
+		module: {
+			rules: [
+				...nuxtWebpackConfig.module.rules.filter(
+					// remove the eslint-loader
+					a => a.loader !== 'eslint-loader'
+				)
+			]
+		},
+		resolve: {
+			...nuxtWebpackConfig.resolve,
+			alias: {
+				...nuxtWebpackConfig.resolve.alias,
+				// Prevent multiple instances of Vue
+	      // See: https://github.com/bootstrap-vue/bootstrap-vue/issues/3351
+	      vue$: resolve( __dirname, 'node_modules/vue/dist/vue.esm.js')
+			}
+		},
+		plugins: [
+			...nuxtWebpackConfig.plugins.filter(
+				// remove plugins that conflict with vue-styleguidist's
+				(p) => FILTERED_PLUGINS.indexOf(p.constructor.name) === -1
+			)
+		]
+	};
 
   return {
     title: 'Europeana Style Guide',
@@ -36,18 +53,18 @@ module.exports = async() => {
         sections: [
           {
             name: 'Font icons',
-            content: '../portal/docs/style/FontIcons.md'
+            content: '../packages/portal/docs/style/FontIcons.md'
           },
           {
             name: 'Bootstrap Vue',
             sections: [
               {
                 name: 'Badge',
-                content: '../portal/docs/style/BootstrapVueBadge.md'
+                content: '../packages/portal/docs/style/BootstrapVueBadge.md'
               },
               {
                 name: 'Button',
-                content: '../portal/docs/style/BootstrapVueButton.md'
+                content: '../packages/portal/docs/style/BootstrapVueButton.md'
               }
             ]
           }
@@ -58,7 +75,7 @@ module.exports = async() => {
         sections: [
           {
             name: 'Page',
-            components: '../portal/src/components/[A-Z]*.vue'
+            components: '../packages/portal/src/components/[A-Z]*.vue'
           }
         ].concat([
           'Account',
@@ -76,20 +93,22 @@ module.exports = async() => {
           'Theme'
         ].map((name) => ({
           name,
-          components: `../portal/src/components/${name.toLowerCase()}/[A-Z]*.vue`
+          components: `../packages/portal/src/components/${name.toLowerCase()}/[A-Z]*.vue`
         })))
       }
     ],
-    assetsDir: '../style',
+    assetsDir: '../packages/style',
     skipComponentsWithoutExample: true,
     require: [
-      resolve(__dirname, '../style/scss/style.scss'),
+			resolve(__dirname, '../packages/portal/src/plugins/vue-filters.js'),
+      resolve(__dirname, '../packages/style/scss/style.scss'),
       resolve(__dirname, './style.scss')
     ],
     renderRootJsx: resolve(__dirname, './styleguide.root.js'),
     template: {
       head: {
         links: [
+          // TODO: jsdelivr?
           { rel: 'stylesheet', href: `https://unpkg.com/bootstrap@${bootstrapVersion}/dist/css/bootstrap.min.css` },
           { rel: 'stylesheet', href: `https://unpkg.com/bootstrap-vue@${bootstrapVueVersion}/dist/bootstrap-vue.min.css` }
         ]
