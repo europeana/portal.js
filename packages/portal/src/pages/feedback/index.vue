d60632633d7bc5204b58b641a4ed73f346d6b922
Richard Doe <richard.doe@rwdit.net>
2024-01-05 10:44:47 +0000

    feat: add feedback page

---
+++ packages/portal/src/pages/feedback/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <FeedbackWidget />
+</template>
+
+<script>
+  import FeedbackWidget from '@/components/feedback/FeedbackWidget';
+  import versions from '../../../pkg-versions';
+
+  export default {
+    name: 'FeedbackIndexPage',
+
+    layout: 'minimal',
+
+    components: {
+      FeedbackWidget
+    },
+
+    head() {
+      return {
+        link: [
+          { rel: 'icon', href: require('@europeana/style/img/favicon.ico').default, type: 'image/x-icon' },
+          { rel: 'stylesheet', href: `https://cdn.jsdelivr.net/npm/bootstrap@${versions.bootstrap}/dist/css/bootstrap.min.css` },
+          { rel: 'stylesheet', href: `https://cdn.jsdelivr.net/npm/bootstrap-vue@${versions['bootstrap-vue']}/dist/bootstrap-vue.min.css` }
+        ]
+      };
+    }
+  };
+</script>
