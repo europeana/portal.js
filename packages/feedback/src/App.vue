0b888cc2aad45d898faf4f17db5456650e2e273b
Richard Doe <richard.doe@rwdit.net>
2024-01-09 14:55:39 +0000

    feat: add feedback package

---
+++ packages/feedback/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <FeedbackWidget />
+  </div>
+</template>
+
+<script>
+import FeedbackWidget from './components/FeedbackWidget.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FeedbackWidget
+  }
+}
+</script>
---
+++ packages/feedback/src/i18n.js
@@ -0,0 +1,66 @@
+export default {
+  "en": {
+    "actions": {
+      "accept": "Accept",
+      "add": "Add",
+      "apply": "Apply",
+      "cancel": "Cancel",
+      "clear": "Clear",
+      "clearFilters": "Clear filters",
+      "close": "close",
+      "continue": "continue",
+      "depublish": "Depublish",
+      "download": "Download",
+      "edit": "Edit",
+      "explore": "Explore",
+      "feedback": "Feedback",
+      "filter": "Filter",
+      "goHome": "Go to the homepage",
+      "hide": "Hide",
+      "learnMore": "Learn more",
+      "like": "Like",
+      "move": "Move",
+      "next": "Next",
+      "pin": "Pin",
+      "preview": "Preview",
+      "previous": "Previous",
+      "publish": "Publish",
+      "readMore": "Read more",
+      "reject": "Reject",
+      "requestDepublication": "Request depublication",
+      "save": "Save",
+      "send": "Send",
+      "share": "Share",
+      "shareOn": "Share on {social}",
+      "show": "Show",
+      "showLess": "Show less",
+      "showMore": "Show more",
+      "skipSend": "Skip and send",
+      "submitForPublication": "Submit for publication",
+      "submittedForPublication": "Submitted for publication",
+      "transcribe": "Transcribe",
+      "transcribeNow": "Transcribe now",
+      "viewAt": "View at {link}",
+      "viewDocument": "View document"
+    },
+    "feedback": {
+      "emailOptional": "We may wish to follow up. Enter your email if you're happy for us to contact you.",
+      "failed": "The request failed. Please try again.",
+      "faq": "Go to frequently asked questions",
+      "form": {
+        "placeholders": {
+          "email": "Enter your email address",
+          "feedback": "Enter your feedback here"
+        }
+      },
+      "policies": "By continuing, you agree to our {0} and acknowledge our {1}.",
+      "privacyPolicy": "Privacy Policy",
+      "success": "Your feedback has been sent.",
+      "termsOfService": "Terms of Service",
+      "thankYou": "Thank you!",
+      "title": "Send feedback",
+      "validEmail": "Enter a valid email address",
+      "validFeedback": "Your feedback has to consist of 5 words at minimum"
+    }
+  }
+};
