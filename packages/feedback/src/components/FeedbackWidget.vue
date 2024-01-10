5c5408aa3c251cf43d5188b1c7b815e32737ca22
Richard Doe <richard.doe@rwdit.net>
2024-01-10 10:48:09 +0000

    refactor: use vite for feedback package

--- packages/feedback/src/components/FeedbackWidget.vue
+++ packages/feedback/src/components/FeedbackWidget.vue
@@ -198,23 +198,6 @@
 
 <script>
   import axios from 'axios';
-  import Vue from 'vue'
-  import VueI18n from 'vue-i18n'
-  import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-  import i18nMessages from '../i18n.js'
-
-  Vue.use(BootstrapVue)
-  Vue.use(BootstrapVueIcons)
-  Vue.use(VueI18n)
-
-  import 'bootstrap/dist/css/bootstrap.css'
-  import 'bootstrap-vue/dist/bootstrap-vue.css'
-  import '@europeana/style'
-
-  const i18n = new VueI18n({
-    locale: 'en', // set locale
-    messages: i18nMessages, // set locale messages
-  })
 
   export default {
     name: 'FeedbackWidget',
@@ -273,8 +256,6 @@
         };
       }
     },
-
-    i18n,
 
     mounted() {
       window.addEventListener('scroll', () => this.bigButton = false, { once: true });
--- packages/feedback/src/main.js
+++ packages/feedback/src/main.js
@@ -1,9 +1,31 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+import { ButtonPlugin, FormPlugin, FormGroupPlugin, FormTextareaPlugin, LinkPlugin } from 'bootstrap-vue'
+
 
 import App from './App.vue'
+import i18nMessages from './i18n.js'
+
+Vue.use(VueI18n)
+Vue.use(LinkPlugin)
+Vue.use(ButtonPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormTextareaPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@europeana/style'
+
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages: i18nMessages, // set locale messages
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
