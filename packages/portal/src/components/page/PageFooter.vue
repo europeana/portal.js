8c4d167d7a8ce6ccab5e6477938ef6b5120ea75a
Richard Doe <richard.doe@rwdit.net>
2024-02-05 11:00:38 +0000

    chore: feedback widget v0.2.0

--- packages/portal/src/components/page/PageFooter.vue
+++ packages/portal/src/components/page/PageFooter.vue
@@ -115,12 +115,12 @@
       />
       <script
         type="module"
-        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0-rc.2/dist/europeana-feedback-widget.js"
-      integrity="sha384-FH4xHXqEZ96qtMH9pc1bD0YLjOmK3UlX/UtIzj4pqG1RUcyUPXC5lTFkLGqTFO8G"
+        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0/dist/europeana-feedback-widget.js"
+        integrity="sha384-u9uk9KOvOrG5X4yAGn1xMj1P5YmrJWanSqij7tvI4eZTjI/UQGK98lq3e/I6XfuP"
       />
       <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0-rc.2/dist/europeana-feedback-widget.css"
+        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0/dist/europeana-feedback-widget.css"
       >
     </client-only>
   </footer>
