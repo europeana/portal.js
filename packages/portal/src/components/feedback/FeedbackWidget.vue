ea6916dd749117f6ee2b461c48377c2b2fd81772
Richard Doe <richard.doe@rwdit.net>
2024-03-07 10:10:55 +0000

    chore: prevent eslint closing script tag
    which SonarCloud objects to

--- packages/portal/src/components/feedback/FeedbackWidget.vue
+++ packages/portal/src/components/feedback/FeedbackWidget.vue
@@ -12,7 +12,9 @@
         type="module"
         src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0/dist/europeana-feedback-widget.js"
         integrity="sha384-u9uk9KOvOrG5X4yAGn1xMj1P5YmrJWanSqij7tvI4eZTjI/UQGK98lq3e/I6XfuP"
-      />
+      >
+        <!-- prevent eslint closing this -->
+      </script>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.2.0/dist/europeana-feedback-widget.css"
