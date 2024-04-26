21884abe67eb4116aeeea0b66a2b348a40801f17
lbiedinger <lutz.biedinger@gmail.com>
2024-04-26 16:24:37 +0200

    specs

--- packages/v-visible-on-scroll/src/index.js
+++ packages/v-visible-on-scroll/src/index.js
@@ -2,8 +2,10 @@ export default {
   inserted: (el, binding, vnode) => {
     const routeHWithHash = vnode.context.$route.hash;
     el.scrolledVisible = !routeHWithHash;
+    el.setAttribute('style', 'transform: translate3d(0, 0, 0)');
     if (process.browser) {
       window.addEventListener('scroll', () => handleScroll(el));
+      // TODO: update to observe ScreenOrientation update instead: screen.orientation.addEventListener("change", (event) => {
       window.addEventListener('orientationchange', () => handleOrientationChange(el));
       window.addEventListener('hashchange', () => el.scrolledVisible = false);
     }
@@ -11,12 +13,16 @@ export default {
 };
 
 const handleScroll = (el) => {
+  const oldPosition = el.scrollPosition ?? 0;
   const newPosition = window.scrollY;
-  if (onDesktop() && el.scrollPosition < newPosition && el.scrollPosition > 150 && el.scrolledVisible) {
+
+  // TODO: When scrolling a lot at once, it's possible to skip the 150px threshold on the first scroll event.
+  // Should we check that the new position is > 150 instead to prevent this?
+  if (onDesktop() && oldPosition < newPosition && oldPosition > 150 && el.scrolledVisible) {
     el.setAttribute('style', 'transform: translate3d(0, -100%, 0)');
     el.scrolledVisible = false;
     el.classList.remove('show');
-  } else if (onDesktop() && ((el.scrollPosition - 5) > newPosition || el.scrollPosition <= 150) && !el.scrolledVisible) {
+  } else if (onDesktop() && ((oldPosition - 5) > newPosition || oldPosition <= 150) && !el.scrolledVisible) {
     el.setAttribute('style', 'transform: translate3d(0, 0, 0)');
     el.classList.add('show');
     el.scrolledVisible = true;
@@ -26,13 +32,13 @@ const handleScroll = (el) => {
 
 const handleOrientationChange = (el) => {
   const orientation = window.orientation;
-  if (orientation === 0 || orientation === 180) {
+  if (orientation === 0 || orientation === 180) { // TODO: Aside from being deprecated, what is this checking?
     el.setAttribute('style', 'transform: translate3d(0, 0, 0)');
     el.scrolledVisible = true;
   }
 };
 
-const desktopWidth = 992;
+const desktopWidth = 992; // Needs to match $bp-large
 
 const onDesktop = () => {
   return desktopWidth <= document.documentElement.clientWidth;
