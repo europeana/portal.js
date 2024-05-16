6427cfca69de05e238371b1e6f0d169407cfe969
lbiedinger <lutz.biedinger@gmail.com>
2024-05-16 09:47:26 +0200

    transform blog entries to stories

--- packages/contentful/migrations/0133-create-story-content-type.cjs
+++ packages/contentful/migrations/0133-create-story-content-type.cjs
@@ -1,3 +1,5 @@
+const MurmurHash3 = require('imurmurhash');
+
 module.exports = function(migration) {
   if (!process.env.ENTITY_SUGGEST_APP_ID) {
     console.log('No app ID specified in ENTITY_SUGGEST_APP_ID; aborting.');
@@ -226,4 +228,31 @@ module.exports = function(migration) {
   story.changeFieldControl('genre', 'builtin', 'checkbox');
 
   story.changeFieldControl('relatedLink', 'app', process.env.ENTITY_SUGGEST_APP_ID);
+
+  migration.transformEntriesToType({
+    sourceContentType: 'blogPosting',
+    targetContentType: 'story',
+    shouldPublish: 'preserve',
+    updateReferences: false,
+    removeOldEntries: false,
+    identityKey(fields) {
+      const value = fields.identifier['en-GB'].toString();
+      return MurmurHash3(value).result().toString();
+    },
+    transformEntryForLocale(fromFields, currentLocale) {
+      return {
+        name: fromFields.name ? fromFields.name[currentLocale] : undefined,
+        identifier: fromFields.identifier,
+        description: fromFields.description ? fromFields.description[currentLocale] : undefined,
+        primaryImageOfPage: fromFields.primaryImageOfPage ? fromFields.primaryImageOfPage[currentLocale] : undefined,
+        hasPart: fromFields.hasPart ? fromFields.hasPart[currentLocale] : undefined,
+        datePublished: fromFields.datePublished ? fromFields.datePublished[currentLocale] : undefined,
+        author: fromFields.author ? fromFields.author[currentLocale] : undefined,
+        relatedLink: fromFields.relatedLink ? fromFields.relatedLink[currentLocale] : undefined,
+        contentWarning: fromFields.contentWarning ? fromFields.contentWarning[currentLocale] : undefined,
+        categories: fromFields.categories ? fromFields.categories[currentLocale] : undefined,
+        genre: fromFields.genre ? fromFields.genre[currentLocale] : undefined
+      };
+    }
+  });
 };
