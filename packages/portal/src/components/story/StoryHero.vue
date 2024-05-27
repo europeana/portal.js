3d1a13e63b8f22e702c89b3103e0f3848f75ba19
Leonie Peters <leoniepeters@gmail.com>
2024-05-27 10:30:42 +0200

    Create util for reusable CSS vars presets

--- packages/portal/src/components/story/StoryHero.vue
+++ packages/portal/src/components/story/StoryHero.vue
@@ -46,18 +46,7 @@
   import parseMarkdownHtmlMixin from '@/mixins/parseMarkdownHtml';
   import { gsap } from 'gsap';
   import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-  const CSS_VARS_PRESETS = {
-    small: { w: 576, h: 896, fit: 'fill' },
-    medium: { w: 768, h: 1080, fit: 'fill' },
-    large: { w: 992, h: 1080, fit: 'fill' },
-    xl: { w: 1200, h: 1080, fit: 'fill' },
-    xxl: { w: 1400, h: 1080, fit: 'fill' },
-    xxxl: { w: 1880, h: 1080, fit: 'fill' },
-    wqhd: { w: 2520, h: 1440, fit: 'fill' },
-    '4k': { w: 3020, h: 1440, fit: 'fill' },
-    '4k+': { w: 3840, h: 2160, fit: 'fill' }
-  };
+  import { FULL_VIEWPORT_CSS_VARS_PRESETS as CSS_VARS_PRESETS } from '@/utils/europeana/imageCropPresets';
 
   export default {
     name: 'StoryHero',
