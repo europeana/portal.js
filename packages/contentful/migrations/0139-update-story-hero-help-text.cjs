20c61b8126cf838ec113f440a8cc1547585aee0f
Lutz Biedinger <lutz.biedinger@gmail.com>
2024-06-13 16:26:46 +0200

    Delete packages/contentful/migrations/0139-update-story-hero-help-text.cjs

--- packages/contentful/migrations/0139-update-story-hero-help-text.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = function(migration) {
-  const story = migration
-    .editContentType('story');
-  story.changeFieldControl('primaryImageOfPage', 'builtin', 'entryLinkEditor', {
-    helpText: 'To use the whole width header: Images wider than 1000 px are required, others will be aligned with the content. For best quality it\'s advised to use images of at least 2,520 px width.'
-  });
-};
