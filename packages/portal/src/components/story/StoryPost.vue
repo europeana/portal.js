729ed5c742c48c3d56dac7db6f8fa9e889cca665
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2024-06-14 10:25:35 +0200

    chore: update text page text styles (#2364)
    * Clean up text-page line-heights
    
    * Increase subtitle and p.lead mobile font-size
    
    * Update subtitle and p.lead (description) styles
    
    * Fix AuthoredHead margins
    
    * Update h2 and h3 styles
    
    * Increase body text only on larger than mobile
    
    * Update and clean up blockquote and cite styles
    
    * Fix related-heading styles
    
    * Semibold text page h2
    
    * Fix exhibtion chapters h2 title
    
    * chore: spacing
    
    ---------
    
    Co-authored-by: Richard Doe <richard.doe@rwdit.net>

--- packages/portal/src/components/story/StoryPost.vue
+++ packages/portal/src/components/story/StoryPost.vue
@@ -30,6 +30,7 @@
             <p
               v-if="showDescriptionInArticle"
               class="lead"
+              :class="{ 'color-black': enableStoryHero }"
               data-qa="article description"
             >
               {{ description }}
@@ -264,8 +265,10 @@
   }
 
   .text-page p.lead {
-    font-size: $font-size-medium;
-    color: $black;
-    margin-bottom: 1.5rem;
+    margin-bottom: 2rem;
+
+    &.color-black {
+      color: $black;
+    }
   }
 </style>
