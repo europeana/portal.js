b2b1511fd6a6be5f291a3b2876fb9bd247c2dd1f
Leonie Peters <leoniepeters@gmail.com>
2024-06-25 12:54:12 +0200

    Align home page card groups and browse pages with 4 col layout

--- packages/portal/src/components/home/HomeLatestGalleries.vue
+++ packages/portal/src/components/home/HomeLatestGalleries.vue
@@ -30,48 +30,9 @@
   @import '@europeana/style/scss/variables';
 
   ::v-deep {
-    h2.card-group-title {
-      color: $mediumgrey;
-      font-size: 2rem;
-      font-weight: 400;
-      text-align: center;
-
-      @media (min-width: $bp-extralarge) {
-        font-size: $font-size-xxl;
-      }
-
-      @media (min-width: $bp-4k) {
-        font-size: $font-size-xxl-4k;
-      }
-    }
-
     .col-12 {
       margin-right: auto;
       margin-left: auto;
-    }
-
-    .card-deck {
-      flex-flow: row wrap;
-      justify-content: center;
-      margin-top: 1.25rem;
-      margin-bottom: 2.25rem;
-
-      @media (min-width: $bp-4k) {
-        margin-top: calc( 1.5 * 1.25rem);
-        margin-bottom: calc( 1.5 * 2.25rem);
-      }
-
-      .content-card.card {
-        @media (max-width: ($bp-medium - 1px)) {
-          &:last-child {
-            margin-bottom: 0;
-          }
-        }
-
-        @media (min-width: $bp-large) {
-          margin-bottom: 0;
-        }
-      }
     }
   }
 
--- packages/portal/src/components/home/HomeLatestStories.vue
+++ packages/portal/src/components/home/HomeLatestStories.vue
@@ -1,14 +1,13 @@
 <template>
   <section
     v-if="cards.length > 0"
-    class="text-center"
     data-qa="latest editorial"
   >
-    <h2>
+    <h2 class="card-group-title">
       {{ $t('homePage.discoverEditorial') }}
     </h2>
     <b-card-group
-      class="card-deck-3-cols justify-content-center"
+      class="card-deck-4-cols justify-content-center"
       deck
     >
       <!-- TODO: use/add image alt description -->
@@ -74,41 +73,3 @@
     }
   };
 </script>
-
-<style lang="scss" scoped>
-  @import '@europeana/style/scss/variables';
-
-  h2 {
-    color: $mediumgrey;
-    font-size: 2rem;
-
-    @media (min-width: $bp-extralarge) {
-      font-size: $font-size-xxl;
-    }
-
-    @media (min-width: $bp-4k) {
-      font-size: $font-size-xxl-4k;
-    }
-  }
-
-  .card-deck {
-    flex-flow: row wrap;
-    margin-top: 2.25rem;
-    margin-bottom: 2.25rem;
-
-    @media (min-width: $bp-4k) {
-      margin-top: calc( 1.5 * 2.25rem);
-      margin-bottom: calc( 1.5 * 2.25rem);
-    }
-
-    .content-card.card {
-      &:last-child {
-        margin-bottom: 0;
-      }
-
-      @media (min-width: $bp-medium) {
-        margin-bottom: 0;
-      }
-    }
-  }
-</style>
