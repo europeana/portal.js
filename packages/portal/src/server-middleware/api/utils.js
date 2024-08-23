d3b7777203495884deab9789a6c7e23606fbe3a6
Richard Doe <richard.doe@rwdit.net>
2024-08-23 13:29:25 +0100

    fix: shared error logging

--- packages/portal/src/server-middleware/api/utils.js
+++ packages/portal/src/server-middleware/api/utils.js
@@ -1,22 +1,24 @@
+import apm from 'elastic-apm-node';
 import defu  from 'defu';
 import createHttpError from 'http-errors';
 
 import nuxtConfig from '../../../nuxt.config.js';
 
-export const errorHandler = (err, req, res) => {
-  console.error(err);
+export const errorHandler = (err, req, res, next) => {
+  if (err) {
+    console.error(process.NODE_ENV === 'production' ? err.message : err.stack);
 
-  let status = err.status || 500;
-  let message = err.message;
+    const errorStatus = err.response?.status || err.status || err.statusCode || 500;
+    const message = err.response?.data?.error || err.response?.data?.errorMessage || err.message;
 
-  if (err.response) {
-    status = err.response.status;
-    if (err.response.data.errorMessage) {
-      message = err.response.data.errorMessage;
+    if (apm.isStarted()) {
+      apm.captureError(err, { message, request: req, response: res });
     }
-  }
 
-  res.status(status).set('Content-Type', 'text/plain').send(message);
+    res.status(errorStatus).send(message);
+  } else {
+    next();
+  }
 };
 
 let runtimeConfig;
