274a8a3bb9a7f3167031d78fcbff16b08330d66b
Lutz Biedinger <1177719+lbiedinger@users.noreply.github.com>
2024-09-03 12:52:02 +0200

    chore: test coverage for feature voting (#2427)
    * specs coverage for feature voting
    
    * linting: add newline
    
    * test: spec error handler when no error
    
    * set auth req headers in spec
    
    * add spec for fetching from votes API
    
    ---------
    
    Co-authored-by: Richard Doe <richard.doe@rwdit.net>

--- packages/portal/tests/unit/components/generic/FeatureIdeas.spec.js
+++ packages/portal/tests/unit/components/generic/FeatureIdeas.spec.js
@@ -9,6 +9,17 @@ import FeatureIdeas from '@/components/generic/FeatureIdeas.vue';
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
+const features = [
+  { identifier: 'feature-1', name: 'Feature 1', text: 'Feature 1 text' },
+  { identifier: 'feature-2', name: 'Feature 2', text: 'Feature 2 text' }
+];
+
+const config = {
+  app: {
+    baseUrl: 'https://www.example.org'
+  }
+};
+
 const factory = ({ propsData = {}, mocks = {} } = {}) => {
   return shallowMountNuxt(FeatureIdeas, {
     localVue,
@@ -17,11 +28,7 @@ const factory = ({ propsData = {}, mocks = {} } = {}) => {
       $auth: {
         loggedIn: false
       },
-      $config: {
-        app: {
-          baseUrl: 'https://www.example.org'
-        }
-      },
+      $config: config,
       $error: sinon.spy(),
       $fetchState: {},
       $nuxt: { context: {} },
@@ -53,23 +60,137 @@ describe('components/generic/FeatureIdeas', () => {
       });
     });
 
-    // describe('when there are features', () => {
-    //   it('fetches the features votes from the API', async() => {
-    //   });
-    // });
+    describe('when there are features', () => {
+      it('fetches the features votes from the API', async() => {
+        const expectedVotesResponse = {
+          'feature-1': { total: 12 },
+          'feature-2': { total: 21 }
+        };
+
+        const wrapper = factory({ propsData: { features } });
+
+        nock(config.app.baseUrl)
+          .get('/_api/votes?candidate=feature-1,feature-2')
+          .reply(200, expectedVotesResponse);
+
+        await wrapper.vm.fetch();
+
+        expect(nock.isDone()).toBe(true);
+        expect(wrapper.vm.votesOnFeatures).toEqual(expectedVotesResponse);
+      });
+    });
   });
 
   describe('when there are features', () => {
     it('renders a list of features', async() => {
-      const features = [
-        { name: 'Feature 1', text: 'Feature 1 text' },
-        { name: 'Feature 2', text: 'Feature 2 text' }
-      ];
       const wrapper = factory({ propsData: { features } });
 
       const featureCards = wrapper.findAll('[data-qa="feature idea card"]');
 
       expect(featureCards.length).toBe(features.length);
+    });
+  });
+
+  describe('methods', () => {
+    describe('headersForAuthorization', () => {
+      describe('when the user is logged in', () => {
+        it('returns the authorisation token', async() => {
+          const wrapper = factory({ mocks: { $auth: { loggedIn: true, getToken: () => 'authToken' } } });
+          expect(wrapper.vm.headersForAuthorization()).toEqual({ authorization: 'authToken' });
+        });
+      });
+      describe('when the user is NOT logged in', () => {
+        it('returns an empty object', async() => {
+          const wrapper = factory();
+          expect(wrapper.vm.headersForAuthorization()).toEqual({});
+        });
+      });
+    });
+
+    describe('voteOnFeature', () => {
+      describe('when the user is logged in', () => {
+        describe('when the user has not yet voted', () => {
+          it('sends a put request the vote endpoint, then re-fetches votes', async() => {
+            const wrapper = factory({ propsData: { features }, mocks: { $auth: { loggedIn: true, getToken: () => 'authToken' } } });
+
+            wrapper.vm.axiosInstance = sinon.spy();
+            wrapper.vm.$fetch = sinon.spy();
+
+            await wrapper.vm.voteOnFeature('feature-1');
+            const expectedPutRequestParams = {
+              url: '/_api/votes/feature-1',
+              method: 'put',
+              headers: { authorization: 'authToken' }
+            };
+
+            expect(wrapper.vm.axiosInstance.calledWith(sinon.match(expectedPutRequestParams))).toBe(true);
+            expect(wrapper.vm.$fetch.calledOnce).toBe(true);
+          });
+        });
+
+        describe('when the user already voted', () => {
+          it('delete to the vote endpoint', async() => {
+            const wrapper = factory({ propsData: { features }, mocks: { $auth: { loggedIn: true, getToken: () => 'authToken' } } });
+            wrapper.vm.votesOnFeatures = { 'feature-1': { votedByCurrentVoter: true } };
+
+            wrapper.vm.axiosInstance = sinon.spy();
+            wrapper.vm.$fetch = sinon.spy();
+
+            await wrapper.vm.voteOnFeature('feature-1');
+            const expectedDeleteRequestParams = {
+              url: '/_api/votes/feature-1',
+              method: 'delete',
+              headers: { authorization: 'authToken' }
+            };
+
+            expect(wrapper.vm.axiosInstance.calledWith(sinon.match(expectedDeleteRequestParams))).toBe(true);
+            expect(wrapper.vm.$fetch.calledOnce).toBe(true);
+          });
+        });
+      });
+
+      describe('when the user is NOT logged in', () => {
+        it('causes a keycloak login to be triggered', async() => {
+          const wrapper = factory({ propsData: { features } });
+
+          wrapper.vm.keycloakLogin = sinon.spy();
+
+          await wrapper.vm.voteOnFeature('feature-1');
+
+          expect(wrapper.vm.keycloakLogin.calledOnce).toBe(true);
+        });
+      });
+    });
+
+    describe('voteCountOnFeature', () => {
+      it('returns the features total from the retrieved totals', () => {
+        const wrapper = factory({ propsData: { features } });
+
+        wrapper.vm.votesOnFeatures = { 'feature-1': { total: 12 } };
+
+        expect(wrapper.vm.voteCountOnFeature('feature-1')).toBe(12);
+      });
+    });
+
+    describe('hasVotedOnFeature', () => {
+      describe('when the user has voted', () => {
+        it('returns true', () => {
+          const wrapper = factory({ propsData: { features } });
+
+          wrapper.vm.votesOnFeatures = { 'feature-1': { total: 12, votedByCurrentVoter: true } };
+
+          expect(wrapper.vm.hasVotedOnFeature('feature-1')).toBe(true);
+        });
+      });
+      describe('when the user has NOT voted', () => {
+        it('returns true', () => {
+          const wrapper = factory({ propsData: { features } });
+
+          wrapper.vm.votesOnFeatures = { 'feature-1': { total: 12 } };
+
+          expect(wrapper.vm.hasVotedOnFeature('feature-1')).toBe(false);
+        });
+      });
     });
   });
 });
