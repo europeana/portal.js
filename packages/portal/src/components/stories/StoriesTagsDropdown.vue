b10a564f7f8947a53fd280b426ff4312404c17e8
Leonie Peters <leoniepeters@gmail.com>
2024-09-18 10:16:27 +0200

    Remove incorrectly used aria-expanded attributes

--- packages/portal/src/components/stories/StoriesTagsDropdown.vue
+++ packages/portal/src/components/stories/StoriesTagsDropdown.vue
@@ -32,7 +32,6 @@
           aria-autocomplete="list"
           :aria-owns="showDropdown ? 'tags-options' : null"
           :aria-controls="showDropdown ? 'tags-options' : null"
-          :aria-expanded="showDropdown"
           :aria-label="$t('categories.label')"
           @focusin="handleFocusin"
         />
