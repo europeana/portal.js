5d26582131f6a54141188a5abb16f9ffdbaffdf6
Leonie Peters <leoniepeters@gmail.com>
2024-11-11 16:57:37 +0100

    Add WIP pagination

--- packages/portal/src/components/media/MediaPDFViewer.vue
+++ packages/portal/src/components/media/MediaPDFViewer.vue
@@ -1,54 +1,18 @@
 <template>
   <div id="pdfvuer">
-    <div class="position-absolute">
-      <a
-        class="item"
-        @click="page > 1 ? page-- : 1"
-      >
-        <span class="left chevron icon" />
-        Back
-      </a>
-      <a class="ui active item">
-        {{ page }} / {{ numPages ? numPages : '∞' }}
-      </a>
-      <a
-        class="item"
-        @click="page < numPages ? page++ : 1"
-      >
-        Forward
-        <i class="right chevron icon" />
-      </a>
-    </div>
-    <div class="position-absolute">
-      <a
-        class="item"
-        @click="scale -= scale > 0.2 ? 0.1 : 0"
-      >
-        <i class="left chevron icon" />
-        Zoom -
-      </a>
-      <a class="ui active item">
-        {{ formattedZoom }} %
-      </a>
-      <a
-        class="item"
-        @click="scale += scale < 2 ? 0.1 : 0"
-      >
-        Zoom +
-        <i class="right chevron icon" />
-      </a>
-    </div>
+    <PaginationNavInput
+      :per-page="1"
+      :total-results="numPages"
+      :button-text="false"
+      :button-icon-class="'icon-arrow-outline'"
+      class="pagination"
+    />
     <PDFViewer
       v-for="i in numPages"
       :id="i"
       :key="i"
       :src="src"
       :page="i"
-      :scale.sync="scale"
-      style="width:100%;margin:20px auto;"
-      :annotation="true"
-      :resize="true"
-      @link-clicked="handlePDFLink"
     >
       <template slot="loading">
         loading content here...
@@ -59,11 +23,13 @@
 
 <script>
   import PDFViewer from 'pdfvuer';
+  import PaginationNavInput from '@/components/generic/PaginationNavInput';
 
   export default {
     name: 'MediaPDFViewer',
 
     components: {
+      PaginationNavInput,
       PDFViewer
     },
     props: {
@@ -80,83 +46,39 @@
 
     data() {
       return {
-        page: 1,
+        page: this.$route.query.page,
         numPages: 0,
         pdfdata: undefined,
         errors: [],
-        scale: 'page-width'
+        scale: 'page-width',
+        src: this.$apis.record.mediaProxyUrl(this.url, this.itemId, { disposition: 'inline' })
       };
     },
 
-    fetch() {
+    mounted() {
       this.getPdf();
     },
 
-    computed: {
-      formattedZoom() {
-        return Number.parseInt(this.scale * 100);
-      },
-      src() {
-        return this.$apis.record.mediaProxyUrl(this.url, this.itemId, { disposition: 'inline' });
-      }
-    },
-
-    watch: {
-      page(p) {
-        if (window.pageYOffset <= this.findPos(document.getElementById(p)) || (document.getElementById(p + 1) && window.pageYOffset >= this.findPos(document.getElementById(p + 1)))) {
-          // window.scrollTo(0,this.findPos(document.getElementById(p)));
-          document.getElementById(p).scrollIntoView();
-        }
-      }
-    },
-
     methods: {
-      handlePDFLink(params) {
-        // Scroll to the appropriate place on our page - the Y component of
-        // params.destArray * (div height / ???), from the bottom of the page div
-        let page = document.getElementById(String(params.pageNumber));
-        page.scrollIntoView();
-      },
       getPdf() {
         let self = this;
         self.pdfdata = PDFViewer.createLoadingTask(this.src);
         self.pdfdata.then(pdf => {
           self.numPages = pdf.numPages;
-          window.onscroll = function() {
-            changePage();
-            // stickyNav();
-          };
-
-          // // Get the offset position of the navbar
-          // let sticky = $('#buttons')[0].offsetTop;
-
-          // // Add the sticky class to the self.$refs.nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
-          // function stickyNav() {
-          //   if (window.pageYOffset >= sticky) {
-          //     $('#buttons')[0].classList.remove('hidden');
-          //   } else {
-          //     $('#buttons')[0].classList.add('hidden');
-          //   }
-          // }
-
-          function changePage() {
-            let i = 1, count = Number(pdf.numPages);
-            do {
-              if (window.pageYOffset >= self.findPos(document.getElementById(i)) &&
-                window.pageYOffset <= self.findPos(document.getElementById(i + 1))) {
-                self.page = i;
-              }
-              i = i + 1;
-            } while (i < count);
-            if (window.pageYOffset >= self.findPos(document.getElementById(i))) {
-              self.page = i;
-            }
-          }
         });
-      },
-      findPos(obj) {
-        return obj.offsetTop;
       }
     }
   };
 </script>
+
+<style lang="scss" scoped>
+  @import '@europeana/style/scss/variables';
+
+  .pagination {
+    background-color: rgba($white, 0.95);
+
+    ::v-deep ul {
+      margin: 0 auto;
+    }
+  }
+</style>
