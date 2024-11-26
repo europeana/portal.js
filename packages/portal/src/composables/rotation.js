c9c4b7eb36b9da6186ffc00965309b34733bb046
Richard Doe <richard.doe@rwdit.net>
2024-11-26 14:15:46 +0000

    fix: revert to global state
    so that multiple components can share

--- packages/portal/src/composables/rotation.js
+++ packages/portal/src/composables/rotation.js
@@ -1,22 +1,22 @@
 import { ref } from 'vue';
 
+// current rotation in radians
+const rotation = ref(0);
+
+const reset = () => {
+  rotation.value = 0;
+};
+const rotateLess = () => {
+  rotation.value = rotation.value - (Math.PI / 2);
+};
+const rotateMore = () => {
+  rotation.value = rotation.value + (Math.PI / 2);
+};
+const setRotation = (value) => {
+  rotation.value = value;
+};
+
 export default function useRotation() {
-  // current rotation in radians
-  const rotation = ref(0);
-
-  const reset = () => {
-    rotation.value = 0;
-  };
-  const rotateLess = () => {
-    rotation.value = rotation.value - (Math.PI / 2);
-  };
-  const rotateMore = () => {
-    rotation.value = rotation.value + (Math.PI / 2);
-  };
-  const setRotation = (value) => {
-    rotation.value = value;
-  };
-
   return {
     reset,
     rotateLess,
