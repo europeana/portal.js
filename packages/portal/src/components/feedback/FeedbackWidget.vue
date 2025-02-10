de11d0b4ab1c8350283202a6a94128d0538b9e05
Leonie Peters <leoniepeters@gmail.com>
2025-02-10 11:28:49 +0100

    chore: update feedback widget to latest production release v0.3.2

--- packages/portal/src/components/feedback/FeedbackWidget.vue
+++ packages/portal/src/components/feedback/FeedbackWidget.vue
@@ -10,7 +10,7 @@
       />
       <script
         type="module"
-        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2-rc.2/dist/europeana-feedback-widget.js"
+        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2/dist/europeana-feedback-widget.js"
         integrity="sha384-xV5RJoIJv97kgg9LQlYymCa5k/KowArkyzGs/YPNAJ/u5r+dww9T2LulYRGV9y0M"
       >
         <!-- prevent eslint closing this -->
@@ -18,11 +18,11 @@
       <link
         rel="preload"
         as="style"
-        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2-rc.2/dist/europeana-feedback-widget.css"
+        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2/dist/europeana-feedback-widget.css"
       >
       <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2-rc.2/dist/europeana-feedback-widget.css"
+        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.2/dist/europeana-feedback-widget.css"
       >
     </client-only>
   </div>
