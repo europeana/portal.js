c3c9841b0f4d334b488a95275ba90c9ae3bafb14
Leonie Peters <leoniepeters@gmail.com>
2025-03-11 11:33:13 +0100

    feat: cancel select on Escape

--- packages/portal/tests/unit/components/item/ItemSelectButton.spec.js
+++ packages/portal/tests/unit/components/item/ItemSelectButton.spec.js
@@ -56,5 +56,38 @@ describe('components/item/ItemSelectButton', () => {
         });
       });
     });
+
+    describe('when select state changes to true', () => {
+      it('starts listening to keyup events', async() => {
+        sinon.spy(window, 'addEventListener');
+        const wrapper = factory();
+        await wrapper.setData({ selected: true });
+
+        expect(window.addEventListener.calledWith('keyup', sinon.match.func)).toBe(true);
+      });
+      describe('and hitting Escape', () => {
+        it('calls toggle to cancel selection', () => {
+          const wrapper = factory();
+          wrapper.vm.toggle = sinon.spy();
+          wrapper.setData({ selected: true });
+          const keyupEscapeEvent = new KeyboardEvent('keyup', { key: 'Escape' });
+
+          wrapper.vm.handleKeyup(keyupEscapeEvent);
+
+          expect(wrapper.vm.toggle.called).toBe(true);
+        });
+      });
+    });
+
+    describe('when select state changes to false', () => {
+      it('stops listening to keyup events', async() => {
+        sinon.spy(window, 'removeEventListener');
+        const wrapper = factory();
+        await wrapper.setData({ selected: true });
+        await wrapper.setData({ selected: false });
+
+        expect(window.removeEventListener.calledWith('keyup', sinon.match.func)).toBe(true);
+      });
+    });
   });
 });
