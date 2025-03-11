4297bcafa30401caff57956724ee0217f7f446d3
Richard Doe <richard.doe@rwdit.net>
2025-03-11 13:14:49 +0000

    test: spec useLikedItems

--- packages/portal/src/composables/likedItems.js
+++ packages/portal/src/composables/likedItems.js
@@ -1,15 +1,14 @@
 import { computed, getCurrentInstance, ref, watchEffect } from 'vue';
 
 export default function useLikedItems(itemIds) {
-  const $root = getCurrentInstance()?.proxy?.$root;
-  const setAPI = $root.$apis?.set;
-
   const likedItems = ref({});
 
-  const setId = computed(() => $root.$store.state.set.likesId);
+  const $root = getCurrentInstance()?.proxy?.$root;
+  const setAPI = $root?.$apis?.set;
+  const setId = computed(() => $root?.$store?.state.set.likesId);
 
   watchEffect(async() => {
-    if (setId.value && itemIds.value) {
+    if (setAPI && setId.value && itemIds?.value) {
       const response = await setAPI.searchItems(setId.value, itemIds.value);
       likedItems.value = [].concat(itemIds.value || []).reduce((memo, itemId) => {
         memo[itemId] = (response.items || []).some((item) => item.endsWith(itemId));
---
+++ packages/portal/tests/unit/composables/likedItems.spec.js
@@ -0,0 +1,96 @@
+import sinon from 'sinon';
+import { computed } from 'vue';
+import useLikedItems from '@/composables/likedItems.js';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+const itemIds = ['/123/abc', '/123/def'];
+const likesId = '123';
+const setApiSearchItemsStub = sinon.stub().resolves({
+  items: ['http://data.europeana.eu/item/123/abc']
+});
+
+const component = {
+  template: '<span />',
+  props: {
+    itemIds: {
+      type: Array,
+      default: null
+    }
+  },
+  setup(props) {
+    const { likedItems } = useLikedItems(computed(() => props.itemIds));
+    return { likedItems };
+  }
+};
+
+const localVue = createLocalVue();
+localVue.use((Vue) => {
+  // mock these here so they apply to the component $root instance
+  Vue.prototype.$apis = {
+    set: {
+      searchItems: setApiSearchItemsStub
+    }
+  };
+  Vue.prototype.$store = {
+    state: {
+      set: {
+        likesId
+      }
+    }
+  };
+});
+
+const factory = ({ propsData = {} } = {}) => shallowMount(component, {
+  localVue,
+  propsData
+});
+
+describe('useLikedItems', () => {
+  afterEach(sinon.resetHistory);
+  afterAll(sinon.reset);
+
+  describe('likedItems', () => {
+    describe('when there are no item IDs', () => {
+      const propsData = { itemIds: undefined };
+
+      it('does not query the Set API', async() => {
+        const wrapper = factory({ propsData });
+
+        await new Promise(process.nextTick);
+
+        expect(setApiSearchItemsStub.called).toBe(false);
+      });
+
+      it('gets value of an empty object', async() => {
+        const wrapper = factory({ propsData });
+
+        await new Promise(process.nextTick);
+
+        expect(wrapper.vm.likedItems).toEqual({});
+      });
+    });
+
+    describe('when there are item IDs', () => {
+      const propsData = { itemIds };
+
+      it('queries the Set API', async() => {
+        factory({ propsData });
+
+        await new Promise(process.nextTick);
+
+        expect(setApiSearchItemsStub.calledWith(likesId, itemIds)).toBe(true);
+      });
+
+      it('gets value of an object with true/false representing liked state', async() => {
+        const wrapper = factory({ propsData });
+
+        await new Promise(process.nextTick);
+
+        expect(wrapper.vm.likedItems).toEqual({
+          '/123/abc': true,
+          '/123/def': false
+        });
+      });
+    });
+  });
+});
