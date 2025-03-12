f02fde21bcca463253ed205426811ff7f17d7136
lbiedinger <lutz.biedinger@gmail.com>
2025-03-12 11:57:22 +0100

    custom tooltip prevention after selection

--- packages/portal/src/components/search/SearchViewToggles.vue
+++ packages/portal/src/components/search/SearchViewToggles.vue
@@ -1,15 +1,28 @@
 <template>
   <b-dropdown
-    v-b-tooltip.bottom="$t('actions.changeView')"
+    id="search-view-toggles"
+    ref="toggles"
     variant="light"
     no-flip
     no-caret
     data-qa="view toggle"
     :toggle-attrs="{ 'aria-label': $t('actions.changeView') }"
   >
+    <b-tooltip
+      id="search-view-toggles-tooltip"
+      ref="tooltip"
+      :disabled.sync="preventTooltip"
+      placement="bottom"
+      target="search-view-toggles"
+    >
+      {{ $t('actions.changeView') }}
+    </b-tooltip>
+
     <template #button-content>
       <span
         :class="`icon-view-${value}`"
+        @mouseover="handleHover"
+        @touchstart="handleHover"
       />
     </template>
     <b-dropdown-item
@@ -46,6 +59,8 @@
 
     data() {
       return {
+        debounceTooltipEnable: false,
+        preventTooltip: false,
         views: ['list', 'grid', 'mosaic']
       };
     },
@@ -56,10 +71,49 @@
       }
     },
 
+    mounted() {
+      // setup custom focusin handler as there are no native bootstrap events to hook into for the button
+      this.$refs.toggles.$el.addEventListener('focusin', this.handleFocusin);
+    },
+
+    beforeDestroy() {
+      this.$refs.toggles.$el.removeEventListener('focusin', this.handleFocusin);
+    },
+
     methods: {
       selectView(view) {
         this.$cookies?.set('searchResultsView', view);
         this.$matomo?.trackEvent('View search results', 'Select view', view);
+
+        // Pevent the tooltip from showing when foucs will be returned to the button.
+        this.debounceTooltipEnable = true;
+        this.setPreventTooltip(true);
+      },
+      setPreventTooltip(value) {
+        if (value && value !== this.preventTooltip) {
+          // Prevent the tooltip from showing. This is skipped if already prevented.
+          this.$refs.tooltip.$emit('disable');
+        } else if (value !== this.preventTooltip) {
+          // Allow the tooltip to show. This is skipped if already allowed.
+          // This is skipped the first time after a selection was made,
+          // to stop the tooltip when the focus goes back to the dropdownbutton.
+          if (!this.debounceTooltipEnable) {
+            this.$refs.tooltip.$emit('enable');
+          }
+
+          // Ensure the next time focus is returned the tooltip can be allowed:
+          this.debounceTooltipEnable = false;
+        }
+      },
+
+      handleHover() {
+        this.setPreventTooltip(false);
+        // Now that the tooltip can be shown, trigger the show again.
+        this.$root.$emit('bv::show::tooltip', 'search-view-toggles-tooltip');
+      },
+      handleFocusin() {
+        // allow showing the tooltip again, but don't trigger a show event
+        this.setPreventTooltip(false);
       }
     }
   };
