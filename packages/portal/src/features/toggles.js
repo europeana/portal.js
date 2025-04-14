d7e04e2b4d80b71070b1e5383a8a8f836ace209a
Richard Doe <richard.doe@rwdit.net>
2025-04-14 10:47:49 +0100

    chore: add feature toggle for managing API keys
    Enable with: `ENABLE_MANAGE_API_KEYS=1`

--- packages/portal/src/features/toggles.js
+++ packages/portal/src/features/toggles.js
@@ -4,6 +4,7 @@ export default [
   { name: 'eventLogging' },
   { name: 'itemMultiSelect' },
   { name: 'jiraServiceDeskFeedbackForm' },
+  { name: 'manageApiKeys' },
   { name: 'mockTrendingItems' },
   { name: 'multilingualSearch' },
   { name: 'rejectEntityRecommendations' },
