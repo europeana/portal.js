56326b11baab0e421058a02f24fa99cca3ecaaca
Leonie Peters <leoniepeters@gmail.com>
2025-04-18 16:46:49 +0200

    feat: create new UserProfileDropdown component and use in UserHeader

---
+++ packages/portal/src/components/user/UserProfileDropdown.vue
@@ -0,0 +1,73 @@
+<template>
+  <b-dropdown
+    boundary="window"
+  >
+    <b-dropdown-item
+      v-for="item, index in links"
+      :key="index"
+      :href="item.url"
+    >
+      {{ item.text }}
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+  export default {
+    name: 'UserProfileDropdown',
+
+    data() {
+      return {
+        links: [{
+          text: this.$t('account.editProfile'),
+          url: this.$keycloak?.accountUrl()
+        }, {
+          text: this.$t('account.linkLogout'),
+          url: '/account/logout'
+        }]
+      };
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@europeana/style/scss/variables';
+
+  ::v-deep .dropdown-toggle {
+    border-radius: 50%;
+    padding: 0.25rem;
+    line-height: 1.25rem;
+    margin-left: 0.5rem;
+
+    @media (min-width: $bp-4k) {
+      padding: 0.625rem;
+    }
+
+    &:after {
+      font-size: 0.5rem;
+      width: 1.25rem;
+      margin: 0;
+
+      @media (min-width: $bp-4k) {
+        font-size: 0.75rem;
+        width: 1.25rem;
+      }
+    }
+
+  }
+
+  .show > ::v-deep .btn-secondary.dropdown-toggle {
+    color: $darkgrey;
+    background: $offwhite;
+    border-color: $offwhite;
+    border-radius: 50%;
+
+    &:hover {
+      color: $blue;
+    }
+
+    &:after {
+      transform: rotateX(180deg);
+    }
+  }
+</style>
