3a16f886123ac39fee7a0e9b499dba8c7e0a23de
Leonie Peters <leoniepeters@gmail.com>
2025-04-22 14:19:51 +0200

    style: dropdown menu and item styles

--- packages/portal/src/components/user/UserProfileDropdown.vue
+++ packages/portal/src/components/user/UserProfileDropdown.vue
@@ -66,7 +66,6 @@
         width: 1.25rem;
       }
     }
-
   }
 
   .show > ::v-deep .btn-secondary.dropdown-toggle {
@@ -83,4 +82,36 @@
       transform: rotateX(180deg);
     }
   }
+
+  ::v-deep .dropdown-menu {
+    width: auto;
+    font-size: $font-size-small;
+    max-height: 15rem;
+    overflow-y: auto;
+    overflow-x: hidden;
+    margin-top: 0.5rem;
+    padding: 0;
+    border-radius: 0 0 0.375rem 0.375rem;
+    box-shadow: $boxshadow;
+    border: 0;
+
+    @media (min-width: $bp-4k) {
+      font-size: $font-size-small-4k;
+      margin-top: 0.75rem;
+    }
+
+    // dropdown is flipped up
+    &[x-placement='top-start'] {
+      border-radius: 0.375rem 0.375rem 0 0;
+    }
+
+    .dropdown-item {
+      padding: 0.875rem 0.75rem;
+
+      @media (min-width: $bp-4k) {
+        padding: calc(1.5 * 0.875rem) calc(1.5 * 0.75rem);
+      }
+    }
+  }
+
 </style>
