f7904c599a08c73a97d551ed3a3ad997da4cc317
Richard Doe <richard.doe@rwdit.net>
2025-04-25 09:48:14 +0100

    feat: re-enable API key info modal

--- packages/portal/src/components/user/UserApiKeyActionsMenu.vue
+++ packages/portal/src/components/user/UserApiKeyActionsMenu.vue
@@ -5,7 +5,6 @@
       :aria-expanded="expanded"
       data-qa="user api key actions menu"
       variant="link"
-      :disabled="apiKey.state === 'disabled'"
       no-caret
     >
       <template #button-content>
@@ -14,17 +13,24 @@
         />
       </template>
       <b-dropdown-item
-        v-if="apiKey.state !== 'disabled'"
+        v-if="apiKey.state === 'disabled'"
+        data-qa="re-enable personal api key button"
+        @click="handleClickReEnableButton(apiKey)"
+      >
+        {{ $t('apiKeys.actions.reEnable') }}
+      </b-dropdown-item>
+      <b-dropdown-item
+        v-else
         data-qa="disable personal api key button"
         @click="handleClickDisableButton(apiKey)"
       >
-        {{ $t('actions.disable') }}
+        {{ $t('apiKeys.actions.disable') }}
       </b-dropdown-item>
     </b-dropdown>
     <ConfirmDangerModal
       v-if="showConfirmDangerModal"
       v-model="showConfirmDangerModal"
-      :confirm-button-text="$t('apiKeys.disable.confirm')"
+      :confirm-button-text="$t('apiKeys.actions.disable')"
       :modal-id="`${id}-confirm-disable-modal`"
       :modal-title="$t('apiKeys.disable.title')"
       :prompt-text="[$t('apiKeys.disable.promptText1'), $t('apiKeys.disable.promptText2')]"
@@ -32,6 +38,27 @@
       @confirm="handleConfirmDisableKey"
       @input="showConfirmDangerModal = $event"
     />
+    <b-modal
+      v-if="showUserReEnableApiKeyModal"
+      id="user-re-enable-api-key-modal"
+      v-model="showUserReEnableApiKeyModal"
+      title-tag="h2"
+      :title="$t('apiKeys.reEnable.title')"
+      header-tag="div"
+      hide-header-close
+      hide-footer
+      data-qa="re-enable api key info modal"
+    >
+      <p>
+        {{ $t('apiKeys.reEnable.text') }}
+      </p>
+      <b-button
+        variant="outline-primary"
+        @click="$bvModal.hide('user-re-enable-api-key-modal')"
+      >
+        {{ $t('actions.close') }}
+      </b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -58,13 +85,18 @@
     data() {
       return {
         expanded: false,
-        showConfirmDangerModal: false
+        showConfirmDangerModal: false,
+        showUserReEnableApiKeyModal: false
       };
     },
 
     methods: {
       handleClickDisableButton() {
         this.showConfirmDangerModal = true;
+      },
+
+      handleClickReEnableButton() {
+        this.showUserReEnableApiKeyModal = true;
       },
 
       async handleConfirmDisableKey() {
