45d568c147441f94e6e325ed41798f480c008c76
Leonie Peters <leoniepeters@gmail.com>
2025-04-30 17:40:43 +0200

    feat: new SkipLink component; set focus to main programmatically

---
+++ packages/portal/src/components/generic/SkipLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-button
+    :class="classes"
+    data-qa="skip to content link"
+    @click="handleClick"
+  >
+    {{ text }}
+  </b-button>
+</template>
+
+<script>
+  export default {
+    name: 'SkipLink',
+
+    props: {
+      selector: {
+        type: String,
+        default: '#main'
+      },
+      classes: {
+        type: String,
+        default: 'skip-main'
+      },
+      text: {
+        type: String,
+        default: this.$t('layout.skipToMain')
+      }
+    },
+
+    methods: {
+      handleClick() {
+        const element = document.querySelector(this.selector);
+        element.setAttribute('tabindex', '-1');
+        element.focus();
+      }
+    }
+  };
+</script>
