8228d641d0c298ab9e00521590c6a56a5bfc2259
Richard Doe <richard.doe@rwdit.net>
2025-05-13 13:50:07 +0100

    chore: rm graphql queries from plugin

--- packages/portal/src/plugins/contentful/index.js
+++ packages/portal/src/plugins/contentful/index.js
@@ -1,12 +1,10 @@
 import assets from './assets.js';
-import query from './query.js';
 
 const MODULE_NAME = 'contentful';
 
 export default ({ $config, $apm }, inject) => {
   const plugin = {
-    assets: assets(),
-    query: query({ $apm, $config })
+    assets: assets()
   };
 
   inject(MODULE_NAME, plugin);
