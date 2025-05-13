9a564bf7e2abcc4c24b01a4fe302b7b846c6a985
Richard Doe <richard.doe@rwdit.net>
2025-05-13 15:43:08 +0100

    refactor: mv contentful assets helpers to utils (#2773)
    to reduce size of shared bundle from including them in module

--- packages/portal/tests/unit/utils/contentful/assets.spec.js
+++ packages/portal/tests/unit/utils/contentful/assets.spec.js
@@ -1,4 +1,4 @@
-import assets from '@/modules/contentful/templates/assets';
+import * as assets from '@/utils/contentful/assets.js';
 
 const responsiveParams = {
   small: { w: 245, h: 440, fit: 'fill' },
@@ -11,76 +11,34 @@ const responsiveParams = {
   '4k': { w: 700, h: 900, fit: 'fill' }
 };
 
-describe('modules/contentful/templates/assets', () => {
+describe('@/utils/contentful/assets.js', () => {
   describe('isValidUrl', () => {
     it('is `true` for URLs on host images.ctfassets.net', () => {
       const src = 'https://images.ctfassets.net/asset.jpeg';
 
-      expect(assets().isValidUrl(src)).toBe(true);
+      expect(assets.isValidUrl(src)).toBe(true);
     });
 
     it('is `false` for other URLs', () => {
       const src = 'https://www.example.org/image.jpeg';
 
-      expect(assets().isValidUrl(src)).toBe(false);
+      expect(assets.isValidUrl(src)).toBe(false);
     });
 
     it('is `false` for non-URLs', () => {
       const src = 'image.jpeg';
 
-      expect(assets().isValidUrl(src)).toBe(false);
+      expect(assets.isValidUrl(src)).toBe(false);
     });
   });
 
   describe('optimisedSrc', () => {
     it('is `null` if no asset passed', () => {
-      expect(assets().optimisedSrc(null)).toBe(null);
+      expect(assets.optimisedSrc(null)).toBe(null);
     });
 
     it('is `null` if asset has no URL', () => {
-      expect(assets().optimisedSrc({ contentType: 'image/jpeg' })).toBe(null);
-    });
-
-    describe('if WebP format accepted by client', () => {
-      const store = { state: { contentful: { acceptedMediaTypes: ['text/html', 'image/apng', 'image/webp'] } } };
-
-      it('respects format if specified', () => {
-        const asset = {
-          url: 'https://images.ctfassets.net/asset.gif',
-          contentType: 'image/gif'
-        };
-        const params = { fm: 'png' };
-
-        expect(assets({ store }).optimisedSrc(asset, params)).toBe('https://images.ctfassets.net/asset.gif?fm=png');
-      });
-
-      it('converts to WebP if format not specified', () => {
-        const asset = {
-          url: 'https://images.ctfassets.net/asset.jpeg',
-          contentType: 'image/jpeg'
-        };
-
-        expect(assets({ store }).optimisedSrc(asset)).toBe('https://images.ctfassets.net/asset.jpeg?fm=webp&q=40');
-      });
-
-      it('respects supplied quality param for WebP', () => {
-        const asset = {
-          url: 'https://images.ctfassets.net/asset.jpeg',
-          contentType: 'image/jpeg'
-        };
-        const params = { q: 50 };
-
-        expect(assets({ store }).optimisedSrc(asset, params)).toBe('https://images.ctfassets.net/asset.jpeg?q=50&fm=webp');
-      });
-
-      it('does not convert SVGs to WebP', () => {
-        const asset = {
-          url: 'https://images.ctfassets.net/asset.svg',
-          contentType: 'image/svg+xml'
-        };
-
-        expect(assets({ store }).optimisedSrc(asset)).toBe('https://images.ctfassets.net/asset.svg');
-      });
+      expect(assets.optimisedSrc({ contentType: 'image/jpeg' })).toBe(null);
     });
 
     it('compresses jpegs', () => {
@@ -89,7 +47,7 @@ describe('modules/contentful/templates/assets', () => {
         contentType: 'image/jpeg'
       };
 
-      expect(assets().optimisedSrc(asset)).toBe('https://images.ctfassets.net/asset.jpeg?fm=webp&q=40');
+      expect(assets.optimisedSrc(asset)).toBe('https://images.ctfassets.net/asset.jpeg?fm=webp&q=40');
     });
 
     it('joins all the options', () => {
@@ -98,7 +56,7 @@ describe('modules/contentful/templates/assets', () => {
         contentType: 'image/jpeg'
       };
 
-      expect(assets().optimisedSrc(asset, { fm: 'jpg', w: 200, q: 80 })).toBe('https://images.ctfassets.net/asset.jpeg?fm=jpg&w=200&q=80&fl=progressive');
+      expect(assets.optimisedSrc(asset, { fm: 'jpg', w: 200, q: 80 })).toBe('https://images.ctfassets.net/asset.jpeg?fm=jpg&w=200&q=80&fl=progressive');
     });
 
     it('applies passed max width', () => {
@@ -107,7 +65,7 @@ describe('modules/contentful/templates/assets', () => {
         contentType: 'image/png'
       };
 
-      expect(assets().optimisedSrc(asset, { w: 40 })).toBe('https://images.ctfassets.net/asset.png?w=40&fm=webp&q=40');
+      expect(assets.optimisedSrc(asset, { w: 40 })).toBe('https://images.ctfassets.net/asset.png?w=40&fm=webp&q=40');
     });
   });
 
@@ -117,7 +75,7 @@ describe('modules/contentful/templates/assets', () => {
         sizes: ['small']
       };
 
-      const sizes = assets().imageDisplayProfileResponsiveSizes(responsiveParams, profile);
+      const sizes = assets.imageDisplayProfileResponsiveSizes(responsiveParams, profile);
 
       expect(sizes).toEqual({ small: responsiveParams.small });
     });
@@ -130,7 +88,7 @@ describe('modules/contentful/templates/assets', () => {
       };
 
       const sizes = { small: { w: 245, h: 440, fit: 'pad' } };
-      const profileSizes = assets().imageDisplayProfileResponsiveSizes(sizes, profile);
+      const profileSizes = assets.imageDisplayProfileResponsiveSizes(sizes, profile);
 
       expect(profileSizes).toEqual({ small: { w: 245, fit: 'pad' } });
     });
@@ -144,7 +102,7 @@ describe('modules/contentful/templates/assets', () => {
           contentType: 'image/jpeg'
         };
 
-        const srcset = assets().responsiveImageSrcset(asset, responsiveParams);
+        const srcset = assets.responsiveImageSrcset(asset, responsiveParams);
 
         expect(srcset).toContain('https://images.ctfassets.net/asset.jpeg?w=245&h=440&fit=fill&fm=webp&q=40 245w,https://images.ctfassets.net/asset.jpeg?w=260&h=420&fit=fill&fm=webp&q=40 260w,https://images.ctfassets.net/asset.jpeg?w=280&h=400&fit=fill&fm=webp&q=40 280w,https://images.ctfassets.net/asset.jpeg?w=300&h=400&fit=fill&fm=webp&q=40 300w,https://images.ctfassets.net/asset.jpeg?w=320&h=370&fit=fill&fm=webp&q=40 320w,https://images.ctfassets.net/asset.jpeg?w=355&h=345&fit=fill&fm=webp&q=40 355w,https://images.ctfassets.net/asset.jpeg?w=510&h=540&fit=fill&fm=webp&q=40 510w,https://images.ctfassets.net/asset.jpeg?w=700&h=900&fit=fill&fm=webp&q=40 700w');
       });
@@ -157,7 +115,7 @@ describe('modules/contentful/templates/assets', () => {
           contentType: 'image/jpeg'
         };
 
-        const srcset = assets().responsiveImageSrcset(asset);
+        const srcset = assets.responsiveImageSrcset(asset);
 
         expect(srcset).toBe(null);
       });
@@ -167,7 +125,7 @@ describe('modules/contentful/templates/assets', () => {
       it('returns `null`', () => {
         const asset = 'image.jpeg';
 
-        const srcset = assets().responsiveImageSrcset(asset);
+        const srcset = assets.responsiveImageSrcset(asset);
 
         expect(srcset).toBe(null);
       });
@@ -182,7 +140,7 @@ describe('modules/contentful/templates/assets', () => {
           contentType: 'image/jpeg'
         };
 
-        const variables = assets().responsiveBackgroundImageCSSVars(asset, responsiveParams);
+        const variables = assets.responsiveBackgroundImageCSSVars(asset, responsiveParams);
         expect(variables['--bg-img-4k']).toBeTruthy();
       });
     });
@@ -193,7 +151,7 @@ describe('modules/contentful/templates/assets', () => {
           url: 'https://www.europeana.eu/asset.jpeg'
         };
 
-        const variables = assets().responsiveBackgroundImageCSSVars(asset);
+        const variables = assets.responsiveBackgroundImageCSSVars(asset);
         expect(variables['--bg-img-small']).toBeTruthy();
         expect(variables['--bg-img-4k']).toBeFalsy();
       });
@@ -203,7 +161,7 @@ describe('modules/contentful/templates/assets', () => {
       it('returns null', () => {
         const asset = {};
 
-        const variables = assets().responsiveBackgroundImageCSSVars(asset);
+        const variables = assets.responsiveBackgroundImageCSSVars(asset);
         expect(variables).toBe(null);
       });
     });
