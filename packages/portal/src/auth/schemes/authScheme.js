39acb572e0e973726ab79717f6a585bef0784d6a
Richard Doe <richard.doe@rwdit.net>
2025-05-20 11:39:56 +0100

    feat: replace url during login

--- packages/portal/src/auth/schemes/authScheme.js
+++ packages/portal/src/auth/schemes/authScheme.js
@@ -1,4 +1,8 @@
-// Custom Nuxt auth scheme extending oAuth2 scheme to support Nuxt runtime config
+// Custom Nuxt auth scheme extending @nuxtjs/auth's oAuth2 scheme to:
+// - support Nuxt runtime config
+// - _replace_ the window location for redirection to oUuth login endpoint
+
+import nanoid from 'nanoid';
 
 // TODO: delete once auth module supports Nuxt runtime config
 // @see https://github.com/nuxt-community/auth-module/issues/713
@@ -33,5 +37,42 @@ export default class RuntimeConfigurableOauth2Scheme extends Oauth2Scheme {
     }
 
     super($auth, configOptions);
+  }
+
+  // copied from @nuxtjs/auth/lib/schemes/oauth2.js, modified to use
+  // `window.location.replace(url)` for the redirection (instead of `window.location = url`)
+  login({ params, state, nonce } = {}) {
+    const opts = {
+      protocol: 'oauth2',
+      'response_type': this.options.response_type,
+      'access_type': this.options.access_type,
+      'client_id': this.options.client_id,
+      'redirect_uri': this['_redirectURI'],
+      scope: this['_scope'],
+      // Note: The primary reason for using the state parameter is to mitigate CSRF attacks.
+      // https://auth0.com/docs/protocols/oauth2/oauth-state
+      state: state || nanoid(),
+      ...params
+    };
+
+    if (this.options.audience) {
+      opts.audience = this.options.audience;
+    }
+
+    // Set Nonce Value if response_type contains id_token to mitigate Replay Attacks
+    // More Info: https://openid.net/specs/openid-connect-core-1_0.html#NonceNotes
+    // More Info: https://tools.ietf.org/html/draft-ietf-oauth-v2-threatmodel-06#section-4.6.2
+    if (opts.response_type.includes('id_token')) {
+      // nanoid auto-generates an URL Friendly, unique Cryptographic string
+      // Recommended by Auth0 on https://auth0.com/docs/api-auth/tutorials/nonce
+      opts.nonce = nonce || nanoid();
+    }
+
+    this.$auth.$storage.setUniversal(this.name + '.state', opts.state);
+
+    const url = new URL(this.options.authorization_endpoint);
+    url.search = new URLSearchParams(opts).toString();
+
+    window.location.replace(url);
   }
 }
