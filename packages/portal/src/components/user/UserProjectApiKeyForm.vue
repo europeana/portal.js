0fab859f7d6f33c93d9482fef0648621e9517b66
Lutz Biedinger <lutz.biedinger@gmail.com>
2025-05-28 13:19:42 +0200

    remove repeating language param

--- packages/portal/src/components/user/UserProjectApiKeyForm.vue
+++ packages/portal/src/components/user/UserProjectApiKeyForm.vue
@@ -22,7 +22,7 @@
 
     computed: {
       embed() {
-        const src = `${ this.$config.app.projectApiKeyFormUrl }/${ this.$i18n.locale }?zf_lang=${ this.$i18n.locale }&first_name=${ this.loggedInUser.given_name }&last_name=${ this.loggedInUser.family_name }&email=${ this.loggedInUser.email }`;
+        const src = `${ this.$config.app.projectApiKeyFormUrl }/${ this.$i18n.locale }?first_name=${ this.loggedInUser.given_name }&last_name=${ this.loggedInUser.family_name }&email=${ this.loggedInUser.email }`;
         return `<iframe src="${ src }" frameborder='0' style='height:2200px;width:100%;' marginwidth='0' marginheight='0' scrolling='auto' allow='geolocation'></iframe>`;
       }
     }
