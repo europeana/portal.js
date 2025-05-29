dba0571caf236200cc0cc96e3bb459b319ff1cdd
Richard Doe <richard.doe@rwdit.net>
2025-05-29 09:32:00 +0100

    test: spec useContentfulGraphql

--- packages/portal/src/composables/contentful/useContentfulGraphql.js
+++ packages/portal/src/composables/contentful/useContentfulGraphql.js
@@ -6,17 +6,21 @@ import { print as printGraphql } from 'graphql/language/printer';
 import { getCurrentInstance } from 'vue';
 
 export function useContentfulGraphql() {
-  const { $config, $apm } = getCurrentInstance()?.proxy?.$root?.context || {};
-
   const axiosInstance = axios.create();
   axiosRetry(axiosInstance);
 
-  const config = $config?.contentful || {};
-  const origin = config.graphQlOrigin || 'https://graphql.contentful.com';
-  const path = `/content/v1/spaces/${config.spaceId}/environments/${config.environmentId || 'master'}`;
+  const instance = getCurrentInstance();
+  const $root = instance.proxy.$root;
 
   const query = (ast, variables = {}) => {
-    const accessToken = variables.preview ? config.accessToken.preview : config.accessToken.delivery;
+    const $apm = $root.$apm;
+    const $config = $root.$config;
+
+    const config = $config?.contentful || {};
+    const origin = config.graphQlOrigin || 'https://graphql.contentful.com';
+    const path = `/content/v1/spaces/${config.spaceId}/environments/${config.environmentId || 'master'}`;
+
+    const accessToken = variables.preview ? config.accessToken?.preview : config.accessToken?.delivery;
 
     const query = printGraphql(ast);
 
@@ -37,15 +41,16 @@ export function useContentfulGraphql() {
       ...variables
     };
 
-    return axiosInstance.post(`${origin}${path}`, body, { headers, params })
-      .catch((error) => {
-        $apm?.captureError(error, {
-          custom: {
-            code: error.code
-          }
-        });
-        throw error;
+    try {
+      return axiosInstance.post(`${origin}${path}`, body, { headers, params });
+    } catch (error) {
+      $apm?.captureError(error, {
+        custom: {
+          code: error.code
+        }
       });
+      throw error;
+    }
   };
 
   return {
