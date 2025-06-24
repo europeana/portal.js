5e7a3a5d82910d5158a20fb48973140520336fc1
Leonie Peters <leoniepeters@gmail.com>
2025-06-24 16:15:13 +0200

    feat: update contentful slug validation app to handle and filter duplicated depending on the selected site

--- packages/portal/src/pages/contentful/slug-validation/index.vue
+++ packages/portal/src/pages/contentful/slug-validation/index.vue
@@ -5,7 +5,7 @@
         <b-form-input
           v-model="value"
           autocomplete="off"
-          :class="errorMessage? 'error' : ''"
+          :class="errorMessage ? 'error' : ''"
           @input="handleSlugChange"
         />
         <label
@@ -30,6 +30,7 @@
         contentTypes: [],
         slugField: null,
         titleField: null,
+        siteField: null,
         slugFromTitle: false,
         errorMessage: null,
         debouncedDuplicateStatus: null,
@@ -52,18 +53,24 @@
     },
 
     mounted() {
-      window.contentfulExtension.init(sdk => {
+      window.contentfulExtension.init((sdk) => {
         this.contentfulExtensionSdk = sdk;
         if (sdk.location.is(window.contentfulExtension.locations.LOCATION_ENTRY_FIELD)) {
           sdk.window.startAutoResizer();
 
-          this.contentTypes = [sdk.contentType.sys.id].concat(sdk.parameters.instance.contentTypes.split(','));
+          this.contentTypes = [sdk.contentType.sys.id].concat(
+            sdk.parameters.instance.contentTypes.split(',')
+          );
 
           this.value = sdk.field.getValue();
           this.slugField = sdk.field;
           const titleFieldName = sdk.contentType.displayField;
           this.titleField = sdk.entry.fields[titleFieldName];
           this.titleField.onValueChanged(this.handleTitleChange);
+          this.siteField = sdk.entry.fields.site;
+          if (this.siteField) {
+            this.siteField.onValueChanged(this.handleSiteChange);
+          }
           const titleValue = this.titleField.getValue();
           if (!this.value || this.value === 'undefined' || this.value === '') {
             // No slug exists, use the title as a default
@@ -116,6 +123,13 @@
       },
 
       /**
+       * Handle change of site value caused by changing the site of the entry.
+       */
+      handleSiteChange() {
+        this.getDebouncedDuplicateStatus(this.value);
+      },
+
+      /**
        * Handle validation status, setting the field to valid/invalid and
        * removing the value in order to prevent publication if the slug is a duplicate.
        */
@@ -124,8 +138,10 @@
         if (status) {
           this.slugField.removeValue();
           this.slugField.setInvalid(true);
-          this.errorMessage =  'This slug has already been published in another entry';
+          this.errorMessage = 'This slug has already been published in another entry';
         } else {
+          // set value in case it had been reset. Otherwise required state might persist.
+          this.slugField.setValue(this.value);
           this.slugField.setInvalid(false);
           this.errorMessage = null;
         }
@@ -156,10 +172,32 @@
         query['sys.id[ne]'] = this.contentfulExtensionSdk.entry.getSys().id;
         query['sys.publishedAt[exists]'] = true;
 
+        const siteFieldValue = this.contentfulExtensionSdk.entry.fields.site?.getValue();
+
         for (const contentType of this.contentTypes) {
           query['content_type'] = contentType;
           const result = await this.contentfulExtensionSdk.space.getEntries(query);
+
           if (result.total >= 1) {
+            // Site filtering applied after getEntries request, bc site field not on every content type which requires additional request to check existence.
+            // Filtering on site only once there are results saves requests on each rebounce.
+            const resultsWithSiteField = result.items?.filter((item) => item.fields?.site);
+            if (resultsWithSiteField.length) {
+              const resultsHaveDataspaceSite = resultsWithSiteField.every(
+                (item) => item.fields.site['en-GB'] === 'dataspace-culturalheritage.eu'
+              );
+              const resultHasSameSite = resultsWithSiteField.some(
+                (item) => item.fields.site['en-GB'] === siteFieldValue
+              );
+
+              // if entry has site field set, only consider duplicate when same site
+              if (siteFieldValue) {
+                return resultHasSameSite;
+              // if entry has no site field, only check for any non dataspace duplicates
+              } else if (resultsHaveDataspaceSite) {
+                return false;
+              }
+            }
             return true;
           }
         }
---
+++ packages/portal/tests/unit/pages/contentful/slug-validation/index.spec.js
@@ -0,0 +1,115 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { fakeContentfulExtension } from '../../../utils';
+import BootstrapVue from 'bootstrap-vue';
+
+import page from '@/pages/contentful/slug-validation/index';
+import sinon from 'sinon';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const factory = () => shallowMount(page, {
+  localVue
+});
+
+const slugToValidate = 'new-slug';
+
+describe('pages/contentful/slug-validation/index', () => {
+  beforeAll(() => {
+    window.getSlug = sinon.stub();
+    window.contentfulExtension = fakeContentfulExtension({
+      contentType: { displayField: 'name', sys: { id: 'landingPage' } },
+      entryFields: ['name', 'site'],
+      fieldReturnValue: slugToValidate,
+      location: 'field',
+      parameters: { instance: { contentTypes: 'landingPage' } }
+    });
+  });
+
+  it('initialises SDK and calls startAutoResizer', () => {
+    const wrapper = factory();
+
+    expect(wrapper.vm.contentfulExtensionSdk).toBeDefined();
+    expect(wrapper.vm.contentfulExtensionSdk.window.startAutoResizer.called).toBe(true);
+  });
+
+  describe('when the entry has a site field', () => {
+    it('handles value change', async() => {
+      const wrapper = factory();
+      wrapper.vm.getDebouncedDuplicateStatus = sinon.spy();
+
+      expect(wrapper.vm.siteField.onValueChanged).toBeDefined();
+
+      wrapper.vm.handleSiteChange();
+      expect(wrapper.vm.getDebouncedDuplicateStatus.calledWith(slugToValidate)).toBe(true);
+    });
+  });
+
+  describe('if the slug has duplicates', () => {
+    describe('if there is no site field on any of the result entries', () => {
+      it('sets invalid state', async() => {
+        const wrapper = factory();
+
+        wrapper.vm.contentfulExtensionSdk.space.getEntries = sinon.stub().resolves({ total: 1, items: [{}] });
+
+        wrapper.vm.handleValueChange(slugToValidate);
+        await new Promise(resolve => setTimeout(resolve, 600)); // wait for debounce
+
+        expect(wrapper.vm.slugField.removeValue.called).toBe(true);
+        expect(wrapper.vm.slugField.setInvalid.calledWith(true)).toBe(true);
+        expect(wrapper.vm.errorMessage).toBe('This slug has already been published in another entry');
+      });
+    });
+    describe('if there is a site field on any of the result entries', () => {
+      describe('and on the current entry', () => {
+        it('only considers as duplicate when they are set to the same site', async() => {
+          const wrapper = factory();
+          const currentSite = 'current-site.eu';
+
+          wrapper.vm.contentfulExtensionSdk.entry.fields.site.getValue = sinon.stub().returns(currentSite);
+
+          wrapper.vm.contentfulExtensionSdk.space.getEntries = sinon.stub().resolves({ total: 1, items: [{ fields: { site: { 'en-GB': currentSite } } }] });
+
+          wrapper.vm.handleValueChange(slugToValidate);
+          await new Promise(resolve => setTimeout(resolve, 600)); // wait for debounce
+
+          expect(wrapper.vm.slugField.removeValue.called).toBe(true);
+          expect(wrapper.vm.slugField.setInvalid.calledWith(true)).toBe(true);
+          expect(wrapper.vm.errorMessage).toBe('This slug has already been published in another entry');
+        });
+        it('ignores duplicates when not the same site', async() => {
+          const wrapper = factory();
+          const currentSite = 'current-site.eu';
+          const otherSite = 'other-site.eu';
+
+          wrapper.vm.contentfulExtensionSdk.entry.fields.site.getValue = sinon.stub().returns(currentSite);
+
+          wrapper.vm.contentfulExtensionSdk.space.getEntries = sinon.stub().resolves({ total: 1, items: [{ fields: { site: { 'en-GB': otherSite } } }] });
+
+          wrapper.vm.handleValueChange(slugToValidate);
+          await new Promise(resolve => setTimeout(resolve, 600)); // wait for debounce
+
+          expect(wrapper.vm.slugField.setValue.calledWith(slugToValidate)).toBe(true);
+          expect(wrapper.vm.slugField.setInvalid.calledWith(false)).toBe(true);
+          expect(wrapper.vm.errorMessage).toBe(null);
+        });
+      });
+    });
+    describe('and there is NO site field on the current entry', () => {
+      it('ignores duplicates when in dataspace scope', async() => {
+        const wrapper = factory();
+        const dataspaceSite = 'dataspace-culturalheritage.eu';
+
+        wrapper.vm.contentfulExtensionSdk.entry.fields.site = undefined;
+        wrapper.vm.contentfulExtensionSdk.space.getEntries = sinon.stub().resolves({ total: 1, items: [{ fields: { site: { 'en-GB': dataspaceSite } } }] });
+
+        wrapper.vm.handleValueChange(slugToValidate);
+        await new Promise(resolve => setTimeout(resolve, 600)); // wait for debounce
+
+        expect(wrapper.vm.slugField.setValue.calledWith(slugToValidate)).toBe(true);
+        expect(wrapper.vm.slugField.setInvalid.calledWith(false)).toBe(true);
+        expect(wrapper.vm.errorMessage).toBe(null);
+      });
+    });
+  });
+});
