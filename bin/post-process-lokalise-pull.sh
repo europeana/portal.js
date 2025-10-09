ae0d2da7e4dc4bc8b37b997f3dec0374398b1f7e
Richard Doe <richard.doe@rwdit.net>
2025-10-09 09:09:46 +0100

    ci: JS post-process scripts for Lokalise pull (#2848)
    to fix the poor a-z key ordering

--- bin/post-process-lokalise-pull.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-# replace uselessly escaped slashes in .js downloads from Lokalise pull
-# GitHub action, i.e. replace "\/" with "/"
-sed -i 's/\\\//\//g' packages/portal/src/i18n/lang/*.js
