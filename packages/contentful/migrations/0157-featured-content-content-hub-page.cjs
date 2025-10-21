fe66058be373f1e21ac4099c40d6f080cecc873e
Leonie Peters <leoniepeters@gmail.com>
2025-10-21 10:12:51 +0200

    feat: ctf migration to add featured content field to content hub page

---
+++ packages/contentful/migrations/0157-featured-content-content-hub-page.cjs
@@ -0,0 +1,26 @@
+module.exports = function(migration) {
+  const contentHubPage = migration.editContentType('contentHubPage');
+
+  contentHubPage
+    .createField('featuredContent')
+    .name('Featured content')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        // TODO: add training and event when available
+        linkContentType: ['blogPosting', 'exhibitionPage', 'project', 'story']
+      }
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Entry');
+
+  contentHubPage.changeFieldControl(
+    'featuredContent',
+    'builtin',
+    'entryLinkEditor',
+    {}
+  );
+};
