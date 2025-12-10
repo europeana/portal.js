2d26f35989eeaea345ef95d7361acbe74493b3fa
Leonie Peters <leoniepeters@gmail.com>
2025-12-10 14:48:31 +0100

    fix: replace display none style with visually hidden styles for small bp

--- packages/portal/src/components/item/ItemLanguageSelector.vue
+++ packages/portal/src/components/item/ItemLanguageSelector.vue
@@ -13,14 +13,14 @@
           v-if="translationLanguage"
           path="multilingual.viewingThisItemIn"
           tag="span"
-          class="d-none d-sm-inline align-middle"
+          class="toggle-text align-middle"
           data-qa="item language selector toggle text translated"
         >
           <strong>{{ translationLanguageLabel }}</strong>
         </i18n>
         <span
           v-else
-          class="d-none d-sm-inline align-middle"
+          class="toggle-text align-middle"
           data-qa="item language selector toggle text suggestion"
         >
           {{ $t('multilingual.viewItemInAnotherLanguage') }}
@@ -163,6 +163,19 @@
 
     strong {
       font-weight: 700;
+    }
+
+    .toggle-text {
+      // apply visually hidden styles for small bp only - keep text accessible
+      @media (max-width: ($bp-small - 1px)) {
+        clip: rect(0 0 0 0);
+        clip-path: inset(50%);
+        height: 1px;
+        overflow: hidden;
+        position: absolute;
+        white-space: nowrap;
+        width: 1px;
+      }
     }
   }
 
