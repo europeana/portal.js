0381a9042cdb9fba66ba70dbb5fa6c4e457e82d5
Richard Doe <richard.doe@rwdit.net>
2025-12-23 10:22:27 +0000

    chore: feedback widget v0.4.0

--- packages/portal/src/components/feedback/FeedbackWidget.vue
+++ packages/portal/src/components/feedback/FeedbackWidget.vue
@@ -10,19 +10,19 @@
       />
       <script
         type="module"
-        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.3/dist/europeana-feedback-widget.js"
-        integrity="sha384-+i+Y8OS8Y5xzrhioamyYOnIULaTwuEbV+JNARgMZfIRUokfdDk3iN0efcgt0gE/l"
+        src="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.4.0/dist/europeana-feedback-widget.js"
+        integrity="sha384-DudYnR5wo4wxA5BczsGlOjzuJUMSegYTQZ8Qa0DdllZg9KRb42VHqcF41fxrDRMR"
       >
         <!-- prevent eslint closing this -->
       </script>
       <link
         rel="preload"
         as="style"
-        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.3/dist/europeana-feedback-widget.css"
+        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.4.0/dist/europeana-feedback-widget.css"
       >
       <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.3.3/dist/europeana-feedback-widget.css"
+        href="https://cdn.jsdelivr.net/npm/@europeana/feedback-widget@0.4.0/dist/europeana-feedback-widget.css"
       >
     </client-only>
   </div>
