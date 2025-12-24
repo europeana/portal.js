30deba29ea21f8a5831f9cd86499f508f5106fb0
Richard Doe <richard.doe@rwdit.net>
2025-12-24 09:33:59 +0000

    refactor: improve error logging

--- packages/portal/src/cachers/index.spec.js
+++ packages/portal/src/cachers/index.spec.js
@@ -85,7 +85,7 @@ describe('@/cachers/index', () => {
       it('logs error to console', async() => {
         await cachers.cli('unknown', 'command');
 
-        expect(consoleErrorStub.calledWith('ERROR: Unknown command "unknown"')).toBe(true);
+        expect(consoleErrorStub.called).toBe(true);
       });
 
       it('exits with code 1', async() => {
