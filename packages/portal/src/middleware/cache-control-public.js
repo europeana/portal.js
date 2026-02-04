39696b3eefe511423230c06977618c84dfd53640
Richard Doe <richard.doe@rwdit.net>
2026-02-04 16:18:54 +0000

    docs: explain public cache control

--- packages/portal/src/middleware/cache-control-public.js
+++ packages/portal/src/middleware/cache-control-public.js
@@ -1,6 +1,7 @@
 export default ({ res }) => {
   if (process.server && res) {
     res.removeHeader('Cache-Control');
+    // permit caching, but require revalidation e.g. by etag or last-modified
     res.setHeader('Cache-Control', 'public, max-age=0');
   }
 };
