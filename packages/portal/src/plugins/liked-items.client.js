84c465244192d2ac321322b815407fbce1b8da90
Richard Doe <richard.doe@rwdit.net>
2026-02-06 11:10:30 +0000

    test: spec liked-items client plugin

--- packages/portal/src/plugins/liked-items.client.js
+++ packages/portal/src/plugins/liked-items.client.js
@@ -1,4 +1,4 @@
-import { computed, nextTick, ref } from 'vue';
+import { computed, nextTick, readonly, ref } from 'vue';
 import uniq from 'lodash/uniq';
 
 export default ({ $apis, store }, inject) => {
@@ -16,7 +16,7 @@ export default ({ $apis, store }, inject) => {
     }
     fetching = true;
     if ($apis.set && setId.value && watchedItemIds.value.length) {
-      // TODO: what is the limit to how many item ids this can handle?
+      // QUESTION: what is the limit to how many item ids this can handle?
       const response = await $apis.set.searchItems(setId.value, uniq(watchedItemIds.value.flat()));
       liked.value = response.items || [];
     }
@@ -36,20 +36,21 @@ export default ({ $apis, store }, inject) => {
   const like = async(itemIds) => {
     if (setId === null) {
       const response = await $apis.set.createLikes();
-      // TODO: does likesId need to be in store?
+      // TODO: does likesId still need to be in the store? or just isolate it to this plugin?
       store.commit('set/setLikesId', response.id);
     }
 
     await $apis.set.insertItems(setId.value, itemIds);
     await fetchLikedItems();
 
-    // notify('like');
     eventTarget.dispatchEvent(new Event('like'));
   };
 
   const unlike = async(itemIds) => {
     await $apis.set.deleteItems(setId.value, itemIds);
+
     await fetchLikedItems();
+
     eventTarget.dispatchEvent(new Event('unlike'));
   };
 
@@ -62,7 +63,7 @@ export default ({ $apis, store }, inject) => {
   };
 
   inject('likedItems', {
-    liked,
+    liked: readonly(liked),
     like,
     off,
     on,
--- packages/portal/src/plugins/liked-items.client.spec.js
+++ packages/portal/src/plugins/liked-items.client.spec.js
@@ -1,101 +1,144 @@
 import sinon from 'sinon';
-import { computed } from 'vue';
-import { useLikedItems } from '@/composables/likedItems.js';
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import likedItemsClientPlugin from './liked-items.client.js';
 
 const itemIds = ['/123/abc', '/123/def'];
-const likesId = '123';
+const moreItemIds = ['/123/ghi'];
+const likedItemsSetId = '123';
 const setApiDeleteItemsStub = sinon.stub();
 const setApiInsertItemsStub = sinon.stub();
 const setApiSearchItemsStub = sinon.stub().resolves({
   items: ['http://data.europeana.eu/item/123/abc']
 });
 
-const component = {
-  template: '<span />',
-  props: {
-    itemIds: {
-      type: Array,
-      default: null
+const $apis = {
+  set: {
+    deleteItems: setApiDeleteItemsStub,
+    insertItems: setApiInsertItemsStub,
+    searchItems: setApiSearchItemsStub
+  }
+};
+const store = {
+  state: {
+    set: {
+      likesId: likedItemsSetId
     }
-  },
-  setup(props) {
-    const { likedItems, like, unlike } = useLikedItems(computed(() => props.itemIds));
-
-    return { likedItems, like, unlike };
   }
 };
 
-const localVue = createLocalVue();
-localVue.use((Vue) => {
-  // mock these here so they apply to the component $root instance
-  Vue.prototype.$apis = {
-    set: {
-      deleteItems: setApiDeleteItemsStub,
-      insertItems: setApiInsertItemsStub,
-      searchItems: setApiSearchItemsStub
-    }
+const factory = (context = { $apis, store }) => {
+  let injected;
+  const inject = (name, injection) => {
+    injected = injection;
   };
-  Vue.prototype.$store = {
-    state: {
-      set: {
-        likesId
-      }
-    }
-  };
-});
 
-const factory = ({ propsData = {} } = {}) => shallowMount(component, {
-  localVue,
-  propsData
-});
+  likedItemsClientPlugin(context, inject);
 
-describe('useLikedItems', () => {
+  return injected;
+};
+
+describe('liked-items.client plugin', () => {
   afterEach(sinon.resetHistory);
   afterAll(sinon.reset);
 
-  describe('likedItems', () => {
-    describe('when there are no item IDs', () => {
-      const propsData = { itemIds: undefined };
+  it('is injected into context as likedItems', () => {
+    const inject = sinon.spy();
 
-      it('does not query the Set API', async() => {
-        factory({ propsData });
+    likedItemsClientPlugin({}, inject);
 
-        await new Promise(process.nextTick);
+    expect(inject.calledWith('likedItems', sinon.match.object)).toBe(true);
+  });
 
-        expect(setApiSearchItemsStub.called).toBe(false);
-      });
+  describe('watchItems', () => {
+    it('queries Set API for liked items', async() => {
+      const { watchItems } = factory();
 
-      it('gets value of an empty object', async() => {
-        const wrapper = factory({ propsData });
+      watchItems(itemIds);
+      await nextTick();
 
-        await new Promise(process.nextTick);
-
-        expect(wrapper.vm.likedItems).toEqual({});
-      });
+      expect(setApiSearchItemsStub.calledWith(likedItemsSetId, itemIds)).toBe(true);
     });
 
-    describe('when there are item IDs', () => {
-      const propsData = { itemIds };
+    it('combines into one Set API query multiple sets of watched items', async() => {
+      const { watchItems } = factory();
 
-      it('queries the Set API', async() => {
-        factory({ propsData });
+      watchItems(itemIds);
+      watchItems(moreItemIds);
+      await nextTick();
 
-        await new Promise(process.nextTick);
+      expect(setApiSearchItemsStub.calledWith(likedItemsSetId, itemIds.concat(moreItemIds))).toBe(true);
+    });
 
-        expect(setApiSearchItemsStub.calledWith(likesId, itemIds)).toBe(true);
-      });
+    it('stores the response in liked ref', async() => {
+      const { liked, watchItems } = factory();
 
-      it('gets value of an object with true/false representing liked state', async() => {
-        const wrapper = factory({ propsData });
+      watchItems(itemIds);
+      await nextTick(); // for the fetch
+      await nextTick(); // for updating the computed liked
 
-        await new Promise(process.nextTick);
+      expect(liked.value).toEqual(['http://data.europeana.eu/item/123/abc']);
+    });
+  });
 
-        expect(wrapper.vm.likedItems).toEqual({
-          '/123/abc': true,
-          '/123/def': false
-        });
-      });
+  describe('like', () => {
+    it('adds the items to the Set API likes set', () => {
+      const { like } = factory();
+
+      like(itemIds);
+
+      expect(setApiInsertItemsStub.calledWith(likedItemsSetId, itemIds)).toBe(true);
+    });
+
+    it('queries Set API for liked items', async() => {
+      const { like, watchItems } = factory();
+
+      watchItems(itemIds);
+      await like(moreItemIds);
+      await nextTick();
+
+      expect(setApiSearchItemsStub.calledWith(likedItemsSetId, itemIds)).toBe(true);
+    });
+
+    it('dispatches "like" event to listeners', async() => {
+      const { like, on } = factory();
+      const onLikeListener = sinon.spy();
+
+      on('like', onLikeListener);
+      like(itemIds);
+      await nextTick();
+
+      expect(onLikeListener.calledWith()).toBe(true);
+    });
+  });
+
+  describe('unlike', () => {
+    it('removes the items from the Set API likes set', () => {
+      const { unlike } = factory();
+
+      unlike(itemIds);
+
+      expect(setApiDeleteItemsStub.calledWith(likedItemsSetId, itemIds)).toBe(true);
+    });
+
+    it('queries Set API for liked items', async() => {
+      const { unlike, watchItems } = factory();
+
+      watchItems(itemIds);
+      await unlike(moreItemIds);
+      await nextTick();
+
+      expect(setApiSearchItemsStub.calledWith(likedItemsSetId, itemIds)).toBe(true);
+    });
+
+    it('dispatches "unlike" event to listeners', async() => {
+      const { on, unlike } = factory();
+      const onUnlikeListener = sinon.spy();
+
+      on('unlike', onUnlikeListener);
+      unlike(itemIds);
+      await nextTick();
+
+      expect(onUnlikeListener.calledWith()).toBe(true);
     });
   });
 });
