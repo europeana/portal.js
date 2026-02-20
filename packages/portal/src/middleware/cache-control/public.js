8803172c2e478175992d65fdca4d333f09ba1702
Richard Doe <richard.doe@rwdit.net>
2026-02-20 10:59:33 +0000

    feat: cache for one hour

--- packages/portal/src/middleware/cache-control/public.js
+++ packages/portal/src/middleware/cache-control/public.js
@@ -1,7 +1,9 @@
 export default ({ res }) => {
   res?.removeHeader('Cache-Control');
-  // // permit caching, but require revalidation e.g. by etag or last-modified
+  // cache for one hour
+  res?.setHeader('Cache-Control', 'public, max-age=3600');
+  // permit public caching, but require revalidation e.g. by etag or last-modified
   // res.setHeader('Cache-Control', 'public, max-age=0');
   // let downstream proxy determine how long to cache for
-  res?.setHeader('Cache-Control', 'public');
+  // res?.setHeader('Cache-Control', 'public');
 };
