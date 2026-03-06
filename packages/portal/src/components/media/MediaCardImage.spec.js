3820d090f1f6d78a1a93aa27b683694a0f3b28bb
Richard Doe <richard.doe@rwdit.net>
2026-03-06 11:09:12 +0000

    test: spec Thumbnail API/IIIF Image API for thumbnails

--- packages/portal/src/components/media/MediaCardImage.spec.js
+++ packages/portal/src/components/media/MediaCardImage.spec.js
@@ -3,7 +3,7 @@ import MediaCardImage from '@/components/media/MediaCardImage.vue';
 import EuropeanaMediaResource from '@/utils/europeana/media/Resource.js';
 
 const localVue = createLocalVue();
-const factory = () => shallowMount(MediaCardImage, {
+const factory = ({ propsData } = {}) => shallowMount(MediaCardImage, {
   localVue,
   stubs: ['b-img-lazy', 'b-link', 'b-img'],
   propsData: {
@@ -11,7 +11,8 @@ const factory = () => shallowMount(MediaCardImage, {
       id: 'http://collections.rmg.co.uk/mediaLib/422/media-422123/large.jpg'
     }),
     lazy: false,
-    europeanaIdentifier: '/123/abcdef'
+    europeanaIdentifier: '/123/abcdef',
+    ...propsData
   },
   mocks: {
     $t: (key) => key,
@@ -44,6 +45,35 @@ describe('components/media/MediaCardImage', () => {
     const image = wrapper.find('b-img-stub');
 
     expect(image.exists()).toBe(true);
+  });
+
+  describe('when the resource has a IIIF Image API service', () => {
+    const resource = new EuropeanaMediaResource({
+      id: 'https://iiif.example.org/image/0001/full/full/0/default.jpg',
+      service: {
+        id: 'https://iiif.example.org/image/0001',
+        profile: 'http://iiif.io/api/image/2/level1.json',
+        type: 'ImageService3'
+      }
+    });
+
+    it('uses the service for the thumbnail', () => {
+      const wrapper = factory({ propsData: { resource } });
+
+      const image = wrapper.find('b-img-stub');
+
+      expect(image.attributes('src')).toBe('https://iiif.example.org/image/0001/full/400,/0/default.jpg');
+    });
+  });
+
+  describe('when the resource has no IIIF Image API service', () => {
+    it('uses the Europeana Thumbnail API for the thumbnail', () => {
+      const wrapper = factory();
+
+      const image = wrapper.find('b-img-stub');
+
+      expect(image.attributes('src')).toBe('https://api.europeana.eu/thumbnail/v3/400/83ef43b6ede8c8b98c7b90b64b717234');
+    });
   });
 
   describe('methods', () => {
