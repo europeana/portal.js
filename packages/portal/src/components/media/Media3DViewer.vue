be4f69ff4c1cb340a687b3d687c258ccd14ab364
Leonie Peters <leoniepeters@gmail.com>
2026-04-14 16:03:06 +0200

    feat: install model-viewer and use in Media3DViewer

---
+++ packages/portal/src/components/media/Media3DViewer.vue
@@ -0,0 +1,25 @@
+<template>
+  <model-viewer
+    v-if="url"
+    alt=""
+    :src="url"
+    poster=""
+    shadow-intensity="1"
+    camera-controls
+    touch-action="pan-y"
+    class="h-100 w-100"
+  />
+</template>
+
+<script>
+  import '@google/model-viewer';
+
+  export default {
+    props: {
+      url: {
+        type: String,
+        required: true
+      }
+    }
+  };
+</script>
