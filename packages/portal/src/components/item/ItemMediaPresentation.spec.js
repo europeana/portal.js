7314183dcde0afcb22d50e1dc250d995270b1115
Richard Doe <richard.doe@rwdit.net>
2026-04-27 09:59:29 +0100

    chore: rm video.js feature toggle

--- packages/portal/src/components/item/ItemMediaPresentation.spec.js
+++ packages/portal/src/components/item/ItemMediaPresentation.spec.js
@@ -16,6 +16,13 @@ const factory = ({ data = {}, propsData = {}, mocks = {} } = {}) => shallowMount
     return { ...data };
   },
   mocks: {
+    $apis: {
+      thumbnail: {
+        forWebResource: () => ({
+          large: '400px'
+        })
+      }
+    },
     $apm: {
       setCustomContext: sinon.spy()
     },
