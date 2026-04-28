9d7dbabb4f28c96fab9ec62c8688bb70f4bcdffa
Richard Doe <richard.doe@rwdit.net>
2026-04-28 13:37:05 +0100

    chore: rm show/accept/reject recommendations features (#3045)
    * chore: rm show/accept/reject recommendations features
    
    as they have not been enabled for years
    
    * chore: linting

--- packages/portal/src/features/toggles.js
+++ packages/portal/src/features/toggles.js
@@ -1,11 +1,7 @@
 export default [
-  { name: 'acceptEntityRecommendations' },
-  { name: 'acceptSetRecommendations' },
   { name: 'eventLogging' },
   { name: 'jiraServiceDeskFeedbackForm' },
   { name: 'mockTrendingItems' },
   { name: 'modelViewer' },
-  { name: 'modelViewerReplacesOembed' },
-  { name: 'rejectEntityRecommendations' },
-  { name: 'showSetRecommendations' }
+  { name: 'modelViewerReplacesOembed' }
 ];
