1ae0ac1e8759dca2d649f66a241846275ebac0f2
Richard Doe <richard.doe@rwdit.net>
2026-05-13 11:52:32 +0100

    chore: set fulltext API base URL w/o gateway (#3078)
    * chore: set fulltext API base URL w/o gateway
    
    as the API gateway still doesn't work with it
    
    * chore: set nginx fulltext upstream

--- packages/portal/src/plugins/europeana/fulltext.js
+++ packages/portal/src/plugins/europeana/fulltext.js
@@ -2,5 +2,7 @@ import EuropeanaApi from './apis/base.js';
 
 export default class EuropeanaFulltextApi extends EuropeanaApi {
   static ID = 'fulltext';
-  static BASE_URL = 'https://api.europeana.eu/fulltext';
+  // NOTE: full text does not work via API gateway
+  // static BASE_URL = 'https://api.europeana.eu/fulltext';
+  static BASE_URL = 'https://newspapers.eanadev.org/api/v2';
 }
