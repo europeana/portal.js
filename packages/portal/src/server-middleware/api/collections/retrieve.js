d0cfbe406b527b9c0a78e0c16ed2562f5731e84b
Richard Doe <richard.doe@rwdit.net>
2026-05-28 12:43:58 +0100

    feat: pick top-level fields

--- packages/portal/src/server-middleware/api/collections/retrieve.js
+++ packages/portal/src/server-middleware/api/collections/retrieve.js
@@ -1,16 +1,27 @@
 import EuropeanaEntityApi from '../../../plugins/europeana/entity.js';
 
-export const fetchData = async(ids, context = {}) => {
+const pickFields = (entity, fields) => {
+  return [].concat(fields).reduce((memo, field) => {
+    memo[field] = entity[field];
+    return memo;
+  }, {});
+};
+
+export const fetchData = async(ids, fields, context = {}) => {
   const api = context.$apis?.entity || new EuropeanaEntityApi(context);
 
-  const entities = await api.retrieve(ids);
+  let entities = await api.retrieve(ids);
+
+  if (fields) {
+    entities = entities.map((entity) => pickFields(entity, fields));
+  }
 
   return entities;
 };
 
 export default (context = {}) => async(req, res, next) => {
   try {
-    const data = await fetchData(req.body, context);
+    const data = await fetchData(req.body, req.query?.fl, context);
 
     res.json(data);
   } catch (e) {
--- packages/portal/src/server-middleware/api/collections/retrieve.spec.js
+++ packages/portal/src/server-middleware/api/collections/retrieve.spec.js
@@ -1,18 +1,33 @@
+import nock from 'nock';
 import sinon from 'sinon';
 
 import serverMiddleware, { fetchData } from './retrieve.js';
 
 describe('server-middleware/api/collections/retrieve.js', () => {
-  const context = { $apis: { entity: { retrieve: sinon.stub().resolves({}) } } };
+  const entities = [
+    {
+      id: 'http://data.europeana.eu/organization/1',
+      prefLabel: { en: 'one' }
+    },
+    {
+      id: 'http://data.europeana.eu/organization/2',
+      prefLabel: { en: 'two' }
+    }
+  ];
   const ids = [
     'http://data.europeana.eu/organization/1',
     'http://data.europeana.eu/organization/2'
   ];
+  const context = { $apis: { entity: { retrieve: sinon.stub().withArgs(ids).resolves(entities) } } };
 
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
   afterEach(() => {
     sinon.resetHistory();
   });
   afterAll(() => {
+    nock.enableNetConnect();
     sinon.restore();
   });
 
@@ -41,10 +56,26 @@ describe('server-middleware/api/collections/retrieve.js', () => {
   });
 
   describe('fetchData', () => {
-    it('retrieves from the Entity API, with IDs from arg', async() => {
-      await fetchData(ids, context);
+    it('retrieves from the Entity API client, with IDs from 1st arg', async() => {
+      await fetchData(ids, undefined, context);
 
       expect(context.$apis.entity.retrieve.calledWith(ids)).toBe(true);
+    });
+
+    it('returns the items from the API client', async() => {
+      const response = await fetchData(ids, undefined, context);
+
+      expect(response).toEqual(entities);
+    });
+
+    describe('picking fields to include in 2nd arg', () => {
+      it('picks single top-level field', async() => {
+        const fields = 'id';
+
+        const response = await fetchData(ids, fields, context);
+
+        expect(response).toEqual([{ id: response[0].id }, { id: response[1].id }]);
+      });
     });
   });
 });
