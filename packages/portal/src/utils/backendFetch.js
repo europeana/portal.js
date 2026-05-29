613b1413f403918161a8937e29c3c0010b94f457
Richard Doe <richard.doe@rwdit.net>
2026-05-29 16:21:02 +0100

    feat: serialize params

--- packages/portal/src/utils/backendFetch.js
+++ packages/portal/src/utils/backendFetch.js
@@ -1,31 +1,32 @@
 import axios from 'axios';
+import qs from 'qs';
 
 let backends;
 
 if (process.server) {
   backends = {
-    collections: {
-      import: () => import('@/server-middleware/api/collections/index.js'),
-      fn: 'fetchData',
-      args: (args = [], context = {}) => [...args, context.$config?.redis]
-    },
     cache: {
       import: () => import('@/server-middleware/api/cache/index.js'),
       fn: 'cached',
+      args: (args = [], context = {}) => [...args, context.$config?.redis]
+    },
+    collections: {
+      import: () => import('@/server-middleware/api/collections/index.js'),
+      fn: 'fetchData',
       args: (args = [], context = {}) => [...args, context.$config?.redis]
     }
   };
 } else {
   backends = {
-    collections: {
-      method: 'get',
-      url: (args) => `/_api/collections/${args[0]}`,
-      params: (args) => args[1]
-    },
     cache: {
       method: 'get',
       url: '/_api/cache',
       params: (args) => ({ id: args[0] })
+    },
+    collections: {
+      method: 'get',
+      url: (args) => `/_api/collections/${args[0]}`,
+      params: (args) => args[1]
     }
   };
 }
@@ -41,7 +42,10 @@ export const backendFetch = (id, args, context = {}) => {
       baseURL: context.$config.app.baseUrl,
       method: backend.method,
       url: typeof backend.url === 'function' ? backend.url(args) : backend.url,
-      params: backend.params(args)
+      params: backend.params(args),
+      paramsSerializer(params) {
+        return qs.stringify(params, { arrayFormat: 'repeat' });
+      }
     })
       .then((response) => response.data);
   }
