c85be7e0cd2671eed3672efa17de77c9d0e53742
Richard Doe <richard.doe@rwdit.net>
2026-05-29 16:36:19 +0100

    refactor: make backends into a fn

--- packages/portal/src/utils/backendFetch.js
+++ packages/portal/src/utils/backendFetch.js
@@ -1,38 +1,38 @@
 import axios from 'axios';
 import qs from 'qs';
 
-let backends;
-
-if (process.server) {
-  backends = {
-    cache: {
-      import: () => import('@/server-middleware/api/cache/index.js'),
-      fn: 'cached',
-      args: (args = [], context = {}) => [...args, context.$config?.redis]
-    },
-    collections: {
-      import: () => import('@/server-middleware/api/collections/index.js'),
-      fn: 'fetchData',
-      args: (args = [], context = {}) => [...args, context.$config?.redis]
-    }
-  };
-} else {
-  backends = {
-    cache: {
-      method: 'get',
-      url: '/_api/cache',
-      params: (args) => ({ id: args[0] })
-    },
-    collections: {
-      method: 'get',
-      url: (args) => `/_api/collections/${args[0]}`,
-      params: (args) => args[1]
-    }
-  };
-}
+const backends = () => {
+  if (process.server) {
+    return {
+      cache: {
+        import: () => import('@/server-middleware/api/cache/index.js'),
+        fn: 'cached',
+        args: (args = [], context = {}) => [...args, context.$config?.redis]
+      },
+      collections: {
+        import: () => import('@/server-middleware/api/collections/index.js'),
+        fn: 'fetchData',
+        args: (args = [], context = {}) => [...args, context.$config?.redis]
+      }
+    };
+  } else {
+    return {
+      cache: {
+        method: 'get',
+        url: '/_api/cache',
+        params: (args) => ({ id: args[0] })
+      },
+      collections: {
+        method: 'get',
+        url: (args) => `/_api/collections/${args[0]}`,
+        params: (args) => args[1]
+      }
+    };
+  }
+};
 
 export const backendFetch = (id, args, context = {}) => {
-  const backend = backends[id];
+  const backend = backends()[id];
 
   if (process.server) {
     return backend.import()
