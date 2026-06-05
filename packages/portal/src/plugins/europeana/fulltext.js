dbc76d42f1b6c7da0b5ebd26762764416e4f4e62
lbiedinger <lutz.biedinger@gmail.com>
2026-06-05 15:32:47 +0200

    chore: api gateway (/fulltext) as default fulltext endpoint; remove newspapers.eanadev.org support for test env

--- packages/portal/src/plugins/europeana/fulltext.js
+++ packages/portal/src/plugins/europeana/fulltext.js
@@ -2,7 +2,5 @@ import EuropeanaApi from './apis/base.js';
 
 export default class EuropeanaFulltextApi extends EuropeanaApi {
   static ID = 'fulltext';
-  // NOTE: full text does not work via API gateway
-  // static BASE_URL = 'https://api.europeana.eu/fulltext';
-  static BASE_URL = 'https://newspapers.eanadev.org/api/v2';
+  static BASE_URL = 'https://api.europeana.eu/fulltext';
 }
