bdee7031f3b3057d52696b969815179c685b3e4b
Richard Doe <richard.doe@rwdit.net>
2026-06-08 12:30:30 +0100

    refactor: atomise fns

--- packages/portal/src/composables/pinnedItems.js
+++ packages/portal/src/composables/pinnedItems.js
@@ -2,6 +2,8 @@ import { getCurrentInstance } from 'vue';
 import { langMapValueForLocale } from '@europeana/i18n';
 import useMakeToast from '@/composables/makeToast.js';
 
+const ENTITY_ITEMS_BEST_SET = 'EntityBestItemsSet';
+
 export function usePinnedItems() {
   const instance = getCurrentInstance();
   const $root = instance.proxy.$root;
@@ -9,39 +11,52 @@ export function usePinnedItems() {
 
   const { makeToast } = useMakeToast();
 
-  const pin = async(itemId, entityId) => {
-    console.log('pin', itemId, entityId);
-    // first, retrieve full entity for prefLabel for set title, & toast
-    const retrieveResponse = await nuxtContext.$apis.entity.retrieve([entityId]);
-    const entity = retrieveResponse[0];
+  const findEntityItemsBestSetId = async(entityId) => {
+    const searchResponse = await nuxtContext.$apis.set.search({
+      profile: 'items',
+      query: `type:${ENTITY_ITEMS_BEST_SET}`,
+      qf: `subject:${entityId}`
+    });
+
+    return searchResponse.items?.[0] || null;
+  };
+
+  const createEntityItemsBestSet = async(entity) => {
     const title = Object.entries(entity?.prefLabel || {}).reduce((memo, [lang, value]) => {
       memo[lang] = Array.isArray(value) ? value[0] : value;
       return memo;
     }, {});
 
-    // find the EntityItemsBestSet for the entityId
-    const searchResponse = await nuxtContext.$apis.set.search({
-      profile: 'items',
-      query: 'type:EntityBestItemsSet',
-      qf: `subject:${entityId}`
-    });
-    let entityBestItemsSetId = searchResponse.items?.[0] || null;
+    const setBody = {
+      type: ENTITY_ITEMS_BEST_SET,
+      title,
+      subject: [entity.id]
+    };
 
-    if (!entityBestItemsSetId) {
+    return await nuxtContext.$apis.set.create(setBody);
+  };
+
+  const pin = async(itemId, entityId) => {
+    // first, retrieve full entity for prefLabel for set title, & toast
+    const retrieveResponse = await nuxtContext.$apis.entity.retrieve([entityId]);
+    const entity = retrieveResponse[0];
+
+    // find the EntityItemsBestSet for the entityId
+    let entityBestItemsSetId = await findEntityItemsBestSetId(entityId);
+    let entityBestItemsSet;
+
+    if (entityBestItemsSetId) {
+      // fetch the full set
+      entityBestItemsSet = await nuxtContext.$apis.set.requestSet(entityBestItemsSetId);
+    } else {
       // no such set exists: create one
-      const setBody = {
-        type: 'EntityBestItemsSet',
-        title,
-        subject: [entityId]
-      };
-      const createResponse = await nuxtContext.$apis.set.create(setBody);
-      entityBestItemsSetId = createResponse.id;
+      entityBestItemsSet = await createEntityItemsBestSet(entity);
+      entityBestItemsSetId = entityBestItemsSet.id;
     }
 
-    // now, fetch the full set to check if it's already full
-    const entityBestItemsSet = await nuxtContext.$apis.set.requestSet(entityBestItemsSetId);
-
+    // check if it's already full of pinned items
     if (entityBestItemsSet.pinned >= 24) {
+      // TODO: throw an error instead, and let consumer show the modal?
       $root.$bvModal.show(`pinned-limit-modal-${itemId}`);
       return;
     }
@@ -49,22 +64,19 @@ export function usePinnedItems() {
     // pin item to set
     await nuxtContext.$apis.set.pinItem(entityBestItemsSetId, itemId);
     const entityPrefLabel = langMapValueForLocale(entity.prefLabel, $root.$i18n.locale).values[0];
+    // TODO: emit an event instead, and let consumer make the toast?
     makeToast($root.$i18n.t('entity.notifications.pinned', { entity: entityPrefLabel }));
   };
 
   const unpin = async(itemId, entityId) => {
     // find the EntityItemsBestSet for the entityId
-    const searchResponse = await nuxtContext.$apis.set.search({
-      profile: 'items',
-      query: 'type:EntityBestItemsSet',
-      qf: `subject:${entityId}`
-    });
-    const entityBestItemsSetId = searchResponse.items?.[0] || null;
+    const entityBestItemsSetId = await findEntityItemsBestSetId(entityId);
     if (!entityBestItemsSetId) {
       throw new Error('Not Found');
     }
 
     await nuxtContext.$apis.set.deleteItems(entityBestItemsSetId, itemId);
+    // TODO: emit an event instead, and let consumer make the toast?
     makeToast($root.$i18n.t('entity.notifications.unpinned'));
   };
 
