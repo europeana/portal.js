d9161d4bb603d8829e1a6751de03747824fcd890
Richard Doe <richard.doe@rwdit.net>
2026-06-29 15:28:28 +0100

    refactor: use EuropeanaMap class from iife build
    and support setting style

--- packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-5">
     <client-only>
-      <div>
+      <div class="m-2 d-inline-block">
         <b-form-checkbox
           v-model="greyscale"
           name="map-greyscale"
@@ -9,6 +9,23 @@
         >
           Greyscale
         </b-form-checkbox>
+
+        <b-dropdown
+          variant="light"
+          toggle-class="text-decoration-none"
+        >
+          <template slot="button-content">
+            {{ selectedStyle.name }}
+          </template>
+
+          <b-dropdown-item
+            v-for="(style, index) in availableStyles"
+            :key="index"
+            :to="{ ...$route, query: { style: style.name } }"
+          >
+            {{ style.name }}
+          </b-dropdown-item>
+        </b-dropdown>
       </div>
       <div
         id="europeana-map"
@@ -16,11 +33,14 @@
         :class="{ 'greyscale': greyscale }"
         width="100vh"
         height="80vh"
-        :data-url="EUROPEANA_MAP_GEO_JSON_URL"
       />
+      <link
+        rel="preload"
+        as="script"
+        :href="`${EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.app.iife.js`"
+      >
       <script
-        type="module"
-        :src="`${EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.app.js`"
+        :src="`${EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.app.iife.js`"
       >
         <!-- prevent eslint closing this -->
       </script>
@@ -38,16 +58,49 @@
 </template>
 
 <script>
+  import waitFor from '@/utils/waitFor.js';
+
   export default {
     name: 'EntityOrganisationsMap',
 
     data() {
       return {
-        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1-map.1/dist',
+        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1-map.2/dist',
         // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
         EUROPEANA_MAP_GEO_JSON_URL: `${this.$config.app.baseUrl}/_api/collections/organisations/geo`,
+        availableStyles: [
+          { name: 'OSM', value: null },
+          { name: 'Positron', value: 'https://tiles.openfreemap.org/styles/positron' },
+          { name: 'Bright', value: 'https://tiles.openfreemap.org/styles/bright' },
+          { name: 'Liberty', value: 'https://tiles.openfreemap.org/styles/liberty' },
+          { name: 'Dark', value: 'https://tiles.openfreemap.org/styles/dark' },
+          { name: 'Fiord', value: 'https://tiles.openfreemap.org/styles/fiord' }
+        ],
+        europeanaMap: null,
         greyscale: false
       };
+    },
+
+    computed: {
+      selectedStyle() {
+        return this.availableStyles.find((style) => style.name === this.$route.query.style) || this.availableStyles[0];
+      }
+    },
+
+    watch: {
+      selectedStyle() {
+        this.europeanaMap.set('style', this.selectedStyle.value);
+      }
+    },
+
+    mounted() {
+      waitFor(() => window.EuropeanaMap, { name: 'EuropeanaMap' })
+        .then(() => {
+          this.europeanaMap = new window.EuropeanaMap('#europeana-map', {
+            style: this.selectedStyle.value,
+            url: this.EUROPEANA_MAP_GEO_JSON_URL
+          });
+        });
     }
   };
 </script>
