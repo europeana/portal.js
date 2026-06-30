fc863e3c170efc0bd5ad65267240f189b4564be2
Richard Doe <richard.doe@rwdit.net>
2026-06-30 07:15:17 +0100

    chore: don't use localhost

--- packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
@@ -65,8 +65,8 @@
 
     data() {
       return {
-        // EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1-map.2/dist',
-        EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
+        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1-map.2/dist',
+        // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
         EUROPEANA_MAP_GEO_JSON_URL: `${this.$config.app.baseUrl}/_api/collections/organisations/geo`,
         availableStyles: [
           { name: 'OpenStreetMap', value: null },
