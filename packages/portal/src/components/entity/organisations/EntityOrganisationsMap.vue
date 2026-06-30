ea84f9a6aec4611b5435347d9439bd71de4dae6c
Richard Doe <richard.doe@rwdit.net>
2026-06-30 13:10:15 +0100

    chore: bump map version

--- packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
@@ -38,7 +38,7 @@
 
     data() {
       return {
-        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1-map.2/dist',
+        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.1/dist',
         // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
         EUROPEANA_MAP_GEO_JSON_URL: `${this.$config.app.baseUrl}/_api/collections/organisations/geo`,
         europeanaMap: null
