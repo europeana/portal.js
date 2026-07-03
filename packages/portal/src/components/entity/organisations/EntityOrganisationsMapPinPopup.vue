a946fb855026e008f6aefb5568441bdb1fb4719c
lbiedinger <lutz.biedinger@gmail.com>
2026-07-03 16:40:08 +0200

    feat: add EntityOrganisationsMapPinPopup.vue component

---
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMapPinPopup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="m-1 popup-content">
+    PIN POPUP {{ id }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EntityOrganisationsMapPinPopup',
+
+    props: {
+      id: {
+        type: String,
+        required: true
+      }
+    },
+
+    data() {
+      return {};
+    },
+
+    // computed: {
+    //   property() {
+    //     return null;
+    //   }
+    // },
+  };
+</script>
+
+<style lang="scss" scoped>
+@import '@europeana/style/scss/variables';
+
+.popup-content {
+  color: $blue;
+}
+</style>
