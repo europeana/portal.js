f20348f24d02a176a7da4987ac9e33c0f3d085bc
Richard Doe <richard.doe@rwdit.net>
2026-07-07 13:15:22 +0100

    feat: localise map style

--- packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
@@ -11,6 +11,7 @@
 
 <script>
   import waitFor from '@/utils/waitFor.js';
+  import versatilesMapStyleNativeLang from '@europeana/style/map/versatiles.json';
 
   export default {
     name: 'EntityOrganisationsMap',
@@ -24,7 +25,7 @@
 
     data() {
       return {
-        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.3/dist',
+        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.4/dist',
         // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
         EUROPEANA_MAP_GEO_JSON_URL: `${this.$config.app.baseUrl}/_api/collections/organisations/geo`,
         europeanaMap: null
@@ -45,14 +46,36 @@
     },
 
     computed: {
+      versatilesMapStyleLocalised() {
+        return {
+          ...versatilesMapStyleNativeLang,
+          layers: versatilesMapStyleNativeLang.layers.map((layer) => {
+            if (layer.layout?.['text-field']) {
+              const textField = layer.layout?.['text-field'];
+              if (textField.length === 2 && textField[0] === 'get' && textField[1] === 'name') {
+                return {
+                  ...layer,
+                  layout: {
+                    ...layer.layout,
+                    'text-field': [
+                      'coalesce',
+                      ['get', `name_${this.$i18n.locale}`],
+                      ['get', 'name']
+                    ]
+                  }
+                };
+              }
+            }
+            return layer;
+          })
+        };
+      },
       mapStyle() {
         if (this.$config.app.map?.style?.startsWith('https://')) {
           return this.$config.app.map.style;
         }
-        // TODO: load this from @europeana/style npm pkg via CDN once published,
-        //       i.e. remove this hard-coded exception then
-        if (this.$config.app.map?.style === '@europeana/style/map/versatiles.json')  {
-          return require('@europeana/style/map/versatiles.json');
+        if (this.$config.app.map?.style === 'versatiles')  {
+          return this.versatilesMapStyleLocalised;
         }
         return null;
       }
