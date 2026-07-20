36fa617616b0bc9300f49467df7fe63dd3ef4d43
Richard Doe <richard.doe@rwdit.net>
2026-07-20 11:06:25 +0100

    feat: use @europeana/map on item page

---
+++ packages/portal/src/components/embed/EmbedEuropeanaMap.vue
@@ -0,0 +1,150 @@
+<template>
+  <div
+    class="embed-map mb-5"
+  >
+    <label
+      v-if="$slots.label"
+      for="europeana-map"
+      class="p-3"
+    >
+      <slot name="label" />
+    </label>
+    <div
+      id="europeana-map"
+      class="europeana-map"
+      width="100vh"
+      height="80vh"
+      data-qa="map embed"
+      :json="json"
+      :url="url"
+    />
+    <slot
+      ref="popover"
+      name="popover"
+    />
+  </div>
+</template>
+
+<script>
+  import waitFor from '@/utils/waitFor.js';
+
+  export default {
+    name: 'EmbedMap',
+
+    props: {
+      hash: {
+        type: Boolean,
+        default: false
+      },
+
+      json: {
+        type: String,
+        default: null
+      },
+
+      on: {
+        type: Object,
+        default: () => {}
+      },
+
+      url: {
+        type: [String, URL],
+        default: null
+      }
+    },
+
+    data() {
+      return {
+        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.7/dist',
+        // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
+        europeanaMap: null
+      };
+    },
+
+    head() {
+      return {
+        link: [
+          { rel: 'preload', as: 'script', href: 'https://cdn.jsdelivr.net/npm/vue@3.5.39/dist/vue.global.prod.js' },
+          // { rel: 'preload', as: 'script', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js` },
+          { rel: 'preload', as: 'style', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.css` },
+          { rel: 'stylesheet', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.css` }
+        ],
+        script: [
+          { src: 'https://cdn.jsdelivr.net/npm/vue@3.5.39/dist/vue.global.prod.js' }
+          // { src: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js` }
+        ]
+      };
+    },
+
+    computed: {
+      mapStyle() {
+        if (this.$config.app.map?.style?.startsWith('https://')) {
+          return this.$config.app.map.style;
+        }
+        if (this.$config.app.map?.style === '@europeana/style/map/versatiles.json') {
+          return require('@europeana/style/map/versatiles.json');
+        }
+        return null;
+      }
+    },
+
+    mounted() {
+      waitFor(() => window.Vue, { name: 'Vue' }).then(this.handleLoadVue);
+      waitFor(() => window.EuropeanaMap, { name: 'EuropeanaMap' }).then(this.handleLoadEuropeanaMap);
+    },
+
+    methods: {
+      // With vue and map scripts both in head script elements, there is a race condition as
+      // to which gets loaded first, resulting in "Vue is not defined" errors from the map
+      // script. This will only add the map script to the document head once Vue is loaded.
+      // TODO: make this work with script elements in the document head
+      handleLoadVue() {
+        const mapScript = document.createElement('script');
+        mapScript.setAttribute('src', `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js`);
+        document.querySelector('head').append(mapScript);
+      },
+      handleLoadEuropeanaMap() {
+        this.europeanaMap = new window.EuropeanaMap.EuropeanaMapWrapper('#europeana-map', {
+          hash: this.hash,
+          json: this.json,
+          pinPopover: this.$refs.popover?.$el,
+          style: this.mapStyle,
+          url: this.url
+        });
+
+        // Add map event listeners
+        for (const eventId in this.on) {
+          this.europeanaMap.olMap.on(eventId, this.on[eventId]);
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@europeana/style/scss/variables';
+
+  .embed-map {
+    .europeana-map {
+      width: 100%;
+      height: 80vh;
+      position: relative;
+
+      ::v-deep .ol-overlay-container {
+        @media (max-width: ($bp-small - 1px)) {
+          transform: none !important;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          z-index: 1;
+        }
+      }
+    }
+
+    label {
+      font-size: $font-size-small;
+      display: block;
+      margin-bottom: 0;
+    }
+  }
+</style>
--- packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMap.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="mb-5">
-    <div
-      id="europeana-map"
-      class="europeana-map"
-      width="100vh"
-      height="80vh"
+    <EmbedEuropeanaMap
+      :hash="hash"
+      :url="EUROPEANA_MAP_GEO_JSON_URL"
+      :on="{ 'change:activefeature': handleChangeActiveFeature }"
     />
     <EntityOrganisationsMapPinPopover
       :id="clickedFeatureId"
-      ref="popover"
       @close="handleClosePopover"
     />
   </div>
 </template>
 
 <script>
-  import waitFor from '@/utils/waitFor.js';
+  import EmbedEuropeanaMap from '@/components/embed/EmbedEuropeanaMap.vue';
   import EntityOrganisationsMapPinPopover from './EntityOrganisationsMapPinPopover.vue';
 
   export default {
     name: 'EntityOrganisationsMap',
 
     components: {
+      EmbedEuropeanaMap,
       EntityOrganisationsMapPinPopover
     },
 
@@ -34,67 +33,12 @@
 
     data() {
       return {
-        EUROPEANA_MAP_CDN_BASE_URL: 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.7/dist',
-        // EUROPEANA_MAP_CDN_BASE_URL: 'http://localhost:4173',
         EUROPEANA_MAP_GEO_JSON_URL: `${this.$config.app.baseUrl}/_api/collections/organisations/geo`,
-        europeanaMap: null,
         clickedFeatureId: null
       };
     },
 
-    head() {
-      return {
-        link: [
-          { rel: 'preload', as: 'script', href: 'https://cdn.jsdelivr.net/npm/vue@3.5.39/dist/vue.global.prod.js' },
-          // { rel: 'preload', as: 'script', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js` },
-          { rel: 'preload', as: 'style', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.css` },
-          { rel: 'stylesheet', href: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.css` }
-        ],
-        script: [
-          { src: 'https://cdn.jsdelivr.net/npm/vue@3.5.39/dist/vue.global.prod.js' }
-          // { src: `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js` }
-        ]
-      };
-    },
-
-    computed: {
-      mapStyle() {
-        if (this.$config.app.map?.style?.startsWith('https://')) {
-          return this.$config.app.map.style;
-        }
-        if (this.$config.app.map?.style === '@europeana/style/map/versatiles.json') {
-          return require('@europeana/style/map/versatiles.json');
-        }
-        return null;
-      }
-    },
-
-    mounted() {
-      waitFor(() => window.Vue, { name: 'Vue' }).then(this.handleLoadVue);
-      waitFor(() => window.EuropeanaMap, { name: 'EuropeanaMap' }).then(this.handleLoadEuropeanaMap);
-    },
-
     methods: {
-      // With vue and map scripts both in head script elements, there is a race condition as
-      // to which gets loaded first, resulting in "Vue is not defined" errors from the map
-      // script. This will only add the map script to the document head once Vue is loaded.
-      // TODO: make this work with script elements in the document head
-      handleLoadVue() {
-        const mapScript = document.createElement('script');
-        mapScript.setAttribute('src', `${this.EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js`);
-        document.querySelector('head').append(mapScript);
-      },
-      handleLoadEuropeanaMap() {
-        this.europeanaMap = new window.EuropeanaMap.EuropeanaMapWrapper('#europeana-map', {
-          hash: this.hash,
-          pinPopover: this.$refs.popover.$el,
-          style: this.mapStyle,
-          url: this.EUROPEANA_MAP_GEO_JSON_URL
-        });
-
-        // Listen to active (clicked) feature changes
-        this.europeanaMap.olMap.on('change:activefeature', this.handleChangeActiveFeature);
-      },
       handleChangeActiveFeature(e) {
         if (e.activeFeatureName) {
           this.clickedFeatureId = e.activeFeatureName;
@@ -106,23 +50,3 @@
     }
   };
 </script>
-
-<style lang="scss" scoped>
-  @import '@europeana/style/scss/variables';
-
-  .europeana-map {
-    width: 100%;
-    height: 80vh;
-    position: relative;
-
-    ::v-deep .ol-overlay-container {
-      @media (max-width: ($bp-small - 1px)) {
-        transform: none !important;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        z-index: 1;
-      }
-    }
-  }
-</style>
