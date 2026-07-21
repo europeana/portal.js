2ad012c3938c958dc908bd0ed11a69b31107e99c
Richard Doe <richard.doe@rwdit.net>
2026-07-21 11:05:38 +0100

    chore: revert cdn

--- packages/portal/src/components/embed/EmbedEuropeanaMap.vue
+++ packages/portal/src/components/embed/EmbedEuropeanaMap.vue
@@ -33,8 +33,8 @@
   const VUE_3_CDN_BASE_URL = 'https://cdn.jsdelivr.net/npm/vue@3.5.39/dist';
   const VUE_3_SCRIPT_URL = `${VUE_3_CDN_BASE_URL}/vue.global.prod.js`;
 
-  // const EUROPEANA_MAP_CDN_BASE_URL = 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.8/dist';
-  const EUROPEANA_MAP_CDN_BASE_URL = 'http://localhost:4173';
+  const EUROPEANA_MAP_CDN_BASE_URL = 'https://cdn.jsdelivr.net/npm/@europeana/map@0.1.8/dist';
+  // const EUROPEANA_MAP_CDN_BASE_URL = 'http://localhost:4173';
   const EUROPEANA_MAP_SCRIPT_URL = `${EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.iife.js`;
   const EUROPEANA_MAP_STYLE_URL = `${EUROPEANA_MAP_CDN_BASE_URL}/europeana-map.css`;
 
