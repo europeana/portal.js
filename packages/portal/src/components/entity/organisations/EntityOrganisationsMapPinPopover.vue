83b93aff770927b2a407902fef723267e80d3978
Richard Doe <richard.doe@rwdit.net>
2026-08-05 12:27:21 +0100

    fix(portal): omit undefined location parts

--- packages/portal/src/components/entity/organisations/EntityOrganisationsMapPinPopover.vue
+++ packages/portal/src/components/entity/organisations/EntityOrganisationsMapPinPopover.vue
@@ -164,7 +164,7 @@
         return this.entity?.logo?.id && getWikimediaThumbnailUrl(this.entity.logo.id, 120);
       },
       location() {
-        return this.entity?.hasAddress && `${this.entity.hasAddress.locality}, ${this.entity.hasAddress.countryName}`;
+        return [this.entity?.hasAddress?.locality, this.entity?.hasAddress?.countryName].filter(Boolean).join(', ');
       }
     },
 
