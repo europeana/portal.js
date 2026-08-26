f26f3aedcd403f7ed6bc357e9c79b9a1b14fd7bf
lbiedinger <lutz.biedinger@gmail.com>
2026-08-26 12:31:02 +0200

    update audiovisual theme identifer

--- packages/portal/src/utils/europeana/themes.js
+++ packages/portal/src/utils/europeana/themes.js
@@ -2,7 +2,7 @@
 const themes = [
   { id: 'archaeology', qf: 'archaeology' },
   { id: 'art', qf: 'art' },
-  { id: 'audio-visual-heritage', qf: 'audiovisual' },
+  { id: 'audiovisual-heritage', qf: 'audiovisual' },
   {
     id: 'fashion',
     qf: 'fashion',
